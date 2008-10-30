bf0482d571ca03112735290b98cc1aacc5fed424
Unknown <sowen@cspool30.cs.man.ac.uk>
2008-10-30 17:06:33 +0000

    improved styling of show person screen

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
+  def is_nil_or_empty? thing
+    thing.nil? or thing.empty?
+  end
 end
