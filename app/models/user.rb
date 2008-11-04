57364ff95512b4a9cff909aa928869ed131d926c
Unknown <sowen@localhost>
2008-11-04 09:20:32 +0000

    workaround to get round duplication of email field in User table and Profile table. Email is currently unused in the User table but is still there. Email will need to be reactivated

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,16 @@ class User < ActiveRecord::Base
   # Virtual attribute for the unencrypted password
   attr_accessor :password
 
-  validates_presence_of     :login, :email
+  #validates_presence_of     :login, :email - removed requirement on email
+  #validates_length_of       :email,    :within => 3..100
+  
+  validates_presence_of     :login
   validates_presence_of     :password,                   :if => :password_required?
   validates_presence_of     :password_confirmation,      :if => :password_required?
   validates_length_of       :password, :within => 4..40, :if => :password_required?
   validates_confirmation_of :password,                   :if => :password_required?
   validates_length_of       :login,    :within => 3..40
-  validates_length_of       :email,    :within => 3..100
+  
   validates_uniqueness_of   :login, :case_sensitive => false
   before_save :encrypt_password
   before_create :make_activation_code 
