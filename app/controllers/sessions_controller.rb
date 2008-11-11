32b638dbac5eaf07a0dfad0c75bb39063b436877
Unknown <sowen@cspool30.cs.man.ac.uk>
2008-11-11 17:21:33 +0000

    added Invalid Login error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,32 @@
 # This controller handles the login/logout function of the site.  
 class SessionsController < ApplicationController
  
-  layout 'logged_out'
+    layout 'logged_out'
   
-  # render new.rhtml
-  def new
-  end
-
-  def create
-    self.current_user = User.authenticate(params[:login], params[:password])
-    if logged_in?
-      if params[:remember_me] == "1"
-        current_user.remember_me unless current_user.remember_token?
-        cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
-      end
-      redirect_back_or_default('/')
-      flash[:notice] = "Logged in successfully"
-    else
-      render :action => 'new'
+    # render new.rhtml
+    def new
     end
-  end
 
-  def destroy
-    self.current_user.forget_me if logged_in?
-    cookies.delete :auth_token
-    reset_session
-    flash[:notice] = "You have been logged out."
-    redirect_back_or_default('/')
-  end
+    def create
+        self.current_user = User.authenticate(params[:login], params[:password])
+        if logged_in?
+            if params[:remember_me] == "1"
+                current_user.remember_me unless current_user.remember_token?
+                cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
+            end
+            redirect_back_or_default('/')
+            flash[:notice] = "Logged in successfully"
+        else
+            flash[:notice] = "Invalid login"
+            render :action => 'new'
+        end
+    end
+
+    def destroy
+        self.current_user.forget_me if logged_in?
+        cookies.delete :auth_token
+        reset_session
+        flash[:notice] = "You have been logged out."
+        redirect_back_or_default('/')
+    end
 end
