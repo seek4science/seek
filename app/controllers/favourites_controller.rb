d940a95762de619693d0f450600617c962a08507
Unknown <sowen@toffee>
2008-11-17 23:24:15 +0000

    fixed error getting id for asset when assigning to Favourite - was a mis placed .last

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -4,7 +4,7 @@ class FavouritesController < ApplicationController
     f=Favourite.new
     f.user=current_user
     f.model_name=split_id[1]
-    f.asset_id=split_id[2].last.to_i
+    f.asset_id=split_id[2].to_i
     f.save
     
   end
