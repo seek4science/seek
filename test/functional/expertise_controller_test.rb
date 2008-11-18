6492dd4f493b91cd0ef24b135bf955e1fba83a4f
Unknown <sowen@cspool30.cs.man.ac.uk>
2008-11-18 15:01:10 +0000

    identify current user in expertise list - currently style just highlights that person in red.

--- test/functional/expertise_controller_test.rb
+++ test/functional/expertise_controller_test.rb
@@ -1,8 +1,25 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ExpertiseControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+  
+    fixtures :people, :expertises, :users
+    
+    include AuthenticatedTestHelper
+    def setup
+        login_as(:quentin)
+    end
+    
+    def test_yourself_in_list
+        
+        expertise = expertises(:fishing)
+        expertise.people << people(:one)
+        expertise.people << people(:two)
+        
+        get :show, :id=>expertise
+        
+        assert_response :success
+        assert_select %w(a.current_user), :text=>"Quentin Jones"
+        assert_select %w(a), :text=>"Aaron Last_name"
+        
+    end
 end
