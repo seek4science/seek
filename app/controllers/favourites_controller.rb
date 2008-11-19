b02e602ec349904bd452e4ddfd8b10cda674b759
Unknown <sowen@toffee>
2008-11-19 21:46:35 +0000

    prevent duplicate favourites and protect that the one being deleted is for the current user. before_filter :login_required too.

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,24 +1,41 @@
 class FavouritesController < ApplicationController
+  
+  before_filter :login_required
+  
   def add
-    #TODO validate id, and only add if not a duplicate
+    #FIXME validate id with a regular expression
     split_id=params[:id].split("_")
     f=Favourite.new
     f.user=current_user
     f.model_name=split_id[1]
     f.asset_id=split_id[2].to_i
-    f.save
-    render :update do |page|
-        page.replace_html "favourite_list", :partial=>"favourites/gadget_list"
-        page.visual_effect :highlight, "drop_favourites", :startcolor=>"#DDDDFF"
+    if Favourite.find_by_user_id_and_model_name_and_asset_id(current_user,f.model_name,f.asset_id).nil?
+      f.save
+      render :update do |page|
+          page.replace_html "favourite_list", :partial=>"favourites/gadget_list"
+          page.visual_effect :highlight, "drop_favourites", :startcolor=>"#DDDDFF"
+      end
+    else
+      render :update do |page|
+        page.visual_effect :highlight, "drop_favourites", :startcolor=>"#FF0000"
+      end
     end
+    
   end
   
   def delete
     id=params[:id].split("_")[1].to_i
-    Favourite.find(id).destroy
-    render :update do |page|
-        page.replace_html "favourite_list", :partial=>"favourites/gadget_list"
-        page.visual_effect :highlight, "drop_favourites", :startcolor=>"#DDDDFF"
-    end 
+    f=Favourite.find(id)
+    if !f.nil? and f.user==current_user
+      f.destroy
+      render :update do |page|
+          page.replace_html "favourite_list", :partial=>"favourites/gadget_list"
+          page.visual_effect :highlight, "drop_favourites", :startcolor=>"#DDDDFF"
+      end 
+    else
+      render :update do |page|
+        page.visual_effect :highlight, "drop_favourites", :startcolor=>"#FF0000"
+      end
+    end
   end
 end
