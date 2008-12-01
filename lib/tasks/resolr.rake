a8b9c2dd73bf8bc25e2443f84f228c2e21a2ae79
Unknown <sowen@toffee>
2008-12-01 19:28:37 +0000

    added resolr tasks as rake task

---
+++ lib/tasks/resolr.rake
@@ -0,0 +1,11 @@
+require 'rubygems'
+require 'rake'
+
+namespace :solr do
+  task(:resolr=>:environment) do
+    Person.find(:all).map do |w| w.solr_save end
+    Project.find(:all).map do |w| w.solr_save end
+    Institution.find(:all).map do |w| w.solr_save end
+    Expertise.find(:all).map do |w| w.solr_save end
+  end
+end
--- resolr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#export RAILS_ENV=production
-
-echo 'Person.find(:all).map do |w| w.solr_save end' | ruby script/console
-echo 'Project.find(:all).map do |w| w.solr_save end' | ruby script/console
-echo 'Institution.find(:all).map do |w| w.solr_save end' | ruby script/console
-echo 'Expertise.find(:all).map do |w| w.solr_save end' | ruby script/console
-
