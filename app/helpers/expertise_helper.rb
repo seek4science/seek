70756b8f61af9823ad04c45156f001352e4a5db3
Unknown <sowen@toffee>
2008-12-03 21:28:42 +0000

    added expertise to person list_item and a bit of extra styling

--- app/helpers/expertise_helper.rb
+++ app/helpers/expertise_helper.rb
@@ -1,2 +1,8 @@
 module ExpertiseHelper
+  def list_item_expertise_list expertise  
+    expertise.map do |e|
+      divider=expertise.last==e ? "" : " | "
+      link_to(h(e.name),url_for(e))+divider
+    end
+  end
 end
