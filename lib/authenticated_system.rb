0e05a46faece5756d182fd90a205a79b27688ea5
Unknown <sowen@toffee>
2008-12-10 19:40:08 +0000

    removed debug trace

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -61,7 +61,6 @@ module AuthenticatedSystem
     # to access the requested action.  For example, a popup window might
     # simply close itself.
     def access_denied
-      pp request.env
       request.format = :html if request.env['HTTP_USER_AGENT'] =~ /msie/i
       
       respond_to do |format|
