7d77d4e7874a587255b837bae4b00e9905a40151
Unknown <sowen@toffee>
2008-12-12 23:46:37 +0000

    fixed SOLR_ENABLED default

--- config/environment.rb
+++ config/environment.rb
@@ -65,10 +65,9 @@ Rails::Initializer.run do |config|
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector
   
-  SOLR_ENABLED=false
-
   load 'config/environment_local.rb' if FileTest.exist?('config/environment_local.rb')
   
   
+  SOLR_ENABLED=false unless Object.const_defined?("SOLR_ENABLED")
 
 end
