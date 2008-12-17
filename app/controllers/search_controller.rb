fd4f39024cd4fc1427862310608ea2d0b41ac916
Unknown <sowen@rpc267>
2008-12-17 13:06:15 +0000

    added selection for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,9 +5,23 @@ class SearchController < ApplicationController
   def index
     
     @query = params[:query]
+    type = params[:type]
+    type=type.downcase unless type.nil?
+    
     @results=[]
-    @results = Person.multi_solr_search(@query, :limit=>100, :models=>[Person, Project, Institution]).results if (SOLR_ENABLED and !@query.nil? and !@query.strip.empty?)
-    @results = @results.select{|r| (!r.instance_of?(Person) || !r.is_dummy?)}
+    case(type)
+    when("people")
+      @results = Person.multi_solr_search(@query, :limit=>100, :models=>[Person]).results if (SOLR_ENABLED and !@query.nil? and !@query.strip.empty?)
+      @results = @results.select{|r| (!r.instance_of?(Person) || !r.is_dummy?)}
+    when("institutions")
+      @results = Institution.multi_solr_search(@query, :limit=>100, :models=>[Institution]).results if (SOLR_ENABLED and !@query.nil? and !@query.strip.empty?)
+    when("projects")
+      @results = Project.multi_solr_search(@query, :limit=>100, :models=>[Project]).results if (SOLR_ENABLED and !@query.nil? and !@query.strip.empty?)
+    else
+      @results = Person.multi_solr_search(@query, :limit=>100, :models=>[Person, Project, Institution]).results if (SOLR_ENABLED and !@query.nil? and !@query.strip.empty?)
+      @results = @results.select{|r| (!r.instance_of?(Person) || !r.is_dummy?)}
+    end
+    
     
   end
   
