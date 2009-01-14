f62d33e1b840b98846bf02f8c12668d0fdcbe66a
Unknown <sowen@rpc267>
2009-01-14 11:33:05 +0000

    added logic that enforces a user without a associated profile to select or create a new profile

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -19,6 +19,18 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  #Overridden from restful_authentication
+  #Does a second check that there is a profile assigned to the user, and if not goes to the profile
+  #selection page (GET people/select)
+  def authorized?
+    if super
+      redirect_to(select_people_path) if current_user.person.nil?
+      true
+    else
+      false
+    end
+  end
+
   def is_current_user_auth
     begin
       @user = User.find(params[:id], :conditions => ["id = ?", current_user.id])
@@ -49,7 +61,7 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.html { redirect_to root_url }
     end
-  end  
+  end
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,32 +1,39 @@
 # This controller handles the login/logout function of the site.  
 class SessionsController < ApplicationController
  
-    layout 'logged_out'
+  layout 'logged_out'
   
-    # render new.rhtml
-    def new
-    end
+  # render new.rhtml
+  def new
+  end
 
-    def create
-        self.current_user = User.authenticate(params[:login], params[:password])
-        if logged_in?
-            if params[:remember_me] == "1"
-                current_user.remember_me unless current_user.remember_token?
-                cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
-            end
-            redirect_back_or_default('/')
-            flash[:notice] = "Logged in successfully"
-        else
-            flash[:error] = "Invalid login"
-            redirect_to :action => 'new'
-        end
-    end
-
-    def destroy
-        self.current_user.forget_me if logged_in?
-        cookies.delete :auth_token
-        reset_session
-        flash[:notice] = "You have been logged out."
+  def create
+    self.current_user = User.authenticate(params[:login], params[:password])
+    if logged_in?
+      if params[:remember_me] == "1"
+        current_user.remember_me unless current_user.remember_token?
+        cookies[:auth_token] = { :value => self.current_user.remember_token , :expires => self.current_user.remember_token_expires_at }
+      end
+      #if the person has registerd but has not yet selected a profile then go to the select person page
+      #otherwise login normally
+      if current_user.person.nil?
+          redirect_to(select_people_path)
+      else
         redirect_back_or_default('/')
+        flash[:notice] = "Logged in successfully"
+      end
+            
+    else
+      flash[:error] = "Invalid login"
+      redirect_to :action => 'new'
     end
+  end
+
+  def destroy
+    self.current_user.forget_me if logged_in?
+    cookies.delete :auth_token
+    reset_session
+    flash[:notice] = "You have been logged out."
+    redirect_back_or_default('/')
+  end
 end
