0a3f6d262d45a6272a5fa5aff965714817d146a6
Unknown <sowen@rpc267>
2009-02-09 17:11:42 +0000

    added welcome message after activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,6 +34,7 @@ class UsersController < ApplicationController
     self.current_user = params[:activation_code].blank? ? false : User.find_by_activation_code(params[:activation_code])
     if logged_in? && !current_user.active?
       current_user.activate
+      Mailer.deliver_welcome current_user, base_host
       flash[:notice] = "Signup complete!"
       redirect_to current_user.person
     else
