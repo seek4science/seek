2e5cca1124dd2241badd38cf235b47fcbb5fce27
Unknown <sowen@rpc267>
2009-02-11 14:07:06 +0000

    added setting for mongrel cluster environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,3 +20,7 @@ config.action_controller.perform_caching             = true
 
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
+#
+#
+SOLR_ENABLED=true
+ACTIVATION_REQUIRED=true
