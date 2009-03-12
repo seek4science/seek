d107d2d9959dd99dd882a2fda4d6faca3d881acd
Unknown <sowen@rpc267>
2009-03-12 16:45:33 +0000

    sergejs attribution work

--- app/models/sop.rb
+++ app/models/sop.rb
@@ -15,4 +15,28 @@ class Sop < ActiveRecord::Base
   # TODO
   # add all required validations here
   
+  
+  # get a list of SOPs with their original uploaders - for autocomplete fields
+  # (authorization is done immediately to save from iterating through the collection again afterwards)
+  #
+  # Parameters:
+  # - user - user that performs the action; this is required for authorization
+  def self.get_all_as_json(user)
+    all_sops = Sop.find(:all, :order => "ID asc")
+    sops_with_contributors = all_sops.collect{ |s| 
+        Authorization.is_authorized?("show", nil, s, user) ?
+        (p = s.asset.contributor.person;
+        { "id" => s.id,
+          "title" => s.title,
+          "contributor" => "by " +
+                           (p.first_name.blank? ? (logger.error("\n----\nUNEXPECTED DATA: person id = #{p.id} doesn't have a first name\n----\n"); "(NO FIRST NAME)") : p.first_name) + " " +
+                           (p.last_name.blank? ? (logger.error("\n----\nUNEXPECTED DATA: person id = #{p.id} doesn't have a last name\n----\n"); "(NO LAST NAME)") : p.last_name),
+          "type" => "SOP"} ) :
+        nil }
+    
+    sops_with_contributors.delete(nil)
+    
+    return sops_with_contributors.to_json
+  end
+  
 end
--- public/javascripts/sharing.js
+++ public/javascripts/sharing.js
@@ -26,14 +26,18 @@ REVIEW_WORK_GROUP_LINK = null;
 
 
 // declarations for autocompleters
+// (IDs can be any strings - the only constraint is that they should hold unique names for each autocompleter)
 var f_group_autocompleter_id = 'f_group_autocompleter';
 var individual_people_autocompleter_id = 'ip_autocompleter';
+var attributions_autocompleter_id = 'attributions_autocompleter';
 // associative array that holds all instances of autocompleters on a page
 var autocompleters = new Array();
 
 var policy_settings = new Object();
 var permissions_for_set = {};
 var permission_settings = new Array();
+
+var attribution_settings = new Array();
 
 var receivedPolicySettings = null;
 var receivedProjectInstitutions = null;
@@ -1137,6 +1141,16 @@ function validateNewSopFields() {
     return(false);
   }
   
+  // check if no tokens remain in the attributions autocompleter
+  // (only do this if the fold with attributions is expanded)
+  if($('attributions_fold_content').style.display == "block" &&
+     autocompleters[attributions_autocompleter_id].getRecognizedSelectedIDs() != "") 
+  {
+    alert('You didn\'t press "Add" link to add items in the attributions autocomplete field.');
+    $('attributions_autocomplete_input').focus();
+    return(false);
+  }
+  
   // filename and title set - can submit
   $('new_sop_submit_btn').disabled = true;
   $('new_sop_submit_btn').value = "Uploading and Saving...";
@@ -1144,411 +1158,124 @@ function validateNewSopFields() {
   return(true);
 }
 
-// ********************************************************
 
-function trimSpaces(str) {
 
-  while ((str.length > 0) && (str.charAt(0) == ' '))
-    str = str.substring(1);
+// ***************  Attributions  *****************
 
-  while ((str.length > 0) && (str.charAt(str.length - 1) == ' '))
-    str = str.substring(0, str.length - 1);
 
-  return str;
-}
-
-// tags
-
-var tags = new Array();
-
-function updateTagList() {
-
-  var markup = '';
-
-  if (tags.length == 0) {
-
-    markup = '<i>None</i>';
-
-  } else {
-
-    for (var i = 0; i < tags.length; i++)
-      markup += tags[i] +
-        '&nbsp;&nbsp;&nbsp;<small>[<a href="" onclick="javascript:deleteTag(\'' + tags[i].replace("'", "\\'") +
-        '\'); return false;">delete</a>]</small><br />';
-  }
-
-  document.getElementById('tags_current_list').innerHTML = markup;
-
-  // also update the web form
-
-  var tag_list = '';
-
-  for (var i = 0; i < tags.length; i++) {
-    tag_list += tags[i];
-
-    if (i != (tags.length - 1))
-      tag_list += ',';
-  }
-
-  document.getElementById('tag_list').value = tag_list;
-}
-
-function addTag(str) {
-
-  var newTags = str.split(',');
+function updateAttributionSettings() {
+  // iterate through all attributions and build the "shared with" list;
+  // in the meanwhile also assemble a minimized version of the array that 
+  // could be posted with the form (this won't have item titles in it)
   
-  for (var i = 0; i < newTags.length; i++) {
-
-    var tag = trimSpaces(newTags[i]);
-
-    tag = tag.replace('"', '');
-
-    if (tag.length == 0)
-      continue;
-
-    if (tags.indexOf(tag) != -1)
-      continue;
-
-    tags.push(tag);
+  var attributed_to = '';
+  var attributed_to_arr = new Array();
+  
+  for(var i = 0; i < attribution_settings.length; i++) {
+    attr_type = attribution_settings[i][0];
+    attr_title = attribution_settings[i][1];
+    attr_id = attribution_settings[i][2];
+    
+    attributed_to += '<b>' + attr_type + '</b>: ' + attr_title
+                           + '&nbsp;&nbsp;&nbsp;<small style="vertical-align: middle;">' 
+                           + '[<a href="" onclick="javascript:deleteAttribution(\''+ attr_type +'\', '+ attr_id +'); return(false);">delete</a>]</small><br/>';
+    
+    attributed_to_arr.push([attr_type, attr_id]);
   }
-
-  updateTagList();
+  
+  
+  // remove the last line break
+  if(attributed_to.length > 0) {
+    attributed_to = attributed_to.slice(0,-5);
+  }
+  
+  
+  // update the page
+  if(attributed_to.length == 0) {
+    $('attributed_to_list').innerHTML = '<span class="none_text">No attributions</span>';
+  }
+  else {
+    $('attributed_to_list').innerHTML = attributed_to;
+  }
+  
+  
+  // UPDATE THE FIELDS WHICH WILL BE SUBMITTED WITH THE PAGE
+  $('attributions').value = Object.toJSON(attributed_to_arr);
 }
 
-function deleteTag(tag) {
 
-  var i = tags.indexOf(tag);
-
-  if (i == -1)
-    return;
-
-  tags.splice(i, 1);
-  updateTagList();
+function checkAttributionNotInList(attributable_type, attributable_id) {
+  rtn = true;
+  
+  for(var i = 0; i < attribution_settings.length; i++)
+	  if(attribution_settings[i][0] == attributable_type && attribution_settings[i][2] == attributable_id) {
+	    rtn = false;
+	    break;
+	  }
+  
+  return(rtn);
 }
 
-// end tags
 
-function toggle_visibility(id) {
-   var e = document.getElementById(id);
-   if(e.style.display == 'block')
-      e.style.display = 'none';
-   else
-      e.style.display = 'block';
+// adds a contributor to "attributed to" list and updates the displayed list
+function addAttribution(attributable_type, attributable_title, attributable_id) {
+  if(checkAttributionNotInList(attributable_type, attributable_id)) {
+	  // add current values into the associative array of permissions:
+	  // first index is the category of contributor type of the permission, the second - consecutive
+	  // number of occurrences of permissions for such type of contributor
+	  attribution_settings.push([attributable_type, attributable_title, attributable_id]);
+	  
+	  // update visible page
+	  updateAttributionSettings();
+	}
+	else {
+	  alert('The following entity was not added (already in the list of attributions):\n\n' + 
+	        attributable_type + ': ' + attributable_name);
+	}
 }
 
-// credit and attribution
 
-var credit_me = true;
-var credit_users = new Object();
-var credit_groups = new Object();
-
-function updateAuthorList() {
-	
-	var markup = '';
-	
-	if (credit_me)
-	{
-		markup += 'Me&nbsp;&nbsp;&nbsp;<small>[<a href="" onclick="javascript:deleteAuthor(\'me\', null); ' +
-    		'return false;">delete</a>]</small><br/>';
-	}
-	
-	for (var key in credit_users)
-	{
-		markup += 'User: ' + credit_users[key] + '&nbsp;&nbsp;&nbsp;<small>[<a href="" onclick="javascript:deleteAuthor(\'user\', ' + key + '); ' +
-    		'return false;">delete</a>]</small><br/>';
-	}
-	
-	for (var key in credit_groups)
-	{
-		markup += 'Group: ' + credit_groups[key] + '&nbsp;&nbsp;&nbsp;<small>[<a href="" onclick="javascript:deleteAuthor(\'group\', ' + key + '); ' +
-    		'return false;">delete</a>]</small><br/>';
-	}
-	
-	if (markup == '')
-	{
-		markup = '<i>None</i>';
-	}
-	
-	document.getElementById('authors_list').innerHTML = markup;
-	
-	// Also update web form (the hidden input fields)
-	
-	// Me
-	if (credit_me)
-	{
-		document.getElementById('credits_me').value = "true";
-	}
-	else 
-	{
-		document.getElementById('credits_me').value = "false";
-	}
-	
-	// Users (friends + other users)
-	var users_list = '';
-	
-	for (var key in credit_users)
-	{
-		users_list += key + ',';
-	}
-	
-	document.getElementById('credits_users').value = users_list;
-	
-	// Groups
-	var groups_list = '';
-	
-	for (var key in credit_groups)
-	{
-		groups_list += key + ',';
-	} 
-	
-	document.getElementById('credits_groups').value = groups_list;
+// removes attribution from "attributed to" list and updates the displayed list
+function deleteAttribution(attributable_type, attributable_id) {
+  // remove the actual record for the attribution
+  for(var i = 0; i < attribution_settings.length; i++)
+    if(attribution_settings[i][0] == attributable_type && attribution_settings[i][2] == attributable_id) {
+      attribution_settings.splice(i, 1);
+      break;
+    }
+  
+  // update the page
+  updateAttributionSettings();
 }
 
-function addAuthor() {
+
+function addAttributions() {
+  var selIDs = autocompleters[attributions_autocompleter_id].getRecognizedSelectedIDs();
+  
+  if(selIDs == "") {
+    // no attributions to add
+    alert("Please choose some assets to add attributions to!");
+    return(false);
+  }
+  else {
+    // some attributions to add - known that don't have duplicates
+    // within the new list, but some entries in the new list
+    // may replicate those in the main attribution list: this
+    // will be checked by addAttribution()
     
-	// Me
-    if (document.getElementById('author_option_1').checked)
-	{
-		credit_me = true;
-	}
-	// One of my Friends 
-	else if (document.getElementById('author_option_2').checked)
-	{
-		var x = document.getElementById('author_friends_dropdown');
-		
-		if (x.options.length > 0)
-		{
-			var y = x.options[x.selectedIndex];
-	     	credit_users[y.value] = y.text;
+    for(var i = 0; i < selIDs.length; i++) {
+      id = parseInt(selIDs[i]);
+      title = autocompleters[attributions_autocompleter_id].getValueFromJsonArray(autocompleters[attributions_autocompleter_id].itemIDsToJsonArrayIDs([id])[0], 'title');
+      type = autocompleters[attributions_autocompleter_id].getValueFromJsonArray(autocompleters[attributions_autocompleter_id].itemIDsToJsonArrayIDs([id])[0], 'type');
+			addAttribution(type, title, id);
 		}
-	}
-	// A user on myExperiment who is not a Friend.
-	else if (document.getElementById('author_option_3').checked)
-	{
-		var x = document.getElementById('author_otheruser_dropdown');
-		
-		if (x.options.length > 0)
-		{
-			var y = x.options[x.selectedIndex];
-	     	credit_users[y.value] = y.text;
-		}
-	}
-	// A myExperiment Group
-	else if (document.getElementById('author_option_4').checked)
-	{
-		var x = document.getElementById('author_networks_dropdown');
-		
-		if (x.options.length > 0)
-		{
-			var y = x.options[x.selectedIndex];
-	     	credit_groups[y.value] = y.text;
-		}
-	}
-	
-	updateAuthorList();
-}
-
-function deleteAuthor(type, key) {
-
-	if (type == 'me')
-	{
-		credit_me = false;
-	}
-	else if (type == 'user')
-	{
-		delete credit_users[key];
-	}
-	else if (type == 'group')
-	{
-		delete credit_groups[key];
-	}
-	
-	updateAuthorList();
-}
-
-function update_author(parentId) {
-
-    if (parentId == 'author_option_2')
-    {
-        document.getElementById('author_friends_box').style.display = 'block';
-        document.getElementById('author_otheruser_box').style.display = 'none';
-        document.getElementById('author_networks_box').style.display = 'none';
-    }
-    else if (parentId == 'author_option_3')
-    {
-        document.getElementById('author_friends_box').style.display = 'none';
-        document.getElementById('author_otheruser_box').style.display = 'block';
-        document.getElementById('author_networks_box').style.display = 'none';
-    }
-    else if (parentId == 'author_option_4')
-    {
-        document.getElementById('author_friends_box').style.display = 'none';
-        document.getElementById('author_otheruser_box').style.display = 'none';
-        document.getElementById('author_networks_box').style.display = 'block';
-    }
-    else
-    {
-        document.getElementById('author_friends_box').style.display = 'none';
-        document.getElementById('author_otheruser_box').style.display = 'none';
-        document.getElementById('author_networks_box').style.display = 'none';
-    }
-}
-
-var attributions_workflows = new Object();
-var attributions_files = new Object();
-
-function updateAttributionsList() {
-	
-	var markup = '';
-	
-	for (var key in attributions_workflows)
-	{
-		markup += 'Workflow: ' + attributions_workflows[key] + '&nbsp;&nbsp;&nbsp;<small>[<a href="" onclick="javascript:deleteAttribution(\'workflow\', ' + key + '); ' +
-    		'return false;">delete</a>]</small><br/>';
-	}
-	
-	for (var key in attributions_files)
-	{
-		markup += 'File: ' + attributions_files[key] + '&nbsp;&nbsp;&nbsp;<small>[<a href="" onclick="javascript:deleteAttribution(\'file\', ' + key + '); ' +
-    		'return false;">delete</a>]</small><br/>';
-	}
-	
-	if (markup == '')
-	{
-		markup = '<i>None</i>';
-	}
-	
-	document.getElementById('attribution_list').innerHTML = markup;
-	
-	// Also update web form (the hidden input fields)
-	
-	var attr_workflows_list = '';
-	
-	for (var key in attributions_workflows)
-	{
-		attr_workflows_list += key + ',';
-	}
-	
-	document.getElementById('attributions_workflows').value = attr_workflows_list;
-	
-	var attr_files_list = '';
-	
-	for (var key in attributions_files)
-	{
-		attr_files_list += key + ',';
-	} 
-	
-	document.getElementById('attributions_files').value = attr_files_list;
-}
-
-function addAttribution(type) {
-	
-	if (type == 'existing_workflow') {
-		var x = document.getElementById('existingworkflows_dropdown');
-		
-		if (x.options.length > 0) {
-			var y = x.options[x.selectedIndex];
-			attributions_workflows[y.value] = y.text;
-		}
-	}
-	else if (type == 'existing_file') {
-		var x = document.getElementById('existingfiles_dropdown');
-		
-		if (x.options.length > 0) {
-			var y = x.options[x.selectedIndex];
-			attributions_files[y.value] = y.text;
-		}
-	} 
-	
-	updateAttributionsList();
-}
-
-function deleteAttribution(type, id) {
-	
-	if (type == 'workflow') {
-		delete attributions_workflows[id];
-	}
-	else if (type == 'file') {
-		delete attributions_files[id];
-	}
-	
-	updateAttributionsList();
-}
-
-// end credit and attribution
-
-function toggle_copy_inherit(obj) {
-    var f = document.getElementById('copy_inherit_sharing_box');
+      
+    // remove all tokens from autocomplete text box 
+    autocompleters[attributions_autocompleter_id].deleteAllTokens();
     
-    if(obj.oldText)
-    {
-        f.style.display = 'none';
-        obj.innerHTML = obj.oldText;
-        obj.oldText = null;
-    } 
-    else 
-    {
-        f.style.display = 'block';
-        obj.oldText = obj.innerHTML;
-        obj.innerHTML = 'Hide';
-    }
+    return(true);
+  }
 }
 
-function update_sharing(mode) {
-    
-    /*
-		if (mode == 5)
-    {
-        document.getElementById('sharing_networks1_box').style.display = 'block';
-        document.getElementById('sharing_networks2_box').style.display = 'none';
-        //document.getElementById('sharing_custom_box').style.display = 'none';
-    }
-    else if (mode == 6)
-    {
-        document.getElementById('sharing_networks1_box').style.display = 'none';
-        document.getElementById('sharing_networks2_box').style.display = 'block';
-        //document.getElementById('sharing_custom_box').style.display = 'none';
-    }
-    else if (mode == 8)
-    {
-        document.getElementById('sharing_networks1_box').style.display = 'none';
-        document.getElementById('sharing_networks2_box').style.display = 'none';
-        //document.getElementById('sharing_custom_box').style.display = 'block';
-    }
-    else 
-    {
-        document.getElementById('sharing_networks1_box').style.display = 'none';
-        document.getElementById('sharing_networks2_box').style.display = 'none';
-        //document.getElementById('sharing_custom_box').style.display = 'none';
-    }
-    */
-}
 
-function update_updating(mode) {
-
-    if (mode == 5)
-    {
-				document.getElementById('updating_somefriends_box').style.display = 'block';
-        //document.getElementById('updating_custom_box').style.display = 'none';
-    }
-    else
-    {
-				document.getElementById('updating_somefriends_box').style.display = 'none';
-        //document.getElementById('updating_custom_box').style.display = 'none';
-    }
-}
-
-function add_updaterpermission_string(permission_string) {
-    var f = document.getElementById('updating_permissions_list');
-    
-    if (!f.hasValues)
-    {
-        f.innerHTML = permission_string;
-        f.hasValues = 'true';
-    }    
-    else
-    {
-        f.innerHTML = f.innerHTML + '<br/>' + permission_string;
-    }
-}
-
+// ********************************************************
