84d87e61ed65b594c95295a21c8f93c1aa5aea65
Unknown <sowen@rpc267>
2009-03-13 17:01:01 +0000

    styling fix for large number of tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -408,7 +408,7 @@ module ApplicationHelper
   def link_for_tag tag, options={}
       link=people_url
       length=options[:truncate_length]
-      length||=30
+      length||=150
       if (options[:type]==:expertise)
         link=people_url(:expertise=>tag.name)
       end
@@ -423,7 +423,7 @@ module ApplicationHelper
 
   def list_item_tags_list tags,options={}
     tags.map do |t|
-      divider=tags.last==t ? "" : "&nbsp;&nbsp;|&nbsp;&nbsp;"
+      divider=tags.last==t ? "" : "  <span class='spacer'>|</span>  "
       link_for_tag(t,options)+divider
     end
   end
