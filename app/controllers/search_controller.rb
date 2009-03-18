d51dd2066c460c12c845c7bc6ddb1fc6566f60a6
Unknown <sowen@rpc267>
2009-03-18 17:42:13 +0000

    added task to reindex sops for search, and downcase queries before doing a search resolve problem with capitialised queries with wildcards not working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,19 +8,21 @@ class SearchController < ApplicationController
     @search_query||=""
     @search_type = params[:search_type]
     type=@search_type.downcase unless @search_type.nil?
+
+    downcase_query = @search_query.downcase
     
     @results=[]
     case(type)
     when("people")
-      @results = Person.multi_solr_search(@search_query, :limit=>100, :models=>[Person]).results if (SOLR_ENABLED and !@search_query.nil? and !@search_query.strip.empty?)      
+      @results = Person.multi_solr_search(downcase_query, :limit=>100, :models=>[Person]).results if (SOLR_ENABLED and !downcase_query.nil? and !downcase_query.strip.empty?)
     when("institutions")
-      @results = Institution.multi_solr_search(@search_query, :limit=>100, :models=>[Institution]).results if (SOLR_ENABLED and !@search_query.nil? and !@search_query.strip.empty?)
+      @results = Institution.multi_solr_search(downcase_query, :limit=>100, :models=>[Institution]).results if (SOLR_ENABLED and !downcase_query.nil? and !downcase_query.strip.empty?)
     when("projects")
-      @results = Project.multi_solr_search(@search_query, :limit=>100, :models=>[Project]).results if (SOLR_ENABLED and !@search_query.nil? and !@search_query.strip.empty?)
+      @results = Project.multi_solr_search(downcase_query, :limit=>100, :models=>[Project]).results if (SOLR_ENABLED and !downcase_query.nil? and !downcase_query.strip.empty?)
     when ("sops")
-      @results = Project.multi_solr_search(@search_query, :limit=>100, :models=>[Sop]).results if (SOLR_ENABLED and !@search_query.nil? and !@search_query.strip.empty?)
+      @results = Project.multi_solr_search(downcase_query, :limit=>100, :models=>[Sop]).results if (SOLR_ENABLED and !downcase_query.nil? and !downcase_query.strip.empty?)
     else
-      @results = Person.multi_solr_search(@search_query, :limit=>100, :models=>[Person, Project, Institution,Sop]).results if (SOLR_ENABLED and !@search_query.nil? and !@search_query.strip.empty?)
+      @results = Person.multi_solr_search(downcase_query, :limit=>100, :models=>[Person, Project, Institution,Sop]).results if (SOLR_ENABLED and !downcase_query.nil? and !downcase_query.strip.empty?)
     end
 
     @results = select_authorised @results
