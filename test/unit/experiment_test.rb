4d4107b6b065423377f9802b9521b0233b06067a
Unknown <sowen@rpc267>
2009-03-20 16:54:54 +0000

    sorted out some basic fixtures for Experiment and associated models, and a basic unit test to check the associations within the fixtures are correct, and will detect if the schema changes

--- test/unit/experiment_test.rb
+++ test/unit/experiment_test.rb
@@ -1,8 +1,23 @@
 require 'test_helper'
 
 class ExperimentTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :experiments,:assays,:topics,:projects,:experiment_types,:assay_types
+  
+  test "associations" do
+    exp=experiments(:metabolomics_exp)
+    assert_equal "A Metabolomics Experiment",exp.title
+
+    assert_not_nil exp.assay
+    assert_not_nil exp.assay.topic
+    assert_not_nil exp.assay.topic.project
+
+    assert_equal "Metabolomics Assay",exp.assay.title
+    assert_equal "Metabolomics Topic",exp.assay.topic.title
+    assert_equal projects(:sysmo_project),exp.assay.topic.project
+
+    assert_equal experiment_types(:catabolic_response),exp.experiment_type
+    assert_equal assay_types(:metabolomics),exp.assay.assay_type
+    
+    
   end
 end
