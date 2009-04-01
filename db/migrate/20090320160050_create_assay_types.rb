9b357d0dca4b450f83e689f7baa71db6c9e453aa
Unknown <sowen@rpc267>
2009-04-01 17:22:04 +0100

    Given assay types a parent_id to form tree structure

--- db/migrate/20090320160050_create_assay_types.rb
+++ db/migrate/20090320160050_create_assay_types.rb
@@ -2,7 +2,7 @@ class CreateAssayTypes < ActiveRecord::Migration
   def self.up
     create_table :assay_types do |t|
       t.string :title
-
+      t.string :parent_assay_type_id
       t.timestamps
     end
   end
