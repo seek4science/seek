6bb0199e6d5bd7d287bef19057544aa64122e37b
Unknown <sowen@rpc267>
2009-04-03 11:33:36 +0100

    added some initial tests for the model associations

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,2 +1,19 @@
+require 'acts_as_resource'
+
 class Model < ActiveRecord::Base
+
+  acts_as_resource
+  
+  validates_presence_of :title
+  
+  # allow same titles, but only if these belong to different users
+  validates_uniqueness_of :title, :scope => [ :contributor_id, :contributor_type ], :message => "error - you already have a SOP with such title."
+
+  belongs_to :content_blob,
+             :dependent => :destroy
+
+  acts_as_solr(:fields=>[:description,:title,:original_filename]) if SOLR_ENABLED
+
+  belongs_to :recommended_environment,:class_name=>"RecommendedModelEnvironment"
+  
 end
--- test/unit/model_test.rb
+++ test/unit/model_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class ModelTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  fixtures :models,:recommended_model_environments,:content_blobs
+
+  test "assocations" do
+    model=models(:teusink)
+    jws_env=recommended_model_environments(:jws)
+
+    assert_equal jws_env,model.recommended_environment
+
+    assert_equal "Teusink",model.title
+
+    blob=content_blobs(:teusink_blob)
+    assert_equal blob,model.content_blob
   end
 end
