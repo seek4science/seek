42b6c6a6491194cb4da324d58c95eba1698ff72f
Unknown <sowen@rpc267>
2009-04-03 10:38:04 +0100

    made full use of the project alias on Experiment rather than using exp.topic.project

--- app/helpers/experiments_helper.rb
+++ app/helpers/experiments_helper.rb
@@ -3,14 +3,7 @@ module ExperimentsHelper
 
   def topics_from_assay assay   
     Topic.find(:all,:conditions=>{:project_id=>project_from_assay(assay)})
-  end
-  
-  def project_from_assay assay
-    project_id=assay.topic.project.id if !assay.topic.project.nil?
-    project_id ||= 0
-
-    return project_id
-  end
+  end  
 
   def new_topic_to_project_popup_link project_id
     return link_to_remote_redbox("New topic",
