92b8836ecba60dfe8ef209ee2da1bb18761a2433
Unknown <sowen@rpc267>
2009-04-06 17:02:36 +0100

    added extra details to list views

--- app/helpers/models_helper.rb
+++ app/helpers/models_helper.rb
@@ -1,2 +1,7 @@
 module ModelsHelper
+
+  def model_environment_text model
+    model.recommended_environment ? h(model.recommended_environment.title) : "<span class='none_text'>Unknown</span>" 
+  end
+
 end
