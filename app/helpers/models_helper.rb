386db5ab2887ffaccfbbe9be88e7bd2e54e6d5df
Unknown <sowen@rpc267>
2009-04-15 11:17:05 +0100

    fixing some styling issues, but still doesn't resolve the minor problem when the applet appears

--- app/helpers/models_helper.rb
+++ app/helpers/models_helper.rb
@@ -11,7 +11,7 @@ module ModelsHelper
   def execute_model_label
     icon_filename=method_to_icon_filename("execute")
 
-    return '<span class="icon">' + image_tag(icon_filename,:alt=>"Execute",:title=>"Execute") + ' Execute model</span>';
+    return '<span class="icon">' + image_tag(icon_filename,:alt=>"Simulate",:title=>"Simulate") + ' Simulate model</span>';
   end
 
 end
