d2ccdbf00d028d92cf5a219fdb4ef39a0279d1dc
Unknown <sowen@Lappy>
2009-05-06 21:34:36 +0100

    added some extra tests for study_controller

--- test/functional/studies_controller_test.rb
+++ test/functional/studies_controller_test.rb
@@ -21,4 +21,33 @@ class StudiesControllerTest < ActionController::TestCase
     assert_response :success
     assert_not_nil assigns(:study)
   end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:study)
+  end
+
+  test "should get edit" do
+    get :edit,:id=>studies(:metabolomics_study)
+    assert_response :success
+    assert_not_nil assigns(:study)
+  end
+
+  test "should update" do
+    s=studies(:metabolomics_study)
+    assert_not_equal "test",s.title
+    put :update,:id=>s.id,:study=>{:title=>"test"}
+    s=assigns(:study)
+    assert_redirected_to study_path(s)
+    assert_equal "test",s.title
+  end
+
+  test "should create" do
+    assert_difference("Study.count") do
+      post :create,:study=>{:title=>"test",:investigation=>investigations(:metabolomics_investigation)}
+    end
+    s=assigns(:study)
+    assert_redirected_to study_path(s)
+  end
 end
