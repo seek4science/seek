dddc02f91d8667bb4dcf73ba19ce1e0d6a9b29a8
Unknown <sowen@rpc267>
2009-05-07 18:07:24 +0100

    added migration to use yml file to import default assay-types

---
+++ db/migrate/20090507162858_assay_type_data.rb
@@ -0,0 +1,8 @@
+require 'default_data_migration'
+
+
+class AssayTypeData < DefaultDataMigration
+  def self.model_class_name
+    "AssayType"
+  end
+end
---
+++ lib/default_data_migration.rb
@@ -0,0 +1,23 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+require 'active_record/fixtures'
+
+class DefaultDataMigration < ActiveRecord::Migration
+
+  def self.up
+    down
+    table_name=model_class_name.pluralize.underscore
+    puts "Data directory=#{self.default_data_directory}, model_class_name=#{model_class_name}, table name=#{table_name}"
+    Fixtures.create_fixtures(self.default_data_directory, table_name)
+  end
+
+  def self.down
+    eval("#{model_class_name}.delete_all")
+  end
+
+  def self.default_data_directory
+    File.join(RAILS_ROOT, "config/default_data" )
+  end
+
+  
+end
