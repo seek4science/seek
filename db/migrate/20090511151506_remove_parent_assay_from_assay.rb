c18a598bab473cec34f388f68f2c0382a413cc70
Unknown <sowen@rpc267>
2009-05-11 17:29:42 +0100

    removed migration to remove parent_assay from assay - seems it no longer exists

--- db/migrate/20090511151506_remove_parent_assay_from_assay.rb
+++
@@ -1,9 +0,0 @@
-class RemoveParentAssayFromAssay < ActiveRecord::Migration
-  def self.up
-    remove_column(:assays, :parent_assay_id)
-  end
-
-  def self.down
-    add_column(:assays,:parent_assay_id,:integer)
-  end
-end
