956560aeb08bf5d24060e222f06329d93c804b86
Unknown <sowen@rpc267>
2009-05-12 16:24:22 +0100

    browsing technology_types

--- app/controllers/technology_types_controller.rb
+++ app/controllers/technology_types_controller.rb
@@ -1,2 +1,15 @@
 class TechnologyTypesController < ApplicationController
+
+  before_filter :login_required
+
+  def show
+    @technology_type = TechnologyType.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.xml { render :xml=>@technology_type}
+    end
+
+  end
+
 end
--- app/models/technology_type.rb
+++ app/models/technology_type.rb
@@ -2,7 +2,20 @@ require 'acts_as_ontology'
 
 class TechnologyType < ActiveRecord::Base
 
+  has_many :assays
+
   acts_as_ontology
+
+  def get_child_assays assay_type=self
+    #TODO: needs unit test
+    #TODO: make Assay independant, and move some of this to acts_as_ontology
+    result = assay_type.assays
+    assay_type.children.each do |child|
+      result = result | child.assays
+      result = result | get_child_assays(child) if child.has_children?
+    end
+    return result
+  end
 
   def title
     super.capitalize
