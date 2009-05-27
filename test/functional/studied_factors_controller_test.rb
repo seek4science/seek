f7b36b162495f1abc0d4a6a14ae8f4fa2de4b763
Unknown <sowen@rpc267>
2009-05-27 13:54:50 +0100

    expanded studied_factors_controller tests a little.

--- test/functional/studied_factors_controller_test.rb
+++ test/functional/studied_factors_controller_test.rb
@@ -14,12 +14,16 @@ class StudiedFactorsControllerTest < ActionController::TestCase
     df=data_files(:downloadable_data_file)
     get :index,:data_file_id=>df.id
     assert_select 'img[title="Start editing"]',:count=>0
+    assert_select 'div[id="edit_on"]',:count=>0
+    assert_select 'div[id="edit_off"]',:count=>0
   end
 
   test "can edit factors studied for editable data file" do
     df=data_files(:editable_data_file)
     get :index,:data_file_id=>df.id
     assert_select 'img[title="Start editing"]',:count=>1
+    assert_select 'div[id="edit_on"]',:count=>1
+    assert_select 'div[id="edit_off"]',:count=>1
   end
 
 end
