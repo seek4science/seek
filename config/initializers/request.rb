e8cced796ba8c1ab1936c3a3c0fe1dfaa8daf1cf
Unknown <sowen@Lappy>
2009-06-01 19:24:15 +0100

    added request.rb to initializers to avoid mongrel error after upgrade to rails 2.3.2

---
+++ config/initializers/request.rb
@@ -0,0 +1,12 @@
+
+module ActionController
+  class AbstractRequest < ActionController::Request
+    def self.relative_url_root=(path)
+      ActionController::Base.relative_url_root=(path)
+    end
+    def self.relative_url_root
+      ActionController::Base.relative_url_root
+    end
+  end
+end
+
