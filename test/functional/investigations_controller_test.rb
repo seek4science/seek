1bce11001f1856c324751c00744047d19e2712bd
Unknown <sowen@rpc267>
2009-06-02 13:57:07 +0100

    added tabs for Investigations and Assays - links do not yet work. Some restyling to accomodate tabs - Admin tab now removed and the admin mode is clickable to get to Admin page

--- test/functional/investigations_controller_test.rb
+++ test/functional/investigations_controller_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class InvestigationsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  fixtures :all
+
+  include AuthenticatedTestHelper
+
+  def setup
+    login_as(:aaron)
   end
+
+  test "should show index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:investigations)
+  end
+
+  test "should show item" do
+    get :show, :id=>investigations(:metabolomics_investigation)
+    assert_response :success
+    assert_not_nil assigns(:investigation)
+  end
+
 end
