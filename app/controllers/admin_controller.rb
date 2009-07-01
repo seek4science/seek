fd04004d1dcf489830916de201ac54355368dd74
Unknown <sowen@rpc267>
2009-07-01 10:42:56 +0100

    scaled y axis for charts. The scale isn't marked on the axis yet. Forced range on x axis to be between 1 Nov 2008, and 'today'

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -16,28 +16,34 @@ class AdminController < ApplicationController
     data=created_at_data_for_model(Sop)
     dataset = GoogleChartDataset.new :data => data, :color => '0000DD'
     @sop_creation_graph = GoogleLineChart.new :width => width, :height => height, :title=>"Sop Creation"
-    @sop_creation_graph.data = GoogleChartData.new :datasets => [dataset]
+    @sop_creation_graph.data = GoogleChartData.new :datasets => [dataset],:min=>0,:max=>data.max
 
     data=created_at_data_for_model(Model)
     dataset = GoogleChartDataset.new :data => data, :color => '0000DD'
     @model_creation_graph = GoogleLineChart.new :width => width, :height => height, :title=>"Model Creation"
-    @model_creation_graph.data = GoogleChartData.new :datasets => [dataset]
+    @model_creation_graph.data = GoogleChartData.new :datasets => [dataset],:min=>0,:max=>data.max
 
     data=created_at_data_for_model(DataFile)
     dataset = GoogleChartDataset.new :data => data, :color => '0000DD'
     @df_creation_graph = GoogleLineChart.new :width => width, :height => height, :title=>"Data File Creation"
-    @df_creation_graph.data = GoogleChartData.new :datasets => [dataset]
+    @df_creation_graph.data = GoogleChartData.new :datasets => [dataset],:min=>0,:max=>data.max
 
     data=created_at_data_for_model(User)
     dataset = GoogleChartDataset.new :data => data, :color => 'DD0000'
     @user_creation_graph = GoogleLineChart.new :width => width, :height => height, :title=>"User creation"
-    @user_creation_graph.data = GoogleChartData.new :datasets => [dataset]
+    @user_creation_graph.data = GoogleChartData.new :datasets => [dataset],:min=>0,:max=>data.max
+    
   end
   
   private
 
   def created_at_data_for_model model
     x={}
+    start="1 Nov 2008"
+    
+    x[Date.parse(start).jd]=0
+    x[Date.today.jd]=0
+
     model.find(:all,:order=>:created_at).each do |i|
       date=i.created_at.to_date
       day=date.jd
