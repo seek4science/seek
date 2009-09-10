835e5a85d85582e5db5de967b95cea24f1e64f15
Unknown <sowen@Toffee>
2009-09-10 12:09:26 +0100

    show investigation on assay page and resource_list_item

--- app/helpers/investigations_helper.rb
+++ app/helpers/investigations_helper.rb
@@ -1,2 +1,11 @@
 module InvestigationsHelper
+
+  def investigation_link investigation
+    unless investigation.nil?
+      link_to h(investigation.title),investigation
+    else
+      "<span class='none_text'>Not associated with an Investigation</span>"      
+    end
+  end
+
 end
