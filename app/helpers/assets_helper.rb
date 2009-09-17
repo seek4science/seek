da82410ff367238cef9c309c59552dcede641fa1
Unknown <sowen@rpc267>
2009-09-17 14:18:58 +0100

    version selection identifies current version, and automatically selects the current version

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -6,12 +6,19 @@ module AssetsHelper
     return '<span class="icon">' + image_tag(icon_filename,:alt=>"Request",:title=>"Request") + " Request #{resource_type}</span>";
   end
 
-  def resource_version_selection versioned_resource
-    versions=versioned_resource.versions
-
-    "<form onsubmit='showResourceVersion(this)'; return false;>"+select_tag(:resource_versions,options_for_select(
-            versions.collect{|v| ["#{v.version.to_s} #{versioned_resource.describe_version(v.version)}",url_for(:id=>versioned_resource,:version=>v.version)]} ),
-            :onchange=>"showResourceVersion(this.form);"
+  def resource_version_selection versioned_resource,displayed_resource_version
+    versions=versioned_resource.versions.reverse
+    disabled=versions.size==1
+    options=""
+    versions.each do |v|
+      options << "<option value='#{url_for(:id=>versioned_resource,:version=>v.version)}'"
+      options << " selected='selected'" if v.version==displayed_resource_version.version
+      options << "> #{v.version.to_s} #{versioned_resource.describe_version(v.version)} </option>"
+    end
+    "<form onsubmit='showResourceVersion(this)'; return false;>"+select_tag(:resource_versions,
+                                                                            options,
+                                                                            :disabled=>disabled,
+                                                                            :onchange=>"showResourceVersion(this.form);"
 
     )+"</form>"
     
