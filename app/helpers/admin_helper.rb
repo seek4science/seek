47e2b985be4b8913f21f28266b5b8768809c0ba1
Unknown <sowen@Toffee>
2009-09-18 10:59:15 +0100

    fixed problem with new asset gadget button in new model view. Also added an admin tool to list people where their is_pal and roles do not match

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -4,4 +4,12 @@ module AdminHelper
   def dubious_tag?(tag)
     tag.name.length>50 || [";",",",":","/","|"].detect{|c| tag.name.include?(c)}
   end
+
+  #returns a list of people where their is_pal? flag doesn't match their profile role
+  def pal_mismatchces
+      pal_role=Role.find(6)
+      Person.find(:all).select do |p|
+        p.is_pal? != p.roles.include?(pal_role)        
+      end
+  end
 end
