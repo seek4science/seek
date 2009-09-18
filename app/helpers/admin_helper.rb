7b8ad316228d639bdb44e812fb8fdab73f5f81e4
Unknown <sowen@Toffee>
2009-09-18 11:09:30 +0100

    fixed test problem caused by finding pal role by ID, changed to find by name and updated test fixtures to include it

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -7,7 +7,7 @@ module AdminHelper
 
   #returns a list of people where their is_pal? flag doesn't match their profile role
   def pal_mismatchces
-      pal_role=Role.find(6)
+      pal_role=Role.find(:first,:conditions=>{:name=>"Sysmo-DB Pal"})
       Person.find(:all).select do |p|
         p.is_pal? != p.roles.include?(pal_role)        
       end
