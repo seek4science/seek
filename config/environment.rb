022364282f0de1f643b24d8b6990927dee6ca485
Unknown <sowen@rpc267>
2009-10-22 11:52:22 +0100

    removed roo and its dependencies - caused too many problems, and we can re-add it later post-release

--- config/environment.rb
+++ config/environment.rb
@@ -25,8 +25,7 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'rubaidh-google_analytics', :lib => 'rubaidh/google_analytics', :source => 'http://gems.github.com'
-  config.gem 'roo'
+  config.gem 'rubaidh-google_analytics', :lib => 'rubaidh/google_analytics', :source => 'http://gems.github.com'  
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
--- vendor/gems/GData-0.0.4/lib/gdata.rb
+++
@@ -1,3 +0,0 @@
-class GData
-  VERSION = '0.0.4'
-end
