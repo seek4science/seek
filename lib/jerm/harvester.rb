650f3da2429254e863784fd95f13fcc7dc32d052
Unknown <sowen@rpc267>
2009-11-10 14:58:27 +0000

    Jerm skeleton classes

---
+++ lib/jerm/harvester.rb
@@ -0,0 +1,24 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+class Harvester
+  
+  def update    
+    authenticate
+    items = changed_since(last_run)
+    items.each do |item|
+      resource = construct_resource(item)
+      populate resource
+    end
+
+  end
+
+  def last_run
+    DateTime.parse("1 Jan 2007")
+  end
+
+  def populate resource
+    puts resource.to_s
+  end
+
+end
---
+++ lib/jerm/resource.rb
@@ -0,0 +1,11 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+class Resource
+
+  attr_accessor :project, :uri, :author_first_name, :author_last_name,:timestamp,:type
+
+  def initialize
+    
+  end
+end
