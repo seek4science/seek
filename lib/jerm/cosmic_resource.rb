11d116a7ec63d4cc36e9b40e5dd92955de51a6f5
Unknown <sowen@rpc267>
2009-11-12 15:25:35 +0000

    some naive code to read the metadata.csv

--- lib/jerm/cosmic_resource.rb
+++ lib/jerm/cosmic_resource.rb
@@ -11,9 +11,21 @@ class CosmicResource < AlfrescoResource
   end
 
   def populate
+    read_metadata(@metadata)
     puts @metadata
     puts @asset
     puts @timestamp    
+  end
+
+  def read_metadata metadata_uri
+    uri = URI.parse(metadata_uri)
+    http=Net::HTTP.new(uri.host,uri.port)
+    http.use_ssl=true if uri.scheme=="https"
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    req = Net::HTTP::Get.new(uri.path)
+    req.basic_auth @username,@password
+    response = http.request(req)
+    puts response.body
   end
   
 end
