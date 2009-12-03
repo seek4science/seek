297a8e9aec14bebd9ce02c449fe47e7edaf28f1c
Unknown <sowen@rpc267>
2009-12-03 17:27:40 +0000

    added support for adding new jerm harvesters following an SPI like pattern - though you need to put them in a specific place at the moment

--- app/controllers/jerm_controller.rb
+++ app/controllers/jerm_controller.rb
@@ -2,6 +2,8 @@ class JermController < ApplicationController
   before_filter :login_required
   before_filter :is_user_admin_auth
 
+  @@harvesters=nil
+
   def index
     
   end
@@ -14,17 +16,44 @@ class JermController < ApplicationController
     @project=Project.find(project_id)
 
     begin
-      harvester = Jerm::CosmicHarvester.new username,password
+      harvester = construct_project_harvester(@project.title,username,password)
       @results = harvester.update
-    rescue
-
+    rescue Exception => @exception
+      puts @exception
     end
 
     render :update do |page|
-      page.replace_html :results,:partial=>"results",:object=>@results      
+      if @exception
+        page.replace_html :results,:partial=>"exception",:object=>@exception
+      else
+        page.replace_html :results,:partial=>"results",:object=>@results
+      end
+      
     end
 
-    
   end
-  
+
+  private
+
+  def construct_project_harvester project_name,uname,pwd
+    discover_harvesters if @@harvesters.nil?
+    harvester_class=@@harvesters.find do |h|      
+      h.name.downcase.include?(project_name.downcase)
+    end
+    return harvester_class.new(uname,pwd)
+  end
+
+  def discover_harvesters
+    Dir.chdir(File.join(RAILS_ROOT, "lib/jerm")) do
+      Dir.glob("*harvester.rb").each do |f|
+        ("jerm/" + f.gsub(/.rb/, '')).camelize.constantize
+      end
+    end
+    harvesters=[]
+    ObjectSpace.each_object(Class) do |c|
+      harvesters << c if c < Jerm::Harvester
+    end
+    puts harvesters.size
+    @@harvesters=harvesters
+  end
 end
