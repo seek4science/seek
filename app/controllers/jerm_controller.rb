b27729f3009987af1992dc7421e7fd63c211a6b6
Unknown <sowen@rpc267>
2009-12-04 12:57:42 +0000

    fix to get sysmo-lab project working in harvester test environment

--- app/controllers/jerm_controller.rb
+++ app/controllers/jerm_controller.rb
@@ -36,10 +36,13 @@ class JermController < ApplicationController
   private
 
   def construct_project_harvester project_name,uname,pwd
+    #removes hyphens from project name
+    clean_project_name=project_name.gsub("-","")
     discover_harvesters if @@harvesters.nil?
     harvester_class=@@harvesters.find do |h|      
-      h.name.downcase.include?(project_name.downcase)
+      h.name.downcase.include?(clean_project_name.downcase)
     end
+    raise Exception.new("Unable to find Harvester for project #{project_name}") if harvester_class.nil?
     return harvester_class.new(uname,pwd)
   end
 
@@ -53,7 +56,7 @@ class JermController < ApplicationController
     ObjectSpace.each_object(Class) do |c|
       harvesters << c if c < Jerm::Harvester
     end
-    puts harvesters.size
+    
     @@harvesters=harvesters
   end
 end
