87da892fc4fb1a70a08d026a9bd7e92e8d83780b
Unknown <sowen@rpc267>
2009-12-18 12:23:48 +0000

    added tag tests (got missed in last commit)

---
+++ test/functional/tags_controller_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class TagsControllerTest< ActionController::TestCase
+  include AuthenticatedTestHelper
+
+  fixtures :all
+
+  def setup
+    login_as(:quentin)
+  end
+
+  test "show" do    
+    get :show,:id=>tags(:fishing)
+    assert_response :success
+  end
+
+  test "dont show duplicates for same tag for expertise and tools" do
+    q=people(:one)
+    q.tool_list="zzzzz"
+    q.expertise_list="zzzzz"
+    q.save!
+    q=Person.find(q.id)
+    assert_equal 1,q.expertise_counts.size,"should be 1 expertise tag"
+    assert_equal 1,q.tool_counts.size,"should be 1 tools tag"
+    tag=q.expertise_counts.first
+    assert_equal "zzzzz",tag.name,"the expected tag name is zzzzz"
+    get :show,:id=>tag.id
+    assert_response :success
+    assert_select "div.list_items_container" do
+      assert_select "a",:text=>"Quentin Jones",:count=>1
+    end
+  end
+
+
+end
