f0843f8a5a2b6881fdd27ec566e9adaeefb20a50
Unknown <finn@ubuntu.ubuntu-domain>
2010-01-27 16:20:54 +0000

    Fixed absolute paths in tag cloud

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -233,19 +233,9 @@ module ApplicationHelper
   end
 
   def link_for_tag tag, options={}
-    link=people_url
     length=options[:truncate_length]
     length||=150
-    if (options[:type]==:expertise)
-      link=people_url(:expertise=>tag.name)
-    end
-    if (options[:type]==:tools)
-      link=people_url(:tools=>tag.name)
-    end
-    if (options[:type]==:organisms)
-      link=projects_url(:organisms=>tag.name)
-    end
-    link = show_tag_url(tag)
+    link = show_tag_path(tag)
     link_to h(truncate(tag.name,:length=>length)), link, :class=>options[:class],:id=>options[:id],:style=>options[:style],:title=>tooltip_title_attrib(tag.name)
   end
 
