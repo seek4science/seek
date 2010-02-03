95aede75b91e9b2b4d0e49b47b694a20ae758c84
Unknown <sowen@rpc267>
2010-02-03 12:52:59 +0000

    removed the google graphs

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,6 @@
 class AdminController < ApplicationController
   before_filter :login_required
-  before_filter :is_user_admin_auth
-
-  use_google_charts
+  before_filter :is_user_admin_auth  
 
   def show
     respond_to do |format|
@@ -12,34 +10,7 @@ class AdminController < ApplicationController
 
   def tags
     @tags=Tag.find(:all)
-  end
-
-  def graphs
-
-    width=300
-    height=200
-
-    data=created_at_data_for_model(Sop)
-    dataset = GoogleChartDataset.new :data => data, :color => '0000DD'
-    @sop_creation_graph = GoogleLineChart.new :width => width, :height => height, :title=>"Sop Creation"
-    @sop_creation_graph.data = GoogleChartData.new :datasets => [dataset], :min=>0, :max=>data.max
-
-    data=created_at_data_for_model(Model)
-    dataset = GoogleChartDataset.new :data => data, :color => '0000DD'
-    @model_creation_graph = GoogleLineChart.new :width => width, :height => height, :title=>"Model Creation"
-    @model_creation_graph.data = GoogleChartData.new :datasets => [dataset], :min=>0, :max=>data.max
-
-    data=created_at_data_for_model(DataFile)
-    dataset = GoogleChartDataset.new :data => data, :color => '0000DD'
-    @df_creation_graph = GoogleLineChart.new :width => width, :height => height, :title=>"Data File Creation"
-    @df_creation_graph.data = GoogleChartData.new :datasets => [dataset], :min=>0, :max=>data.max
-
-    data=created_at_data_for_model(User)
-    dataset = GoogleChartDataset.new :data => data, :color => 'DD0000'
-    @user_creation_graph = GoogleLineChart.new :width => width, :height => height, :title=>"User creation"
-    @user_creation_graph.data = GoogleChartData.new :datasets => [dataset], :min=>0, :max=>data.max
-
-  end
+  end  
 
   def edit_tag
     if request.post?
