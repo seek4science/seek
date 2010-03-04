5cfb11db929d4e073d6e50468404f5d9357fe444
Unknown <sowen@rpc267>
2010-03-04 11:26:59 +0000

    changed generator from NamedBase to Base

--- vendor/plugins/bioportal/generators/bioportal_migration/bioportal_migration_generator.rb
+++ vendor/plugins/bioportal/generators/bioportal_migration/bioportal_migration_generator.rb
@@ -1,4 +1,4 @@
-class BioportalMigrationGenerator < Rails::Generator::NamedBase
+class BioportalMigrationGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       m.migration_template 'migration.rb', 'db/migrate', :migration_file_name => "bioportal_concept_migration"
