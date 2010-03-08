1c26a24c658009e983bbd3efcd8d16639aec4cc7
Unknown <sowen@rpc267>
2010-03-08 17:14:10 +0000

    moved the visualize partial over to the plugin

--- vendor/plugins/bioportal/init.rb
+++ vendor/plugins/bioportal/init.rb
@@ -1,6 +1,4 @@
 require 'bioportal'
-require 'app/helpers/bio_portal_helper'
-require 'app/models/bioportal_concept'
 
 ActionView::Base.send(:include, BioPortal::BioPortalHelper)
 ActiveRecord::Base.send(:include,BioPortal::Acts)
