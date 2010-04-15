d75f7c449f2e403b9c49696344a68f02efbac381
Unknown <sowen@rpc267>
2010-04-15 13:09:48 +0100

    displays ontology details about the organism on the #show page, and includes a link to the NCBI taxonomy browser

--- app/helpers/organisms_helper.rb
+++ app/helpers/organisms_helper.rb
@@ -18,4 +18,12 @@ module OrganismsHelper
     return link_list    
   end
 
+  def link_to_ncbi_taxonomy_browser organism,text,html_options={}
+    html_options[:alt]||=text
+    html_options[:title]||=text
+    concept_uri=organism.concept_uri
+    ncbi_id=concept_uri.split(":")[1]
+    link_to h(text),"http://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=info&id=#{ncbi_id}",html_options
+  end
+
 end
