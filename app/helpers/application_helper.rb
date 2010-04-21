2d20bf65994113ea3319a57bfb90dd22aaefe918
Unknown <sowen@sowen-laptop-linux>
2010-04-21 14:46:27 +0100

    a bit of css refactoring, moving the footer in the main style sheet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -343,8 +343,6 @@ module ApplicationHelper
     return title
   end  
 
-  
-  
   private  
   PAGE_TITLES={"home"=>"Home", "projects"=>"Projects","institutions"=>"Institutions", "people"=>"People", "sessions"=>"Login","users"=>"Signup","search"=>"Search","experiments"=>"Experiments","sops"=>"Sops","models"=>"Models","data_files"=>"Data","publications"=>"Publications"}
   
