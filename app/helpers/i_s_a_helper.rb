5520410d9d4bce66b3df07e6a6f8902feceb9073
Unknown <sowen@sowen-laptop-linux>
2010-04-22 16:50:42 +0100

    when creating a modelling anaylsis, the text displays analysis rather than assay

--- app/helpers/i_s_a_helper.rb
+++ app/helpers/i_s_a_helper.rb
@@ -3,11 +3,13 @@ module ISAHelper
   NO_DELETE_EXPLANTIONS={Assay=>"You cannot delete an assay that has Data or SOPs associated with it.",Study=>"You cannot delete a Study that has Assays associated with it.",Investigation=>"You cannot delete an Investigation that has Studies associated with it."}
 
   def delete_ISA_icon model_item, user
+    item_name = model_item.class.name
+    item_name = "Analysis" if (model_item.kind_of?(Assay) && model_item.is_modelling?)
     if model_item.can_delete?(user)
-      return "<li>"+image_tag_for_key('destroy',url_for(model_item),"Delete #{model_item.class.name}", {:confirm=>"Are you sure?",:method=>:delete },"Delete #{model_item.class.name}") + "</li>"
+      return "<li>"+image_tag_for_key('destroy',url_for(model_item),"Delete #{item_name}", {:confirm=>"Are you sure?",:method=>:delete },"Delete #{item_name}") + "</li>"
     elsif !model_item.can_delete?(user) && model_item.can_edit?(user)
       explanation=unable_to_delete_text model_item
-      return "<li><span class='disabled_icon disabled' onclick='javascript:alert(\"#{explanation}\")' title='#{tooltip_title_attrib(explanation)}' >"+image('destroy', {:alt=>"Delete",:class=>"disabled"}) + " Delete #{model_item.class.name} </span></li>"
+      return "<li><span class='disabled_icon disabled' onclick='javascript:alert(\"#{explanation}\")' title='#{tooltip_title_attrib(explanation)}' >"+image('destroy', {:alt=>"Delete",:class=>"disabled"}) + " Delete #{item_name} </span></li>"
     end
   end
 
