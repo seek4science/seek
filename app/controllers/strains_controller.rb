c4317750490b2e14cad4b020bb948f75785c7882
Unknown <sowen@sowen-laptop-linux>
2010-04-27 09:52:39 +0100

    improved text for adding/selecting a strain for an organism

--- app/controllers/strains_controller.rb
+++ app/controllers/strains_controller.rb
@@ -1,18 +1,18 @@
 class StrainsController < ApplicationController
-
+  before_filter :login_required
   before_filter :get_strains,:only=>:show_existing_strains
 
   def show_existing_strains
     element=params[:element]    
     render :update do |page|
-      page.replace_html element,:partial=>"strains/existing_strains",:object=>@strains
+      page.replace_html element,:partial=>"strains/existing_strains",:object=>@strains,:locals=>{:organism=>@organism}
     end
   end
 
   def get_strains
     if params[:organism_id]
-      o=Organism.find_by_id(params[:organism_id])
-      @strains=o.strains
+      @organism=Organism.find_by_id(params[:organism_id])
+      @strains=@organism.strains
     end
   end
 
