a57b9b860daf6520a777ca2b6e81d23be551a544
Unknown <finn@ubuntu.ubuntu-domain>
2010-04-30 15:17:15 +0100

    schema...

--- db/schema.rb
+++ db/schema.rb
@@ -148,7 +148,7 @@ ActiveRecord::Schema.define(:version => 20100429092436) do
     t.datetime "last_used_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "version"
+    t.integer  "version",                        :default => 1
     t.string   "first_letter",      :limit => 1
     t.text     "other_creators"
   end
@@ -297,7 +297,7 @@ ActiveRecord::Schema.define(:version => 20100429092436) do
     t.integer  "organism_id"
     t.integer  "model_type_id"
     t.integer  "model_format_id"
-    t.integer  "version"
+    t.integer  "version",                                 :default => 1
     t.string   "first_letter",               :limit => 1
     t.text     "other_creators"
   end
@@ -484,7 +484,7 @@ ActiveRecord::Schema.define(:version => 20100429092436) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "last_used_at"
-    t.integer  "version"
+    t.integer  "version",                        :default => 1
     t.string   "first_letter",      :limit => 1
     t.text     "other_creators"
   end
