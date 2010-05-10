a35161762854b65b965eecf27828986fa1400f1f
Stuart at work <unknown>
2010-05-10 10:34:43 +0100

    updated headline_announcement to work through a named scope

--- vendor/plugins/site_announcements/app/models/site_announcement.rb
+++ vendor/plugins/site_announcements/app/models/site_announcement.rb
@@ -1,16 +1,18 @@
 class SiteAnnouncement < ActiveRecord::Base
   belongs_to :site_announcement_category
   belongs_to :announcer,:polymorphic=>true
+
+  named_scope :headline_announcements,:conditions=>(["is_headline = true and expires_at > ? ",Time.now]),:order=>"created_at DESC",:limit=>1
   
   validates_presence_of :title
 
-  def self.headline_announcements
-    self.find(:first,:conditions=>(["is_headline = true and expires_at > ? ",Time.now]),:order=>"created_at DESC")
+  def self.headline_announcement
+    self.headline_announcements.first
   end
 
   def self.feed_announcements options={}
     options[:limit] ||= 5
     self.find(:all,:conditions=>(["show_in_feed = true"]),:order=>"created_at DESC",:limit=>options[:limit])
-
   end
+  
 end
--- vendor/plugins/site_announcements/test/unit/site_announcements_test.rb
+++ vendor/plugins/site_announcements/test/unit/site_announcements_test.rb
@@ -14,32 +14,32 @@ class SiteAnnouncementsTest < ActiveSupport::TestCase
   end
 
   test "headline" do
-    hl=SiteAnnouncement.headline_announcements
+    hl=SiteAnnouncement.headline_announcement
     assert_not_nil hl
     assert_equal "This is a headline and feed announcement",hl.title
     
     hl.expires_at = Time.now - 1.hour
     hl.save!
 
-    hl=SiteAnnouncement.headline_announcements
+    hl=SiteAnnouncement.headline_announcement
     assert_not_nil hl
     assert_equal "This is a headline only announcement",hl.title
 
     hl.expires_at = Time.now - 1.hour
     hl.save!
 
-    assert_nil SiteAnnouncement.headline_announcements
+    assert_nil SiteAnnouncement.headline_announcement
   end
 
   test "new headline comes out top" do
-    hl=SiteAnnouncement.headline_announcements
+    hl=SiteAnnouncement.headline_announcement
     assert_not_nil hl
     assert_equal "This is a headline and feed announcement",hl.title
 
     ann=SiteAnnouncement.new(:title=>"brand new",:expires_at=>Time.now + 1.day,:is_headline=>true)
     ann.save!
     
-    hl=SiteAnnouncement.headline_announcements
+    hl=SiteAnnouncement.headline_announcement
     assert_not_nil hl
     assert_equal "brand new",hl.title
   end
