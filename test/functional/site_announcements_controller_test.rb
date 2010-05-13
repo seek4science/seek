097e053492307e3022f0615e55d61a8d9105426e
Stuart at Home <unknown>
2010-05-13 21:01:13 +0100

    added basic controller tests for announcements

---
+++ test/functional/site_announcements_controller_test.rb
@@ -0,0 +1,94 @@
+require 'test_helper'
+
+class SiteAnnouncementsControllerTest < ActionController::TestCase
+  
+  fixtures :users,:people,:site_announcements
+
+  include AuthenticatedTestHelper
+
+  def setup
+    login_as(:quentin)
+  end
+  
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:site_announcements)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should show" do
+    announcement=site_announcements(:feed)
+    assert_not_nil announcement.announcer
+    get :show,:id=>announcement
+    assert_response :success
+  end
+  
+  test "should get edit" do
+    announcement=site_announcements(:feed)
+    assert_not_nil announcement.announcer
+    get :edit,:id=>announcement
+    assert_response :success
+  end
+  
+  test "should create" do
+    assert_difference("SiteAnnouncement.count") do
+      post :create,:site_announcement=>{:title=>"fred"}
+    end    
+    assert_equal users(:quentin).person,assigns(:site_announcement).announcer
+  end
+  
+  test "should update" do
+    ann=site_announcements(:feed)
+    put :update,:id=>ann,:site_announcement=>{:title=>"bob"}
+    ann=SiteAnnouncement.find(ann.id)
+    assert_equal "bob",ann.title
+  end
+  
+  test "should not get new" do
+    login_as(:aaron)
+    get :new
+    assert_response :redirect
+    assert_redirected_to(root_url)    
+    assert_not_nil flash[:error]
+  end
+  
+  test "should not get edit" do
+    login_as(:aaron)
+    announcement=site_announcements(:feed)
+    assert_not_nil announcement.announcer
+    get :edit,:id=>announcement
+    assert_response :redirect
+    assert_redirected_to(root_url)
+    assert_not_nil flash[:error]
+  end
+  
+  test "should not create" do
+    login_as(:aaron)
+    assert_no_difference("SiteAnnouncement.count") do
+      post :create,:site_announcement=>{:title=>"fred"}
+    end    
+    assert_response :redirect
+    assert_redirected_to(root_url)
+    assert_not_nil flash[:error]
+  end
+  
+  test "should not update" do
+    login_as(:aaron)
+    ann=site_announcements(:feed)
+    put :update,:id=>ann,:site_announcement=>{:title=>"bob"}
+    
+    assert_response :redirect
+    assert_redirected_to(root_url)
+    assert_not_nil flash[:error]
+    
+    ann=SiteAnnouncement.find(ann.id)    
+    assert_equal "a feed announcement",ann.title
+    
+  end
+  
+end
