cf0e9066dd357fb5b87ac78338cb469aab5ac2d9
Unknown <finn@ubuntu.ubuntu-domain>
2010-05-19 15:18:29 +0100

    Fixed schema

--- db/schema.rb
+++ db/schema.rb
@@ -255,7 +255,6 @@ ActiveRecord::Schema.define(:version => 20100517150441) do
 
   create_table "help_images", :force => true do |t|
     t.integer  "help_document_id"
-    t.string   "title"
     t.string   "content_type"
     t.string   "filename"
     t.integer  "size"
