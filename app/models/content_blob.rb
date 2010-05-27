a8ce47b56e77281d026e73683bf3c4db5dcd1df8
Unknown <sowen@sowen-laptop-linux>
2010-05-27 19:23:13 +0100

    part way through storing of data to filesystem rather than db

--- app/models/content_blob.rb
+++ app/models/content_blob.rb
@@ -2,12 +2,17 @@ require 'digest/md5'
 require 'net/http'
 require 'open-uri'
 require 'acts_as_uniquely_identifiable'
-
+require 'tmpdir'
 
 class ContentBlob < ActiveRecord::Base
-  validates_presence_of :data, :if => Proc.new { |blob| blob.url.blank? }  
+  
+  DATA_STORAGE_PATH = "filestore/content_blobs/"
+  
+  
+  #validates_presence_of :data, :if => Proc.new { |blob| blob.url.blank? }  
   
   acts_as_uniquely_identifiable
+  before_save :dump_data_to_file
   
   before_save :calculate_md5
   
@@ -20,7 +25,7 @@ class ContentBlob < ActiveRecord::Base
     end
     super
   end
-  
+      
   def calculate_md5
     #FIXME: only recalculate if the data has changed (should be able to do this with changes.keys.include?("data") or along those lines).
     unless self.data.nil?
@@ -28,6 +33,32 @@ class ContentBlob < ActiveRecord::Base
       digest << self.data
       self.md5sum = digest.hexdigest
     end
-  end  
+  end        
+  
+#  def data
+#    if File.exist?(filepath)
+#      File.open(filepath,"rb").read
+#    else
+#      dump_data_to_file
+#      File.open(filepath,"rb").read
+#    end
+#  end
+  
+  def filepath
+    if RAILS_ENV == "test"
+      path = "#{Dir::tmpdir}/seek_content_blobs/"
+    else
+      path = "#{RAILS_ROOT}/#{DATA_STORAGE_PATH}"
+    end
+    FileUtils.mkdir_p(path)
+    return "#{path}/#{uuid}.dat"
+  end
+  
+  def dump_data_to_file    
+    File.open(filepath,"w+") do |f|      
+      f.write(data)    
+    end
+    #self.data=nil
+  end
   
 end
