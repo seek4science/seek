2b06d070bb181e8de84557a6c832655bf28ae8c1
Unknown <sowen@sowen-laptop-linux>
2010-05-27 19:23:13 +0100

    part way through storing of data to filesystem rather than db

--- test/unit/content_blob_test.rb
+++ test/unit/content_blob_test.rb
@@ -9,11 +9,36 @@ class ContentBlobTest < ActiveSupport::TestCase
     assert_equal "2288e57a82162f5fd7fa7050ebadbcba",blob.md5sum
   end
   
+  def test_uuid_doesnt_change
+    blob=content_blobs(:picture_blob)
+    assert_equal "zzz",blob.uuid
+    blob.save!
+    assert_equal "zzz",blob.uuid
+  end
+  
+  def test_file_dump
+    pic=content_blobs(:picture_blob)
+    blob=ContentBlob.new(:data=>pic.data)
+    blob.save!
+    assert_not_nil blob.filepath
+    data=nil
+    File.open(blob.filepath,"rb") do |f|
+      data=f.read
+    end
+    assert_not_nil data
+    assert_equal test_data('file_picture.png'),data
+  end
+  
   def test_uuid
     pic=content_blobs(:picture_blob)
     blob=ContentBlob.new(:data=>pic.data)
     blob.save!
     assert_not_nil blob.uuid
     assert_not_nil ContentBlob.find(blob.id).uuid
+  end
+  
+  def test_data filename
+    file = "#{RAILS_ROOT}/test/fixtures/files/#{filename}"
+    return File.open(file,"rb").read
   end
 end
