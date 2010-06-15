849bf27228ad9a1456cc25d202917a7833d215f8
Unknown <sowen@sowen-laptop-linux>
2010-06-15 11:19:49 +0100

    fix to expire organism gadget when new organism is created or updated or destroyed

--- app/controllers/organisms_controller.rb
+++ app/controllers/organisms_controller.rb
@@ -4,6 +4,8 @@ class OrganismsController < ApplicationController
   before_filter :is_user_admin_auth,:only=>[:edit,:update,:new,:create]
   before_filter :find_organism,:only=>[:show,:edit,:more_ajax,:visualise]
   layout "main",:except=>:visualise
+  
+  cache_sweeper :organisms_sweeper,:only=>[:update,:create,:destroy]
 
   include BioPortal::RestAPI
 
