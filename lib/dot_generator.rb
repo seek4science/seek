8159a5418c7ef791be8e075417748106043697d6
Unknown <sowen@sowen-laptop-linux>
2010-06-28 16:35:37 +0100

    fix to dot generation, caused by tooltips. Added a tooltip method that strips the title

--- lib/dot_generator.rb
+++ lib/dot_generator.rb
@@ -21,7 +21,7 @@ module DotGenerator
   
   def to_dot_inv investigation, show_assets=false
     dot = ""
-    dot << "Inv_#{investigation.id} [label=\"#{multiline(investigation.title)}\",tooltip=\"#{investigation.title}\",shape=box,style=filled,fillcolor=skyblue3,URL=\"#{polymorphic_path(investigation)}\",target=\"_top\"];\n"
+    dot << "Inv_#{investigation.id} [label=\"#{multiline(investigation.title)}\",tooltip=\"#{tooltip(investigation)}\",shape=box,style=filled,fillcolor=skyblue3,URL=\"#{polymorphic_path(investigation)}\",target=\"_top\"];\n"
     investigation.studies.each do |s|
       dot << to_dot_study(s,show_assets)
       dot << "Inv_#{investigation.id} -- Study_#{s.id}\n"
@@ -31,7 +31,7 @@ module DotGenerator
   
   def to_dot_study study, show_assets=true
     dot = ""
-    dot << "Study_#{study.id} [label=\"#{multiline(study.title)}\",tooltip=\"#{study.title}\",shape=box,style=filled,fillcolor=chocolate,URL=\"#{polymorphic_path(study)}\",target=\"_top\"];\n"
+    dot << "Study_#{study.id} [label=\"#{multiline(study.title)}\",tooltip=\"#{tooltip(study)}\",shape=box,style=filled,fillcolor=chocolate,URL=\"#{polymorphic_path(study)}\",target=\"_top\"];\n"
     study.assays.each do |assay|
       dot << to_dot_assay(assay, show_assets)
       dot << "Study_#{study.id} -- Assay_#{assay.id}\n"
@@ -41,12 +41,12 @@ module DotGenerator
   
   def to_dot_assay assay, show_assets=true
     dot = ""
-    dot << "Assay_#{assay.id} [label=\"#{multiline(assay.title)}\",tooltip=\"#{assay.title}\",shape=folder,style=filled,fillcolor=burlywood,URL=\"#{polymorphic_path(assay)}\",target=\"_top\"];\n"    
+    dot << "Assay_#{assay.id} [label=\"#{multiline(assay.title)}\",tooltip=\"#{tooltip(assay)}\",shape=folder,style=filled,fillcolor=burlywood,URL=\"#{polymorphic_path(assay)}\",target=\"_top\"];\n"    
     if (show_assets) 
       assay.assets.each do |asset|
         asset_type=asset.resource.class.name
         if Authorization.is_authorized?("view",nil,asset,current_user)
-          dot << "Asset_#{asset.resource.id} [label=\"#{multiline(asset.resource.title)}\",tooltip=\"#{asset.resource.title}\",shape=box,fontsize=7,style=filled,fillcolor=gold,URL=\"#{polymorphic_path(asset.resource)}\",target=\"_top\"];\n"
+          dot << "Asset_#{asset.resource.id} [label=\"#{multiline(asset.resource.title)}\",tooltip=\"#{tooltip(asset.resource)}\",shape=box,fontsize=7,style=filled,fillcolor=gold,URL=\"#{polymorphic_path(asset.resource)}\",target=\"_top\"];\n"
           dot << "Assay_#{assay.id} -- Asset_#{asset.resource.id}\n"
         else
           dot << "Asset_#{asset.resource.id} [label=\"Hidden Item\",tooltip=\"Hidden Item\",shape=box,fontsize=6,style=filled,fillcolor=lightgray];\n"
@@ -57,6 +57,9 @@ module DotGenerator
     return dot
   end
   
+  def tooltip resource
+    resource.title.strip
+  end
   
   def to_svg thing,deep=false
     tmpfile = Tempfile.new("#{thing.class.name}_dot")
@@ -93,8 +96,7 @@ module DotGenerator
   
   def multiline str,line_len=3    
     new_str=str[0..80]
-    
-    str+=" ..." if str.length>500
+        
     word_arr=new_str.split
     x=line_len
     while x<new_str.split.length do
@@ -103,7 +105,7 @@ module DotGenerator
     end
     
     end_str = (new_str.length!=str.length) ? " ..." : ""
-    word_arr.join(" ") + end_str
+    (word_arr.join(" ") + end_str).strip   
   end
   
 end
