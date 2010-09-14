821d9235242da36cd7785d3baf2f121d859a3b83
Unknown <sowen@sowen-laptop-linux>
2010-09-14 14:03:58 +0100

    removed call to check open_id cookie, as this behaviour is currently disabled

--- lib/authenticated_system.rb
+++ lib/authenticated_system.rb
@@ -49,7 +49,7 @@ module AuthenticatedSystem
   #   skip_before_filter :login_required
   #
   def login_required      
-    authorized? || login_from_open_id_cookie || access_denied      
+    authorized? || access_denied      
   end
   
   # Redirect as appropriate when an access request fails.
