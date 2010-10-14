258229a8622baac3485fa0edf83ca4d5ac093c04
Unknown <sowen@vaio-laptop-linux>
2010-10-14 16:54:59 +0100

    fixed colour of publication in graph

--- lib/dot_generator.rb
+++ lib/dot_generator.rb
@@ -2,7 +2,14 @@ require 'libxml'
 
 module DotGenerator
   
-  FILL_COLOURS = {Sop=>"cadetblue3",Model=>"yellow3",DataFile=>"burlywood2",Investigation=>"#C7E9C0",Study=>"#91c98b",Assay=>"#64b466",Publication=>"#FFEEEE"}
+  FILL_COLOURS = {Sop=>"cadetblue3",
+                  Model=>"yellow3",
+                  DataFile=>"burlywood2",
+                  Investigation=>"#C7E9C0",
+                  Study=>"#91c98b",
+                  Assay=>"#64b466",
+                  Publication=>"cornflowerblue"
+                  }
   HIGHLIGHT_ATTRIBUTE="color=blue,penwidth=2," #trailing comma is required
   
   def to_dot root_item, deep=false, current_item=nil
