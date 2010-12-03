c93e1bf7aeda818e20277d76ceae2c1d71808d77
Unknown <sowen@vaio-laptop-linux>
2010-12-03 11:37:51 +0000

    increased length of file checked whilst looking for sbml, and made case insensitive

--- lib/seek/model_type_detection.rb
+++ lib/seek/model_type_detection.rb
@@ -6,22 +6,23 @@ module Seek
       check_content model.content_blob.filepath,"begin name",25000
     end                      
     
-    def is_sbml? model      
-      return false if !model.original_filename.end_with?(".xml")
+    def is_sbml? model            
+      return false if !model.original_filename.end_with?(".xml")      
       check_content model.content_blob.filepath,"<sbml"
     end
     
-    def check_content filepath, str, max_length=100      
+    def check_content filepath, str, max_length=1500      
       char_count=0      
       begin
         f = File.open(filepath, "r") 
         f.each_line do |line|
           char_count += line.length
-          return true  if line.include?(str)
+          return true  if line.downcase.include?(str)
           break if char_count>=max_length        
         end 
       rescue
-      end
+    end
+    
       false
     end
     
