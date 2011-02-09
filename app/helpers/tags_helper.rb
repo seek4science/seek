abf531726ac5e4751c7cf549fe8ea7a780751607
Unknown <sowen@vaio-laptop-linux>
2011-02-09 16:53:22 +0000

    fixed tag cloud ordering and weighting. also increased the number of shown tags to 20

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -2,7 +2,8 @@ module TagsHelper
   include ActsAsTaggableOn::TagsHelper
 
   def tag_cloud(tags, classes)
-    max_count = tags.sort_by(&:count).last.count.to_f
+    tags = tags.sort{|a,b| b.count<=>a.count}
+    max_count = tags.first.count.to_f
     if max_count < 1
       max_count = 1
     end
@@ -14,7 +15,8 @@ module TagsHelper
   end
 
   def overall_tag_cloud(tags, classes)
-    max_count = tags.sort_by(&:overall_total).last.count.to_f
+    tags = tags.sort{|a,b| b.overall_total<=>a.overall_total}
+    max_count = tags.first.overall_total.to_f
     if max_count < 1
       max_count = 1
     end
