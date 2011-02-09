328bc39fcde457bda7a13a309e0662c5b7e8472d
Unknown <sowen@toffee>
2011-02-09 20:16:04 +0000

    removed old pagination code and partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -245,34 +245,7 @@ module ApplicationHelper
     name ||=""
     name += " (Development)" if RAILS_ENV=="development"
     return "#{APPLICATION_TITLE} "+name
-  end
-
-  # http://www.igvita.com/blog/2006/09/10/faster-pagination-in-rails/
-  def windowed_pagination_links(pagingEnum, options)
-    link_to_current_page = options[:link_to_current_page]
-    always_show_anchors = options[:always_show_anchors]
-    padding = options[:window_size]
-
-    current_page = pagingEnum.page
-    html = ''
-
-    #Calculate the window start and end pages
-    padding = padding < 0 ? 0 : padding
-    first = pagingEnum.page_exists?(current_page  - padding) ? current_page - padding : 1
-    last = pagingEnum.page_exists?(current_page + padding) ? current_page + padding : pagingEnum.last_page
-
-    # Print start page if anchors are enabled
-    html << yield(1) if always_show_anchors and not first == 1
-
-    # Print window pages
-    first.upto(last) do |page|
-      (current_page == page && !link_to_current_page) ? html << page : html << yield(page)
-    end
-
-    # Print end page if anchors are enabled
-    html << yield(pagingEnum.last_page) if always_show_anchors and not last == pagingEnum.last_page
-    html
-  end
+  end  
 
   def show_tag?(tag)
     #FIXME: not sure this is required or works any more. was originally to work around a bug in acts-as-taggable-on
