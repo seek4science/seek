ee99c54cf3f80b60b3d86476f79addfa17551dc2
quyen nguyen (quyen.nguyen@gmail.com) <unknown>
2011-02-15 15:45:23 +0100

    paginating configuration using rails-setting plugin

---
+++ vendor/plugins/rails-settings/test/test_helper.rb
@@ -0,0 +1,33 @@
+gem 'activesupport', '< 3'
+gem 'activerecord', '< 3'
+
+require 'rubygems'
+require 'active_support'
+require 'active_support/test_case'
+require 'active_record'
+require 'test/unit'
+
+require "#{File.dirname(__FILE__)}/../init"
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+
+class User < ActiveRecord::Base
+  has_settings
+end
+
+def setup_db
+  ActiveRecord::Schema.define(:version => 1) do
+    create_table :settings do |t|
+      t.string :var, :null => false
+      t.text   :value, :null => true
+      t.integer :target_id, :null => true
+      t.string :target_type, :limit => 30, :null => true
+      t.timestamps
+    end
+    add_index :settings, [ :target_type, :target_id, :var ], :unique => true
+    
+    create_table :users do |t|
+      t.string :name
+    end
+  end
+end
