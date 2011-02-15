4e271f2830df2ce6f568b7fa93789b92527bed57
David Shockley <David.Shockley@h-its.org>
2011-02-15 17:36:49 +0100

    Settings table can't be accessed in an initializer. Changed to use Settings.defaults

--- config/initializers/paginate_configuration.rb
+++ config/initializers/paginate_configuration.rb
@@ -1,5 +1,5 @@
 require 'settings'
 class PaginateConfiguration
-  Settings.index = {:people => 'latest', :projects => 'latest', :institutions => 'latest', :investigations => 'latest',:studies => 'latest', :assays => 'latest',
+  Settings.defaults[:index] = {:people => 'latest', :projects => 'latest', :institutions => 'latest', :investigations => 'latest',:studies => 'latest', :assays => 'latest',
                     :data_files => 'latest', :models => 'latest',:sops => 'latest', :publications => 'latest',:events => 'latest'}
 end
