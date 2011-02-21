e956044c7d7b6550be79771f3c132f166ff269f4
quyen nguyen (quyen.nguyen@gmail.com) <unknown>
2011-02-21 15:32:07 +0100

    datafile-event association, publication-event asscociation, fix test fails

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -115,9 +115,9 @@ class PublicationsController < ApplicationController
     end
 
     respond_to do |format|
-      publication_params = params[:publication]
+      publication_params = params[:publication]||{}
       publication_params[:event_ids] = params[:event_ids]
-      if valid && @publication.update_attributes(attributes)
+      if valid && @publication.update_attributes(publication_params)
         to_add.each {|a| @publication.creators << a}
         to_remove.each {|a| a.destroy}
         
