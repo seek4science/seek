3cef1bc63959cd2d750ec9efd207db96272f1250
Unknown <sowen@vaio-laptop-linux>
2011-02-22 10:58:09 +0000

    silence warnings about warning: already initialized constant APPLICATION_NAME and others when running tests

--- config/initializers/seek_testing.rb
+++ config/initializers/seek_testing.rb
@@ -1,15 +1,17 @@
 #Default values required for the automated unit, functional and integration testing to behave as expected.
 if Rails.env.test?
-  APPLICATION_NAME = 'Sysmo SEEK'
-  APPLICATION_TITLE = 'The Sysmo SEEK'
-  PROJECT_NAME = 'Sysmo'
-  PROJECT_TITLE = 'The Sysmo Consortium'
-  DM_PROJECT_NAME = 'Sysmo-DB'
-  NOREPLY_SENDER="no-reply@sysmo-db.org"
+  silence_warnings do
+    APPLICATION_NAME       = 'Sysmo SEEK'
+    APPLICATION_TITLE      = 'The Sysmo SEEK'
+    PROJECT_NAME           = 'Sysmo'
+    PROJECT_TITLE          = 'The Sysmo Consortium'
+    DM_PROJECT_NAME        = 'Sysmo-DB'
+    NOREPLY_SENDER         ="no-reply@sysmo-db.org"
 
-  CROSSREF_API_EMAIL = "sowen@cs.man.ac.uk"
+    CROSSREF_API_EMAIL     = "sowen@cs.man.ac.uk"
 
-  PAGINATION_CONFIG_FILE = "config/paginate.yml"
+    PAGINATION_CONFIG_FILE = "config/paginate.yml"
 
-  EVENTS_ENABLED = true
+    EVENTS_ENABLED         = true
+  end
 end
