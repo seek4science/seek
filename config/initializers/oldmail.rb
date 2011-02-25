2d75bbe20e9d5503dc42c47ca5acc47cb16be151
David Shockley <David.Shockley@h-its.org>
2011-02-25 13:42:38 +0100

    remove oldmail.rb

--- config/initializers/oldmail.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.smtp_settings= {
-  :address => "vbemail30.villa-bosch.de",
-  :port => "25",
-  :domain => "",
-  :authentication => :plain,
-  :user_name => "",
-  :password  => ""
-}
