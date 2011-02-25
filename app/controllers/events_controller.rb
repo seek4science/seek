3e31a48c34487eff4925e9fa43a128392f9af4b8
David Shockley <David.Shockley@h-its.org>
2011-02-25 15:16:16 +0100

    fixed publication association for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -45,12 +45,11 @@ class EventsController < ApplicationController
     end
     params.delete :data_file_ids
 
-    publication_ids = params[:publication_ids] || []
-    publication_ids.each do |text|
-      a_id, r_type = text.split(",")
-      @event.publications << Publication.find(a_id)
+    publication_ids = params[:related_publication_ids] || []
+    publication_ids.each do |id|
+      @event.publications << Publication.find(id)
     end
-    params.delete :publication_ids
+    params.delete :related_publication_ids
 
     respond_to do | format |
       if @event.save
@@ -84,13 +83,12 @@ class EventsController < ApplicationController
     end
     params.delete :data_file_ids
 
-    publication_ids = params[:publication_ids] || []
+    publication_ids = params[:related_publication_ids] || []
     @event.publications = []
-    publication_ids.each do |text|
-      a_id, r_type = text.split(",")
-      @event.publications << Publication.find(a_id)
+    publication_ids.each do |id|
+      @event.publications << Publication.find(id)
     end
-    params.delete :publication_ids
+    params.delete :related_publication_ids
 
     respond_to do | format |
       if @event.update_attributes params[:event]
