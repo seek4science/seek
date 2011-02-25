34eeb9b2558bb82d357be3f15f1074028ca3a1e5
David Shockley <David.Shockley@h-its.org>
2011-02-25 13:24:11 +0100

    changed default pagination to all for publications and events

--- config/initializers/paginate_configuration.rb
+++ config/initializers/paginate_configuration.rb
@@ -1,5 +1,5 @@
 require 'settings'
 class PaginateConfiguration
   Settings.defaults[:index] = {:people => 'latest', :projects => 'latest', :institutions => 'latest', :investigations => 'latest',:studies => 'latest', :assays => 'latest',
-                    :data_files => 'latest', :models => 'latest',:sops => 'latest', :publications => 'latest',:events => 'latest'}
+                    :data_files => 'latest', :models => 'latest',:sops => 'latest', :publications => 'all',:events => 'all'}
 end
