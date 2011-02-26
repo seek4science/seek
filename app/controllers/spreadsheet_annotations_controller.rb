1188e3c58c86aeed1d828ca7d6337dfcfbf0a576
Finn Bacall <bacallf7@cs.man.ac.uk>
2011-02-26 18:58:12 +0000

    Improved annotation look. Started work on update/delete

--- app/controllers/spreadsheet_annotations_controller.rb
+++ app/controllers/spreadsheet_annotations_controller.rb
@@ -18,9 +18,45 @@ class SpreadsheetAnnotationsController < ApplicationController
     new_annotation = SpreadsheetAnnotation.new(:data_file => data_file, :sheet => params[:sheet_id],
                               :start_row => start_row, :start_column => start_column,
                               :end_row => end_row, :end_column => end_column,
-                              :source => current_user, :annotation_type => "test",
+                              :source => current_user, :annotation_type => params[:annotation_type],
                               :content => params[:content])
     if(new_annotation.save)
+      annotations = data_file.spreadsheet_annotations
+      respond_to do |format|
+        format.html { render :partial => "annotations/annotations", :locals=>{ :annotations => annotations} } 
+      end
+    end
+  end 
+
+  def update
+    annotation = SpreadsheetAnnotation.find(params[:annotation_id])
+    start_cell, end_cell = params[:cell_coverage].split(":")
+    start_column, start_row = SpreadsheetAnnotation.from_alpha(start_cell.sub(/[0-9]+/,"")), start_cell.sub(/[A-Z]+/,"").to_i
+    end_column, end_row = nil, nil
+    if end_cell.nil?
+      end_column = start_column
+      end_row = start_row
+    else
+      end_column, end_row = SpreadsheetAnnotation.from_alpha(end_cell.sub(/[0-9]+/,"")), end_cell.sub(/[A-Z]+/,"").to_i  
+    end
+           
+        
+    if annotation.update_attributes(:start_row => start_row, :start_column => start_column,
+                                                  :end_row => end_row, :end_column => end_column,
+                                                  :annotation_type => params[:annotation_type],
+                                                  :content => params[:content])
+      annotations = annotation.data_file.spreadsheet_annotations
+      respond_to do |format|
+        format.html { render :partial => "annotations/annotations", :locals=>{ :annotations => annotations} } 
+      end
+    end
+  end 
+  
+  def destroy
+    annotation = SpreadsheetAnnotation.find(params[:annotation_id])
+    data_file = annotation.data_file
+            
+    if annotation.destroy
       annotations = data_file.spreadsheet_annotations
       respond_to do |format|
         format.html { render :partial => "annotations/annotations", :locals=>{ :annotations => annotations} } 
