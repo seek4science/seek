9de3d308b8312a69affb7e4c9618444a163e7987
Unknown <sowen@vaio-laptop-linux>
2011-02-28 11:14:57 +0000

    removed some return statements

--- app/helpers/models_helper.rb
+++ app/helpers/models_helper.rb
@@ -29,23 +29,23 @@ module ModelsHelper
   end
 
   def model_jws_online_compatible? model
-    return !model.recommended_environment.nil? && model.recommended_environment.title.downcase=="jws online"
+    !model.recommended_environment.nil? && model.recommended_environment.title.downcase=="jws online"
   end
 
   def execute_model_label
     icon_filename=icon_filename_for_key("execute")
 
-    return '<span class="icon">' + image_tag(icon_filename,:alt=>"Run",:title=>"Run") + ' Run model</span>';
+    '<span class="icon">' + image_tag(icon_filename,:alt=>"Run",:title=>"Run") + ' Run model</span>';
   end
 
   def model_type_text model_type
     return "<span class='none_text'>Not specified</span>" if model_type.nil?
-    return h(model_type.title)
+    h(model_type.title)
   end
 
   def model_format_text model_format
     return "<span class='none_text'>Not specified</span>" if model_format.nil?
-    return h(model_format.title)
+    h(model_format.title)
   end
   
   def authorised_models
