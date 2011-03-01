99fd556d342540e30d4e14092432de83b8d1f1be
quyen nguyen (quyen.nguyen@gmail.com) <unknown>
2011-03-01 18:13:58 +0100

    adding settings table

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110218163214) do
+ActiveRecord::Schema.define(:version => 20110301171256) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
