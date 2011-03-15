03a6e0f53644be44fa5e195020be3225e49555eb
Unknown <sowen@vaio-laptop-linux>
2011-03-15 16:03:47 +0000

    expire all fragment caching when changing settings

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,6 @@
 class AdminController < ApplicationController
+  include CommonSweepers
+  
   before_filter :login_required
   before_filter :is_user_admin_auth
 
@@ -100,8 +102,7 @@ class AdminController < ApplicationController
       Seek::ApplicationConfiguration.google_analytics_enabled= false
     end
     Seek::ApplicationConfiguration.google_analytics_tracker_id= params[:google_analytics_tracker_id]
-    flash[:notice] = 'To apply the change, please click the "Restart server" button if your webserver is Apache, or manually restart the server'
-    redirect_to :action=>:show
+    finalize_config_changes
   end
 
   def rebrand
@@ -138,8 +139,7 @@ class AdminController < ApplicationController
       Seek::ApplicationConfiguration.copyright_addendum_enabled= false
     end
     Seek::ApplicationConfiguration.copyright_addendum_content= params[:copyright_addendum_content]
-    flash[:notice] = 'To apply the change, please click the "Restart server" button if your webserver is Apache, or manually restart the server'
-    redirect_to :action=>:show
+    finalize_config_changes
   end
 
   def update_pagination
@@ -155,8 +155,7 @@ class AdminController < ApplicationController
    Seek::ApplicationConfiguration.set_default_page "publications", params[:publications]
    Seek::ApplicationConfiguration.set_default_page "events", params[:events]
    Seek::ApplicationConfiguration.limit_latest= params[:limit_latest]
-   flash[:notice] = 'To apply the change, please click the "Restart server" button if your webserver is Apache, or manually restart the server'
-   redirect_to :action=>:show
+   finalize_config_changes
   end
 
   def update_others
@@ -172,7 +171,13 @@ class AdminController < ApplicationController
     Seek::ApplicationConfiguration.open_id_authentication_store= params[:open_id_authentication_store]
     Seek::ApplicationConfiguration.tag_threshold= params[:tag_threshold]
     Seek::ApplicationConfiguration.max_visible_tags= params[:max_visible_tags]
+
+  end
+
+  def finalize_config_changes
     flash[:notice] = 'To apply the change, please click the "Restart server" button if your webserver is Apache, or manually restart the server'
+    #expires all fragement caching
+    expire_all_fragments
     redirect_to :action=>:show
   end
 
--- app/sweepers/common_sweepers.rb
+++ app/sweepers/common_sweepers.rb
@@ -4,6 +4,16 @@
 #includes some helper methods for commonly used fragament expirations
 module CommonSweepers
 
+  def expire_all_fragments
+    expire_tag_fragments
+    expire_all_favourite_fragments
+    expire_organism_gadget
+  end
+
+  def expire_tag_fragments
+    expire_fragment("sidebar_tag_cloud")
+  end  
+
   #expires ALL fragment caches related to favourites
   def expire_all_favourite_fragments
     expire_fragment(/\/favourites\/user\/.*/)
