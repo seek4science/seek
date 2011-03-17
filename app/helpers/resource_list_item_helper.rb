f1099893f2a5cb0b357e84dce2489505cce44a76
Unknown <sowen@toffee>
2011-03-17 11:51:10 +0000

    removed use of :type in tag links

--- app/helpers/resource_list_item_helper.rb
+++ app/helpers/resource_list_item_helper.rb
@@ -58,7 +58,7 @@ module ResourceListItemHelper
   end
 
   def list_item_tag_list resource
-    list_item_simple_list(resource.tag_counts, "Tags") {|i| link_for_tag(i, :type=>:tool)}
+    list_item_simple_list(resource.tag_counts, "Tags") {|i| link_for_tag(i)}
   end
 
   def list_item_simple_list items, attribute
