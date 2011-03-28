85b5a69cf314ba467169a1b07ee430d77fa1311a
Unknown <sowen@vaio-laptop-linux>
2011-03-28 16:27:11 +0100

    moved onlyNumbers to application.js and removed sharing.js from admin pages that require that utility function

--- public/javascripts/sharing.js
+++ public/javascripts/sharing.js
@@ -1367,12 +1367,4 @@ function addAttributions() {
     }
 }
 
-function onlyNumbers(evt)
- {
-    var charCode = (evt.which) ? evt.which : event.keyCode
-    if (charCode > 31 && (charCode < 48 || charCode > 57))
-       return false;
 
-    return true;
- }
-// ********************************************************
