f867ef00a4afdc3ba6c8f8dab3f0468db33eba52
Unknown <sowen@vaio-laptop-linux>
2011-03-30 15:58:06 +0100

    login required, and added a comment

--- app/controllers/svg_controller.rb
+++ app/controllers/svg_controller.rb
@@ -1,4 +1,9 @@
+# Primary use is to locally serve SVG diagrams provided by JWS Online (The schema diagrams) as the SVGWEB javascript library
+# requires that it is served locally, and also that the URL has a .svg extension
+
 class SvgController < ApplicationController
+
+  before_filter :login_required
 
   layout nil
 
