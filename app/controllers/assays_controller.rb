5456f2cffa7c1dda9dd892335f25c49915e17590
David Shockley <David.Shockley@h-its.org>
2011-04-12 11:14:51 +0200

    added sharing form to assay form

--- app/controllers/assays_controller.rb
+++ app/controllers/assays_controller.rb
@@ -4,11 +4,11 @@ class AssaysController < ApplicationController
   include IndexPager
   include Seek::TaggingCommon
 
-  before_filter :find_assets,:only=>[:index]
+  before_filter :find_assets, :only=>[:index]
   before_filter :login_required
-  before_filter :is_project_member,:only=>[:create,:new]
+  before_filter :is_project_member, :only=>[:create, :new]
   before_filter :check_can_edit, :only=>[:edit, :update, :destroy]
-  before_filter :delete_allowed,:only=>[:destroy]  
+  before_filter :delete_allowed, :only=>[:destroy]
 
   def new
     @assay=Assay.new
@@ -31,16 +31,16 @@ class AssaysController < ApplicationController
   end
 
   def create
-    @assay = Assay.new(params[:assay])
+    @assay        = Assay.new(params[:assay])
 
-    organisms = params[:assay_organism_ids] || []
-    sop_ids = params[:assay_sop_ids] || []
+    organisms     = params[:assay_organism_ids] || []
+    sop_ids       = params[:assay_sop_ids] || []
     data_file_ids = params[:data_file_ids] || []
-    model_ids = params[:assay_model_ids] || []
+    model_ids     = params[:assay_model_ids] || []
 
     update_tags @assay
-    
-    @assay.owner=current_user.person     
+
+    @assay.owner=current_user.person
 
     respond_to do |format|
       if @assay.save
@@ -55,38 +55,45 @@ class AssaysController < ApplicationController
           @assay.relate(Sop.find(a_id))
         end
         organisms.each do |text|
-          o_id,strain,culture_growth_type_text=text.split(",")
+          o_id, strain, culture_growth_type_text=text.split(",")
           culture_growth=CultureGrowthType.find_by_title(culture_growth_type_text)
-          @assay.associate_organism(o_id,strain,culture_growth)
+          @assay.associate_organism(o_id, strain, culture_growth)
         end
-        
+
         # update related publications
-        Relationship.create_or_update_attributions(@assay, params[:related_publication_ids].collect {|i| ["Publication", i.split(",").first]}.to_json, Relationship::RELATED_TO_PUBLICATION) unless params[:related_publication_ids].nil?
-        
-        flash[:notice] = 'Assay was successfully created.'
-        format.html { redirect_to(@assay) }
-        format.xml  { render :xml => @assay, :status => :created, :location => @assay }
-      else        
-        format.html { render :action => "new"}
-        format.xml  { render :xml => @assay.errors, :status => :unprocessable_entity }
+        Relationship.create_or_update_attributions(@assay, params[:related_publication_ids].collect { |i| ["Publication", i.split(",").first] }.to_json, Relationship::RELATED_TO_PUBLICATION) unless params[:related_publication_ids].nil?
+
+        policy_err_msg = Policy.create_or_update_policy(@event, current_user, params)
+
+        if policy_err_msg.blank?
+          flash[:notice] = 'Assay was successfully created.'
+          format.html { redirect_to(@assay) }
+          format.xml { render :xml => @assay, :status => :created, :location => @assay }
+        else
+          flash[:notice] = "Assay metadata was successfully updated. However some problems occurred, please see these below.</br></br><span style='color: red;'>" + policy_err_msg + "</span>"
+          format.html { redirect_to assay_edit_path(@assay) }
+        end
+      else
+        format.html { render :action => "new" }
+        format.xml { render :xml => @assay.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   def update
-    @assay=Assay.find(params[:id])
-    
+    @assay                =Assay.find(params[:id])
+
     #FIXME: would be better to resolve the differences, rather than keep clearing and reading the assets and organisms
     #DOES resolve differences for assets now
     @assay.assay_organisms=[]
-    
-    organisms = params[:assay_organism_ids] || []
-    sop_ids = params[:assay_sop_ids] || []
-    data_file_ids = params[:data_file_ids] || []
-    model_ids = params[:assay_model_ids] || []
+
+    organisms             = params[:assay_organism_ids] || []
+    sop_ids               = params[:assay_sop_ids] || []
+    data_file_ids         = params[:data_file_ids] || []
+    model_ids             = params[:assay_model_ids] || []
 
     update_tags @assay
-    
+
     assay_assets_to_keep = [] #Store all the asset associations that we are keeping in this
 
     respond_to do |format|
@@ -102,26 +109,34 @@ class AssaysController < ApplicationController
           assay_assets_to_keep << @assay.relate(Sop.find(a_id))
         end
         #Destroy AssayAssets that aren't needed
-        (@assay.assay_assets - assay_assets_to_keep.compact).each {|a| a.destroy}
-        
+        (@assay.assay_assets - assay_assets_to_keep.compact).each { |a| a.destroy }
+
         organisms.each do |text|
-          o_id,strain,culture_growth_type_text=text.split(",")
+          o_id, strain, culture_growth_type_text=text.split(",")
           culture_growth=CultureGrowthType.find_by_title(culture_growth_type_text)
-          @assay.associate_organism(o_id,strain,culture_growth)
-        end   
-        
+          @assay.associate_organism(o_id, strain, culture_growth)
+        end
+
         # update related publications
-        Relationship.create_or_update_attributions(@assay, params[:related_publication_ids].collect {|i| ["Publication", i.split(",").first]}.to_json, Relationship::RELATED_TO_PUBLICATION) unless params[:related_publication_ids].nil?
-        
+        Relationship.create_or_update_attributions(@assay, params[:related_publication_ids].collect { |i| ["Publication", i.split(",").first] }.to_json, Relationship::RELATED_TO_PUBLICATION) unless params[:related_publication_ids].nil?
+
         #FIXME: required to update timestamp. :touch=>true on AssayAsset association breaks acts_as_trashable
         @assay.updated_at=Time.now
         @assay.save!
-        flash[:notice] = 'Assay was successfully updated.'
-        format.html { redirect_to(@assay) }
-        format.xml  { head :ok }
+
+        policy_err_msg = Policy.create_or_update_policy(@event, current_user, params)
+
+        if policy_err_msg.blank?
+          flash[:notice] = 'Assay was successfully updated.'
+          format.html { redirect_to(@assay) }
+          format.xml { head :ok }
+        else
+          flash[:notice] = "Assay metadata was successfully updated. However some problems occurred, please see these below.</br></br><span style='color: red;'>" + policy_err_msg + "</span>"
+          format.html { redirect_to assay_edit_path(@assay) }
+        end
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @assay.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @assay.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -130,27 +145,27 @@ class AssaysController < ApplicationController
     @assay=Assay.find(params[:id])
     respond_to do |format|
       format.html
-      format.xml      
-      format.svg { render :text=>to_svg(@assay.study,params[:deep]=='true',@assay)}
-      format.dot { render :text=>to_dot(@assay.study,params[:deep]=='true',@assay)}
-      format.png { render :text=>to_png(@assay.study,params[:deep]=='true',@assay)}
+      format.xml
+      format.svg { render :text=>to_svg(@assay.study, params[:deep]=='true', @assay) }
+      format.dot { render :text=>to_dot(@assay.study, params[:deep]=='true', @assay) }
+      format.png { render :text=>to_png(@assay.study, params[:deep]=='true', @assay) }
     end
   end
 
   def destroy
-    
+
     respond_to do |format|
       if @assay.can_delete?(current_user) && @assay.destroy
         format.html { redirect_to(assays_url) }
-        format.xml  { head :ok }
+        format.xml { head :ok }
       else
         flash.now[:error]="Unable to delete the assay" if !@assay.study.nil?
         format.html { render :action=>"show" }
-        format.xml  { render :xml => @assay.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @assay.errors, :status => :unprocessable_entity }
       end
     end
   end
-  
+
   def update_types
     render :update do |page|
       page.replace_html "favourite_list", :partial=>"favourites/gadget_list"
@@ -159,22 +174,22 @@ class AssaysController < ApplicationController
 
   def preview
     element=params[:element]
-    assay=Assay.find_by_id(params[:id])
+    assay  =Assay.find_by_id(params[:id])
 
     render :update do |page|
       if assay.try :can_view?
-        page.replace_html element,:partial=>"assays/associate_resource_list_item",:locals=>{:resource=>assay}
+        page.replace_html element, :partial=>"assays/associate_resource_list_item", :locals=>{:resource=>assay}
       else
-        page.replace_html element,:text=>"Nothing is selected to preview."
+        page.replace_html element, :text=>"Nothing is selected to preview."
       end
     end
   end
 
-  private  
-  
+  private
+
   def find_assays
-    @assays = Assay.find(:all)    
-    @assays=apply_filters(@assays)        
+    @assays = Assay.find(:all)
+    @assays =apply_filters(@assays)
   end
 
   def delete_allowed
