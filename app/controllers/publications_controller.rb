452d45d7ee6c7baa84a70a8456a3a0b2cdcd5f23
Stuart Owen <stuart.owen@manchester.ac.uk>
2011-04-19 10:52:47 +0100

    added authorization check for when associating publications to assays - checks that the assay is editable

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -70,7 +70,7 @@ class PublicationsController < ApplicationController
         end
 
         Assay.find(assay_ids).each do |assay|
-          Relationship.create_or_update_attributions(assay,{"Publication", @publication.id}.to_json, Relationship::RELATED_TO_PUBLICATION)
+          Relationship.create_or_update_attributions(assay,{"Publication", @publication.id}.to_json, Relationship::RELATED_TO_PUBLICATION) if assay.can_edit?(current_user)
         end
 
         #Make a policy
@@ -133,7 +133,7 @@ class PublicationsController < ApplicationController
 
         # Update relationship
         Assay.find(assay_ids).each do |assay|
-          if Relationship.find_all_by_object_id(@publication.id, :conditions => "subject_id = #{assay.id}").empty?
+          if assay.can_edit?(current_user) && Relationship.find_all_by_object_id(@publication.id, :conditions => "subject_id = #{assay.id}").empty?
             Relationship.create_or_update_attributions(assay,{"Publication", @publication.id}.to_json, Relationship::RELATED_TO_PUBLICATION)
           end
         end
@@ -141,7 +141,9 @@ class PublicationsController < ApplicationController
         associate_relationships = Relationship.find(:all,:conditions=>["object_id = ? and subject_type = ?",@publication.id,"Assay"])
         logger.info associate_relationships
         associate_relationships.each do |associate_relationship|
-          Relationship.destroy(associate_relationship.id) unless assay_ids.include? associate_relationship.subject_id.to_s
+          if associate_relationship.subject.can_edit?(current_user) && !assay_ids.include?(associate_relationship.subject_id.to_s)
+            Relationship.destroy(associate_relationship.id)
+          end
         end
 
         #Create policy if not present (should be)
--- test/functional/publications_controller_test.rb
+++ test/functional/publications_controller_test.rb
@@ -31,7 +31,19 @@ class PublicationsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should not relate assays thay are not authorized for edit during create publication" do
+    assay=assays(:metabolomics_assay)
+    assert_difference('Publication.count') do
+      post :create, :publication => {:pubmed_id => 3,:project_id=>projects(:sysmo_project).id },:assay_ids=>[assay.id.to_s]
+    end
+
+    assert_redirected_to edit_publication_path(assigns(:publication))
+    p=assigns(:publication)
+    assert_equal 0,p.related_assays.count
+  end
+
   test "should create publication" do
+    login_as(:model_owner) #can edit assay
     assay=assays(:metabolomics_assay)
     assert_difference('Publication.count') do
       post :create, :publication => {:pubmed_id => 3,:project_id=>projects(:sysmo_project).id },:assay_ids=>[assay.id.to_s]
@@ -62,7 +74,7 @@ class PublicationsControllerTest < ActionController::TestCase
   end
 
   test "associates assay" do
-
+    login_as(:model_owner) #can edit assay
     p = publications(:taverna_paper_pubmed)
     original_assay = assays(:assay_with_a_publication)
     assert p.related_assays.include?(original_assay)
@@ -85,6 +97,32 @@ class PublicationsControllerTest < ActionController::TestCase
 
     assert p.related_assays.include?(new_assay)
     assert new_assay.related_publications.include?(p)
+
+  end
+
+  test "do not associate assays unauthorized for edit" do
+    p = publications(:taverna_paper_pubmed)
+    original_assay = assays(:assay_with_a_publication)
+    assert p.related_assays.include?(original_assay)
+    assert original_assay.related_publications.include?(p)
+
+    new_assay=assays(:metabolomics_assay)
+    assert new_assay.related_publications.empty?
+
+    put :update, :id => p,:author=>{},:assay_ids=>[new_assay.id.to_s]
+
+    assert_redirected_to publication_path(p)
+    p.reload
+    original_assay.reload
+    new_assay.reload
+
+    assert_equal 1, p.related_assays.count
+
+    assert p.related_assays.include?(original_assay)
+    assert original_assay.related_publications.include?(p)
+
+    assert !p.related_assays.include?(new_assay)
+    assert !new_assay.related_publications.include?(p)
 
   end
 
