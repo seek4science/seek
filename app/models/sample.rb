2491f847638b50a04bbf6b492780fcd08b385149
Lihua An <lihua.an@h-its.org>
2011-04-29 13:18:26 +0200

    sample acts_as_authorized

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -3,14 +3,12 @@ require 'acts_as_authorized'
 
 class Sample < ActiveRecord::Base
   belongs_to :specimen
-#  belongs_to :project
   belongs_to :institution
-#  belongs_to :policy
   has_many :experiments
 
   has_many :assets_creators, :dependent => :destroy, :as => :asset, :foreign_key => :asset_id
   has_many :creators, :class_name => "Person", :through => :assets_creators, :order=>'assets_creators.id'
-#  belongs_to :contributor, :polymorphic => true
+
 
   alias_attribute :description, :comments
   validates_presence_of :title
