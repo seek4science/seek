37f7519f8bc9a3f404f4fce5a11ebc30454f387e
David Shockley <David.Shockley@h-its.org>
2011-04-29 21:18:54 +0200

    added handling for controllers that dont have a _path helper to project_membership_required

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -116,10 +116,10 @@ class ApplicationController < ActionController::Base
   private
 
   def project_membership_required
-    unless current_user and current_user.person and current_user.person.member?
+    unless try_block {current_user.person.member?}
 #    if current_user and current_user.person and current_user.person.member?
       flash[:error] = "Only members of known projects, institutions or work groups are allowed to create new content."
-      redirect_to eval("#{controller_name}_path")
+      try_block {redirect_to eval("#{controller_name}_path")} or redirect_to root_url
     end
 
   end
