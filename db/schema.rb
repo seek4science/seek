b5b39d3146a6f93d202b536ddc2e6549cf81ec65
David Shockley <David.Shockley@h-its.org>
2011-04-29 11:15:29 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110426163203) do
+ActiveRecord::Schema.define(:version => 20110427134457) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
@@ -260,6 +260,7 @@ ActiveRecord::Schema.define(:version => 20110426163203) do
     t.integer  "policy_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "assay_id"
   end
 
   create_table "favourite_group_memberships", :force => true do |t|
