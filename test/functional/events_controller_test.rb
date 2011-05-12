f75e42e0c511f26ddc21f9614e22e85889c18f16
David Shockley <David.Shockley@h-its.org>
2011-05-12 11:24:49 +0200

    removed contributor and project from event index listing

--- test/functional/events_controller_test.rb
+++ test/functional/events_controller_test.rb
@@ -21,6 +21,16 @@ class EventsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:events)
   end
 
+  test "index should not show contributor or project" do
+    e = Factory :event,
+                :contributor => Factory(:user, :person => Factory(:person ,:first_name => "Dont", :last_name => "Display Person")),
+                :project => Factory(:project, :title => "Dont Display Project"),
+                :policy => Factory(:public_policy)
+    get :index
+    assert !(/Dont Display Person/ =~ @response.body)
+    assert !(/Dont Display Project/ =~ @response.body)
+  end
+
   test "shouldn't show hidden items in index" do
     login_as(:aaron)
     get :index, :page => "all"
