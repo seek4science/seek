c0845891fb7613b57651c0c6e59298d89fb8ed95
quyen <unknown>
2011-05-12 13:25:44 +0200

    showing error message when substance is blank or multiple

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,6 +2,6 @@ class Unit < ActiveRecord::Base
 
   has_many :studied_factors
   has_many :experimental_conditions
-  named_scope :for_factors_studied?,:conditions=>{:factors_studied=>true}
+  named_scope :factors_studied_units,:conditions=>{:factors_studied=>true}
 
 end
