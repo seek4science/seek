b09c39e749edc92155cfc91f9449e868aeab3c51
Lihua An <lihua.an@h-its.org>
2011-05-17 11:48:36 +0200

    update specimen form

--- app/helpers/specimens_helper.rb
+++ app/helpers/specimens_helper.rb
@@ -1,19 +1,5 @@
 module SpecimensHelper
 
-   def specimen_organism_list_item attribute,specimen,none_text="Not specified"
-    result = "<span class='none_text'>#{none_text}</span>"
-    unless specimen.nil?
-    result = link_to h(specimen.organism.try(:title)),specimen.organism
-    if specimen.strain
-      result += ": #{specimen.strain.try(:title)}"
-    end
-    if specimen.culture_growth_type
-      result += " (#{specimen.culture_growth_type.try(:title)})"
-    end
-    end
-
-    return "<p class=\"list_item_attribute\"><b>#{attribute}</b>: #{result}</p>"
-   end
    def specimen_organism_title specimen
      title = ""
      unless specimen.nil?
