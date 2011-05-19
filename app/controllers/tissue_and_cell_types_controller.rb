051ea854440c5531612229736c7d75c592394b84
Unknown <lihuaan@localhost6.localdomain6>
2011-05-19 11:25:29 +0200

    1.reset tissue_and_cell_type select box when user wants to specify one 2.remove the code never used

--- app/controllers/tissue_and_cell_types_controller.rb
+++ app/controllers/tissue_and_cell_types_controller.rb
@@ -1,17 +1,6 @@
 class TissueAndCellTypesController < ApplicationController
 
-  before_filter :find_and_auth,:only=>{:show_existing_tissue_and_cell_types,:index}
 
-  def show_existing_tissue_and_cell_types
-    element=params[:element]
-    render :update do |page|
-      page.replace_html element,:partial=>"tissue_and_cell_types/existing_tissue_and_cell_types",:object=>@tissue_and_cell_types
-    end
-  end
-
-  def get_existing_tissue_and_cell_types
-    @tissue_and_cell_types=TissueAndCellType.all
-  end
 
   def show
     @tissue_and_cell_type=TissueAndCellType.find(params[:id])
