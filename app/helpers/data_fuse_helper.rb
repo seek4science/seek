338e9be49c7c7eca6ab61eb6ce667675ef4e1ba0
Stuart Owen <stuart.owen@manchester.ac.uk>
2011-05-19 12:53:36 +0100

    now plotting graph from csv data, and also presented alongside mathematica graph.

--- app/helpers/data_fuse_helper.rb
+++ app/helpers/data_fuse_helper.rb
@@ -1,16 +1,23 @@
 module DataFuseHelper
 
   def csv_to_google_data csv_url
+    puts "csv url = #{csv_url}"
+    res = ""
     csv = open(csv_url).read
     row_count=0
-    headings=nil
-    FasterCSV.parse(csv) do |row|
+    rows = FasterCSV.parse(csv)
+    rows.each do |row|
       if row_count==0
-        headings=row
-
+        row.each do |entry|
+           res << "data.addColumn('number','#{entry}');\n"
+        end
+        #res << "data.addRows(#{rows.count});\n"
+      else
+        row_values = row.join(", ").gsub(".,",",")
+        res << "data.addRow([#{row_values}]);\n"
       end
       row_count+=1
     end
-
+    res
   end
 end
