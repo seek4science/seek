b4b572a8708fcba56743e69ace2d33ba0abf8a41
Unknown <sowen@vaio-laptop-linux>
2011-05-23 13:10:56 +0100

    updated schema after running migrations

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110516170953) do
+ActiveRecord::Schema.define(:version => 20110519214627) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
