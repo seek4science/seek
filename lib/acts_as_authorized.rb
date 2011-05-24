30ea92636ed1eca80b993f963cf5c3454848dcd5
David Shockley <David.Shockley@h-its.org>
2011-05-24 19:01:05 +0200

    can_* methods should interpert an explicitly passed nil value as an anonymous user

--- lib/acts_as_authorized.rb
+++ lib/acts_as_authorized.rb
@@ -14,8 +14,8 @@ module Acts #:nodoc:
       false
     end
 
-    def can_perform? action, user=nil
-      user ? send("can_#{action}?", user) : send("can_#{action}?")
+    def can_perform? action, *args
+      send "can_#{action}?", *args
     end
 
     AUTHORIZATION_ACTIONS = [:view, :edit, :download, :delete, :manage]
@@ -135,10 +135,11 @@ module Acts #:nodoc:
       end
 
       AUTHORIZATION_ACTIONS.each do |action|
-        define_method "can_#{action}?" do |*args|
-          user = args[0] || User.current_user
-          new_record? or Authorization.is_authorized? action.to_s, nil, self, user
-        end
+        eval <<-END_EVAL
+          def can_#{action}? user = User.current_user
+            new_record? or Authorization.is_authorized? "#{action}", nil, self, user
+          end
+        END_EVAL
       end
 
       #returns a list of the people that can manage this file
