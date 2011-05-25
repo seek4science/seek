2e8b02da64c0c6dd1b10e120315132500064133c
Unknown <lihuaan@localhost6.localdomain6>
2011-05-25 17:33:09 +0200

    new migration: associate sops with samples(many->many)

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110519150718) do
+ActiveRecord::Schema.define(:version => 20110525084126) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
@@ -586,6 +586,12 @@ ActiveRecord::Schema.define(:version => 20110519150718) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "sample_sops", :force => true do |t|
+    t.integer "sample_id"
+    t.integer "sop_id"
+    t.integer "sop_version"
   end
 
   create_table "samples", :force => true do |t|
