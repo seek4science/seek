d47717bfff4b2125d14f0be145700b58c704a5fe
Unknown <lihuaan@localhost6.localdomain6>
2011-06-09 16:32:27 +0200

    Create New Assay Type/New Technology Type should be also possible for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -142,12 +142,8 @@ class ApplicationController < ActionController::Base
 
     case Seek::Config.type_managers
       when "admins"
-      if User.admin_logged_in?
         return true
-      else
-        error("Admin rights required to manage types", "...")
-        return false
-      end
+
       when "pals"
       if User.admin_logged_in? || User.pal_logged_in?
         return true
