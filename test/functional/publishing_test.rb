800f4032cf41fc89c7f324b17b2bcddc788fad31
Unknown <sowen@vaio-laptop-linux>
2011-06-22 11:56:13 +0100

    created publishing specific functional tests class, based upon data_files, and moved data file publishing tests over to it

---
+++ test/functional/publishing_test.rb
@@ -0,0 +1,52 @@
+require 'test_helper'
+
+class PublishingTest < ActionController::TestCase
+
+  tests DataFilesController
+
+  fixtures :all
+
+  include AuthenticatedTestHelper
+  
+  def setup
+    login_as(:datafile_owner)
+    @object=data_files(:picture)
+  end
+  
+  test "do publish" do
+    df=data_files(:picture)
+
+    assert df.can_manage?,"The datafile must be manageable for this test to succeed"
+    post :publish,:id=>df
+    assert_response :success
+    assert_nil flash[:error]
+    assert_not_nil flash[:notice]
+  end
+
+  test "do not publish if not can_manage?" do
+    login_as(:quentin)
+    df=data_files(:picture)
+    assert !df.can_manage?,"The datafile must not be manageable for this test to succeed"
+    post :publish,:id=>df
+    assert_redirected_to data_file_path(df)
+    assert_not_nil flash[:error]
+    assert_nil flash[:notice]
+  end
+
+  test "get preview_publish" do
+    df=data_files(:picture)
+    assert df.can_manage?,"The datafile must be manageable for this test to succeed"
+    get :preview_publish, :id=>df
+    assert_response :success
+  end
+
+  test "cannot get preview_publish when not manageable" do
+    login_as(:quentin)
+    df = data_files(:picture)
+    assert !df.can_manage?,"The datafile must not be manageable for this test to succeed"
+    get :preview_publish, :id=>df
+    assert_redirected_to data_file_path(df)
+    assert flash[:error]
+  end
+  
+end
