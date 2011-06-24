bc75793fd7d79a40d244ed6838b45d2403e7c09c
Unknown <sowen@vaio-laptop-linux>
2011-06-24 16:08:29 +0100

    added complex test for publishing notification email delivery, and the details in the response

--- test/functional/publishing_test.rb
+++ test/functional/publishing_test.rb
@@ -178,10 +178,123 @@ class PublishingTest < ActionController::TestCase
     assert flash[:error]
   end
 
+  test "notification email delivery count and response with complex ownerships" do
+    inv,study,assay,df1,df2,df3 = isa_with_complex_sharing
+    params={:publish=>{}}
+    [inv,study,assay,df1,df2,df3].each do |r|
+      params[:publish][r.class.name]||={}
+      params[:publish][r.class.name][r.id.to_s]="1"
+    end
+    assert_emails 2 do
+      post :publish,params.merge(:id=>df3)
+    end
+    assert_response :success
+
+    assert_select "ul#published" do
+      assert_select "li",:text=>/Investigation: #{inv.title}/,:count=>1
+      assert_select "li",:text=>/Study: #{study.title}/,:count=>0
+      assert_select "li",:text=>/Assay: #{assay.title}/,:count=>0
+      assert_select "li",:text=>/Data file: #{df1.title}/,:count=>0
+      assert_select "li",:text=>/Data file: #{df2.title}/,:count=>1
+      assert_select "li",:text=>/Data file: #{df3.title}/,:count=>1
+    end
+    personB=study.contributor
+    personC=df2.contributor
+    personA=df3.contributor
+
+    assert_select "ul#notified" do
+      assert_select "li",:text=>/Study: #{study.title}/,:count=>1
+      assert_select "li",:text=>/Assay: #{assay.title}/,:count=>1
+      assert_select "li",:text=>/Data file: #{df1.title}/,:count=>1
+      
+      assert_select "li > a[href=?]",person_path(personB),:text=>/#{personB.name}/,:count=>3
+      assert_select "li > a[href=?]",person_path(personC),:text=>/#{personC.name}/,:count=>2
+      assert_select "li > a[href=?]",person_path(personA),:text=>/#{personA.name}/,:count=>0
+    end
+
+    assert_select "ul#problematic" do
+      assert_select "li",1
+      assert_select "li",:text=>/No items/,:count=>1
+    end
+  end
+
   private
 
   def data_file_for_publishing(owner=users(:datafile_owner))
     Factory :data_file, :contributor=>owner, :project=>owner.person.projects.first
+  end
+
+  def isa_with_complex_sharing
+    userA=users(:datafile_owner) #logged in user
+    userB=Factory :user
+    p=userA.person
+    userC=Factory :user
+    userD=Factory :user #doesn't manage anything, just can_view
+
+    assay = Factory :assay, :contributor=>userB.person,
+                    :study=>Factory(:study,:contributor=>userB.person,
+                      :investigation=>Factory(:investigation, :contributor=>userA.person))
+    study=assay.study
+    inv=study.investigation
+
+    assay.policy.permissions << Factory(:permission,:policy=>assay.policy,:contributor=>userC.person,:access_type=>Policy::MANAGING)
+    study.policy.permissions << Factory(:permission,:policy=>study.policy,:contributor=>userC.person,:access_type=>Policy::MANAGING)
+    inv.policy.permissions << Factory(:permission, :policy=>inv.policy,:contributor=>userB.person,:access_type=>Policy::MANAGING)
+
+    df1 = Factory :data_file,:contributor=>userB.person,:project=>userB.person.projects.first
+    df2 = Factory :data_file,:contributor=>userC.person,:project=>userC.person.projects.first
+    df3 = Factory :data_file,:contributor=>userA.person,:project=>userA.person.projects.first
+
+    df1.policy.permissions << Factory(:permission,:policy=>df1.policy,:contributor=>userD.person, :access_type=>Policy::VISIBLE)
+    df2.policy.permissions << Factory(:permission,:policy=>df2.policy,:contributor=>userD.person, :access_type=>Policy::VISIBLE)
+    df2.policy.permissions << Factory(:permission,:policy=>df2.policy,:contributor=>userA.person, :access_type=>Policy::MANAGING)
+
+    disable_authorization_checks do
+      assay.relate(df1)
+      assay.relate(df2)
+      assay.relate(df3)
+    end
+    #some sanity checks that the data structure is as expected
+    assert_equal 1, inv.studies.count
+    assert_equal 1, inv.studies.first.assays.count
+
+    assert assay.can_manage?(userB)
+    assert assay.can_manage?(userC)
+    assert !assay.can_manage?(userA)
+    assert !assay.can_manage?(userD)
+
+    assert study.can_manage?(userB)
+    assert study.can_manage?(userC)
+    assert !study.can_manage?(userA)
+    assert !study.can_manage?(userD)
+    
+    assert inv.can_manage?(userA)
+    assert inv.can_manage?(userB)
+    assert !inv.can_manage?(userC)
+    assert !inv.can_manage?(userD)
+
+    assert df1.can_manage?(userB)
+    assert !df1.can_manage?(userC)
+    assert !df1.can_manage?(userA)
+    assert !df1.can_manage?(userD)
+
+    assert df2.can_manage?(userC)
+    assert df2.can_manage?(userA)
+    assert !df2.can_manage?(userB)
+    assert !df2.can_manage?(userD)
+
+    assert df3.can_manage?(userA)
+    assert !df3.can_manage?(userB)
+    assert !df3.can_manage?(userC)
+    assert !df3.can_manage?(userD)
+
+    assert_equal 3,assay.assets.count
+
+    assert assay.assets.include?(df1.versions.first)
+    assert assay.assets.include?(df2.versions.first)
+    assert assay.assets.include?(df3.versions.first)
+    
+    [inv,study,assay,df1,df2,df3]
   end
 
   def data_with_isa
