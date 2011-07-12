e5cb2815ca9fd0219b3046650e13904360fbf802
Unknown <sowen@vaio-laptop-linux>
2011-07-12 13:51:36 +0100

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -482,6 +482,12 @@ ActiveRecord::Schema.define(:version => 20110602160032) do
     t.datetime "updated_at"
   end
 
+  create_table "official_compounds", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "organisms", :force => true do |t|
     t.string   "title"
     t.integer  "ncbi_id"
@@ -498,7 +504,7 @@ ActiveRecord::Schema.define(:version => 20110602160032) do
 
   create_table "people", :force => true do |t|
     t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "updated_at",                                             :null => false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
