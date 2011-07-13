5a318e4a58ae72698075bd1d1922260d8646d61f
David Shockley <David.Shockley@h-its.org>
2011-07-13 11:50:57 +0200

    moved template requirement to the method that actually needs it

--- app/models/sub_mailer.rb
+++ app/models/sub_mailer.rb
@@ -1,4 +1,5 @@
-class SubMailer < ActionMailer::Base
+class
+SubMailer < ActionMailer::Base
 
 
   def send_digest_subscription person, activity_logs
@@ -7,11 +8,11 @@ class SubMailer < ActionMailer::Base
     from Seek::Config.noreply_sender
     sent_on Time.now
     content_type "text/html"
-    template "send_digest_subscription"
     body :activity_logs=> activity_logs
   end
 
   def send_immediate_subscription person, activity_log
+    template "send_digest_subscription"
     send_digest_subscription person, [activity_log]
   end
 end
