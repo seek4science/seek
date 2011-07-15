58b2754451ca36d0f6ca41e102bd4945472269be
David Shockley <unknown>
2011-07-15 19:20:09 +0200

    subscribe_to_all_in_project bugfix

--- app/models/project_subscription.rb
+++ app/models/project_subscription.rb
@@ -47,11 +47,11 @@ class ProjectSubscription < ActiveRecord::Base
   after_create :subscribe_to_all_in_project
 
   def subscribe_to_all_in_project
-    all_in_project.each(&:subscribe).each {|i| i.save if i.changed?}
+    all_in_project.each(&:subscribe).each {|i| i.save if i.changed_for_autosave?}
   end
 
   def unsubscribe_to_all_in_project
-    all_in_project.each(&:unsubscribe).each {|i| i.save if i.changed?}
+    all_in_project.each(&:unsubscribe).each {|i| i.save if i.changed_for_autosave?}
   end
 
   private
