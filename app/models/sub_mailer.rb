a437dd620f6e379ed75ff7f9fe9891f1b352344f
Lihua An <lihua.an@h-its.org>
2011-07-15 15:50:10 +0200

    missing sub_mailer model during merge

---
+++ app/models/sub_mailer.rb
@@ -0,0 +1,17 @@
+class SubMailer < ActionMailer::Base
+
+
+  def send_digest_subscription person, activity_logs
+    subject "#{person.user.login}: SEEK subscription report"
+    recipients person.email_with_name
+    from Seek::Config.noreply_sender
+    sent_on Time.now
+    content_type "text/html"
+    template "send_digest_subscription"
+    body :activity_logs=> activity_logs
+  end
+
+  def send_immediate_subscription person, activity_log
+    send_digest_subscription person, [activity_log]
+  end
+end
