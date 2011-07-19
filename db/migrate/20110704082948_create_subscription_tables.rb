20c92234c7923c8a9ec12ffef8f178196fe8d442
David Shockley <David.Shockley@h-its.org>
2011-07-19 16:57:52 +0200

    updated migration to work with new set_default_subscriptions method, which wants a current user

--- db/migrate/20110704082948_create_subscription_tables.rb
+++ db/migrate/20110704082948_create_subscription_tables.rb
@@ -15,7 +15,7 @@ class CreateSubscriptionTables < ActiveRecord::Migration
       t.string :frequency
     end
     #refactor to use sql, or at least not depend on app code
-    disable_authorization_checks {Person.all.each(&:set_default_subscriptions).each &:save}
+    Person.all.each {|p| User.with_current_user(p.user) {p.set_default_subscriptions; p.save!}}
   end
 
   def self.down
