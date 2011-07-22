0adf5f0e1c7ef41b5dde0ebbdab8b44ef40fba1b
David Shockley <David.Shockley@h-its.org>
2011-07-22 16:59:56 +0200

    you do not need to be allowed to access an item to subscribe to it

--- lib/subscribable.rb
+++ lib/subscribable.rb
@@ -1,7 +1,7 @@
 module Subscribable
   def self.included klass
     klass.class_eval do
-      has_many :subscriptions, :as => :subscribable, :dependent => :destroy, :autosave => true
+      has_many :subscriptions, :required_access_to_owner => false, :as => :subscribable, :dependent => :destroy, :autosave => true, :before_add => proc {|item, sub| sub.subscribable = item}
       before_create :set_default_subscriptions
       extend ClassMethods
     end
