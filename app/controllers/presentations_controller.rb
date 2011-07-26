b2f1067ce02bb0d515549094ca566c728f813d19
Lihua An <lihua.an@h-its.org>
2011-07-26 11:18:06 +0200

    comment flash stuff out temporarily

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -1,4 +1,4 @@
-require "flash_tool"
+#require "flash_tool"
 class PresentationsController < ApplicationController
 
 
@@ -92,39 +92,36 @@ class PresentationsController < ApplicationController
 
   end
 
-  def convert_to_swf
-      file_type = nil
-      if @presentation
-        case @presentation.content_type
-          when "application/pdf"
-             file_type = "pdf"
-          when "application/text"
-            file_type = "text"
-          else
-
-        end
-
-        begin
-          FlashTool::FlashObject.new("#{ContentBlob::DATA_STORAGE_PATH}#{RAILS_ENV}/#{@presentation.content_blob.uuid}.dat", file_type) do |f|
-            f.output("public/swf/presentations/#{@presentation.title}.swf")
-            f.fonts
-            f.flashversion("9")
-            f.stop
-            f.set('param="storeallcharacters"')
-            f.flatten
-            f.save()
-          end
-        rescue Exception=>e
-
-          flash[:error]=e.message#[0..50]
-
-        end
-      end
-
-
-
-
-  end
+#  def convert_to_swf
+#      file_type = nil
+#      if @presentation
+#        case @presentation.content_type
+#          when "application/pdf"
+#             file_type = "pdf"
+#          when "application/text"
+#            file_type = "text"
+#          else
+#
+#        end
+#
+#        begin
+#          FlashTool::FlashObject.new("#{ContentBlob::DATA_STORAGE_PATH}#{RAILS_ENV}/#{@presentation.content_blob.uuid}.dat", file_type) do |f|
+#            f.output("public/swf/presentations/#{@presentation.title}.swf")
+#            f.fonts
+#            f.flashversion("9")
+#            f.stop
+#            f.set('param="storeallcharacters"')
+#            f.flatten
+#            f.save()
+#          end
+#        rescue Exception=>e
+#
+#          flash[:error]=e.message#[0..50]
+#
+#        end
+#      end
+#
+#  end
   # GET /presentations/1
   # GET /presentations/1.xml
   def show
