5662be8241865936d8ef38f4f615d7317d8d5415
Finn Bacall <unknown>
2011-07-29 10:10:54 +0100

    Added some authentication/authorization to spreadsheet annotations controller. Also fixed tests

--- test/functional/spreadsheet_annotations_controller_test.rb
+++ test/functional/spreadsheet_annotations_controller_test.rb
@@ -12,34 +12,70 @@ class SpreadsheetAnnotationsControllerTest < ActionController::TestCase
 
 
   test "create new annotation" do
-    assert true
-  end
-
-  test "update an annotation" do
-    assert true
-  end
-
-  test "delete an annotation" do
-    assert true
-  end
-
-
-  test "cannot add cell range for private datafile user does not own" do
-    user_logged_in = login_as(:datafile_owner)
-    puts "
-          content blob id = #{content_blobs(:private_spreadsheet_blob).id}
-          worksheet id =    #{worksheets(:private_worksheet).id}"
-
-    post :create, :annotation_content_blob_id => content_blobs(:private_spreadsheet_blob).id,
-         :annotation_sheet_id => worksheets(:private_worksheet).id,
-         :annotation_cell_coverage => "A1:B2",
-         :annotation_content => "Annotation!",
-         :id => user_logged_in.id
+    assert_difference("Annotation.count", 1) do
+      xhr  :post, :create, {:annotation_content_blob_id => content_blobs(:private_spreadsheet_blob).id,
+           :annotation_sheet_id => worksheets(:private_worksheet).sheet_number,
+           :annotation_cell_coverage => "A1:B2",
+           :annotation_content => "Annotation!"}
+    end
 
     assert_response :success
   end
 
-  test "" do
+  test "can't create new annotation on inaccessible spreadsheet" do
+    login_as(:quentin)
 
+    assert_no_difference("Annotation.count") do
+      xhr  :post, :create, {:annotation_content_blob_id => content_blobs(:private_spreadsheet_blob).id,
+           :annotation_sheet_id => worksheets(:private_worksheet).sheet_number,
+           :annotation_cell_coverage => "A1:B2",
+           :annotation_content => "Annotation!"}
+    end
+
+    assert_response :redirect
+
+    assert_equal flash[:error], "You are not permitted to annotate this spreadsheet."
+  end
+
+  test "update an annotation" do
+    xhr  :put, :update, {:id => annotations(:annotation_1).id,
+         :annotation_content => "Updated"}
+
+    assert_response :success
+
+    assert_equal "Updated", assigns(:annotation).value.text
+  end
+
+  test "can't update others' annotations" do
+    login_as(:quentin)
+
+    xhr  :put, :update, {:id => annotations(:annotation_1).id,
+         :annotation_content => "Updated"}
+
+    assert_response :error
+
+    assert assigns(:annotation).errors.full_messages.include?("You may not edit or remove other users' annotations.")
+
+    assert_equal "HELLO WORLD", assigns(:annotation).value.text
+  end
+
+  test "delete an annotation" do
+    assert_difference("Annotation.count", -1) do
+      xhr  :delete, :destroy, {:id => annotations(:annotation_1).id}
+    end
+
+    assert_response :success
+  end
+
+  test "can't delete others' annotations" do
+    login_as(:quentin)
+
+    assert_no_difference("Annotation.count") do
+      xhr  :delete, :destroy, {:id => annotations(:annotation_1).id}
+    end
+
+    assert_response :error
+
+    assert assigns(:annotation).errors.full_messages.include?("You may not edit or remove other users' annotations.")
   end
 end
