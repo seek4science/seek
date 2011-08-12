ee37abd9266ee014b3166244514bb5f6b7fe4847
Lihua An <lihua.an@h-its.org>
2011-08-12 12:34:39 +0200

    new scales list, old scales and scalings will be destroyed

--- test/unit/config_test.rb
+++ test/unit/config_test.rb
@@ -28,7 +28,7 @@ class ConfigTest < ActiveSupport::TestCase
   end
 
   test "scales" do
-    assert_equal ["Organ","Tissue","Cellular","Molecular"],Seek::Config.scales
+    assert_equal ["organism","liver","liverLobe","liverLobule","intercellular","cell"],Seek::Config.scales
   end
 
   test "seek_video_link" do
