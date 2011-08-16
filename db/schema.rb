1dea9f3bfbe925bed8fee9dbfe6ae91ca9dba876
Unknown <sowen@vaio-laptop-linux>
2011-08-16 10:11:10 +0100

    updated schema after migrate

--- db/schema.rb
+++ db/schema.rb
@@ -504,6 +504,12 @@ ActiveRecord::Schema.define(:version => 20110727154707) do
     t.datetime "updated_at"
   end
 
+  create_table "official_compounds", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "organisms", :force => true do |t|
     t.string   "title"
     t.integer  "ncbi_id"
@@ -520,7 +526,7 @@ ActiveRecord::Schema.define(:version => 20110727154707) do
 
   create_table "people", :force => true do |t|
     t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "updated_at",                                             :null => false
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
