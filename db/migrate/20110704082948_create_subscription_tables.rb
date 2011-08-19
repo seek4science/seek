117f822a6b118cdd4c81ff9f090c821f215f07e7
Unknown <sowen@vaio-laptop-linux>
2011-08-19 12:55:42 +0100

    commented out app code in migration, ran all migrations to correct schema

--- db/migrate/20110704082948_create_subscription_tables.rb
+++ db/migrate/20110704082948_create_subscription_tables.rb
@@ -15,7 +15,7 @@ class CreateSubscriptionTables < ActiveRecord::Migration
       t.string :frequency
     end
     #refactor to use sql, or at least not depend on app code
-    Person.all.each {|p| User.with_current_user(p.user) {p.set_default_subscriptions; p.save!}}
+    #Person.all.each {|p| User.with_current_user(p.user) {p.set_default_subscriptions; p.save!}}
   end
 
   def self.down
