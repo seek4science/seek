75f6bf9912bcf9bdad28c0a76e64bfa9c0c76b01
Unknown <sowen@vaio-laptop-linux>
2011-09-01 15:10:27 +0100

    fixed investigation validation without project problem. Removed check for project membership that was causing the redirect. This will be replaced with a validator

--- app/models/investigation.rb
+++ app/models/investigation.rb
@@ -10,6 +10,7 @@ class Investigation < ActiveRecord::Base
 
 
   validates_presence_of :projects
+  validates_presence_of :title
   validates_uniqueness_of :title
 
   has_many :assays,:through=>:studies
