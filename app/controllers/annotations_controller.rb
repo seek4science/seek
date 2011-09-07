f0cbc97e1fbf0ada44b56006ed97dd50e8097902
Unknown <sowen@vaio-laptop-linux>
2011-09-07 11:41:02 +0100

    minor refactoring of variable names in annotations_controller#show

--- app/controllers/annotations_controller.rb
+++ app/controllers/annotations_controller.rb
@@ -8,12 +8,12 @@ class AnnotationsController < ApplicationController
   include Annotations
   unloadable
   def show
-      @original_tag = Annotation.find(params[:id])
-      @other_tagging_annotations = @original_tag.value.annotations
+      annotation = Annotation.find(params[:id])
+      other_annotations = annotation.value.annotations
 
 
       @other_tagging_assets = []
-      @other_tagging_annotations.each do |annotation|
+      other_annotations.each do |annotation|
         annotatable=annotation.annotatable
         @other_tagging_assets << annotatable unless @other_tagging_assets.include?(annotatable)
       end
@@ -22,9 +22,9 @@ class AnnotationsController < ApplicationController
       #TextValue.find(:all, @original_tag.value_id).each do
 
       if @other_tagging_assets.empty?
-        flash.now[:notice]="No objects (or none that you are authorized to view) are tagged with '<b>#{@original_tag.value.text}</b>'."
+        flash.now[:notice]="No objects (or none that you are authorized to view) are tagged with '<b>#{annotation.value.text}</b>'."
       else
-        flash.now[:notice]="#{@other_tagging_assets.size} #{@other_tagging_assets.size==1 ? 'item' : 'items'} tagged with '<b>#{@original_tag.value.text}</b>'."
+        flash.now[:notice]="#{@other_tagging_assets.size} #{@other_tagging_assets.size==1 ? 'item' : 'items'} tagged with '<b>#{annotation.value.text}</b>'."
       end
       respond_to do |format|
         format.html # show.html.erb
