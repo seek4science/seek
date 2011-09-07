c185cfa4576be49678c69e3c95e214559c46fce2
Niall Beard <beardn9@cs.man.ac.uk>
2011-09-07 17:22:57 +0100

    Weighting for tag clouds added

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -13,21 +13,6 @@ module TagsHelper
   end
 
   def tag_cloud(tags, classes,counter_method=:count)
-    tags = tags.sort_by{|t| t.name.downcase}
-    max_count = tags.max_by(&counter_method).send(counter_method).to_f
-    if max_count < 1
-      max_count = 1
-    end
-
-
-    tags.each do |tag|
-      index = ((tag.send(counter_method) / max_count) * (classes.size - 1)).round
-      yield tag, classes[index]
-    end
-  end
-
-
-  def ann_cloud(tags, classes,counter_method=:count)
     tags = tags.sort_by{|t| t.value.text.downcase}
     max_count = tags.max_by(&counter_method).send(counter_method).to_f
     if max_count < 1
@@ -42,8 +27,25 @@ module TagsHelper
   end
 
 
-  def overall_tag_cloud(tags, classes,&block)
-    tag_cloud(tags,classes,:overall_total, &block)
+  def ann_cloud(tags, classes, counter_method=:count)
+    tags = tags.sort_by{|t| t.value.text.downcase}
+
+    max_count = 0
+
+    tags.each do |tag|
+      tag_count = tag.value.annotations.count
+      max_count = tag_count if max_count < tag_count
+    end
+
+    tags.each do |tag|
+      index = ((tag.value.annotations.count / max_count) * (classes.size - 1)).round
+      yield tag, classes[index]
+    end
+  end
+
+
+  def overall_tag_cloud(tags,classes,&block)
+    ann_cloud(tags, classes, &block)
   end
   
 
