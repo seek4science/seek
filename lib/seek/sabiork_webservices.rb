d0df57a85069c5b3b18b52a9e28f4fcfa6c2f540
quyen <unknown>
2011-09-13 18:59:57 +0200

    escape compound_name to solve problem of special character, ex. +

--- lib/seek/sabiork_webservices.rb
+++ lib/seek/sabiork_webservices.rb
@@ -1,10 +1,13 @@
 require 'rest_client'
 require 'libxml'
+require 'cgi'
 
 module Seek
   class SabiorkWebservices
     def get_compound_annotation compound_name
-      url=URI.encode(webservice_base_url+"compounds?compoundName=#{compound_name}")
+      url=URI.encode(webservice_base_url + "compounds?compoundName=")
+      compound_name = URI.escape(compound_name, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+      url.concat(compound_name)
       doc = get_xml_doc url
       compound_annotations = {'synonyms' => [], 'chebi_ids' => [], 'kegg_ids' => []}
 
