7798f5b9860e7352afcf561f88295f59284a625c
quyen <unknown>
2011-09-16 16:48:43 +0200

    remove the color on organism, strains, culture growth in select_organisms partial

--- public/javascripts/assays.js
+++ public/javascripts/assays.js
@@ -430,10 +430,10 @@ function updateOrganisms() {
         culture_growth=organism[3];
         titleText = '<span title="' + title + '">' + title.truncate(100);
         if (strain.length>0) {
-            titleText += ":"+ "<span class='assay_strain_info'> " + strain+ "</span>";
+            titleText += ":"+ "<span> " + strain+ "</span>";
         }
         if (culture_growth.length>0 && culture_growth!='Not specified') {
-            titleText += " <span class='assay_item_sup_info'>("+culture_growth+")</span>";
+            titleText += " <span>("+culture_growth+")</span>";
         }
         titleText +=  '</span>';
         organism_text += '<li>' + titleText +
