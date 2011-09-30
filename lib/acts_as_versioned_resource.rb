1f4add0bb36b197ba9b2671191aaf6ee93ee3cfa
Unknown <lihuaan@localhost6.localdomain6>
2011-09-30 16:58:01 +0200

    For Model (which has multiple content_blobs), the last content_blob(latest) is selected as old 'content_blob'  by default

--- lib/acts_as_versioned_resource.rb
+++ lib/acts_as_versioned_resource.rb
@@ -43,7 +43,7 @@ module Acts #:nodoc:
         if self.respond_to? :content_blob
           ContentBlob.find(:first, :conditions => ["asset_id =? and asset_type =? and asset_version =?", self.parent.id, self.parent.class.name, self.version])
         elsif self.respond_to? :content_blobs
-          self.content_blobs.first
+          self.content_blobs.last(:order=>"id asc")
         end
       end
 
