8f2d70d1d2a6add0dd23549ed95eb209b26cc356
David Shockley <David.Shockley@h-its.org>
2011-10-12 14:02:02 +0200

    reseting cached table information for models in this migration

--- db/migrate/20110704082948_create_subscription_tables.rb
+++ db/migrate/20110704082948_create_subscription_tables.rb
@@ -59,6 +59,8 @@ class CreateSubscriptionTables < ActiveRecord::Migration
       t.string :frequency
     end
 
+    [Project, WorkGroup, GroupMembership, ProjectSubscription, Subscription, Person].each {|e| e.reset_column_information}
+
     Person.all.each { |p| p.set_default_subscriptions }
   end
 
