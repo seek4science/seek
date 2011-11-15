02d4c00dd7a3522c3dd892d8d4698c7c69b06cb4
Lihua An <lihua.an@h-its.org>
2011-11-15 15:43:40 +0100

    move creatable classes order test from UsersControllerTest to ConfigTest

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -17,14 +17,6 @@ class UsersControllerTest < ActionController::TestCase
     @response   = ActionController::TestResponse.new
   end
 
-
-  test "creatable classes order" do
-    oldval = Seek::Config.is_virtualliver
-    Seek::Config.is_virtualliver = true
-    assert_equal [DataFile,Model,Presentation,Publication,Sop,Assay,Investigation,Study,Event,Sample,Specimen],@controller.send(:user_creatable_classes)
-    Seek::Config.is_virtualliver = oldval
-  end
-  
   def test_title
     get :new
     assert_select "title",:text=>/The Sysmo SEEK.*/, :count=>1
