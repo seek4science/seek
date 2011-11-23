bfda796987df4c82ff55ccd9b7f6ce96d002a1db
Unknown <sowen@vaio-laptop-linux>
2011-11-23 11:01:42 +0000

    quick hack to cache Person.projects

--- app/models/person.rb
+++ app/models/person.rb
@@ -142,7 +142,8 @@ class Person < ActiveRecord::Base
 
   def projects
     #updating workgroups doesn't change groupmemberships until you save. And vice versa.
-    work_groups.collect {|wg| wg.project }.uniq | group_memberships.collect{|gm| gm.work_group.project}
+    @known_projects ||= work_groups.collect {|wg| wg.project }.uniq | group_memberships.collect{|gm| gm.work_group.project}
+    @known_projects
   end
 
   def member?
