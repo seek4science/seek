770d792eafb26641d82c1275d5ad5980d916fcf4
Lihua An <lihua.an@h-its.org>
2011-12-13 16:28:10 +0100

    DataFileTest: convert to presentation failed for scalings

--- test/unit/data_file_test.rb
+++ test/unit/data_file_test.rb
@@ -217,6 +217,7 @@ class DataFileTest < ActiveSupport::TestCase
       data_file.creators = [Factory(:person),Factory(:person)]
       Factory :annotation,:attribute_name=>"tags",:annotatable=> data_file,:attribute_id => AnnotationAttribute.create(:name=>"tags").id
       data_file.events = [Factory(:event)]
+      Factory :scaling, :person=> Factory(:person),:scalable=>data_file, :scale => Factory(:scale)
       data_file.save!
 
       data_file.reload
@@ -245,7 +246,7 @@ class DataFileTest < ActiveSupport::TestCase
       assert_equal data_file.project_ids,data_file_converted.project_ids
       assert_equal data_file.assays,data_file_converted.assays
       assert_equal data_file.event_ids, data_file_converted.event_ids
-
+      assert_equal data_file.scalings,data_file_converted.scalings
       assert_equal data_file.versions.map(&:updated_at).sort, data_file_converted.versions.map(&:updated_at).sort
     }
   end
