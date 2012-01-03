486ee016c10f211f7ee82940f967c2d2afac2323
Lihua An <lihua.an@h-its.org>
2012-01-03 12:02:16 +0100

    add  'model_image' as the image of models (similar to avatars)

---
+++ app/models/model_image.rb
@@ -0,0 +1,77 @@
+class ModelImage < ActiveRecord::Base
+  Model_IMAGE_PATH = 'filestore/model_images'
+  LARGE_SIZE = "1000x1000"
+  belongs_to :model
+  after_create :change_filename
+
+  acts_as_fleximage do
+    image_directory Model_IMAGE_PATH
+    use_creation_date_based_directories false
+    image_storage_format :jpg
+    output_image_jpg_quality 85
+    require_image true
+    missing_image_message 'is required'
+    invalid_image_message 'was not a readable image'
+  end
+
+  validates_presence_of :model
+
+  def image_storage_mime_type
+    mine_type = case ModelImage.image_storage_format
+                  when "jpg"
+                    "image/jpeg"
+                  when "png"
+                    "image/png"
+                  when "bmp"
+                    "image/bmp"
+                  when "tif","tiff"
+                    "image/tiff"
+                  when "png"
+                    "image/png"
+                  when "png"
+                    "image/png"
+                  when "gif"
+                    "image/gif"
+                end
+  end
+  def original_image_format
+    original_content_type.split("/").last
+  end
+
+  def original_path
+    "#{RAILS_ROOT}/#{Model_IMAGE_PATH}/original"
+  end
+
+  def image_file= file
+    # save_original_file
+    if file.respond_to?(:content_type) && file.respond_to?(:original_filename)
+      format = file.content_type.split("/").last
+      FileUtils.mkdir_p(original_path)
+      File.open("#{original_path}/#{file.original_filename}.#{format}", 'w') do |f|
+        file.rewind
+        f.write file.read
+      end
+    end
+
+    super
+  end
+
+  def change_filename
+    File.rename "#{original_path}/#{original_filename}.#{original_image_format}", "#{original_path}/#{id}.#{original_image_format}"
+  end
+
+
+  def select!
+    unless selected?
+      model.update_attribute :model_image_id, id
+      return true
+    else
+      return false
+    end
+  end
+
+  def selected?
+    model.model_image_id && model.model_image_id.to_i == id.to_i
+  end
+
+end
