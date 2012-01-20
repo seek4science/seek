68c50207f79664166b24ba427ed60e5b46440300
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-01-20 15:52:28 +0000

    updated util test

--- test/unit/util_test.rb
+++ test/unit/util_test.rb
@@ -3,8 +3,6 @@ require 'test_helper'
 class UtilTest < ActiveSupport::TestCase
 
   test "creatable types" do
-    as_virtualliver do
-      assert_equal [DataFile,Model,Presentation,Publication,Sop,Assay,Investigation,Study,Event,Sample],Seek::Util.user_creatable_types
-    end
+    assert_equal [DataFile,Model,Presentation,Publication,Sop,Assay,Investigation,Study,Event,Sample],Seek::Util.user_creatable_types
   end
 end
