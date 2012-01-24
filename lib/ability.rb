21c8198c2e15c66cf3ed23956a488255da820bc5
quyen <unknown>
2012-01-24 11:51:35 +0100

    add cancan gem and ability class

---
+++ lib/ability.rb
@@ -0,0 +1,11 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    @user = user || User.new # for guest
+    @user.roles.each { |role| send(role) }
+  end
+
+  def admin
+  end
+end
