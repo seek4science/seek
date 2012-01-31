32fa4c1604625104c03480fc17aa84c17b3ed6fa
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-01-31 13:44:44 +0000

    added additional test case for cases where treatment cell is not in the first row

--- test/unit/treatments_test.rb
+++ test/unit/treatments_test.rb
@@ -24,6 +24,27 @@ class TreatmentsTest < ActiveSupport::TestCase
 
   end
 
+  test "extract when treatment not in first row" do
+    xml = xml_for_file("treatments-not-in-first-row.xls")
+    assert xml.include?("workbook")
+    treatment = Seek::Treatments.new xml
+
+    assert_equal 2,treatment.values.keys.count
+    assert_equal ["Dilution_rate","pH"],treatment.values.keys.sort
+    assert_equal 19,treatment.values["pH"].count
+    assert_equal 19,treatment.values["Dilution_rate"].count
+    assert_equal "6.5",treatment.values["pH"].first
+    assert_equal "9.5",treatment.values["pH"].last
+
+    assert_equal "0.25",treatment.values["Dilution_rate"].first
+    assert_equal "0.15",treatment.values["Dilution_rate"].last
+
+    assert_equal 19, treatment.sample_names.count
+    assert_equal "1.0",treatment.sample_names.first
+    assert_equal "17.0",treatment.sample_names.last
+
+  end
+
   test "extract test2" do
     xml = xml_for_file("treatments-test2.xls")
     assert xml.include?("workbook")
