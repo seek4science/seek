b0f44fe2b05e705bba94fe38683e917a11d12927
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-02-08 09:32:03 +0000

    updated schema after migrations

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120124111514) do
+ActiveRecord::Schema.define(:version => 20120207171753) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(:version => 20120124111514) do
     t.datetime "updated_at"
     t.string   "http_referer"
     t.string   "user_agent"
-    t.text     "data",                   :limit => 16777215
+    t.text     "data",                   :limit => 2147483647
     t.string   "controller_name"
   end
 
