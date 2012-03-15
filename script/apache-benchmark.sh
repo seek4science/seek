2051177d4115f99cc58947745a2bf4b0cba58484
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-03-15 16:55:50 +0000

    minor change to apache benchmark script

--- script/apache-benchmark.sh
+++ script/apache-benchmark.sh
@@ -11,10 +11,10 @@ stty echo
 echo
 
 read -p "Path: " url
-read -p "Number of requests" n
+read -p "Number of requests: " n
 
 
-for c in 2
+for c in 1 5 10
 do
 	ab -n $n -c $c -A$username:$password $url
 done
