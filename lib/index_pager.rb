5aabe392fa58f62a8ac423e7cfbbcf46620cd417
quyen <unknown>
2012-03-16 19:40:26 +0100

    cache for the whole collection of items

--- lib/index_pager.rb
+++ lib/index_pager.rb
@@ -11,25 +11,27 @@ module IndexPager
     
     if !objects.empty? && Authorization::authorization_supported?(objects.first)
       authorized=[]
-      auth_pages={}      
-      objects.each do |object|
-        if params[:page]=="all"
-          authorized << object if object.can_view?
-        #In the next 2 cases we need to authorize at most one item for the non displayed pages,so we keep a hash and skip over any once we've collected
-        #one for that page. Then paginate_after_fetch will then contain a page_total of 1 for the other pages, so that it is enabled in the view. 
-        #I don't want to put this logic in the GroupedPagination library, as I wish to keep it authorization agostic and record correct page_totals in normal use.
-        elsif params[:page]=="latest" && (authorized.size<model_class.latest_limit || auth_pages[object.first_letter].nil?)          
-          if object.can_view?
-            authorized << object
-            auth_pages[object.first_letter]=true
-          end          
-        elsif model_class.pages.include?(object.first_letter) && (object.first_letter == params[:page] || auth_pages[object.first_letter].nil?)                    
-          if object.can_view?
-            authorized << object
-            auth_pages[object.first_letter]=true
-          end                             
+      auth_pages={}
+      if params[:page]=="all"
+        authorized |= authorized_objects objects, 'view'
+      else
+        objects.each do |object|
+          #In the next 2 cases we need to authorize at most one item for the non displayed pages,so we keep a hash and skip over any once we've collected
+          #one for that page. Then paginate_after_fetch will then contain a page_total of 1 for the other pages, so that it is enabled in the view.
+          #I don't want to put this logic in the GroupedPagination library, as I wish to keep it authorization agostic and record correct page_totals in normal use.
+          if params[:page]=="latest" && (authorized.size<model_class.latest_limit || auth_pages[object.first_letter].nil?)
+            if object.can_view?
+              authorized << object
+              auth_pages[object.first_letter]=true
+            end
+          elsif model_class.pages.include?(object.first_letter) && (object.first_letter == params[:page] || auth_pages[object.first_letter].nil?)
+            if object.can_view?
+              authorized << object
+              auth_pages[object.first_letter]=true
+            end
+          end
         end
-      end      
+      end
       objects=authorized
     end
     
@@ -41,6 +43,17 @@ module IndexPager
       format.xml
     end
     
+  end
+
+  def authorized_objects objects, action
+    person = User.current_user.try(:person)
+    key = person.nil? ? ['anonymous_user'] : person.generate_person_key
+    key << action
+    key << objects.first.class.name
+    authorized_object_ids =  Rails.cache.fetch(key){
+      objects.select{|o| o.send "can_#{action}?"}.collect(&:id)
+    }
+    objects.select{|o| authorized_object_ids.include?(o.id)}
   end
   
   def find_assets    
