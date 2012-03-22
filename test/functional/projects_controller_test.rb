b60b0a87b62c0a4b028c5fe2cecf23848302769d
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-03-22 14:54:05 +0000

    fixed project controller tests due to change in wording

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -199,7 +199,7 @@ class ProjectsControllerTest < ActionController::TestCase
     asset_manager = Factory(:asset_manager)
     get :show,:id=>asset_manager.projects.first
 		assert_select "div.box_about_actor p.asset_managers" do
-			assert_select "label",:text=>"SysMO-DB Asset Managers:",:count=>1
+			assert_select "label",:text=>"Asset Managers:",:count=>1
 			assert_select "a",:count=>1
 			assert_select "a[href=?]",person_path(asset_manager),:text=>asset_manager.name,:count=>1
 		end
@@ -209,7 +209,7 @@ class ProjectsControllerTest < ActionController::TestCase
 		project_manager = Factory(:project_manager)
     get :show,:id=>project_manager.projects.first
 		assert_select "div.box_about_actor p.project_managers" do
-			assert_select "label",:text=>"SysMO-DB Project Managers:",:count=>1
+			assert_select "label",:text=>"Project Managers:",:count=>1
 			assert_select "a",:count=>1
 			assert_select "a[href=?]",person_path(project_manager),:text=>project_manager.name,:count=>1
 		end
@@ -219,7 +219,7 @@ class ProjectsControllerTest < ActionController::TestCase
     publisher = Factory(:publisher)
     get :show, :id => publisher.projects.first
     assert_select "div.box_about_actor p.publishers" do
-      assert_select "label", :text => "SysMO-DB Publishers:", :count => 1
+      assert_select "label", :text => "Publishers:", :count => 1
       assert_select "a", :count => 1
       assert_select "a[href=?]", person_path(publisher), :text => publisher.name, :count => 1
     end
@@ -246,7 +246,7 @@ class ProjectsControllerTest < ActionController::TestCase
 		project = Factory(:project)
     get :show,:id=>project
 		assert_select "div.box_about_actor p.asset_managers" do
-			assert_select "label",:text=>"SysMO-DB Asset Managers:",:count=>1
+			assert_select "label",:text=>"Asset Managers:",:count=>1
 			assert_select "a",:count=>0
 			assert_select "span.none_text",:text=>"No Asset Managers for this project",:count=>1
 		end
@@ -256,7 +256,7 @@ class ProjectsControllerTest < ActionController::TestCase
 		project = Factory(:project)
     get :show,:id=>project
 		assert_select "div.box_about_actor p.project_managers" do
-			assert_select "label",:text=>"SysMO-DB Project Managers:",:count=>1
+			assert_select "label",:text=>"Project Managers:",:count=>1
 			assert_select "a",:count=>0
 			assert_select "span.none_text",:text=>"No Project Managers for this project",:count=>1
 		end
@@ -266,7 +266,7 @@ class ProjectsControllerTest < ActionController::TestCase
 		project = Factory(:project)
     get :show,:id=>project
 		assert_select "div.box_about_actor p.publishers" do
-			assert_select "label",:text=>"SysMO-DB Publishers:",:count=>1
+			assert_select "label",:text=>"Publishers:",:count=>1
 			assert_select "a",:count=>0
 			assert_select "span.none_text",:text=>"No Publishers for this project",:count=>1
 		end
