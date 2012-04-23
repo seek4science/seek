6ea8a393adbb854bebc8536bf15e1eceb6dd1500
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-04-23 14:36:08 +0100

    use lookup table for all index pages now, instead of just data files

--- app/controllers/data_files_controller.rb
+++ app/controllers/data_files_controller.rb
@@ -13,7 +13,7 @@ class DataFilesController < ApplicationController
 
   #before_filter :login_required
   
-  #before_filter :find_assets, :only => [ :index ]
+  before_filter :find_assets, :only => [ :index ]
   before_filter :find_and_auth, :except => [ :index, :new, :upload_for_tool, :create, :request_resource, :preview, :test_asset_url, :update_annotations_ajax]
   before_filter :find_display_asset, :only=>[:show,:download,:explore]
 
--- lib/index_pager.rb
+++ lib/index_pager.rb
@@ -1,7 +1,6 @@
 module IndexPager    
 
   def index
-    raise Exception.new
     controller = self.controller_name.downcase    
     model_name=controller.classify
     model_class=eval(model_name)
@@ -10,29 +9,29 @@ module IndexPager
     @hidden=0
     params[:page] ||= model_class.default_page
     
-    if !objects.empty? && Authorization::authorization_supported?(objects.first)
-      authorized=[]
-      auth_pages={}      
-      objects.each do |object|
-        if params[:page]=="all"
-          authorized << object if object.can_view?
-        #In the next 2 cases we need to authorize at most one item for the non displayed pages,so we keep a hash and skip over any once we've collected
-        #one for that page. Then paginate_after_fetch will then contain a page_total of 1 for the other pages, so that it is enabled in the view. 
-        #I don't want to put this logic in the GroupedPagination library, as I wish to keep it authorization agostic and record correct page_totals in normal use.
-        elsif params[:page]=="latest" && (authorized.size<model_class.latest_limit || auth_pages[object.first_letter].nil?)          
-          if object.can_view?
-            authorized << object
-            auth_pages[object.first_letter]=true
-          end          
-        elsif model_class.pages.include?(object.first_letter) && (object.first_letter == params[:page] || auth_pages[object.first_letter].nil?)                    
-          if object.can_view?
-            authorized << object
-            auth_pages[object.first_letter]=true
-          end                             
-        end
-      end      
-      objects=authorized
-    end
+    #if !objects.empty? && Authorization::authorization_supported?(objects.first)
+    #  authorized=[]
+    #  auth_pages={}
+    #  objects.each do |object|
+    #    if params[:page]=="all"
+    #      authorized << object if object.can_view?
+    #    #In the next 2 cases we need to authorize at most one item for the non displayed pages,so we keep a hash and skip over any once we've collected
+    #    #one for that page. Then paginate_after_fetch will then contain a page_total of 1 for the other pages, so that it is enabled in the view.
+    #    #I don't want to put this logic in the GroupedPagination library, as I wish to keep it authorization agostic and record correct page_totals in normal use.
+    #    elsif params[:page]=="latest" && (authorized.size<model_class.latest_limit || auth_pages[object.first_letter].nil?)
+    #      if object.can_view?
+    #        authorized << object
+    #        auth_pages[object.first_letter]=true
+    #      end
+    #    elsif model_class.pages.include?(object.first_letter) && (object.first_letter == params[:page] || auth_pages[object.first_letter].nil?)
+    #      if object.can_view?
+    #        authorized << object
+    #        auth_pages[object.first_letter]=true
+    #      end
+    #    end
+    #  end
+    #  objects=authorized
+    #end
     
     objects=model_class.paginate_after_fetch(objects, :page=>params[:page]) unless objects.respond_to?("page_totals")
     eval("@"+controller+"= objects")
@@ -47,7 +46,7 @@ module IndexPager
   def find_assets    
     controller = self.controller_name.downcase
     model_class=controller.classify.constantize
-    found = model_class.find(:all)
+    found = model_class.all_authorized_for "view",User.current_user
     found = apply_filters(found)
     
     eval("@" + controller + " = found")
