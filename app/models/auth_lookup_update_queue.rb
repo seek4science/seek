82c758957a65edb10f75510cf5de537de273c738
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-04-25 10:38:57 +0100

    fixed exists? method

--- app/models/auth_lookup_update_queue.rb
+++ app/models/auth_lookup_update_queue.rb
@@ -2,7 +2,7 @@ class AuthLookupUpdateQueue < ActiveRecord::Base
   belongs_to :item, :polymorphic=>:true
 
   def self.exists?(item)
-    !AuthLookupUpdateQueue.find(:first,:conditions=>{:item=>item}).nil?
+    !AuthLookupUpdateQueue.find(:first,:conditions=>{:item_id=>item.id,:item_type=>item.class.name}).nil?
   end
 
 end
