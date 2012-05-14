f7b2367c6b196c8b2dcc80f87b44737926a2de66
Lihua An <lihua.an@h-its.org>
2012-05-14 16:18:08 +0200

    fix samples controller test: projects are required(for SysMO only)

--- test/functional/samples_controller_test.rb
+++ test/functional/samples_controller_test.rb
@@ -141,6 +141,7 @@ fixtures :all
       post :create, :sample => {:title=>"test",
                                 :lab_internal_number =>"Do232",
                                 :donation_date => Date.today,
+                                 :project_ids =>[Factory(:project).id],
                                 :specimen => Factory(:specimen, :contributor => User.current_user)},
              :sample_data_file_ids => [Factory(:data_file,:title=>"testDF",:contributor=>User.current_user).id],
              :sample_model_ids => [Factory(:model,:title=>"testModel",:contributor=>User.current_user).id],
