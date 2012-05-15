e8784fe31b86d7c7be944e00b90768714188b3e8
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-05-15 12:10:26 +0100

    added some unit testing for authlookup job and queues

--- test/unit/auth_lookup_update_queue_test.rb
+++ test/unit/auth_lookup_update_queue_test.rb
@@ -1,8 +1,37 @@
 require 'test_helper'
 
 class AuthLookupUpdateQueueTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @val = Seek::Config.auth_lookup_enabled
+    Seek::Config.auth_lookup_enabled=true
+    AuthLookupUpdateQueue.destroy_all
+    Delayed::Job.destroy_all
+  end
+
+  def teardown
+    Seek::Config.auth_lookup_enabled=@val
+  end
+
+  test "exists" do
+    sop = Factory :sop
+    model = Factory :model
+    AuthLookupUpdateQueue.destroy_all
+    assert !AuthLookupUpdateQueue.exists?(sop)
+    assert !AuthLookupUpdateQueue.exists?(model)
+    assert !AuthLookupUpdateQueue.exists?(nil)
+
+    disable_authorization_checks do
+      AuthLookupUpdateQueue.create :item=>sop
+    end
+
+    assert AuthLookupUpdateQueue.exists?(sop)
+    assert !AuthLookupUpdateQueue.exists?(model)
+    assert !AuthLookupUpdateQueue.exists?(nil)
+
+    AuthLookupUpdateQueue.create :item=>nil
+
+    assert AuthLookupUpdateQueue.exists?(sop)
+    assert !AuthLookupUpdateQueue.exists?(model)
+    assert AuthLookupUpdateQueue.exists?(nil)
   end
 end
