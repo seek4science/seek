5661b3bdce60c49234730e6a4ecc51d68637e24f
David Shockley <unknown>
2012-05-16 11:37:32 -0400

    remove duplicate migration

--- db/migrate/20120511134443_change_sex_from_boolean_to_integer.rb
+++
@@ -1,9 +0,0 @@
-class ChangeSexFromBooleanToInteger < ActiveRecord::Migration
-  def self.up
-    change_column :specimens, :sex, :integer
-  end
-
-  def self.down
-    change_column :specimens, :sex, :boolean
-  end
-end
