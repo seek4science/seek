8880ed0e15c4315c98452a96971911772d804222
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-05-18 11:27:13 +0100

    put the test and development environments back to what they were regarding caching policies

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,10 +11,7 @@ config.whiny_nils = true
 
 # Show full error reports and disable caching
 config.action_controller.consider_all_requests_local = true
-#config.action_controller.perform_caching             = false
-
-config.action_controller.perform_caching             = true
-config.action_controller.cache_store = [:file_store, "#{RAILS_ROOT}/tmp/cache"]
+config.action_controller.perform_caching             = false
 
 # Disable request forgery protection in test environment
 config.action_controller.allow_forgery_protection    = false
