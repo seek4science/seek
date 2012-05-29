c070bc59355ac71949aebbecb2cbb334c97ee218
Lihua An <lihua.an@h-its.org>
2012-05-29 12:16:39 +0200

    remove DataFileWithSample in the util  test

--- test/unit/util_test.rb
+++ test/unit/util_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UtilTest < ActiveSupport::TestCase
 
   test "creatable types" do
-    assert_equal [DataFile,Model,Presentation,Publication,Sop,Assay,Investigation,Study,DataFileWithSample,Event,Sample],Seek::Util.user_creatable_types
+    assert_equal [DataFile,Model,Presentation,Publication,Sop,Assay,Investigation,Study,Event,Sample],Seek::Util.user_creatable_types
   end
 
   test "authorized types" do
