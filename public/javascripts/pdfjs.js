a2b69cc9f03e5d3de50b6c66f28fd3d9f3fc8ac8
quyen <unknown>
2012-06-11 11:38:20 +0200

    pdf inline only works on  html5 supporting browser

--- public/javascripts/pdfjs.js
+++ public/javascripts/pdfjs.js
@@ -1,23 +1,32 @@
 function viewPDFContent(url) {
-    PDFJS.disableWorker = true;
-    'use strict';
+    if (isCanvasSupportBrowser){
+        PDFJS.disableWorker = true;
+        'use strict';
 
-    pdfDoc = null;
-    pageNum = 1;
-    scale = 1;
-    document.getElementById('pdf_content_display').style['display'] = 'block';
-    canvas = document.getElementById('pdf-display-canvas');
-    ctx = canvas.getContext('2d');
+        pdfDoc = null;
+        pageNum = 1;
+        scale = 1;
+        document.getElementById('pdf_content_display').style['display'] = 'block';
+        canvas = document.getElementById('pdf-display-canvas');
+        ctx = canvas.getContext('2d');
 
-    //
-    // Asynchronously download PDF as an ArrayBuffer
-    //
-    show_large_ajax_loader("ajax_loader");
-    PDFJS.getDocument(url).then(function getPdfHelloWorld(_pdfDoc) {
-        pdfDoc = _pdfDoc;
-        renderPage(pageNum);
-    });
+        //
+        // Asynchronously download PDF as an ArrayBuffer
+        //
+        show_large_ajax_loader("ajax_loader");
+        PDFJS.getDocument(url).then(function getPdfHelloWorld(_pdfDoc) {
+            pdfDoc = _pdfDoc;
+            renderPage(pageNum);
+        });
+    }else{
+        alert('Your browser does not support html5 to view the pdf file inline, please upgrade the browser')
+    }
+}
 
+function isCanvasSupportBrowser(){
+    var test_canvas = document.createElement("canvas") //try and create sample canvas element
+    var canvas_check=(test_canvas.getContext)? true : false //check if object supports getContext() method, a method of the canvas element
+    return canvas_check;
 }
 
 function hidePDFContent(){
