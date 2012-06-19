fe71da45f4f7b3b92963351be8947ab8664f7efd
quyen <unknown>
2012-06-19 16:56:08 +0200

    only add activity log to queue for limited actions

--- lib/send_subscriptions_when_activity_logged.rb
+++ lib/send_subscriptions_when_activity_logged.rb
@@ -2,7 +2,7 @@ ActivityLog.class_eval do
   after_create :send_notification
 
   def send_notification
-    if Seek::Config.email_enabled
+    if Seek::Config.email_enabled && activity_loggable.subscribers_are_notified_of?(activity_log.action)
       SubscriptionJob.add_items_to_queue self.id
     end
   end
