89b473999c5f87c998daa31c6ef89360bbc1deae
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-07-02 12:00:36 +0100

    handle exception when sending emails to each individual person, to allow it to continue to send to subsequent people

--- app/jobs/send_periodic_emails_job.rb
+++ app/jobs/send_periodic_emails_job.rb
@@ -35,14 +35,18 @@ class SendPeriodicEmailsJob < Struct.new(:frequency)
 
   def send_subscription_mails logs, frequency
     if Seek::Config.email_enabled
-      Person.scoped(:include => :subscriptions).select{|p|p.receive_notifications?}.each do |person|
-         activity_logs = person.subscriptions.scoped(:include => :subscribable).select{|s|s.frequency == frequency}.collect do |sub|
+      Person.scoped(:include => :subscriptions).select { |p| p.receive_notifications? }.each do |person|
+        begin
+          activity_logs = person.subscriptions.scoped(:include => :subscribable).select { |s| s.frequency == frequency }.collect do |sub|
             logs.select do |log|
               log.activity_loggable.try(:can_view?, person.user) && log.activity_loggable.subscribable? && log.activity_loggable.subscribers_are_notified_of?(log.action) && log.activity_loggable == sub.subscribable
             end
-         end.flatten(1)
-         SubMailer.deliver_send_digest_subscription person, activity_logs, frequency unless activity_logs.blank?
-       end
+          end.flatten(1)
+          SubMailer.deliver_send_digest_subscription person, activity_logs, frequency unless activity_logs.blank?
+        rescue Exception => e
+          Delayed::Job.logger.error("Error sending subscription emails to person #{person.id} - #{e.message}")
+        end
       end
     end
+  end
 end
