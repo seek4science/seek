e84b1d4a4865f03d99944403e188bba16744c282
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-07-16 16:28:33 +0100

    try installing bundler if missing

--- script/build-cruise.sh
+++ script/build-cruise.sh
@@ -3,6 +3,8 @@ source $HOME/.rvm/scripts/rvm
 
 source .rmvrc
 
+gem list --local bundler | grep bundler || gem install bundler || exit 1
+
 bundle check || bundle install &&
 
 bundle exec rake cruise2
