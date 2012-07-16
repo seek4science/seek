96febb65830d2d7683c6acaa446f6e68d04e21c8
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-07-16 16:44:11 +0100

    enhanced cruise build script (for rvm and bundler)  according to example at http://pivotallabs.com/users/mbarinek/blog/articles/1450-rails-3-with-rvm-and-cruise-control

--- script/build-cruise.sh
+++ script/build-cruise.sh
@@ -1,10 +1,41 @@
-#!/bin/sh
-source $HOME/.rvm/scripts/rvm
+#!/bin/bash
 
-rvm use ruby-1.8.7@seek015 --create
+#based on example at http://pivotallabs.com/users/mbarinek/blog/articles/1450-rails-3-with-rvm-and-cruise-control
 
+desired_ruby=ruby-1.8.7
+project_name=seek015
+
+# remove annoying "warning: Insecure world writable dir"
+function remove_annoying_warning() {
+  chmod go-w $HOME/.rvm/gems/${desired_ruby}{,@{global,${project_name}}}{,/bin} 2>/dev/null
+}
+
+# enable rvm for ruby interpreter switching
+source $HOME/.rvm/scripts/rvm || exit 1
+
+# show available (installed) rubies (for debugging)
+rvm list
+
+# install our chosen ruby if necessary
+rvm list | grep $desired_ruby > /dev/null || rvm install $desired_ruby || exit 1
+
+# use our ruby with a custom gemset
+rvm use ${desired_ruby}@${project_name} --create
+remove_annoying_warning
+
+# install bundler if necessary
 gem list --local bundler | grep bundler || gem install bundler || exit 1
 
-bundle check || bundle install &&
+# debugging info
+echo USER=$USER && ruby --version && which ruby && which bundle
 
+# conditionally install project gems from Gemfile
+bundle check || bundle install || exit 1
+
+# remove the warning again after we've created all the gem directories
+remove_annoying_warning
+
+# finally, run rake
 bundle exec rake cruise2
+
+
