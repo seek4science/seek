a9f2a94cb9bd0ca6bcc0cced58eb11728a6e0c4c
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-07-16 13:56:49 +0100

    require thread to allow use of later versions of rubygems

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,8 @@
 # Don't change this file!
 # Configure your app in config/environment.rb and config/environments/*.rb
 
+require 'thread'
+
 RAILS_ROOT = "#{File.dirname(__FILE__)}/.." unless defined?(RAILS_ROOT)
 
 module Rails
