49eed8797375cee40a8ff140ae29eecf3c95a48e
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-07-20 15:50:30 +0100

    fixed the code for determining the frequency for subscriptions not linked to project_subscriptions, and made it clearer to understand

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -22,12 +22,24 @@ class Subscription < ActiveRecord::Base
     end
   end
 
-  #TODO: add a way for the user to set a frequency for projects they don't subscribe to.
-  def generic_frequency
-    ProjectSubscription.find_all_by_person_id(person_id).map(&:frequency).inject('weekly') {|slowest, current|  FREQUENCIES.index(current) > FREQUENCIES.index(slowest) ? current : slowest}
+  #uses the frequency based upon the related project_subscription, however,
+  #if this item was subscribed to individually (i.e with no project or frequency), then it makes an intelligent guess based on other project subscriptions
+  #as a last resort, the frequency will be weekly
+  def frequency
+    project_subscription.try(:frequency) || fastest_related_project_subscription_frequency || slowest_fallback_related_frequency || "weekly"
   end
 
-  def frequency
-    project_subscription.try(:frequency) || generic_frequency
+  private
+
+  #picks out the most frequent frequency that this person has for the projects related to this subscribable
+  def fastest_related_project_subscription_frequency
+    proj_subs = subscribable.projects.collect{|p|ProjectSubscription.find_by_person_id_and_project_id person_id, p.try(:id) }.compact
+    proj_subs.collect(&:frequency).inject {|fastest, current| FREQUENCIES.index(current) < FREQUENCIES.index(fastest) ? current : fastest}
+  end
+
+  #TODO: add a way for the user to set a frequency for projects they don't subscribe to.
+  #final fall back for when the person of the subscribable is not subscribed to any related projects - just picks their slowest frequency subscription of all project subscriptions
+  def slowest_fallback_related_frequency
+    ProjectSubscription.find_all_by_person_id(person_id).map(&:frequency).inject{|slowest, current|  FREQUENCIES.index(current) > FREQUENCIES.index(slowest) ? current : slowest}
   end
 end
