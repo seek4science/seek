8aaaa43a8f7cd2faeafaa6c809a12291df574cad
quyen <unknown>
2012-07-25 16:30:46 +0200

    add tests for showing/not showing the publish request notice in preview permissions popup

--- test/functional/policies_controller_test.rb
+++ test/functional/policies_controller_test.rb
@@ -82,6 +82,21 @@ class PoliciesControllerTest < ActionController::TestCase
       assert_select "p",:text=>"#{asset_manager.name} can manage as an asset manager", :count=>0
   end
 
+  test 'should show notice message when an item is requested to be published' do
+      gatekeeper = Factory(:gatekeeper)
+      sop = Factory(:sop)
+      login_as(sop.contributor)
+      post :preview_permissions, :sharing_scope => Policy::EVERYONE, :access_type => Policy::VISIBLE, :is_new_file => "false", :resource_name => 'sop', :resource_id => sop.id,:project_ids => gatekeeper.projects.first.id.to_s
+
+      assert_select "span",:text=>"(One email will be sent to the gatekeepers of the projects associated with this SOP to ask for publishing approval. This SOP is not published until one of the gatekeeper approves)", :count=>1
+  end
+
+  test 'should not show notice message when an item can be published right away' do
+      post :preview_permissions, :sharing_scope => Policy::EVERYONE, :access_type => Policy::VISIBLE, :is_new_file => "true", :resource_name => 'sop', :project_ids => Factory(:project).id.to_s
+
+      assert_select "span",:text=>"(One email will be sent to the gatekeepers of the projects associated with this SOP to ask for publishing approval. This SOP is not published until one of the gatekeeper approves)", :count=>0
+  end
+
   test 'when creating an item, can not publish the item if associate to it the project which has gatekeeper' do
       gatekeeper = Factory(:gatekeeper)
       a_person = Factory(:person)
