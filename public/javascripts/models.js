1a8aa158dc0424eefd6108a888b6e688774f9a5f
quyen <unknown>
2012-07-31 15:53:08 +0200

    ignore case sensitive when checking image file format

--- public/javascripts/models.js
+++ public/javascripts/models.js
@@ -64,7 +64,7 @@ function model_format_selection_changed() {
 function detectFileFormat(image_id){
     var file_path_array = $(image_id).value.split('.');
     var file_format = file_path_array[file_path_array.length -1];
-    if (file_format != 'jpg' && file_format != 'gif' && file_format != 'png'){
+    if (file_format.toLowerCase() != 'jpg'.toLowerCase() && file_format.toLowerCase() != 'gif'.toLowerCase() && file_format.toLowerCase() != 'png'.toLowerCase()){
         $(image_id).value = '';
         alert('Only jpg, gif and png formats are supported.');
     }
