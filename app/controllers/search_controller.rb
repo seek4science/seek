36b30b992ce9a495c3b07395de87bf40d9e74617
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-08-01 13:14:22 +0100

    checkbox to select that external search should be included or not

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -71,7 +71,7 @@ class SearchController < ApplicationController
   private
 
   def include_external_search?
-    Seek::Config.external_search_enabled
+    Seek::Config.external_search_enabled && params[:include_external_search]
   end
 
   #Removes all results from the search results collection passed in that are not Authorised to show for the current user (if one is logged in)
