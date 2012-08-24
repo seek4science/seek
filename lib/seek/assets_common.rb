5b8f33ffb37ab4df65269af4baa9d2c2ec6ca68e
quyen <unknown>
2012-08-24 13:49:11 +0200

    use is_content_viewable? instead

--- lib/seek/assets_common.rb
+++ lib/seek/assets_common.rb
@@ -290,7 +290,7 @@ module Seek
       name = self.controller_name.singularize
       asset = name.camelize.constantize.find(params[:id])
       display_asset = asset.find_version(params[:version])
-      if asset.can_download? && display_asset.can_download?
+      if display_asset.is_content_viewable?
         eval "@#{name} = asset"
         eval "@display_#{name} = display_asset"
       else
