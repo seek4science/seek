84f6d877c1e27ca583c7fae86e8e0f8b57c0c29e
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-08-29 17:42:34 +0100

    fixed additional merge error - remvoed reintroduction of old to_presentation methods

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -159,54 +159,6 @@ class DataFile < ActiveRecord::Base
     flds.flatten.uniq
   end
 
-  def to_presentation!
-    returning self.to_presentation do |presentation|
-      class << presentation
-        #disabling versioning, since I have manually copied the versions of the data file over
-        def save_version_on_create
-        end
-
-        def set_new_version
-          self.version = DataFile.find(self.orig_data_file_id).version
-        end
-      end
-
-      #TODO: should we throw an exception if the user isn't authorized to make these changes?
-      if User.current_user.admin? or self.can_delete?
-        disable_authorization_checks {
-          presentation.save!
-          #TODO: perhaps the deletion of the data file should also be here? We are already throwing an exception if save fails for some reason
-        }
-      end
-
-      #copying annotations has to be done after saving the presentation due to limitations of the annotation plugin
-      disable_authorization_checks do #disabling because annotations should be copied over even if the user would normally lack permission to do so
-        presentation.annotations = self.annotations
-      end
-    end
-  end
-
-  def to_presentation
-    presentation_attrs = attributes.delete_if { |k, v| !Presentation.new.attributes.include? k}
-
-    returning Presentation.new(presentation_attrs) do |presentation|
-      DataFile.reflect_on_all_associations.select { |a| [:has_many, :has_and_belongs_to_many, :has_one].include?(a.macro) && !a.through_reflection }.each do |a|
-        #disabled, because even if the user doing the conversion would not normally
-        #be able to associate an item with his data_file/presentation, the pre-existing
-        #association created by someone who was allowed, should carry over to the presentation
-        #based on the data file.
-        disable_authorization_checks do
-          #annotations and versions have to be handled specially
-          presentation.send("#{a.name}=", send(a.name)) if presentation.respond_to?("#{a.name}=") and a.name != :annotations and a.name != :versions
-        end
-      end
-
-      disable_authorization_checks { presentation.versions = versions.map(&:to_presentation_version) }
-      presentation.policy = policy.deep_copy
-      presentation.orig_data_file_id = id
-    end
-  end
-
   def to_presentation
     presentation_attrs = attributes.delete_if { |k, v| !Presentation.new.attributes.include? k}
 
