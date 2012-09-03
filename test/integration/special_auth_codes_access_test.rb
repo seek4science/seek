44d85a70c2d8fe356d1bf3bd689481d195c2a432
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-09-03 10:11:40 +0100

    removed some debugging trace from tests

--- test/integration/special_auth_codes_access_test.rb
+++ test/integration/special_auth_codes_access_test.rb
@@ -17,7 +17,6 @@ class SpecialAuthCodesAccessTest < ActionController::IntegrationTest
     User.current_user = Factory(:user, :login => 'test')
     post '/sessions/create', :login => 'test', :password => 'blah'
     ASSETS_WITH_AUTH_CODES.each do |type_name|
-      p type_name
       get "/#{type_name}/new"
       assert_select "form div#temporary_links", :count => 0
 
@@ -30,7 +29,6 @@ class SpecialAuthCodesAccessTest < ActionController::IntegrationTest
     ASSETS_WITH_AUTH_CODES.each do |type_name|
       item = Factory(type_name.singularize.to_sym, :policy => Factory(:private_policy))
       item.special_auth_codes << Factory(:special_auth_code, :asset => item)
-      p type_name
 
       code = CGI::escape(item.special_auth_codes.first.code)
       get "/#{type_name}/show/#{item.id}?code=#{code}"
