c0c36ef3a1d163cbcb06b4d7484f0b3c635f8b26
Stuart Owen <stuart.owen@manchester.ac.uk>
2012-09-06 12:22:36 +0100

    forced migration to run to fix schema version

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120628164147) do
+ActiveRecord::Schema.define(:version => 20120803084456) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
