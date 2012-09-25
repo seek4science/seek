0b2362bea74788a7785bc93e50d9eb88f3a15237
bittkomk <unknown>
2012-09-25 10:35:10 +0200

    forgot to add migration file, updated associations of treatments

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,6 +1,13 @@
 class Treatment < ActiveRecord::Base
+
   belongs_to :sample
+  belongs_to :specimen
+  belongs_to :compound
   belongs_to :unit
+
+  belongs_to :incubation_time_unit, :class_name => "Unit", :foreign_key => "incubation_time_unit_id"
+  belongs_to :type, :class_name => "MeasuredItem", :foreign_key => "type"
+
 
   TREATMENT_PROTOCOLS = ["NO PERTURBATION","PERTURBATION ROTENONE FISH","PERTURBATION ROTENONE MOUSE","PERTURBATION ROTENONE WORM","PERTURBATION ROTENONE FIBROBLAST"]
 end
