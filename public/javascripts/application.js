210d3ea02c3d36f5457299bca9ea8e713338da00
quyen <unknown>
2012-10-09 12:01:46 +0200

    move clickLink to application.js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -182,3 +182,23 @@ function onlyNumbers(evt)
 
     return true;
  }
+
+//perform the click link action
+function clickLink(link) {
+    var cancelled = false;
+    if (document.createEvent) {
+        var event = document.createEvent("MouseEvents");
+        event.initMouseEvent("click", true, true, window,
+            0, 0, 0, 0, 0,
+            false, false, false, false,
+            0, null);
+        cancelled = !link.dispatchEvent(event);
+    }
+    else if (link.fireEvent) {
+        cancelled = !link.fireEvent("onclick");
+    }
+
+    if (!cancelled) {
+        window.location = link.href;
+    }
+}
--- public/javascripts/resource.js
+++ public/javascripts/resource.js
@@ -55,25 +55,6 @@ function checkProjectExists(prefix) {
     return(true);
 }
 
-function clickLink(link) {
-    var cancelled = false;
-    if (document.createEvent) {
-        var event = document.createEvent("MouseEvents");
-        event.initMouseEvent("click", true, true, window,
-            0, 0, 0, 0, 0,
-            false, false, false, false,
-            0, null);
-        cancelled = !link.dispatchEvent(event);
-    }
-    else if (link.fireEvent) {
-        cancelled = !link.fireEvent("onclick");
-    }
-
-    if (!cancelled) {
-        window.location = link.href;
-    }
-}
-
 function createOrUpdateResource(is_new_file, resource_name){
     // filename and title set - can submit
     $(resource_name + '_submit_btn').disabled = true;
