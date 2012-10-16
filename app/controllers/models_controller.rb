018385de8f9d390a534bde2b4c0930b12b15dd20
quyen <unknown>
2012-10-16 12:22:42 +0200

    rename method send_image by send_model_image

--- app/controllers/models_controller.rb
+++ app/controllers/models_controller.rb
@@ -13,7 +13,7 @@ class ModelsController < ApplicationController
   
   before_filter :find_assets, :only => [ :index ]
   before_filter :find_and_auth, :except => [ :build,:index, :new, :create,:create_model_metadata,:update_model_metadata,:delete_model_metadata,:request_resource,:preview,:test_asset_url, :update_annotations_ajax]
-  before_filter :find_display_asset, :only=>[:show,:download,:execute,:builder,:simulate,:submit_to_jws,:matching_data,:visualise,:export_as_xgmml,:send_image]
+  before_filter :find_display_asset, :only=>[:show,:download,:execute,:builder,:simulate,:submit_to_jws,:matching_data,:visualise,:export_as_xgmml,:send_model_image]
     
   before_filter :jws_enabled,:only=>[:builder,:simulate,:submit_to_jws]
 
@@ -52,7 +52,7 @@ class ModelsController < ApplicationController
      render :cytoscape_web,:layout => false
   end
 
-  def send_image
+  def send_model_image
       image = @display_model.model_image
       send_file "#{image.file_path  }", :type=>"JPEG", :disposition=>'inline'
   end
