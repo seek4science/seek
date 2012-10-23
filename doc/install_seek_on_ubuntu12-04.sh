ee0c4f25046f93347664fed1391bd122fc1e528c
quyen <unknown>
2012-10-23 15:28:03 +0200

    point to correct google code version

--- doc/install_seek_on_ubuntu12-04.sh
+++ doc/install_seek_on_ubuntu12-04.sh
@@ -6,7 +6,7 @@
 
 SEEK_PATH="/home/$(whoami)" 
 SEEK_DIRECTORY="seek"
-REPOSITORY="https://sysmo-db.googlecode.com/hg/ -r v0.12.2"
+REPOSITORY="https://sysmo-db.googlecode.com/hg/ -r v0.15.1"
 
 txtgrn=$(tput setaf 2) # Green
 txtrst=$(tput sgr0) # Text reset
