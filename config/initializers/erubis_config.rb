790e3cbdb18f5e60146ba5fa726848582fd74fc1
bittkomk <unknown>
2012-11-22 15:59:04 +0100

    removed initializer file for erubis -- not needed anymore

--- config/initializers/erubis_config.rb
+++
@@ -1,5 +0,0 @@
-require 'erubis/helpers/rails_helper'
-
-Erubis::Helpers::RailsHelper.engine_class = Erubis::Eruby
-Erubis::Helpers::RailsHelper.preprocessing = true
-Erubis::Helpers::RailsHelper.init_properties = {:bufvar => '@output_buffer'}
