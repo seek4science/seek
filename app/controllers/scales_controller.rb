2cee2794e5cf74a1c6416e06aa3759f213dbf8e7
quyen <unknown>
2012-11-26 11:15:05 +0100

    only use collection authorization for scaled items when the lookup table is consistent

--- app/controllers/scales_controller.rb
+++ app/controllers/scales_controller.rb
@@ -35,9 +35,8 @@ class ScalesController < ApplicationController
 
     resource_hash.each do |key,res|
       unless res[:items].empty?
-        all_authorized_items = key.constantize.all_authorized_for('view')
         total_count = res[:items].size
-        res[:items] = res[:items] & all_authorized_items
+        res[:items] = authorized_related_items res[:items], key
         res[:hidden_count] = total_count - res[:items].size
       end
     end
@@ -54,6 +53,22 @@ class ScalesController < ApplicationController
 
    end
 
+  #FIXME: refractor this method with the method in assets_helper
+  def authorized_related_items related_items, item_type
+    user_id = current_user.nil? ? 0 : current_user.id
+    assets = []
+    authorized_related_items = []
+    lookup_table_name = item_type.underscore + 'auth_lookup'
+    asset_class = item_type.constantize
+    if (asset_class.lookup_table_consistent?(user_id))
+      Rails.logger.info("Lookup table #{lookup_table_name} used for authorizing related items is complete for user_id = #{user_id}")
+      assets = asset_class.lookup_for_action_and_user 'view', user_id, nil
+      authorized_related_items = assets & related_items
+    else
+      authorized_related_items = related_items.select(&:can_view?)
+    end
+    authorized_related_items
+  end
 
   private
 
