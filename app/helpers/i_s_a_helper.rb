5c3f60f26435aaef3d59c0cff56cffa4173b5977
quyen <unknown>
2013-01-04 14:50:28 +0100

    dont display svg if only one node

--- app/helpers/i_s_a_helper.rb
+++ app/helpers/i_s_a_helper.rb
@@ -32,7 +32,10 @@ module ISAHelper
       current_item||=root_item
       html = '<script src="/javascripts/svg/svg.js" data-path="/javascripts/svg/"></script>'
       html << "\n"
-      html << "<div id='isa_svg'><script type=\'image/svg+xml'>#{to_svg(root_item,deep,current_item)}</script></div>"
+      svg = to_svg(root_item,deep,current_item)
+      unless  svg.blank?
+        html << "<div id='isa_svg'><script type=\'image/svg+xml'>#{svg}</script></div>"
+      end
       html
     rescue Exception=>e
       "<div id='isa_svg' class='none_text'>Currently unable to display the graph for this item</div>"
