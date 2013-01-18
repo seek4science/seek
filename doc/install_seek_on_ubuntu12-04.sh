ca195adbcada4a228d7389f37ee627409f7ec11c
quyen <unknown>
2013-01-18 17:29:52 +0100

    updating the install_seek script to install rubygem 1.6.2

--- doc/install_seek_on_ubuntu12-04.sh
+++ doc/install_seek_on_ubuntu12-04.sh
@@ -25,11 +25,12 @@ sudo apt-get install libmysqlclient-dev libmagick++-dev libxslt1-dev libxml++2.6
 echo "${txtgrn} *********************************** ${txtrst}"
 echo "${txtgrn} Installing rubygems ${txtrst}"
 cd /tmp
-wget http://production.cf.rubygems.org/rubygems/rubygems-1.3.7.tgz
-tar zfxv rubygems-1.3.7.tgz
-cd rubygems-1.3.7/
+wget http://production.cf.rubygems.org/rubygems/rubygems-1.6.2.tgz
+tar zfxv rubygems-1.6.2.tgz
+cd rubygems-1.6.2/
 sudo ruby ./setup.rb
 cd /usr/bin/
+
 symboliclink="gem"
 if [ -L "$symboliclink" ]; then
     sudo rm "$symboliclink"
