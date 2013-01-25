ee0f2a6b3d1bdfb45e367a2d4f3cd6b371eaeb57
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-01-25 16:56:08 +0000

    migration to convert download of content-blob activity logs to inline_view. This will include some cases where individual files are downloaded from a multiple set for models, but these are likely to be small in number currently

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130124171456) do
+ActiveRecord::Schema.define(:version => 20130125164227) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
