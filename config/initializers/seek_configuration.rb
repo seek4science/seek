a476972655f23adb9216b140b6687c1be0c36371
savorain <unknown>
2013-02-19 17:06:28 +0100

    Contibutor

--- config/initializers/seek_configuration.rb
+++ config/initializers/seek_configuration.rb
@@ -72,7 +72,7 @@ Seek::Config.default :seek_video_link, "http://www.youtube.com/user/elinawetschH
 Seek::Config.default :sample_parent_term, "cell culture"
 Seek::Config.default :specimen_culture_starting_date,"Culture starting date"
 Seek::Config.default :sample_age, "Age at sampling(hours)"
-Seek::Config.default :specimen_creators, "Creators"
+Seek::Config.default :specimen_creators, "Contributors"
 
 
 Seek::Config.default :max_attachments_num,100
