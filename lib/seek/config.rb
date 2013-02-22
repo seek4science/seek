2072ddf79b94835a25de6d14b5784927d13d89d1
Lihua An <lihua.an@h-its.org>
2013-02-22 15:58:51 +0100

    configurable noreply_sender for ExceptionNotifier

--- lib/seek/config.rb
+++ lib/seek/config.rb
@@ -83,7 +83,7 @@ module Seek
       if self.exception_notification_enabled
         ExceptionNotifier.render_only            = false
         ExceptionNotifier.send_email_error_codes = %W( 400 406 403 405 410 500 501 503 )
-        ExceptionNotifier.sender_address         = %w(no-reply@sysmo-db.org)
+        ExceptionNotifier.sender_address         = [self.noreply_sender]
         ExceptionNotifier.email_prefix           = "[ #{self.application_title} ERROR ] "
         ExceptionNotifier.exception_recipients   = self.exception_notification_recipients.split %r([, ])
       else
