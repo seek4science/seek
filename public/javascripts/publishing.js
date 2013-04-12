e0b6bf01b3c663e15b9a181ae19778fb9479d32a
quyen <unknown>
2013-04-12 14:14:12 +0200

    refractor some javascript methods

--- public/javascripts/publishing.js
+++ public/javascripts/publishing.js
@@ -28,3 +28,57 @@ function getJsonPublishParams() {
     }
     return JSON.stringify(json);
 }
+
+function checkGatekeeperRequired(all_items) {
+    var gatekeeper_required = 'false';
+    var publishing_items = getPublishingItems();
+
+    for (var i = 0; i < publishing_items.length; i++) {
+        var asset_type = publishing_items[i][0];
+        var asset_id = publishing_items[i][1];
+        for (var j = 0; j < all_items.length; j++) {
+            if (asset_type == all_items[j][0] && asset_id == all_items[j][1]) {
+                gatekeeper_required = all_items[j][3];
+                if (gatekeeper_required == 'true')
+                    break;
+            }
+        }
+    }
+
+    if (gatekeeper_required == 'true') {
+        clickLink($('waiting_approval_list'));
+    }
+    else {
+        $('submit_publishing').disable_with = 'Submitting ...';
+        $('submit_publishing').form.submit();
+    }
+}
+
+function checkRelatedItems(all_items) {
+    var publishing_items = getPublishingItems();
+    var contain_related_items = 'false';
+    for (var i = 0; i < publishing_items.length; i++) {
+        var asset_type = publishing_items[i][0];
+        var asset_id = publishing_items[i][1];
+        for (var j = 0; j < all_items.length; j++) {
+            if (asset_type == all_items[j][0] && asset_id == all_items[j][1]) {
+                contain_related_items = all_items[j][2];
+                if (contain_related_items == 'true')
+                    break;
+            }
+        }
+    }
+
+    //confirmation if you wish to publish related items?
+    if (contain_related_items == 'true'){
+        if (confirm("There are items related to this asset. They are ISA items and their assets. Would you like to process publishing them as well?")) {
+            var submit_button = $('submit_publishing');
+            submit_button.disable_with = 'Submitting ...';
+            submit_button.form.action = 'publish_related_items';
+            submit_button.form.method = 'get';
+            $('submit_publishing').form.submit();
+        } else
+            checkGatekeeperRequired(all_items);
+    }else
+        checkGatekeeperRequired(all_items);
+}
