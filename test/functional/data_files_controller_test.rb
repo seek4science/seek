a9cfaa79ec12aadb422322f2dcce0af30a4b50b0
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-04-19 15:20:16 +0100

    added a big download button to the usage info panel if the item is downloadable

--- test/functional/data_files_controller_test.rb
+++ test/functional/data_files_controller_test.rb
@@ -609,7 +609,9 @@ class DataFilesControllerTest < ActionController::TestCase
       assert_select "a",:text=>/Request/,:count=>0
     end
 
-    assert_select "div.contribution_section_box > p.usage_info" do
+    assert_select "div.contribution_section_box div.big_download_button", :count=>0
+
+    assert_select "div.contribution_section_box > div.usage_info" do
       assert_select "b",:text=>/Downloads/,:count=>0
     end
   end
@@ -624,11 +626,17 @@ class DataFilesControllerTest < ActionController::TestCase
       assert_select "a",:text=>/Download Data file/,:count=>1
     end
 
-    assert_select "div.contribution_section_box > p.usage_info" do
+    assert_select "div.contribution_section_box div.big_download_button" do
+      assert_select "a[href=?]",download_data_file_path(df,:version=>df.version)
+    end
+
+    assert_select "div.contribution_section_box > div.usage_info" do
       assert_select "b",:text=>/Downloads/,:count=>1
     end
 
   end
+
+
 
   test "should download datafile from standard route" do
     df = Factory :rightfield_datafile, :policy=>Factory(:public_policy)
