8814e7279ca9b529b148718de05b7d70f068682e
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-05-08 13:24:17 +0100

    fixed module name (was SeekRails3) in generated config files

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-SeekRails3::Application.config.session_store :cookie_store, :key => '_seek-rails3_session'
+#SEEK::Application.config.session_store :cookie_store, :key => '_seek-rails3_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# SeekRails3::Application.config.session_store :active_record_store
+SEEK::Application.config.session_store :active_record_store
