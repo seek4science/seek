9fbff06180a361c97fe83f1f65821e5f7e816af8
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-05-08 11:52:44 +0100

    updated environment files with SEEK::Application.configure wrapping

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,25 +1,27 @@
 # Settings specified here will take precedence over those in config/environment.rb
-
-# The production environment is meant for finished, "live" apps.
-# Code is not reloaded between requests
-config.cache_classes = true
+SEEK::Application.configure do
+  # The production environment is meant for finished, "live" apps.
+  # Code is not reloaded between requests
+  config.cache_classes = true
 
 # Use a different logger for distributed setups
 # config.logger = SyslogLogger.new
 
 # Full error reports are disabled and caching is turned on
-config.action_controller.consider_all_requests_local = false
+  config.action_controller.consider_all_requests_local = false
 
-config.action_controller.perform_caching             = true
-config.action_controller.cache_store = [:file_store, "#{RAILS_ROOT}/tmp/cache"]
-config.cache_store = [:file_store, "#{RAILS_ROOT}/tmp/cache"]
-#config.action_view.cache_template_loading            = true
+  config.action_controller.perform_caching             = true
+  config.action_controller.cache_store = [:file_store, "#{RAILS_ROOT}/tmp/cache"]
+  config.cache_store = [:file_store, "#{Rails.root}/tmp/cache"]
+  #config.action_view.cache_template_loading            = true
 
 
-# Enable serving of images, stylesheets, and javascripts from an asset server
-# config.action_controller.asset_host                  = "http://assets.example.com"
+  # Enable serving of images, stylesheets, and javascripts from an asset server
+  # config.action_controller.asset_host                  = "http://assets.example.com"
 
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
-#
-#
+  # Disable delivery errors, bad email addresses will be ignored
+  # config.action_mailer.raise_delivery_errors = false
+  #
+  #
+end
+
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,28 +1,29 @@
 # Settings specified here will take precedence over those in config/environment.rb
-
-# The test environment is used exclusively to run your application's
-# test suite.  You never need to work with it otherwise.  Remember that
-# your test database is "scratch space" for the test suite and is wiped
-# and recreated between test runs.  Don't rely on the data there!
-config.cache_classes = true
+SEEK::Application.configure do
+  # The test environment is used exclusively to run your application's
+  # test suite.  You never need to work with it otherwise.  Remember that
+  # your test database is "scratch space" for the test suite and is wiped
+  # and recreated between test runs.  Don't rely on the data there!
+  config.cache_classes = true
 
 # Log error messages when you accidentally call methods on nil.
-config.whiny_nils = true
+  config.whiny_nils = true
 
 # Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = false
+  config.action_controller.consider_all_requests_local = true
+  config.action_controller.perform_caching             = false
 
 # Disable request forgery protection in test environment
-config.action_controller.allow_forgery_protection    = false
+  config.action_controller.allow_forgery_protection    = false
 
 # Tell Action Mailer not to deliver emails to the real world.
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
-config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :test
 
 #config.cache_store = [:file_store, "/tmp/seek-test-cache"]
-config.cache_store = :memory_store
+  config.cache_store = :memory_store
 
 # Raise exception on mass assignment protection for Active Record models
-config.active_record.mass_assignment_sanitizer = :strict
+  config.active_record.mass_assignment_sanitizer = :strict
+end
