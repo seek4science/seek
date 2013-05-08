66e4e3402a6d0c85e6aecb03a048c182c582b283
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-05-08 13:21:28 +0100

    removed call to debug_rjs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ SEEK::Application.configure do
 
 # Show full error reports and disable caching
   config.action_controller.consider_all_requests_local = true
-  config.action_view.debug_rjs = true
+
   config.action_controller.perform_caching = false
 
 # Don't care if the mailer can't send
