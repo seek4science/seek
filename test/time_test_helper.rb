4e6e3784cf3ea8e8858c03fddcca6bb28b3afd16
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-05-09 16:46:23 +0100

    refactor to replace obselete attr_accessor_with_default method

--- test/time_test_helper.rb
+++ test/time_test_helper.rb
@@ -5,13 +5,18 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 
 class Time
   class <<self
-    attr_accessor_with_default :testing_offset,0
+    attr_writer :testing_offset
     attr_accessor :fake_now
     alias_method :real_now, :now
     def now
       fake_now || real_now - testing_offset
     end
     alias_method :new, :now
+
+    def testing_offset
+      @testing_offset || 0
+    end
+
   end
 end
 
