e702d9cf8358ea21160126ac8afe9d3b35dcfd93
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-05-09 15:41:22 +0100

    use .table_name= instead of set_table_name

--- lib/explicit_versioning.rb
+++ lib/explicit_versioning.rb
@@ -60,7 +60,7 @@ module Jits
             end
           end
 
-          versioned_class.set_table_name versioned_table_name
+          versioned_class.table_name = versioned_table_name
           versioned_class.belongs_to self.to_s.demodulize.underscore.to_sym,
             :class_name  => "::#{self.to_s}",
             :foreign_key => versioned_foreign_key
