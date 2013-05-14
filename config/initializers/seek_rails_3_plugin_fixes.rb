65b38fa16ca675a089f9aa3c87e8bc16f8e66d70
quyen <unknown>
2013-05-14 10:50:17 +0200

    load pubmed_record to avoid NameError: uninitialized constant PubmedQuery::PubmedRecord

--- config/initializers/seek_rails_3_plugin_fixes.rb
+++ config/initializers/seek_rails_3_plugin_fixes.rb
@@ -1,6 +1,7 @@
 #A placeholder for little hacks required to get various plugins working during the upgrade to rails 3
 #these are items that will need revisiting
 require 'bioportal'
+require 'pubmed_record'
 
 SEEK::Application.configure do
 
@@ -8,6 +9,7 @@ SEEK::Application.configure do
   require_dependency File.join(Gem.loaded_specs['my_savage_beast'].full_gem_path,'app','models',"moderatorship")
   require_dependency File.join(Gem.loaded_specs['my_savage_beast'].full_gem_path,'app','models',"posts_sweeper")
 
+  require_dependency File.join(Gem.loaded_specs['my_savage_beast'].full_gem_path,'app','models',"posts_sweeper")
 
   require_dependency File.join(Gem.loaded_specs['bioportal'].full_gem_path,'app','helpers',"bio_portal_helper")
   require_dependency File.join(Gem.loaded_specs['bioportal'].full_gem_path,'app','models',"bioportal_concept")
