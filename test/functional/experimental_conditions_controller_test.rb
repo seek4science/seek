3eda590e680134957df8e938908828fd8990e746
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-05-23 16:20:10 +0100

    fixed test for checking creation of experimental conditions from existing ones

--- test/functional/experimental_conditions_controller_test.rb
+++ test/functional/experimental_conditions_controller_test.rb
@@ -213,7 +213,7 @@ class ExperimentalConditionsControllerTest < ActionController::TestCase
       i +=1
     end
 
-    post :create_from_existing, :sop_id => sop.id, :version => sop.latest_version, "checkbox_#{ec_array.first.id}" => ec_array.first.id, "checkbox_#{ec_array[1].id}" => ec_array[1].id, "checkbox_#{ec_array[2].id}" => ec_array[2].id
+    post :create_from_existing, :sop_id => sop.id, :version => sop.latest_version.version, "checkbox_#{ec_array.first.id}" => ec_array.first.id, "checkbox_#{ec_array[1].id}" => ec_array[1].id, "checkbox_#{ec_array[2].id}" => ec_array[2].id
 
     sop.reload
     assert_equal sop.experimental_conditions.count, 3
