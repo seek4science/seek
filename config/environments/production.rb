f5314d0fa587d1b43d6852bda39669158fc7fcbf
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-05-28 10:57:43 +0100

    serve static assets in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,7 @@ SEEK::Application.configure do
   config.cache_store = [:file_store, "#{Rails.root}/tmp/cache"]
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
