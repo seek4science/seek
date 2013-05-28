9df61f112ed81a5ffff6e1661b206e3a00775ae6
quyen <unknown>
2013-05-28 11:16:27 +0200

    require recaptcha to fix an upgrade task

--- config/initializers/seek_main.rb
+++ config/initializers/seek_main.rb
@@ -29,6 +29,8 @@ require 'strategic_eager_loading'
 require 'seek/breadcrumbs'
 require 'string_extension'
 require 'seek/project_hierarchies'
+require 'recaptcha'
+
 GLOBAL_PASSPHRASE="ohx0ipuk2baiXah" unless defined? GLOBAL_PASSPHRASE
 
 ASSET_ORDER                = ['Person', 'Project', 'Institution', 'Investigation', 'Study', 'Assay', 'Sample','Specimen','Strain', 'DataFile', 'Model', 'Sop', 'Publication', 'Presentation','SavedSearch', 'Organism', 'Event']
