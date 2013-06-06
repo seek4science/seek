1f785dcbc949b1d8b32c3df465c4c7255a711e1e
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-06-06 14:59:18 +0100

    handling callbacks :loaded and :before for form_for

--- app/helpers/ajax_helper.rb
+++ app/helpers/ajax_helper.rb
@@ -18,13 +18,32 @@ module AjaxHelper
   end
 
   def form_tag_with_callbacks url_for_options = {}, options = {}, &block
-    loading = options.delete(:loading)
-    id = options[:id]
-    html = form_tag url_for_options, options, &block
-    html << render(:template=>"misc/form_tag_callbacks",:locals=>{:id=>id, :loading=>loading})
-
-
+    js = form_callback_javascript options
+    html = form_tag(url_for_options, options, &block)
+    html << js
     html
   end
+
+  def form_for_with_callbacks record, options={}, &block
+    pp options
+    js = form_callback_javascript options
+    html = form_for record, options, &block
+    html << js
+    html
+  end
+
+  private
+
+  def form_callback_javascript options
+    before = options.delete(:loading)
+    before ||= options.delete(:before)
+    loaded = options.delete(:loaded)
+    id = options[:id]
+    id ||= options[:html][:id] if options[:html]
+    render(:template=>"misc/form_tag_callbacks",:locals=>{:id=>id, :before=>before, :loaded=>loaded})
+  end
+
+
+
 
 end
