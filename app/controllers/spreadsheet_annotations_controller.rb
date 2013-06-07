fdd676cf346c79ca7b95e64afa0b75bf46232bbe
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-06-07 14:25:05 +0100

    updated spreadsheet annotations to use form_tag_with_callbacks, and fixed duplicate element ids. Still need to get the :update's updated

--- app/controllers/spreadsheet_annotations_controller.rb
+++ app/controllers/spreadsheet_annotations_controller.rb
@@ -21,11 +21,11 @@ class SpreadsheetAnnotationsController < ApplicationController
 
         if(@annotation.save)
           respond_to do |format|
-            format.html { render :partial => "spreadsheets/annotations", :locals=>{ :annotations => @content_blob.spreadsheet_annotations }}
+            format.js { render :partial => "spreadsheets/annotations", :locals=>{ :annotations => @content_blob.spreadsheet_annotations }}
           end
         else
           respond_to do |format|
-            format.html { render :partial => "spreadsheets/spreadsheet_errors", :status => 500, :locals=>{ :verb => "adding", :errors => @annotation.errors} }
+            format.js { render :partial => "spreadsheets/spreadsheet_errors", :status => 500, :locals=>{ :verb => "adding", :errors => @annotation.errors} }
           end
         end
       else
