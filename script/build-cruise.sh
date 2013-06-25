46a95087bbca8814715b3e1b4852e276ede08460
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-06-25 10:32:39 +0100

    updated rvm and cruise build for ruby 1.9 and the correct gemset

--- script/build-cruise.sh
+++ script/build-cruise.sh
@@ -14,7 +14,7 @@ rm -rf filestore/
 #rvm handling is based on example at http://pivotallabs.com/users/mbarinek/blog/articles/1450-rails-3-with-rvm-and-cruise-control
 
 desired_ruby=ruby-1.9.3
-project_name=seek_rails3_ruby19
+project_name=seek018
 rubygems=1.8.25
 
 # remove annoying "warning: Insecure world writable dir"
