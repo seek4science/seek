f2ce7b63c4ae73431ec78aed68682de5bebdd511
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-06-26 10:19:18 +0100

    use DefaultReporter for minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'rails/test_help'
 
 require "minitest/reporters"
-MiniTest::Reporters.use!
+MiniTest::Reporters.use! MiniTest::Reporters::DefaultReporter.new
 
 #require 'test_benchmark'
 require 'rest_test_cases'
