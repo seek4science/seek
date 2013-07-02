78a88248a6a6970c812f1226a38d0076c8d9078f
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-07-02 15:47:17 +0100

    filtering search terms mustn't change the current term, as the string may be frozen if it has come form SOLR

--- lib/seek/search/search_term_filter.rb
+++ lib/seek/search/search_term_filter.rb
@@ -2,10 +2,10 @@ module Seek
   module Search
     class SearchTermFilter
       def self.filter query
-        query.strip!
+        query = query.strip
         query = query.gsub("*", "")
         query = query.gsub("?", "")
-        query.chop! if query.end_with?(":")
+        query = query.chop if query.end_with?(":")
 
         query = query.strip
         query = "" if query=="-"
