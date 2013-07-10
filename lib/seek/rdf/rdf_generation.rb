f23c059fed143ee0582ba214b85eb79b06db24b3
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-07-10 11:22:46 +0100

    split the rdf generation and storage into seperate modules

--- lib/seek/rdf/rdf_generation.rb
+++ lib/seek/rdf/rdf_generation.rb
@@ -4,6 +4,7 @@ require 'csv'
 module Seek
   module Rdf
     module RdfGeneration
+      include RdfStorage
       include RightField
 
       def self.included(base)
@@ -38,49 +39,6 @@ module Seek
       def rdf_resource
         uri = Seek::Config.site_base_host+"/#{self.class.name.tableize}/#{self.id}"
         RDF::Resource.new(uri)
-      end
-
-      def save_rdf
-        #seperate public and private (to an outside user) into separate directories
-        if self.can_view?(nil)
-          path = public_rdf_storage_path
-          other_path = private_rdf_storage_path
-        else
-          path = private_rdf_storage_path
-          other_path = public_rdf_storage_path
-        end
-
-        #this is necessary to remove the old rdf if the permissions switched from public to private, or vice-versa
-        FileUtils.rm other_path if File.exists?(other_path)
-
-        #need to get the rdf first, before creating the file
-        rdf = self.to_rdf
-
-        File.open(path,"w") do |f|
-          f.write(rdf)
-          f.flush
-        end
-        path
-      end
-
-      private
-
-      def private_rdf_storage_path
-        rdf_storage_path "private"
-      end
-
-      def public_rdf_storage_path
-        rdf_storage_path "public"
-      end
-
-      def rdf_storage_path inner_dir
-        path = File.join(Seek::Config.rdf_filestore_path,inner_dir)
-        if !File.exists?(path)
-          FileUtils.mkdir_p(path)
-        end
-        unique_id="#{self.class.name}-#{self.id}"
-        filename = "#{unique_id}.rdf"
-        File.join(path,filename)
       end
 
 
---
+++ lib/seek/rdf/rdf_storage.rb
@@ -0,0 +1,47 @@
+module Seek
+  module Rdf
+    module RdfStorage
+      def save_rdf
+        #seperate public and private (to an outside user) into separate directories
+        if self.can_view?(nil)
+          path = public_rdf_storage_path
+          other_path = private_rdf_storage_path
+        else
+          path = private_rdf_storage_path
+          other_path = public_rdf_storage_path
+        end
+
+        #this is necessary to remove the old rdf if the permissions switched from public to private, or vice-versa
+        FileUtils.rm other_path if File.exists?(other_path)
+
+        #need to get the rdf first, before creating the file
+        rdf = self.to_rdf
+
+        File.open(path,"w") do |f|
+          f.write(rdf)
+          f.flush
+        end
+        path
+      end
+
+      def private_rdf_storage_path
+        rdf_storage_path "private"
+      end
+
+      def public_rdf_storage_path
+        rdf_storage_path "public"
+      end
+
+      def rdf_storage_path inner_dir=nil?
+        inner_dir ||= self.can_view?(nil) ? "public" : "private"
+        path = File.join(Seek::Config.rdf_filestore_path,inner_dir)
+        if !File.exists?(path)
+          FileUtils.mkdir_p(path)
+        end
+        unique_id="#{self.class.name}-#{self.id}"
+        filename = "#{unique_id}.rdf"
+        File.join(path,filename)
+      end
+    end
+  end
+end
