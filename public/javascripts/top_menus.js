fe15f73d5e3a8c76ea1a3ef9920dfd8a530fb056
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-07-30 14:22:06 +0100

    styling related to search box width and position when resizing.

--- public/javascripts/top_menus.js
+++ public/javascripts/top_menus.js
@@ -13,3 +13,22 @@ function update_menu_text(source_id,fadein) {
 function select_menu_item(section_id) {
     update_menu_text(section_id,false);
 }
+
+
+// handle the width of the search box according to the width of the screen
+Event.observe(window,"resize", function() {
+    set_searchbox_width();
+});
+document.observe("dom:loaded",function() {
+    set_searchbox_width();
+});
+
+function set_searchbox_width() {
+    if (document.viewport.getWidth()<=1180) {
+        $(search_query).style.width="19em";
+    }
+    else {
+        $(search_query).style.width="30em";
+    }
+    $(search_query).show();
+}
