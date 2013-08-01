b4f0d88f6e4ac44b3cbd2e5e5b9156fd1fa91915
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-08-01 11:17:59 +0100

    jerm should be disabled by default

--- config/initializers/seek_configuration.rb
+++ config/initializers/seek_configuration.rb
@@ -6,7 +6,7 @@ SEEK::Application.configure do
   Seek::Config.default :public_seek_enabled,true
   Seek::Config.default :sycamore_enabled,false
   Seek::Config.default :events_enabled,true
-  Seek::Config.default :jerm_enabled,true
+  Seek::Config.default :jerm_enabled,false
   Seek::Config.default :email_enabled,false
   Seek::Config.default :smtp, {:address => '', :port => '25', :domain => '', :authentication => :plain, :user_name => '', :password => '', :enable_starttls_auto=>false}
   Seek::Config.default :noreply_sender, 'no-reply@sysmo-db.org'
