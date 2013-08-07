91908ff7a583b6f6ffa3c71ac749194a3d9ae7ea
quyen <unknown>
2013-08-07 18:09:15 +0200

    make clearer the logic of gatekeeper method based on right and state

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -47,8 +47,14 @@ class Ability
   #gatekeeper can publish the assets belonging to their project if as well can manage or the item is waiting for his approval
   def gatekeeper gatekeeper
     can :publish, :all do |item|
-      if gatekeeper.is_gatekeeper_of?(item) && !item.is_published?
-       item.can_manage?(gatekeeper.user) || item.is_waiting_approval?(nil,1.year.ago)
+      if gatekeeper.is_gatekeeper_of?(item)
+        if item.can_manage?(gatekeeper.user) && !item.is_published?
+          true
+        elsif !item.is_published? && item.is_waiting_approval?(nil, 5.years.ago)
+          true
+        else
+          false
+        end
       else
         false
       end
