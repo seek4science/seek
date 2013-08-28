2a96e72d22bd32d189186bebd2688e1cb186e2ff
quyen <unknown>
2013-08-28 11:12:56 +0200

    dont escape name

--- app/helpers/i_s_a_helper.rb
+++ app/helpers/i_s_a_helper.rb
@@ -56,7 +56,7 @@ module ISAHelper
       if item.can_view?
         cytoscape_node_elements << {:group => 'nodes',
                                     :data => {:id => node,
-                                              :name => truncate(item_type.humanize + ': ' +  h(item.title)) ,
+                                              :name => truncate(item_type.humanize + ': ' + item.title),
                                               :full_title => ("<b>#{item_type.humanize}: </b>" +  h(item.title)) ,
                                               :path => polymorphic_path(item),
                                               :faveColor => (FILL_COLOURS[item_type] || FILL_COLOURS.default),
