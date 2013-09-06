599024f67dc47353c71191e2c51a2499701fda5d
quyen <unknown>
2013-09-06 14:16:50 +0200

    fix one jws online test, related to error handle, other test still fails because of jws online problem

--- test/unit/jws_unit_test.rb
+++ test/unit/jws_unit_test.rb
@@ -8,11 +8,11 @@ class JwsUnitTest < ActiveSupport::TestCase
   end
 
   test "jws online response handled when errors present" do
-    skip("temporarily skipped whilst jws online undergoes some upgrades")
     blob=Factory :invalid_sbml_content_blob
-    params_hash,attributions,saved_file,objects_hash,error_keys = @builder.builder_content blob
-    assert !error_keys.empty?
-    assert error_keys.include?("parameters")
+    exception = assert_raises(Exception) { params_hash,attributions,saved_file,objects_hash,error_keys = @builder.builder_content blob }
+    assert_not_nil exception.message
+    #assert !error_keys.empty?
+    #assert error_keys.include?("parameters")
   end if Seek::Config.jws_enabled
 
   test "jws online response with valid SBML model" do
