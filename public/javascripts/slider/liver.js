115efe33222ff5ba76250a0d73cbe207f9140232
quyen <unknown>
2013-09-24 15:50:22 +0200

    clean up some bit of liver.js

--- public/javascripts/slider/liver.js
+++ public/javascripts/slider/liver.js
@@ -2,10 +2,7 @@ $.noConflict();
 
 jQuery(document).ready(function($) {
 
-
     $(function() {
-
-        //IMG ARRAY
 
         var imgarray = [];
 
@@ -17,7 +14,6 @@ jQuery(document).ready(function($) {
 
         // same order as scales_arr
         imgarray.reverse();
-
 
         var curImage = imgarray[0];
         var previousValue;
@@ -37,44 +33,32 @@ jQuery(document).ready(function($) {
             value: current_scale,
             animate:true,
             slide: function (event, ui) {
-                //alert("#slider.slide"+ ui.value) ;
                 previousValue = val;
                 val = ui.value;
-                //$('#scale').val(scales_arr[val]) ; // change scale filter for searching
-                //valCheck();
                 scale_id = "#" + scales_arr[val];
                 $(scale_id).click();
             },
         });
 
 
-
         //valCheck() just checks the variable val and then runs a function called imageSwap, passing in the current image and the image we want to display,
         //then we change the current image to the new image.
         function valCheck() {
             if (val > previousValue) {
-                 //alert("val =" + val + "|| previousValue=" + previousValue);
-                imageSwap3(curImage, imgarray[val]);
+                imageSwapIn(curImage, imgarray[val]);
                 curImage = imgarray[val];
 
             }
 
             else {
-               // alert("val =" + val + " &&  previousValue=" + previousValue);
-                imageSwap(curImage, imgarray[val]);
+                imageSwapOut(curImage, imgarray[val]);
                 curImage = imgarray[val];
-
-
             }
-
-
         }
 
         //ANIMATION if images are changing!!
         //we�re just passing in the current image and the new image, fading out the current one and then fading in the new image
-        function imageSwap(curImage, newImage) {
-            /* $(curImage).fadeOut();
-             $(newImage).fadeIn(); */
+        function imageSwapOut(curImage, newImage) {
             $(curImage).css({'opacity':'1'});
             $(newImage).css({'opacity':'0'});
             $(newImage).css({'height':'25%', 'marginLeft':'200px', 'marginTop':'105px'});
@@ -89,7 +73,6 @@ jQuery(document).ready(function($) {
                 duration: 2000,
                 queue: false,
                 complete: function() {
-                    //$(this).hide();
                 }
             });
             $(newImage).animate({
@@ -101,20 +84,13 @@ jQuery(document).ready(function($) {
                 duration: 2000,
                 queue: false,
                 complete: function() {
-                    //this.show();
                 }
             });
 
 
         }
 
-        function imageSwap2(curImage, newImage) {
-            $(curImage).fadeOut();
-            $(newImage).fadeIn();
-
-        }
-
-        function imageSwap3(curImage, newImage) {
+        function imageSwapIn(curImage, newImage) {
             $(curImage).css({'opacity':'1'});
             $(newImage).css({'opacity':'0'});
             $(newImage).css({'height':'250%', 'marginLeft':'-120px', 'marginTop':'-222px'});
@@ -141,44 +117,9 @@ jQuery(document).ready(function($) {
                 duration: 2000,
                 queue: false,
                 complete: function() {
-                    //this.show();
                 }
             });
-
-            /* alert(newImage + curImage); */
-            // alert(val+',' + previousValue);
-
         }
-
-        //ANIMATION if images are changing!!
-        //we�re just passing in the current image and the new image, fading out the current one and then fading in the new image
-        function imageSwapElina(curImage, newImage) {
-            /* $(curImage).fadeOut();
-             $(newImage).fadeIn(); */
-            $(curImage).css({'opacity':'1'});
-
-            $(curImage).animate({
-                height: "250%",
-                marginLeft: "-50px",
-                marginTop: "-150px",
-
-            }, 2000);
-
-            $(curImage).fadeOut();
-
-            $(newImage).fadeIn(2000, function() {
-                $(newImage).animate({
-                    height: "100%",
-                    marginLeft: "100px",
-                    marginTop: "0px",
-                    queue:false,
-
-                }, 1000);
-            });
-
-        }
-
-
 
         //We�re just setting val to be the same as num and then running valCheck when the user clicks one of the links, otherwise, the images would only swap when the user slides the handle back and forth. Now the image swap will work if the user either clicks the links or moves the slider.
         function moveSlider(e, num) {
@@ -188,9 +129,6 @@ jQuery(document).ready(function($) {
                     [num]
                     );
             val = num;
-
-           // scale_id = "#" + scales_arr[val];
-           // $(scale_id).click();
             valCheck();
         }
 
@@ -222,9 +160,6 @@ jQuery(document).ready(function($) {
         $(scale_id).click();
 
     });
-
-
-
 
 
     /* Source Code: http://atomicrobotdesign.com/blog/web-development/controlling-html-using-the-jquery-ui-slider-and-links/ */
