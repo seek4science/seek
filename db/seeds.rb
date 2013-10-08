00ab3754028604d0f207be227dbe8b8841301cff
Aleksandra Nenadic <anenadic@cs.man.ac.uk>
2013-10-08 21:20:10 +0100

    Added db/seeds.rb to seed the SEEK database with workflow-specific data.

---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# Seeds the database with BioVeL-specific data
+
+# Seeds workflow categories
+workflow_categories = ['Taxonomic Refinement', 'Ecological Niche Modelling', 'Metagenomics', 'Phylogenetics', 'Population Modelling', 'Ecosystem Functioning and Valuation', 'Other']
+
+WorkflowCategory.delete_all
+
+workflow_categories.each do |category|
+  WorkflowCategory.create!(name: category)
+end
+
+# Seeds workflow input port types
+workflow_input_port_types = ['Data', 'Parameter']
+
+WorkflowInputPortType.delete_all
+
+workflow_input_port_types.each do |type|
+  WorkflowInputPortType.create!(name: type)
+end
+
+# Seeds workflow output port types
+workflow_output_port_types = ['Result', 'Error/Log']
+
+WorkflowOutputPortType.delete_all
+
+workflow_output_port_types.each do |type|
+  WorkflowOutputPortType.create!(name: type)
+end
