d92e69ed18eb5ed9aaf543bfbf592c8709159a09
Aleksandra Nenadic <anenadic@cs.man.ac.uk>
2013-10-10 10:46:36 +0100

    Added category to the workfow/show page and replaced the mock up for input and output ports with real data.

--- app/models/workflow_input_port.rb
+++ app/models/workflow_input_port.rb
@@ -1,5 +1,5 @@
 class WorkflowInputPort < ActiveRecord::Base
-  belongs_to :port_type, :class_name => 'WorkflowInputPortType'
+  belongs_to :type, :class_name => 'WorkflowInputPortType'
   belongs_to :example_data_file, :class_name => 'DataFile'
   belongs_to :workflow
 
