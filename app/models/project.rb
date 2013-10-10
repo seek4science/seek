be24b65691715e57b30709c248728d85339640e1
quyen <unknown>
2013-10-10 16:14:40 +0200

    slightly improve the error message

--- app/models/project.rb
+++ app/models/project.rb
@@ -56,7 +56,7 @@ class Project < ActiveRecord::Base
   def group_memberships_empty? institution
     work_group = WorkGroup.where(['project_id=? AND institution_id=?', self.id, institution.id]).first
     if !work_group.people.empty?
-      raise Exception.new("Cannot delete the " +work_group.description+ ". This Work Group has "+work_group.people.size.to_s+" people associated with it.
+      raise Exception.new("You can not delete the " +work_group.description+ ". This Work Group has "+work_group.people.size.to_s+" people associated with it.
                            Please disassociate first the people from this Work Group.")
     end
   end
