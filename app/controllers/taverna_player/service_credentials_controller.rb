7e00cadfc3c339869ae861c989e7b296a54b7a67
Aleksandra Nenadic <anenadic@cs.man.ac.uk>
2013-10-15 14:24:16 +0100

    Generated taverna-player views and some controllers. Started work on overriding run/new page.

---
+++ app/controllers/taverna_player/service_credentials_controller.rb
@@ -0,0 +1,9 @@
+
+module TavernaPlayer
+  class ServiceCredentialsController < TavernaPlayer::ApplicationController
+    # Do not remove the next line.
+    include TavernaPlayer::Concerns::Controllers::ServiceCredentialsController
+
+    # Extend the ServiceCredentialsController here.
+  end
+end
