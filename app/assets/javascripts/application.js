3e8257a861d3e78735378f9fdec631b2fc87393c
quyen <unknown>
2013-10-17 13:55:25 +0200

    reorganize javascripts/css and images

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+// ...
+ // require_tree .
+ // require_tree ../../../vendor/assets/javascripts/
+ // require prototype
+ // require prototype_ujs
+ // require effects
+ // require dragdrop
+ // require controls
+
+ 
