a193c9d83a0a79ae20a8f54bb1724dc80ec385f4
Lihua An <lihua.an@h-its.org>
2013-10-21 13:53:05 +0200

    correct delete text for ISA , specimens, samples

--- app/helpers/i_s_a_helper.rb
+++ app/helpers/i_s_a_helper.rb
@@ -17,16 +17,32 @@ module ISAHelper
     if model_item.can_delete?(user)
       return "<li>"+image_tag_for_key('destroy',url_for(model_item),"Delete #{item_name}", {:confirm=>"Are you sure?",:method=>:delete },"Delete #{item_name}") + "</li>"
     elsif !model_item.can_delete?(user) && model_item.can_edit?(user)
-      explanation=unable_to_delete_text model_item
+      explanation = unable_to_delete_text model_item
       return "<li><span class='disabled_icon disabled' onclick='javascript:alert(\"#{explanation}\")' title='#{tooltip_title_attrib(explanation)}' >"+image('destroy', {:alt=>"Delete",:class=>"disabled"}) + " Delete #{item_name} </span></li>"
     end
   end
   
   def unable_to_delete_text model_item
-    text=NO_DELETE_EXPLANTIONS[model_item.class] || "You are unable to delete this #{model_item.class.name}"
+    associated = false
+    case model_item.class
+      when Assay
+        associated = !(model_item.assets.empty? && model_item.related_publications.empty?)
+      when Study
+        associated = !model_item.assays.empty?
+      when Investigation
+        associated = !model_item.studies.empty?
+      when Specimen
+        associated = !model_item.sampes.empty?
+      when Sample
+        associated = !model_item.assays.empty?
+      else
+
+    end
+    text =   associated==true ?  NO_DELETE_EXPLANTIONS[model_item.class] : "You are not authorized to delete this #{model_item.class.name}"
     return text
   end
-  
+
+
   def embedded_isa_svg root_item,deep=true,current_item=nil
     begin
       current_item||=root_item
