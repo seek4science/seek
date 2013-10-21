6202adc7c1c935615055b9d5c8f1616b60f4ed02
quyen <unknown>
2013-10-21 15:19:58 +0200

    precompile javascript and css for homepage

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,29 @@
 // ...
  // require_tree .
  // require_tree ../../../vendor/assets/javascripts/
- // require prototype
- // require prototype_ujs
- // require effects
- // require dragdrop
- // require controls
+ //= require prototype
+ //= require prototype_ujs
+ //= require effects
+ //= require dragdrop
+ //= require controls
 
+//= savage_beast/application.js
+//= require scriptaculous.js
+//= require top_menu.js
+//= require dynamic_menu.js
+
+//= require boxover.js
+//= require redbox.js
+//= require builder.js
+//= require tagging.js
+//= require autocompleter_advanced.js
+//= require publication.js
+
+//= require jquery-1.5.1.min.js
+//= require jscrollpane/jquery.jscrollpane.min
+//= require jscrollpane/jquery.mousewheel
+
+//= require tabber-minimized
+
+//= require sharing.js
  
