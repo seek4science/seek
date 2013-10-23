e94b1fd2b843bf17e6b9694d846570b732d6a8cf
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-10-23 16:40:12 +0100

    refactored how the roles mask is generated and looked up, so that it is handled in one place through a method

--- lib/seek/roles.rb
+++ lib/seek/roles.rb
@@ -2,9 +2,6 @@ module Seek
   module Roles
 
     ROLES = %w[admin pal project_manager asset_manager gatekeeper]
-    ROLES_MASK_FOR_ADMIN = 2**ROLES.index('admin')
-    ROLES_MASK_FOR_PAL = 2**ROLES.index('pal')
-    ROLES_MASK_FOR_PROJECT_MANAGER = 2**ROLES.index('project_manager')
 
     def self.included(mod)
       mod.extend(ClassMethods)
@@ -18,6 +15,8 @@ module Seek
               roles.include?('#{role}')
             end
 
+
+
             def is_#{role}=(yes)
               if yes
                 add_roles ['#{role}']
@@ -30,34 +29,45 @@ module Seek
     end
 
     module ClassMethods
+      def mask_for_role(role)
+        2**ROLES.index(role)
+      end
+
       ROLES.each do |role|
         eval <<-END_EVAL
           def #{role}s
             self.all.select(&:is_#{role}?)
           end
+          def mask_for_#{role}
+            self.mask_for_role('#{role}')
+          end
         END_EVAL
       end
     end
 
+    def mask_for_role(role)
+      self.class.mask_for_role(role)
+    end
+
     #the roles defined within SEEK
     def roles=(roles)
-      self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.sum
+      self.roles_mask = (roles & ROLES).map { |r| self.class.mask_for_role(r) }.sum
     end
 
     def roles
       ROLES.reject do |r|
-        ((roles_mask || 0) & 2**ROLES.index(r)).zero?
+        ((roles_mask || 0) & mask_for_role(r)).zero?
       end
     end
 
     def add_roles roles
       add_roles = roles - (roles & self.roles)
-      self.roles_mask = self.roles_mask.to_i + ((add_roles & ROLES).map { |r| 2**ROLES.index(r) }.sum)
+      self.roles_mask = self.roles_mask.to_i + ((add_roles & ROLES).map { |r| mask_for_role(r) }.sum)
     end
 
     def remove_roles roles
       remove_roles = roles & self.roles
-      self.roles_mask = self.roles_mask.to_i - ((remove_roles & ROLES).map { |r| 2**ROLES.index(r) }.sum)
+      self.roles_mask = self.roles_mask.to_i - ((remove_roles & ROLES).map { |r| mask_for_role(r) }.sum)
     end
 
     def is_gatekeeper_of? item
