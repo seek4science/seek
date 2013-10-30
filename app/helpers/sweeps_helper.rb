8deb9b0f092911ad2c2a85fd556bf7b00ac41ff5
Aleksandra Nenadic <anenadic@cs.man.ac.uk>
2013-10-30 09:42:02 +0000

    Merged

--- app/helpers/sweeps_helper.rb
+++ app/helpers/sweeps_helper.rb
@@ -1,7 +1,7 @@
 module SweepsHelper
 
   # Return an array of table rows, one for each workflow output port
-  def sweep_results_table(sweep)
+  def sweep_results_table_upside_down(sweep)
     workflow = sweep.workflow
     output_ports = workflow.output_ports
 
@@ -23,7 +23,8 @@ module SweepsHelper
     end
   end
 
-  def sweep_results_table_upside_down(sweep)
+  # Return an array of table rows, one for each run in the sweep
+  def sweep_results_table(sweep)
     workflow = sweep.workflow
     output_ports = workflow.output_ports
 
@@ -36,10 +37,6 @@ module SweepsHelper
       row = [ link_to("#{image('simple_run')} #{run.name}".html_safe, taverna_player.run_path(run))]
 
       output_ports.each do |output_port|
-
-        # Checkbox to select the whole row
-        #row << [check_box_tag(output_port.name, output_port.name, false, {:class => 'chk-select-row'})]  # second parameter is used to populate the element's value attribute and gets passed in params
-
         chk_box = check_box_tag("download[#{run.id}][]", output_port.name, false, {:multiple => true, :class => 'chk-select', :id => "download_run#{i+1}_#{output_port.name}"})
         link_to_view = link_to 'View', view_result_sweep_path(:run_id => run.id, :output_port_name => output_port.name), remote: true
         row << "#{chk_box} &nbsp;&nbsp;#{link_to_view}".html_safe
