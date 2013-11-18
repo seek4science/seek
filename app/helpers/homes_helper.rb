a6b8228d6a96a72ca168e413e899bf73e2aa49ea
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-11-18 15:29:03 +0000

    flag to turn off the guide box

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -11,6 +11,10 @@ module HomesHelper
     simple_format(auto_link(Seek::Config.home_description.html_safe,:sanitize=>false),{},:sanitize=>false)
   end
 
+  def show_guide_box?
+    Seek::Config.guide_box_enabled && ((!logged_in? && cookies[:hide_guide_box].nil?) || (logged_in? && current_user.try(:show_guide_box?)))
+  end
+
   def recent_project_changes_hash
 
     projects=current_user.person.projects
--- config/initializers/seek_configuration.rb
+++ config/initializers/seek_configuration.rb
@@ -44,6 +44,7 @@ SEEK::Application.configure do
   Seek::Config.default :modelling_analysis_enabled,true
   Seek::Config.default :organisms_enabled,true
   Seek::Config.default :models_enabled,true
+  Seek::Config.default :guide_box_enabled,true
 
 #time in minutes that the feeds on the front page are cached for
   Seek::Config.default :home_feeds_cache_timeout,2
