b8edb2a5c5fa4da40faa979faca68ed0b0a352b2
Finn Bacall <finn.bacall@cs.man.ac.uk>
2013-11-28 11:23:28 +0000

    Output MIME type fixer now applies recursively to values in lists if arbitrary depth

--- lib/taverna_player_callbacks.rb
+++ lib/taverna_player_callbacks.rb
@@ -1,9 +1,26 @@
-def fix_run_output_mime_types(run)
+  def fix_run_output_mime_types(run)
   run.outputs.each do |output|
     port = run.workflow.output_ports.detect {|o| o.name == output.name }
     if port && !port.mime_type.blank?
-      output.metadata[:type] = port.mime_type
+      if output.depth > 0
+        output.metadata[:type] = recursively_set_type(output.metadata[:type], output.depth, port.mime_type)
+      else
+        output.metadata[:type] = port.mime_type
+      end
       output.save
+    end
+  end
+end
+
+private
+
+def recursively_set_type(list, depth, type)
+  depth -= 1
+  list.map do |el|
+    if depth == 0
+      type
+    else
+      recursively_set_type(el, depth, type)
     end
   end
 end
