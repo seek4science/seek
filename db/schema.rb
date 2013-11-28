776faf4a2f0334234b70449cf420c887d44dd743
Stuart Owen <stuart.owen@manchester.ac.uk>
2013-11-28 17:37:47 +0000

    migration to transfer technology types and uris over to the assay

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131128162518) do
+ActiveRecord::Schema.define(:version => 20131128173209) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
