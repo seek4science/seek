85f9ef69ed04e3df169249794e7ea47f036e06e1
Finn Bacall <finn.bacall@cs.man.ac.uk>
2013-12-16 12:37:23 +0000

    Display file extension in MIME types selection

--- app/helpers/workflows_helper.rb
+++ app/helpers/workflows_helper.rb
@@ -1,7 +1,12 @@
 module WorkflowsHelper
 
   # Get the MIME types in an array suitable for the use in forms for 'select' fields
-  mime_types_names_list = Seek::MimeTypes::MIME_MAP.map{|key, value| value[:name]}.uniq
+  mime_types_names_list = Seek::MimeTypes::MIME_MAP.map do |key, value|
+    s = "#{value[:name]}"
+    s += " (.#{value[:extensions].first})" unless value[:extensions].first.blank?
+    s
+  end.uniq.sort
+
   $mime_types_list = [['', '']]
   mime_types_names_list.each do |name|
     $mime_types_list << [ name, (Seek::MimeTypes::MIME_MAP.map{ |key, value|  key if value[:name] == name }.compact)[0] ]
