6c1e1298292b96b4a415a18c9c19945b70c50110
savorain <unknown>
2013-12-18 16:10:11 +0100

    Countries page is closed for everybody who is not logged in. It is configurable just for VLN.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -7,7 +7,16 @@ class CountriesController < ApplicationController
     @institutions = Institution.where(["country LIKE ?", @country])
     
     respond_to do |format|
-      format.html # show.html.erb
+      if Seek::Config.is_virtualliver
+        if !User.current_user.nil?
+          format.html # show.html.erb
+        else
+        flash[:error] = "You are not authorized to view institutions and people in this country, you may need to login first."
+        format.html { redirect_to home_url}
+        end
+      else
+        format.html # show.html.erb
+      end
     end
   end
 end
