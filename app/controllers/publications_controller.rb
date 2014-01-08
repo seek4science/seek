780c3a3e8eabedd1b4b912771b8d1f6acc3961a8
quyen <unknown>
2014-01-08 16:42:56 +0100

    remove a pubmedquerytool require

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -5,9 +5,7 @@ class PublicationsController < ApplicationController
   include DotGenerator
   include Seek::AssetsCommon
   include Seek::BioExtension
-  
-  require 'pubmed_query_tool'
-  
+
   before_filter :find_assets, :only => [ :index ]
   #before_filter :find_and_authorize_requested_item, :only => [:show, :edit, :update, :destroy]
   before_filter :associate_authors, :only => [:edit, :update]
