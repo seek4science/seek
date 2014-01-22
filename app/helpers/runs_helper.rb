4d6be82a82d60e2e6ffe345a369225d48e05e2a2
Finn Bacall <finn.bacall@cs.man.ac.uk>
2014-01-22 11:35:09 +0000

    Fixed date columns being sorted alphabetically in runs table view

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -6,10 +6,22 @@ module RunsHelper
       created_at = "#{time_ago_in_words(run.created_at)} ago"
 
       if run.is_a?(Sweep) # If a sweep, get the latest finish time of its runs
-        finish_time = run.runs.any? {|r| r.finish_time.blank? } ? '' :
-            "#{time_ago_in_words(run.runs.map { |r| r.finish_time }.max)} ago"
+        if run.runs.any? {|r| r.finish_time.blank? }
+          finish_time = 0
+          finish_time_text = "-"
+        else
+          max_time = run.runs.map { |r| r.finish_time }.max
+          finish_time = max_time.to_i
+          finish_time_text = "#{time_ago_in_words(max_time)} ago"
+        end
       else
-        finish_time = run.finish_time.blank? ? '' : "#{time_ago_in_words(run.finish_time)} ago".html_safe
+        if run.finish_time.blank?
+          finish_time = 0
+          finish_time_text = "-"
+        else
+          finish_time = run.finish_time.to_i
+          finish_time_text = "#{time_ago_in_words(run.finish_time)} ago"
+        end
       end
 
       action_buttons = render(:partial => "taverna_player/runs/delete_or_cancel_button", :locals => { :run => run, :redirect_to => redirect_to })
@@ -21,15 +33,11 @@ module RunsHelper
        workflow.category.name,
        "#{run.state} #{(run.complete? ? '' : image_tag('ajax-loader.gif', :style => "vertical-align: middle"))}".html_safe,
        created_at,
+       run.created_at.to_i,
+       finish_time_text,
        finish_time,
        action_buttons]
     end
-  end
-
-  def mime_type_options(original)
-    types = [["text/plain", "text/plain"], ["text/csv", "text/csv"]]
-    types.prepend([original, original]) unless types.any? { |a| a[0] == original }
-    options_for_select([[p.metadata[:type], p.metadata[:type]], ["text/csv", "text/csv"]])
   end
 
 end
