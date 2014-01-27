05995628ef35f555235c1175bc3ecef4a342b1a8
Robert Haines <rhaines@manchester.ac.uk>
2014-01-27 15:59:27 +0000

    Increase delayed job time to a day for now.
    Might need to higher in future...

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -3,12 +3,9 @@ SEEK::Application.configure do
   Delayed::Worker.destroy_failed_jobs = false
   Delayed::Worker.sleep_delay = 3
   Delayed::Worker.max_attempts = 1
-  Delayed::Worker.max_run_time = 1.hour
+  Delayed::Worker.max_run_time = 1.day
   Delayed::Worker.backend = :active_record
 
   #Delayed::Worker.logger = Delayed::Worker.logger = ActiveSupport::BufferedLogger.new(Rails.root.join('log/worker.log'),Logger::INFO)
   #Delayed::Worker.logger.auto_flushing = 1
-
 end
-
-
