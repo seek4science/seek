b3686e9fabd123d0b206a2307744df5d3328db23
Robert Haines <rhaines@manchester.ac.uk>
2014-01-28 16:03:52 +0000

    Force all delayed jobs to have a queue name.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -6,6 +6,7 @@ SEEK::Application.configure do
   Delayed::Worker.max_run_time = 1.day
   Delayed::Worker.read_ahead = 20
   Delayed::Worker.backend = :active_record
+  Delayed::Worker.default_queue_name = 'default'
 
   #Delayed::Worker.logger = Delayed::Worker.logger = ActiveSupport::BufferedLogger.new(Rails.root.join('log/worker.log'),Logger::INFO)
   #Delayed::Worker.logger.auto_flushing = 1
