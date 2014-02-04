47f0bf4b41273635c6ffead5119062698bfca7a2
Lihua An <lihua.an@h-its.org>
2014-02-04 16:17:02 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -330,7 +330,7 @@ ActiveRecord::Schema.define(:version => 20131203082953) do
     t.datetime "last_used_at"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "version"
+    t.integer  "version",                       :default => 1
     t.string   "first_letter",     :limit => 1
     t.text     "other_creators"
     t.string   "uuid"
@@ -715,7 +715,7 @@ ActiveRecord::Schema.define(:version => 20131203082953) do
     t.integer  "organism_id"
     t.integer  "model_type_id"
     t.integer  "model_format_id"
-    t.integer  "version"
+    t.integer  "version",                                 :default => 1
     t.string   "first_letter",               :limit => 1
     t.text     "other_creators"
     t.string   "uuid"
@@ -1271,7 +1271,7 @@ ActiveRecord::Schema.define(:version => 20131203082953) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "last_used_at"
-    t.integer  "version"
+    t.integer  "version",                       :default => 1
     t.string   "first_letter",     :limit => 1
     t.text     "other_creators"
     t.string   "uuid"
