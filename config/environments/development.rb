686b9eb859f14462be36d5cb1a1572beb963dd71
Unknown <lihua@rh02684>
2014-03-27 16:03:15 +0000

    add assets prefix to development mode to avoid accessing to precompiled assets for production mode

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,9 @@ SEEK::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  #Avoid development mode using precompiled assets
+  config.assets.prefix = "/assets_dev"
+
   # Do not compress assets
   config.assets.compress = false
 
