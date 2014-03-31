be18e376e4db483883b35e08bf3f17c44fb37feb
quyen <unknown>
2014-03-31 17:35:55 +0200

    refractor generating exhibit tree for HierarchicalFacet

---
+++ app/helpers/facets_helper.rb
@@ -0,0 +1,18 @@
+module FacetsHelper
+  def exhibit_tree tree_class, facet
+    tree = []
+    class_hierarchy = tree_class.constantize.instance.class_hierarchy
+    tree.push({'type' => facet, 'label' => class_hierarchy.label})
+    tree |=  generate_tree(class_hierarchy, facet)
+    tree
+  end
+
+  def generate_tree parent_class, facet
+    result = []
+    parent_class.subclasses.each do |child|
+      result << {'type' => facet, 'label' => child.label, 'subclassOf' => child.parents.first.label}
+      result = result + generate_tree(child, facet) unless child.subclasses.empty?
+    end
+    result
+  end
+end
