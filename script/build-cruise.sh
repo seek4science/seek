e26fa2aa18a5d0ff4c78660b2ee33040f1066ea6
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-03-31 11:19:51 +0100

    use ruby 2 by default now

--- script/build-cruise.sh
+++ script/build-cruise.sh
@@ -13,9 +13,9 @@ rm -rf filestore/
 
 #rvm handling is based on example at http://pivotallabs.com/users/mbarinek/blog/articles/1450-rails-3-with-rvm-and-cruise-control
 
-desired_ruby=ruby-1.9.3-p545
+desired_ruby=ruby-2.1.1
 project_name=seek-0-21
-rubygems=1.8.25
+rubygems=2.2.2
 
 # remove annoying "warning: Insecure world writable dir"
 function remove_annoying_warning() {
