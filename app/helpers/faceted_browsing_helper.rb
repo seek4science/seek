226306cb0be8954348e3abaeed6d02a9363903dd
quyen <unknown>
2014-04-01 16:14:26 +0200

    escape string, need to escape 2 times for exhibit_items

--- app/helpers/faceted_browsing_helper.rb
+++ app/helpers/faceted_browsing_helper.rb
@@ -2,7 +2,7 @@ module FacetedBrowsingHelper
   def exhibit_tree tree_class, facet
     tree = []
     class_hierarchy = tree_class.constantize.instance.class_hierarchy
-    tree.push({'type' => facet, 'label' => class_hierarchy.label})
+    tree.push({'type' => facet, 'label' => h(class_hierarchy.label)})
     tree |=  generate_tree(class_hierarchy, facet)
     tree
   end
@@ -10,7 +10,7 @@ module FacetedBrowsingHelper
   def generate_tree parent_class, facet
     result = []
     parent_class.subclasses.each do |child|
-      result << {'type' => facet, 'label' => child.label, 'subclassOf' => child.parents.first.label}
+      result << {'type' => facet, 'label' => h(child.label), 'subclassOf' => h(child.parents.first.label)}
       result = result + generate_tree(child, facet) unless child.subclasses.empty?
     end
     result
