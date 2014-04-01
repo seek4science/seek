f94c01f4489bd2be205d7d122a0e4ae24ee62515
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-04-01 15:42:57 +0100

    flipped view over to SEEK look - currently uaing a temporary is_biovel flag which I intend to remove later on

--- lib/seek/config.rb
+++ lib/seek/config.rb
@@ -291,6 +291,11 @@ module Seek
     extend Propagators
     extend CustomAccessors
 
+    #FIXME: temporary hack. Ideally I'd like to avoid a config like, like we have for VL
+    def self.is_biovel?
+      false
+    end
+
     #Basic settings
     settings = [:home_description, :home_feeds_cache_timeout, :public_seek_enabled, :bioportal_api_key, :no_reply,
       :jws_online_root, :hide_details_enabled, :activation_required_enabled, :project_name, :smtp, :default_pages, :project_type, :project_link, :header_image,
