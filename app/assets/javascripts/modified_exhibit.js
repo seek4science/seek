17540ca0be508bd6452aa77724657af40c6b517c
quyen <unknown>
2014-04-02 17:12:38 +0200

    rename Hierachical Facet Missing Field of the root level

--- app/assets/javascripts/modified_exhibit.js
+++ app/assets/javascripts/modified_exhibit.js
@@ -1,4 +1,6 @@
 function updateFirstPage(){
+    //temporary put this function here, need to find better place or rename the updateFirstPage
+    renameHFMissingField();
     var items = Exhibit.jQuery('div[itemid]');
     var item_type = getItemType(items);
     var item_ids = getItemIds(items);
@@ -41,6 +43,17 @@ function decodeValueTooltip(){
     Exhibit.jQuery('.exhibit-facet-value').map(function(){
         var title = Exhibit.jQuery(this).attr("title");
         Exhibit.jQuery(this).attr("title", decodeHTML(title));
+    })
+}
+
+//rename Hierachical Facet Missing Field of the root level: from others to missing this field
+function renameHFMissingField(){
+    var missing_field_elements = Exhibit.jQuery('.exhibit-flowingFacet-body').children("[title='(others)']");
+    var replaced_term = "(missing this field)";
+    missing_field_elements.map(function(){
+        Exhibit.jQuery(this).attr("title", replaced_term);
+        var value_link = Exhibit.jQuery(this).children('.exhibit-flowingFacet-value-link');
+        value_link.html("<span class='exhibit-facet-value-missingThisField'>(missing this field)</span>");
     })
 }
 
