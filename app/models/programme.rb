62efd8582f5e0d9a593fd60bc05aa3f8f22a072f
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-04-30 17:20:48 +0100

    added search stuff to programme

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -2,6 +2,7 @@ class Programme < ActiveRecord::Base
   attr_accessible :avatar_id, :description, :first_letter, :title, :uuid, :web_page
 
   acts_as_yellow_pages
+  include BackgroundReindexing
 
   #associations
   has_many :projects
@@ -10,6 +11,19 @@ class Programme < ActiveRecord::Base
   validates :title,:uniqueness=>true
 
   scope :default_order, order('title')
+
+  #TODO: why wasn't this added to the BackgroundReindexing module?
+  after_save :queue_background_reindexing if Seek::Config.solr_enabled
+
+  searchable(:auto_index=>false) do
+    text :title,:description
+    text :projects do
+      projects.compact.map(&:title)
+    end
+    text :institutions do
+      institutions.compact.map(&:title)
+    end
+  end if Seek::Config.solr_enabled
 
   def people
     projects.collect{|p| p.people}.flatten.uniq
