7a38a3dbe5bacbf4706048e42fc78cc7f9d341ee
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-05-07 13:39:35 +0100

    use ruby 2.1.0 - due to a Hash bug in 2.1.1 - https://github.com/rails/rails/issues/14188

--- script/build-cruise.sh
+++ script/build-cruise.sh
@@ -16,7 +16,7 @@ rm -rf filestore/
 
 #rvm handling is based on example at http://pivotallabs.com/users/mbarinek/blog/articles/1450-rails-3-with-rvm-and-cruise-control
 
-desired_ruby=ruby-2.1.1
+desired_ruby=ruby-2.1.0
 project_name=biovel-seek-portal
 rubygems=2.2.2
 
