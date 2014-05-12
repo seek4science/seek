e37cf9b7b7a704ae581824a000e3a75d89e4e7d4
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-05-12 11:49:13 +0100

    fixed missing html tag

--- app/helpers/programmes_helper.rb
+++ app/helpers/programmes_helper.rb
@@ -1,8 +1,14 @@
 module ProgrammesHelper
 
   def list_item_programme_attribute(project)
-    html = "<p class=\"list_item_attribute\"><b>#{t('programme')}:</b> "
-    html << programme_link(project)
+    html = content_tag :p,:class=>"list_item_attribute" do
+      label = content_tag :b do
+        t('programme')
+
+      end
+      label + ": " + programme_link(project)
+
+    end
     html.html_safe
   end
 
