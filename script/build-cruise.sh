c44fe1284dd3ad19c3f15e5d9031633f9d50a400
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-05-12 11:14:23 +0100

    updated to use and test against ruby 2.1.2

--- script/build-cruise.sh
+++ script/build-cruise.sh
@@ -16,8 +16,8 @@ rm -rf filestore/
 
 #rvm handling is based on example at http://pivotallabs.com/users/mbarinek/blog/articles/1450-rails-3-with-rvm-and-cruise-control
 
-desired_ruby=ruby-2.1.0
-project_name=biovel-seek-portal
+desired_ruby=ruby-2.1.2
+project_name=seek-0-21
 rubygems=2.2.2
 
 # remove annoying "warning: Insecure world writable dir"
