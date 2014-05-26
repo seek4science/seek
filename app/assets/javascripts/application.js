fdc857fd33f097caeeb66d6d0df200d673629e2c
quyen <unknown>
2014-05-26 14:59:58 +0200

    start to embed facet into seach result

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,7 +29,7 @@
 //= require fancy_multiselect
 //= require parseuri
 //= require folds
-//= require tabber-minimized
+// require tabber-minimized
 //= require detect_browser
 //= require easytabs
 
