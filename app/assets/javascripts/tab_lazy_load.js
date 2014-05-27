3409be4cd0d7bf08c3e06e2dfa8e2c23f47acd58
quyen <unknown>
2014-05-27 15:53:26 +0200

    modify tab_on_click ajax

--- app/assets/javascripts/tab_lazy_load.js
+++ app/assets/javascripts/tab_lazy_load.js
@@ -5,46 +5,25 @@ function load_tabs() {
     tabberAutomatic(tabberOptions);
 }
 
-function tab_on_click(scale_title, resource_type, resource_ids, actions_partial_disable){
+function tab_on_click(scale_title, resource_type, resource_ids){
     var click_tab = document.getElementsByClassName(scale_title + '_' + resource_type)[0];
     click_tab.onclick = function(){
         deactivate_previous_tab(scale_title);
         click_tab.parentElement.className = 'tabberactive';
-
-        //if the content of the click_tab is already loaded, just show it, otherwise call ajax to get the content
-        var tab_content_view_some_id = scale_title + '_' + resource_type + '_view_some';
-        var tab_content_view_some = $(tab_content_view_some_id);
-        var tab_content_view_all_id = scale_title + '_' + resource_type + '_view_all';
-        var tab_content_view_all = $(tab_content_view_all_id);
-        if (tab_content_view_all.childNodes.length > 0){
-            tab_content_view_all.show();
-        }else if (tab_content_view_some.childNodes.length > 0){
-            tab_content_view_some.show();
-        }else{
-            tab_content_view_some.show();
-            request = new Ajax.Request('/search/search_items',
-                {
-                    method: 'get',
-                    parameters: {
-                        resource_type: resource_type,
-                        resource_ids: resource_ids,
-                        scale_title: scale_title,
-                        view_type: 'view_some',
-                        actions_partial_disable: actions_partial_disable
-                    },
-                    onLoading: show_large_ajax_loader(tab_content_view_some_id),
-                    onFailure: function(transport){
-                        alert('Something went wrong, please try again...');
-                    },
-                    onSuccess: function(data){
-                        $('facet_search_result').innerHTML = data.facets_for_items;
-                        initializationFunction(Exhibit.jQuery);
-                        Exhibit.jQuery(document).trigger("scriptsLoaded.exhibit");
-                    }
-                });
-        }
+        jQuery.noConflict();
+        var $j = jQuery;
+        $j.ajax({
+            url: search_items_url,
+            async: false,
+            data: { item_ids: resource_ids.split(','),
+                    item_type: resource_type}
+        })
+        .done(function( data ) {
+            var tab_content_id = scale_title + '_' + 'faceted_search_result';
+            $j('#' + tab_content_id).html(data.facets_for_items);
+            Exhibit.jQuery(document).trigger("scriptsLoaded.exhibit");
+        });
     }
-
 }
 
 function deactivate_previous_tab(scale_title){
@@ -52,30 +31,6 @@ function deactivate_previous_tab(scale_title){
     var scale_result = $(scale_title + '_results');
     var previous_active_tab = scale_result.getElementsByClassName('tabberactive')[0];
     previous_active_tab.className = '';
-    //Then hide the content of the tab
-    var scale_and_type = previous_active_tab.childNodes[0].className;
-    //the content could come from external search
-    if (scale_and_type.match("external_result") != null){
-        scale_and_type = scale_and_type.split('external_result')[0];
-        var resource_type = scale_and_type.split('_')[1].strip();
-        var external_tab_content = $(resource_type)
-        if (external_tab_content != null){
-            external_tab_content.hide();
-        }
-    }else{
-        var scale = scale_and_type.split('_')[0].strip();
-        var resource_type = scale_and_type.split('_')[1].strip();
-        var tab_content_view_some_id = scale + '_' + resource_type + '_view_some';
-        var tab_content_view_all_id = scale + '_' + resource_type + '_view_all';
-        var tab_content_view_some = $(tab_content_view_some_id);
-        var tab_content_view_all = $(tab_content_view_all_id);
-        if (tab_content_view_some != null){
-            tab_content_view_some.hide();
-        }
-        if (tab_content_view_all != null){
-            tab_content_view_all.hide();
-        }
-    }
 }
 
 function check_tab_content(show_tab_content_id, hide_tab_content_id){
