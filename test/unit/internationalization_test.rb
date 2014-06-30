3192eba4fb42512cdf3c5a1d7af045740cf6b37a
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-06-30 15:37:19 +0100

    fix to the internationalization test, with a comment to revist and make less dependent on the flavour of the en.yml file

--- test/unit/internationalization_test.rb
+++ test/unit/internationalization_test.rb
@@ -3,65 +3,48 @@ require 'test_helper'
 class InternationalizationTest < ActiveSupport::TestCase
 
   test "biosamples renaming" do
-       assert_equal "Specimen", (I18n.t "biosamples.sample_parent_term")
+       #MERGENOTE - would be better to make these tests less dependent on the flavour of en.yml, so that it would still work for VLN/ Biovel etc.
+       assert_equal "Cell culture", (I18n.t "biosamples.sample_parent_term")
        assert_equal "Culture starting date", (I18n.t "biosamples.specimen_culture_starting_date")
        assert_equal "Age at sampling", (I18n.t "biosamples.sample_age")
        assert_equal "Creators", (I18n.t "biosamples.specimen_creators")
   end
 
   test "assay" do
-    as_not_virtualliver do
-      assert_equal "Assay", (I18n.t "assays.assay")
-      assert_equal "Experimental assay", (I18n.t "assays.experimental_assay")
-      assert_equal "Modelling analysis", (I18n.t "assays.modelling_analysis")
-    end
+    assert_equal "Assay", (I18n.t "assays.assay")
+    assert_equal "Experimental assay", (I18n.t "assays.experimental_assay")
+    assert_equal "Modelling analysis", (I18n.t "assays.modelling_analysis")
   end
 
   test "sop" do
-    as_not_virtualliver do
-      assert_equal "SOP", (I18n.t "sop")
-    end
+    assert_equal "SOP", (I18n.t "sop")
   end
 
   test "presentation" do
-    as_not_virtualliver do
-      assert_equal "Presentation", (I18n.t "presentation")
-    end
+    assert_equal "Presentation", (I18n.t "presentation")
   end
 
   test "data file" do
-    as_not_virtualliver do
-      assert_equal "Data file", (I18n.t "data_file")
-    end
+    assert_equal "Data file", (I18n.t "data_file")
   end
 
   test "investigation" do
-    as_not_virtualliver do
-      assert_equal "Investigation", (I18n.t "investigation")
-    end
+    assert_equal "Investigation", (I18n.t "investigation")
   end
 
   test "study" do
-    as_not_virtualliver do
-      assert_equal "Study", (I18n.t "study")
-    end
+    assert_equal "Study", (I18n.t "study")
   end
 
   test "model" do
-    as_not_virtualliver do
-      assert_equal "Model", (I18n.t "model")
-    end
+    assert_equal "Model", (I18n.t "model")
   end
 
   test "event" do
-    as_not_virtualliver do
-      assert_equal "Event", (I18n.t "event")
-    end
+    assert_equal "Event", (I18n.t "event")
   end
 
   test "project" do
-    as_not_virtualliver do
-      assert_equal "Project", (I18n.t "project")
-    end
+    assert_equal "Project", (I18n.t "project")
   end
 end
