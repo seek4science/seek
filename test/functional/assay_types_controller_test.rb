e1ce5ce60f93a674f908297cac70baef3b4742fe
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-07-01 16:46:26 +0100

    fix to assay type label check, made case-insensitive with a comment to revisit this

--- test/functional/assay_types_controller_test.rb
+++ test/functional/assay_types_controller_test.rb
@@ -89,11 +89,13 @@ class AssayTypesControllerTest < ActionController::TestCase
       assert_select "div.list_item div.list_item_content div.list_item_title a[href=?]", assay_path(assay), :text => /#{assay.title}/
     end
   end
+
   test "no label passed render the same page as long as the same ontolgoy uri is passed" do
     assay = Factory :experimental_assay, :policy => Factory(:public_policy)
     #without label
     get :show, :uri => assay.assay_type_uri
-    assert_select "h1", :text => /Assay type &#x27;#{assay.assay_type_label}&#x27;/
+    #MERGENOTE - made text check case insensitive - need to revisit and check this isn't too loose
+    assert_select "h1", :text => /Assay type &#x27;#{assay.assay_type_label}&#x27;/i
     assert_select "div.list_items_container" do
       assert_select "div.list_item div.list_item_content div.list_item_title a[href=?]", assay_path(assay), :text => /#{assay.title}/
     end
