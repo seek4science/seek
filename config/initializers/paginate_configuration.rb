cbe5454e4a46e972890f426e29372181da4d229a
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-07-09 17:42:56 +0100

    removed the paginate_configuration
    this appears to be an old artifact replaced by Seek::Config.default_pages. If it IS still needed, then its use
    should be replaced by the default_pages config setting

--- config/initializers/paginate_configuration.rb
+++
@@ -1,5 +0,0 @@
-require 'settings'
-class PaginateConfiguration
-  Settings.defaults[:index] = {:people => 'latest', :projects => 'latest', :institutions => 'latest', :investigations => 'latest',:studies => 'latest', :assays => 'latest',
-                    :data_files => 'latest', :models => 'latest',:sops => 'latest', :publications => 'all',:events => 'all'}
-end
