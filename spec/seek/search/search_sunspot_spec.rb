f5b0b20d5908e50282d136558210a1cab213bac9
quyen <quyennguyen128@gmail.com>
2014-07-11 17:12:08 +0200

    spec all the should searchable fields of datafile

--- spec/seek/search/search_sunspot_spec.rb
+++ spec/seek/search/search_sunspot_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 describe DataFile do
+  before(:all) { Seek::Config.solr_enabled = true }
 
-    before(:context) { Seek::Config.solr_enabled = true }
-    context 'when solr is enabled' do
-      it { should have_searchable_field(:title) }
-      it { should have_searchable_field(:description) }
-    end
+  it { should have_searchable_field(:title) }
+  it { should have_searchable_field(:description) }
+  it { should have_searchable_field(:searchable_tags) }
+  it { should have_searchable_field(:contributor) }
+  it { should have_searchable_field(:projects) }
 
+  it { should have_searchable_field(:creators) }
+  it { should have_searchable_field(:other_creators) }
+  it { should have_searchable_field(:content_blob) }
+
+  it { should have_searchable_field(:spreadsheet_annotation_search_fields) }
+  it { should have_searchable_field(:fs_search_fields) }
+  it { should have_searchable_field(:assay_type_titles) }
+  it { should have_searchable_field(:technology_type_titles) }
+  it { should have_searchable_field(:spreadsheet_contents_for_search) }
 end
+
