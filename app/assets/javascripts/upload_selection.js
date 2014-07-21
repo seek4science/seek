ac62b58dc41959cc51e21f9d2f47e8e5131155f1
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-07-21 17:27:11 +0100

    minor tweak to remove the copy option when entering a new url

--- app/assets/javascripts/upload_selection.js
+++ app/assets/javascripts/upload_selection.js
@@ -18,7 +18,9 @@ function setup_url_field(upload_field, examine_url_path,examine_button_id) {
         update_url_checked_status(false);
     });
 };
+
 function submit_url_for_examination() {
+    disallow_copy_option();
     $j('#test_url_result')[0].innerHTML="<p id='large_spinner'/>";
     var data_url = upload_url_field.val();
     $j.post(examine_url_href + "?data_url=" + data_url, function(data){} );
