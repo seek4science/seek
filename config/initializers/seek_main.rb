1228e87086934c7a3d134abf4abf51b2140182f4
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-07-30 19:27:24 +0100

    Removed Asset class
    it appears to be an old unused class

--- config/initializers/seek_main.rb
+++ config/initializers/seek_main.rb
@@ -2,7 +2,6 @@
 #TO MODIFY THE DEFAULT SETTINGS, COPY seek_local.rb.pre to seek_local.rb AND EDIT THAT FILE INSTEAD
 
 require 'object'
-require 'asset'
 require 'active_record_extensions'
 require 'acts_as_taggable_extensions'
 require 'acts_as_isa'
--- test/unit/asset_test.rb
+++ test/unit/asset_test.rb
@@ -151,34 +151,6 @@ class AssetTest < ActiveSupport::TestCase
 
   end
 
-  test "classifying and authorizing resources" do
-    resource_array = []
-    sop=sops(:my_first_sop)
-    model=models(:teusink)
-    data_file=data_files(:picture)
-    user=users(:owner_of_my_first_sop)        
-    
-    sop_version1 = sop.find_version(1)
-    model_version2 = model.find_version(2)
-    
-    resource_array << sop_version1
-    resource_array << model_version2
-    resource_array << data_file
-    
-    assert_equal 1, sop.version
-    assert_equal 2, model.version
-    assert_equal 1, data_file.version
-    
-        
-    result = Asset.classify_and_authorize_resources(resource_array, true, user)    
-    
-    assert_equal 3, result.length
-    
-    assert result["Sop"].include?(sop_version1)    
-    assert result["Model"].include?(model_version2)
-    assert result["DataFile"].include?(data_file)
-  end
-
   test "managers" do
     person=Factory(:person)
     person2=Factory(:person,:first_name=>"fred",:last_name=>"bloggs")
