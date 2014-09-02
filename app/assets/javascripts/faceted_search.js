7e1f83107da34dd8b3032e5ee084fc5b938d3c93
quyen <thucquyendn@gmail.com>
2014-09-02 16:14:55 +0200

    minor fix

--- app/assets/javascripts/faceted_search.js
+++ app/assets/javascripts/faceted_search.js
@@ -78,11 +78,14 @@ function show_specified_facets_for_active_tab(active_tab) {
     var more_facet_id = "specified_" + active_tab + "_facets";
     //(does not work with jquery)
     var more_facet_element = document.getElementById(more_facet_id);
-    more_facet_element.show();
+    if (more_facet_element != null)
+        more_facet_element.show();
 
     //display more-link for the first time
     var more_link = $('more_' + active_tab);
     var less_link = $('less_' + active_tab);
-    if (more_link.offsetParent == null && less_link.offsetParent == null)
-        more_link.show();
+
+    if (more_link != null)
+        if (more_link.offsetParent == null && less_link.offsetParent == null)
+            more_link.show();
 }
