6112f18d1ad25bcc50b8306a6356c37072106638
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-09-04 13:57:15 +0100

    fix to javascript error when validating form fields - SYSMO-1634

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -428,9 +428,7 @@ module ApplicationHelper
         + '&creators=' + encodeURIComponent(getCreators()) + '&contributor_id=' + '#{contributor_id}' + '&resource_name=' + '#{resource_name}' + '&resource_id=' + '#{resource_id}' + '&is_new_file=' + '#{is_new_file}'"},
       { :id => 'preview_permission',
         :style => 'display:none'
-      } #,
-      #:alt => "Click to create a new favourite group (opens popup window)",#options[:tooltip_text],
-      #:title => tooltip_title_attrib("Opens a popup window, where you can create a new favourite<br/>group, add people to it and set individual access rights.") }  #options[:tooltip_text]
+      }
     )
   end
   #Return whether or not to hide contact details from this user
