3002ed4d34a5bd453b83dcaec79934ca88c3e059
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-09-05 18:02:14 +0100

    Some minor refactoring and code tidying

--- app/models/person.rb
+++ app/models/person.rb
@@ -470,27 +470,28 @@ class Person < ActiveRecord::Base
   end
 
   def orcid_id_must_be_valid_or_blank
-    valid = true
-    unless orcid.blank? #blank or nil is OK
-
-      id = orcid
-      id = orcid.gsub("http://orcid.org/","") if orcid.start_with?("http://orcid.org")
-      #check structure with regular expression
-      if id =~ /[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9,X]{4}/
-        id = id.gsub("-","")
-
-        #calculating the checksum according to ISO/IEC 7064:2003, MOD 11-2 ; see - http://support.orcid.org/knowledgebase/articles/116780-structure-of-the-orcid-identifier
-        total=0
-        (0...15).each{|x| total = (total + id[x].to_i) * 2}
-        remainder = total % 11
-        result = (12 - remainder) % 11
-        result = result == 10 ? "X" : result.to_s
-        valid = id[15] == result
-      else
-        valid = false
-      end
-      errors.add("Orcid identifier"," isn't a valid ORCID identifier.") unless valid
+    unless orcid.blank? || valid_orcid_id?(orcid.gsub("http://orcid.org/",""))
+        errors.add("Orcid identifier"," isn't a valid ORCID identifier.")
     end
+  end
+
+  #checks the structure of the id, and whether is conforms to ISO/IEC 7064:2003
+  def valid_orcid_id? id
+    if id =~ /[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9,X]{4}/
+      id = id.gsub("-","")
+      id[15] == orcid_checksum(id)
+    else
+      false
+    end
+  end
+
+  #calculating the checksum according to ISO/IEC 7064:2003, MOD 11-2 ; see - http://support.orcid.org/knowledgebase/articles/116780-structure-of-the-orcid-identifier
+  def orcid_checksum(id)
+    total=0
+    (0...15).each { |x| total = (total + id[x].to_i) * 2 }
+    remainder = total % 11
+    result = (12 - remainder) % 11
+    result == 10 ? "X" : result.to_s
   end
 
   include Seek::ProjectHierarchies::PersonExtension if Seek::Config.project_hierarchy_enabled
