91f820245af116941952df044aceceaa379cb468
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-09-11 14:13:29 +0100

    rejigging migrations slightly with respect to treatments
    to regain consistency between seek and vln-seek.
    If treatment_type_id already exists, then it is renamed to measured_item_id instead of being dropped.
    add incubation_time and incubation_time_unit_if they don't exist.

--- db/migrate/20140326133055_add_new_treatment_fields.rb
+++ db/migrate/20140326133055_add_new_treatment_fields.rb
@@ -1,7 +1,7 @@
 class AddNewTreatmentFields < ActiveRecord::Migration
 
   def change
-    add_column :treatments, :measured_item_id, :integer if !column_exists?(:treatments,:sample_id,:integer)
+    add_column :treatments, :measured_item_id, :integer if !column_exists?(:treatments,:measured_item_id,:integer)
     add_column :treatments, :start_value,:float if !column_exists?(:treatments,:start_value,:float)
     add_column :treatments, :end_value, :float if !column_exists?(:treatments,:end_value,:float)
     add_column :treatments, :standard_deviation, :float if !column_exists?(:treatments,:standard_deviation,:float)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140908142454) do
+ActiveRecord::Schema.define(:version => 20140911131032) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
@@ -1712,6 +1712,8 @@ ActiveRecord::Schema.define(:version => 20140908142454) do
     t.string   "medium_title"
     t.float    "time_after_treatment"
     t.integer  "time_after_treatment_unit_id"
+    t.float    "incubation_time"
+    t.float    "incubation_time_unit_id"
   end
 
   create_table "units", :force => true do |t|
