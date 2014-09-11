f27060d7287c295fa5867302c91f759ac6025bb7
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-09-11 14:42:23 +0100

    fix schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140908142454) do
+ActiveRecord::Schema.define(:version => 20140911131032) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
