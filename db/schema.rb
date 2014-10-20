425fb10cb205de2789c177bf28ed848dab8b7b4e
quyen <thucquyendn@gmail.com>
2014-10-20 15:09:15 +0200

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -688,9 +688,9 @@ ActiveRecord::Schema.define(:version => 20141016093319) do
     t.text     "other_creators"
     t.string   "uuid"
     t.integer  "policy_id"
-    t.integer  "model_image_id"
     t.string   "imported_source"
     t.string   "imported_url"
+    t.integer  "model_image_id"
   end
 
   add_index "model_versions", ["contributor_id", "contributor_type"], :name => "index_model_versions_on_contributor_id_and_contributor_type"
@@ -718,9 +718,9 @@ ActiveRecord::Schema.define(:version => 20141016093319) do
     t.text     "other_creators"
     t.string   "uuid"
     t.integer  "policy_id"
-    t.integer  "model_image_id"
     t.string   "imported_source"
     t.string   "imported_url"
+    t.integer  "model_image_id"
   end
 
   add_index "models", ["contributor_id", "contributor_type"], :name => "index_models_on_contributor_id_and_contributor_type"
@@ -1177,9 +1177,9 @@ ActiveRecord::Schema.define(:version => 20141016093319) do
     t.string   "provider_name"
     t.string   "age_at_sampling"
     t.string   "uuid"
+    t.integer  "age_at_sampling_unit_id"
     t.string   "sample_type"
     t.string   "treatment"
-    t.integer  "age_at_sampling_unit_id"
   end
 
   create_table "samples_tissue_and_cell_types", :id => false, :force => true do |t|
@@ -1650,10 +1650,10 @@ ActiveRecord::Schema.define(:version => 20141016093319) do
   end
 
   create_table "text_value_versions", :force => true do |t|
-    t.integer  "text_value_id",                            :null => false
-    t.integer  "version",                                  :null => false
+    t.integer  "text_value_id",                          :null => false
+    t.integer  "version",                                :null => false
     t.integer  "version_creator_id"
-    t.text     "text",               :limit => 2147483647, :null => false
+    t.text     "text",               :limit => 16777215, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -1663,7 +1663,7 @@ ActiveRecord::Schema.define(:version => 20141016093319) do
   create_table "text_values", :force => true do |t|
     t.integer  "version"
     t.integer  "version_creator_id"
-    t.text     "text",               :limit => 2147483647, :null => false
+    t.text     "text",               :limit => 16777215, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
