9c35f8c650c1285490fe61fcb69c862b6c18a851
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-10-24 14:19:34 +0100

    code tidying up of models_helper

--- app/helpers/models_helper.rb
+++ app/helpers/models_helper.rb
@@ -1,64 +1,69 @@
 module ModelsHelper
-
-  def model_environment_text model
-    model.recommended_environment ? h(model.recommended_environment.title) : "<span class='none_text'>Not specified</span>".html_safe
+  def model_environment_text(model)
+    environment = model.recommended_environment
+    environment ? environment.title : "<span class='none_text'>Not specified</span>".html_safe
   end
 
-
   def execute_model_label
-    icon_filename=icon_filename_for_key("execute")
-    html = '<span class="icon">' + image_tag(icon_filename,:alt=>"Run",:title=>"Run") + ' Run model</span>';
+    icon_filename = icon_filename_for_key('execute')
+    html = '<span class="icon">' + image_tag(icon_filename, alt: 'Run', title: 'Run') + ' Run model</span>'
     html.html_safe
   end
 
-  def model_type_text model_type
-    return "<span class='none_text'>Not specified</span>".html_safe if model_type.nil?
-    h(model_type.title)
+  def model_type_text(model_type)
+    model_type ? model_type.title : "<span class='none_text'>Not specified</span>".html_safe
   end
 
-  def model_format_text model_format
-    return "<span class='none_text'>Not specified</span>".html_safe if model_format.nil?
-    h(model_format.title)
-  end
-  
-  def authorised_models projects=nil
-    authorised_assets(Model,projects)
+  def model_format_text(model_format)
+    model_format ? model_format.title : "<span class='none_text'>Not specified</span>".html_safe
   end
 
-  def cytoscapeweb_supported? model
-      model.contains_xgmml?
+  def authorised_models(projects = nil)
+    authorised_assets(Model, projects)
   end
 
+  def cytoscapeweb_supported?(model)
+    model.contains_xgmml?
+  end
 
-  def allow_model_comparison model,displayed_model,user=User.current_user
-    return false unless model.is_a?(Model)
-    return false unless model.can_download?(user)
-    return false unless displayed_model.contains_sbml?
-    return false unless model.versions.select{|v| v.contains_sbml?}.count > 1
+  def allow_model_comparison(model, displayed_model, user = User.current_user)
+    return false unless model.is_a?(Model) && model.can_download?(user) && displayed_model.contains_sbml?
+    return false unless model.versions.select { |version| version.contains_sbml? }.count > 1
     true
   end
 
-  def compare_model_version_selection versioned_model, displayed_resource_version
-    versions=versioned_model.versions.reverse
-    disabled=versions.size==1
-    options=""
-    versions.each do |v|
-      if (v.version==displayed_resource_version.version || !v.contains_sbml?)
-        options << "<option value='' disabled"
-      else
-        compare_path = compare_versions_model_path(versioned_model,:version=>displayed_resource_version.version,:other_version=>v.version)
-        options << "<option value='#{compare_path}'"
-      end
-
-      options << " selected='selected'" if v.version==displayed_resource_version.version
-      text = "#{v.version.to_s} #{versioned_model.describe_version(v.version)}"
-      options << "> #{text} </option>"
+  def compare_model_version_selection(versioned_model, displayed_resource_version)
+    versions = versioned_model.versions.reverse
+    disabled = versions.size == 1
+    options = ''
+    versions.each do |model_version|
+      options << generate_model_version_comparison_option(displayed_resource_version, model_version, versioned_model)
     end
+    compare_model_version_select_tag(disabled, options)
+  end
+
+  def compare_model_version_select_tag(disabled, options)
     select_tag(:compare_versions,
                options.html_safe,
-               :disabled=>disabled,
-               :onchange=>"showCompareVersions($('compare_versions_form'));"
+               disabled: disabled,
+               onchange: "showCompareVersions($('compare_versions_form'));"
     ) + "<form id='compare_versions_form' onsubmit='showCompareVersions(this); return false;'></form>".html_safe
   end
 
+  def generate_model_version_comparison_option(displayed_model_version, other_model_version, versioned_model)
+    options = ''
+    other_version = other_model_version.version
+    displayed_version = displayed_model_version.version
+    if other_version == displayed_version.version || !other_model_version.contains_sbml?
+      options << "<option value='' disabled"
+    else
+      compare_path = compare_versions_model_path(versioned_model, version: displayed_version, other_version: other_version)
+      options << "<option value='#{compare_path}'"
+    end
+
+    options << " selected='selected'" if other_version == displayed_version
+    text = "#{other_version} #{versioned_model.describe_version(other_version)}"
+    options << "> #{text} </option>"
+    options
+  end
 end
