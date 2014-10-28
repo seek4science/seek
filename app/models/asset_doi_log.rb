e0951ff37979271ea3a754b6c96af446f5a62ced
quyen <thucquyendn@gmail.com>
2014-10-28 17:20:13 +0100

    add associations

--- app/models/asset_doi_log.rb
+++ app/models/asset_doi_log.rb
@@ -1,3 +1,8 @@
 class AssetDoiLog < ActiveRecord::Base
   attr_accessible :action, :asset_id, :asset_type, :asset_version, :comment
+
+  belongs_to :asset, :polymorphic => true #, :required_access => false
+  belongs_to :user #, :required_access => false
+
+  ACTIONS = %w(mint, delete, un-publish)
 end
