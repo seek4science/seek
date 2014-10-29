72918fd3ec8a1ef22fb3b85f471dd2bae9850b0c
quyen <thucquyendn@gmail.com>
2014-10-29 15:40:41 +0100

    is_doi_minted_for?

--- app/models/asset_doi_log.rb
+++ app/models/asset_doi_log.rb
@@ -7,4 +7,8 @@ class AssetDoiLog < ActiveRecord::Base
   MINT = 1
   DELETE = 2
   UNPUBLISH = 3
+
+  def self.is_doi_minted_for?(asset_type, asset_id)
+    !AssetDoiLog.where(asset_type: asset_type, asset_id: asset_id, action: AssetDoiLog::MINT).empty?
+  end
 end
--- test/unit/asset_doi_log_test.rb
+++ test/unit/asset_doi_log_test.rb
@@ -4,4 +4,13 @@ class AssetDoiLogTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "is_doi_minted_for?" do
+    df = Factory :data_file
+    another_df = Factory :data_file
+    AssetDoiLog.create(:asset_type => df.class.name, :asset_id => df.id, :action => AssetDoiLog::MINT)
+    assert AssetDoiLog.is_doi_minted_for?(df.class.name, df.id)
+    assert !AssetDoiLog.is_doi_minted_for?(another_df.class.name, another_df.id)
+  end
+
 end
