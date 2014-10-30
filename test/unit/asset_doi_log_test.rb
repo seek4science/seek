746f82ec247d47d87a2dc9ca35fe9a99d22b3ffe
quyen <thucquyendn@gmail.com>
2014-10-30 14:15:09 +0100

    add version to was_doi_minted_for? method

--- test/unit/asset_doi_log_test.rb
+++ test/unit/asset_doi_log_test.rb
@@ -5,12 +5,12 @@ class AssetDoiLogTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test "was_doi_minted_for??" do
+  test "was_doi_minted_for?" do
     df = Factory :data_file
     another_df = Factory :data_file
-    AssetDoiLog.create(:asset_type => df.class.name, :asset_id => df.id, :action => AssetDoiLog::MINT)
-    assert AssetDoiLog.was_doi_minted_for?(df.class.name, df.id)
-    assert !AssetDoiLog.was_doi_minted_for?(another_df.class.name, another_df.id)
+    AssetDoiLog.create(:asset_type => df.class.name, :asset_id => df.id, :asset_version => df.version, :action => AssetDoiLog::MINT)
+    assert AssetDoiLog.was_doi_minted_for?(df.class.name, df.id, df.version)
+    assert !AssetDoiLog.was_doi_minted_for?(another_df.class.name, another_df.id, df.version)
   end
 
 end
