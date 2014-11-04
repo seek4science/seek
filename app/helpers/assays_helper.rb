6cfc5f605eeaf53eb920ea8d47b266b0ea074688
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-11-04 17:20:04 +0000

    removed the old ontology view helper module
    no longer used.

--- app/helpers/assays_helper.rb
+++ app/helpers/assays_helper.rb
@@ -1,8 +1,4 @@
-require 'acts_as_ontology_view_helper'
-
 module AssaysHelper
-
-  include Stu::Acts::Ontology::ActsAsOntologyViewHelper
 
   #assays that haven't already been associated with a study
   def assays_available_for_study_association
