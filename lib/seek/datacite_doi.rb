6be98404424dc0ec66146c818777709f13f357fc
quyen <thucquyendn@gmail.com>
2014-11-07 16:36:05 +0100

    some server code

--- lib/seek/datacite_doi.rb
+++ lib/seek/datacite_doi.rb
@@ -1,13 +1,15 @@
 module Seek
   module DataciteDoi
     def self.included(base)
-      base.before_filter :set_asset
+      base.before_filter :set_asset_version
       base.before_filter :mint_doi_auth
       base.after_filter :log_minting_doi, :only=>[:mint]
     end
 
     def mint_doi_preview
-
+      respond_to do |format|
+        format.html { render :template => "datacite_doi/mint_doi_preview"}
+      end
     end
 
     def mint
@@ -32,8 +34,19 @@ module Seek
 
     private
 
-    def set_asset
-      #with version
+    def set_asset_version
+      begin
+        asset = self.controller_name.classify.constantize.find(params[:id])
+        if params[:version]
+          #find version
+          #TODO @asset_version could be nil
+          @asset_version = asset.find_version(params[:version].to_i)
+        else
+          @asset_version = asset.latest_version
+        end
+      rescue ActiveRecord::RecordNotFound
+        error("This resource is not found","not found resource")
+      end
     end
 
     def mint_doi_auth
