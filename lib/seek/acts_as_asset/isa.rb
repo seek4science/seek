e4ef236054e6ea97cdf552510ee8b8d19af70136
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-12-01 15:54:11 +0000

    removing what looks like an unused method

--- lib/seek/acts_as_asset/isa.rb
+++ lib/seek/acts_as_asset/isa.rb
@@ -7,17 +7,6 @@ module Seek
           assays.map { |a| a.study }.uniq
         end
 
-        def project_assays
-          all_assays = Assay.all.select { |assay| assay.can_edit?(User.current_user) }.sort_by &:title
-          all_assays = all_assays.select do |assay|
-            assay.is_modelling?
-          end if self.is_a? Model
-
-          project_assays = all_assays.select { |df| User.current_user.person.projects.include?(df.project) }
-
-          project_assays
-        end
-
         def assay_type_titles
           assays.map { |at| at.try(:assay_type_label) }.compact
         end
