8bfa575dc2505e423764ab4af449d5137ab39cde
Finn Bacall <finn.bacall@manchester.ac.uk>
2014-12-03 14:31:06 +0000

    Search bar length

--- app/assets/javascripts/top_menu.js
+++ app/assets/javascripts/top_menu.js
@@ -13,25 +13,3 @@ function update_menu_text(source_id,fadein) {
 function select_menu_item(section_id) {
     update_menu_text(section_id,false);
 }
-
-
-// handle the width of the search box according to the width of the screen
-Event.observe(window,"resize", function() {
-    set_searchbox_width();
-});
-document.observe("dom:loaded",function() {
-    set_searchbox_width();
-});
-
-function set_searchbox_width() {
-    search_query = $("search_query");
-    if (search_query != null){
-        if (document.viewport.getWidth()<=1199) {
-            search_query.style.width="15em";
-        }
-        else {
-            search_query.style.width="26em";
-        }
-        search_query.show();
-    }
-}
