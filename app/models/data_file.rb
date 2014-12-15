cb58ce9f3d78c1b1f506d2ec19cf252eadfb1435
Stuart Owen <stuart.owen@manchester.ac.uk>
2014-12-15 15:45:01 +0000

    fix for matching models
    even though the button has been removed the route is still available and being hit by google.
    Fix updates after swticing to soffice for indexing, so need to use content_blobs search field rather than spreadsheet_contents_for_search

--- app/models/data_file.rb
+++ app/models/data_file.rb
@@ -198,7 +198,7 @@ class DataFile < ActiveRecord::Base
     results = {}
 
     if Seek::Config.solr_enabled && contains_extractable_spreadsheet?
-      search_terms = spreadsheet_annotation_search_fields | spreadsheet_contents_for_search | fs_search_fields | searchable_tags
+      search_terms = spreadsheet_annotation_search_fields | content_blob | fs_search_fields | searchable_tags
       #make the array uniq! case-insensistive whilst mainting the original case
       dc = []
       search_terms = search_terms.inject([]) do |r,v|
