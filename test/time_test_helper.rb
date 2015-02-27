9b3cd474f8fbb1b85b8ff2364c78b58c8b1a6f83
Stuart Owen <stuart.owen@manchester.ac.uk>
2015-02-27 11:26:52 +0000

    trying to track down test error with time
    now just comment out the changes to Time

--- test/time_test_helper.rb
+++ test/time_test_helper.rb
@@ -21,32 +21,32 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 # end
 #
 #
-# class Test::Unit::TestCase
-#
-#   # to get local/current time with specified time zone, e.g. with_time_zone("UTC")) {Time.now} returns the current time of time zone 'UTC'
-#   def with_time_zone(tz_name)
-#     prev_tz = ENV['TZ']
-#     ENV['TZ'] = tz_name
-#     yield
-#   ensure
-#     ENV['TZ'] = prev_tz
-#   end
-#
-#   def pretend_now_is(time)
-#     begin
-#       Time.testing_offset = Time.now - time
-#       yield
-#     ensure
-#       Time.testing_offset = 0
-#     end
-#   end
-#
-#   def force_now_is(time)
-#     begin
-#       Time.fake_now = time
-#       yield
-#     ensure
-#       Time.fake_now = nil
-#     end
-#   end
-# end
+class Test::Unit::TestCase
+
+  # to get local/current time with specified time zone, e.g. with_time_zone("UTC")) {Time.now} returns the current time of time zone 'UTC'
+  def with_time_zone(tz_name)
+    prev_tz = ENV['TZ']
+    ENV['TZ'] = tz_name
+    yield
+  ensure
+    ENV['TZ'] = prev_tz
+  end
+
+  def pretend_now_is(time)
+    begin
+      Time.testing_offset = Time.now - time
+      yield
+    ensure
+      Time.testing_offset = 0
+    end
+  end
+
+  def force_now_is(time)
+    begin
+      Time.fake_now = time
+      yield
+    ensure
+      Time.fake_now = nil
+    end
+  end
+end
