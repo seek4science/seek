86901c9af123fc285688e4058975f8d0c910b18b
Stuart Owen <stuart.owen@manchester.ac.uk>
2015-03-17 17:02:04 +0000

    a little refactorign of RelatedItemsHelper
    extracting some methods

--- app/helpers/related_items_helper.rb
+++ app/helpers/related_items_helper.rb
@@ -1,5 +1,5 @@
 module RelatedItemsHelper
-  
+
   def prepare_resource_hash(resource_hash, authorization_already_done = false, limit = nil, show_empty_tabs = false)
     perform_authorization(resource_hash) unless authorization_already_done
     limit_items(resource_hash, limit) unless limit.nil?
@@ -10,15 +10,19 @@ module RelatedItemsHelper
   private
 
   def perform_authorization(resource_hash)
-    resource_hash.each_value do |res|
+    resource_hash.each_value do |resource_item|
       unless res[:items].empty?
-        total_count = res[:items].size
-        total = res[:items]
-        res[:items] = res[:items].select &:can_view?
-        res[:hidden_items] = total - res[:items]
-        res[:hidden_count] = total_count - res[:items].size
+        update_resource_items_for_authorization(resource_item)
       end
     end
+  end
+
+  def update_resource_items_for_authorization(resource_item)
+    total_count = resource_item[:items].size
+    total = resource_item[:items]
+    resource_item[:items] = resource_item[:items].select &:can_view?
+    resource_item[:hidden_items] = total - resource_item[:items]
+    resource_item[:hidden_count] = total_count - resource_item[:items].size
   end
 
   def limit_items(resource_hash, limit)
@@ -32,24 +36,30 @@ module RelatedItemsHelper
   end
 
   def sort_items(resource_hash)
-    ordered_keys = resource_hash.keys.sort_by do |asset|
-      ASSET_ORDER.index(asset) || (resource_hash[asset][:is_external] ? 10000 : 1000)
+    ordered_keys(resource_hash).map { |key| resource_hash[key].merge(:type => key) }.each do |resource_type|
+      update_resource_type(resource_type)
     end
+  end
 
-    ordered_keys.map {|key| resource_hash[key].merge(:type => key)}.each do |resource_type|
-      resource_type[:hidden_count] ||= 0
-      resource_type[:items] || []
-      resource_type[:extra_count] ||= 0
-      resource_type[:is_external] ||= false
+  def update_resource_type(resource_type)
+    resource_type[:hidden_count] ||= 0
+    resource_type[:items] || []
+    resource_type[:extra_count] ||= 0
+    resource_type[:is_external] ||= false
 
-      resource_type[:visible_resource_type] = internationalized_resource_name(resource_type[:type],!resource_type[:is_external] )
-      resource_type[:tab_title] = "#{resource_type[:visible_resource_type]} "+
-          "(#{(resource_type[:items].length+resource_type[:extra_count]).to_s}" +
-          ((resource_type[:hidden_count]) > 0 ? "+#{resource_type[:hidden_count]}":"") + ")"
+    resource_type[:visible_resource_type] = internationalized_resource_name(resource_type[:type], !resource_type[:is_external])
+    resource_type[:tab_title] = "#{resource_type[:visible_resource_type]} "+
+        "(#{(resource_type[:items].length+resource_type[:extra_count]).to_s}" +
+        ((resource_type[:hidden_count]) > 0 ? "+#{resource_type[:hidden_count]}" : "") + ")"
 
-      resource_type[:tab_id] = resource_type[:type].downcase.pluralize.html_safe
-      resource_type[:title_class] = resource_type[:is_external] ? "external_result" : ""
-      resource_type[:total_visible] = resource_type[:items].count + resource_type[:extra_count]
+    resource_type[:tab_id] = resource_type[:type].downcase.pluralize.html_safe
+    resource_type[:title_class] = resource_type[:is_external] ? "external_result" : ""
+    resource_type[:total_visible] = resource_type[:items].count + resource_type[:extra_count]
+  end
+
+  def ordered_keys(resource_hash)
+    resource_hash.keys.sort_by do |asset|
+      ASSET_ORDER.index(asset) || (resource_hash[asset][:is_external] ? 10000 : 1000)
     end
   end
 
@@ -58,5 +68,5 @@ module RelatedItemsHelper
       resource_hash.delete(key) if (res[:items].size + res[:hidden_count]) == 0
     end
   end
-  
+
 end
