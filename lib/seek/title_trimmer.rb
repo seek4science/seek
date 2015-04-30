90a5a393aee77fdc5ba714cfdb6f0e474527a160
Stuart Owen <stuart.owen@manchester.ac.uk>
2015-04-30 17:29:07 +0100

    test fix

--- lib/seek/title_trimmer.rb
+++ lib/seek/title_trimmer.rb
@@ -1,13 +1,11 @@
-#A common pattern to trim titles before the are saved. This is used in most assets
+# A common pattern to trim titles before the are saved. This is used in most assets
 module Seek
   module TitleTrimmer
-
     def self.included(mod)
       mod.extend(ClassMethods)
     end
 
     module ClassMethods
-
       def title_trimmer
         before_save :trim_title
         include Seek::TitleTrimmer::InstanceMethods
@@ -16,14 +14,11 @@ module Seek
 
     module InstanceMethods
       def trim_title
-        self.title=title.strip unless title.nil?
+        self.title = title.strip if has_attribute?(:title) && title
       end
     end
-
   end
 end
-
-
 
 ActiveRecord::Base.class_eval do
   include Seek::TitleTrimmer
