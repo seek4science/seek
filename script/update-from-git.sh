127943e0c54998c5e7459bd434cb1e083d29f2e0
Stuart Owen <stuart.owen@manchester.ac.uk>
2015-06-08 17:03:10 +0100

    improved description in update script

--- script/update-from-git.sh
+++ script/update-from-git.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
-# use for updating small changes for fairdomhub from git repo
+# for small incremental updates from git. For full upgrades please see http://seek4science.org/installing
+# should be run from the root folder of the SEEK installation
 
 set -e
 
@@ -18,11 +19,10 @@ git pull
 cd .. && cd - #this is to allow RVM to pick up the ruby and gemset changes
 echo "${GREEN}bundle install${NC}"
 bundle install --deployment
-echo "${GREEN} db:migrate${NC}"
-bundle exec rake db:migrate
+echo "${GREEN} seek:upgrade${NC}"
+bundle exec rake seek:upgrade
 echo "${GREEN} precompile assets${NC}"
 bundle exec rake assets:precompile # this task will take a while
-bundle exec rake seek:reindex_all
 
 bundle exec rake sunspot:solr:start
 bundle exec rake seek:workers:start
