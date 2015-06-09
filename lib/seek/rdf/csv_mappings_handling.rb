89e20f86c4159367abe1645af85bb61fd97ae167
Stuart Owen <stuart.owen@manchester.ac.uk>
2015-06-09 13:48:56 +0100

    OPSK-360 - fix to dc:hasVersion
    should be a uri rather than a literal

---
+++ lib/seek/rdf/csv_mappings_handling.rb
@@ -0,0 +1,79 @@
+module Seek
+  module Rdf
+    module CSVMappingsHandling
+      MAPPINGS_FILE = File.join(File.dirname(__FILE__), 'rdf_mappings.csv')
+      def generate_from_csv_definitions(rdf_graph)
+        # load template
+        rows = Rails.cache.fetch('rdf_definitions', expires_in: 1.hour) do
+          CSV.read(MAPPINGS_FILE)
+        end
+        rows.each do |row|
+          unless row[0].downcase == 'class'
+            rdf_graph = generate_for_csv_row(rdf_graph, row)
+          end
+        end
+        rdf_graph
+      end
+
+      def generate_for_csv_row(rdf_graph, row)
+        klass = row[0].strip
+        method = row[1]
+        property = row[2]
+        uri_or_literal = row[3].downcase
+        transform = row[4]
+        collection_transform = row[5]
+        if (klass == '*' || self.class.name == klass) && self.respond_to?(method)
+          rdf_graph = generate_triples_for_csv_row(self, method, property, uri_or_literal, transform, collection_transform, rdf_graph)
+        elsif self.class.name == klass # matched the class but the method isnt found
+          Rails.logger.warn "Expected to find method #{method} for class #{klass}"
+        end
+        rdf_graph
+      end
+
+      def generate_triples_for_csv_row(subject, method, property, uri_or_literal, transformation, collection_transform, rdf_graph)
+        resource = subject.rdf_resource
+        items = subject.send(method)
+        #may be an array of items or a single item. Cant use Array(item) or [*item] here cos it screws up times and datetimes
+        items = [items] unless items.is_a?(Array)
+
+        transformation.strip! if transformation
+        collection_transform.strip! if collection_transform
+        unless collection_transform.blank?
+          items = eval("items.#{collection_transform}")
+        end
+        items.each do |item|
+          property_uri = eval(property)
+          item = eval(transformation) unless transformation.blank?
+          o = if uri_or_literal.downcase == 'u'
+                handle_uri_for_item(item)
+              else
+                handle_literal_for_item(item)
+              end
+          rdf_graph << [resource, property_uri, o] unless o.nil?
+        end
+        rdf_graph
+      end
+
+      def handle_literal_for_item(item)
+        item.nil? ? '' : item
+      end
+
+      def handle_uri_for_item(item)
+        if item.respond_to?(:rdf_resource)
+          item.rdf_resource
+        elsif item.class.name.end_with?('::Version') # FIXME: this should be put into explicit_versioning
+          uri = item.parent.rdf_resource
+          uri.query = "version=?#{item.version}"
+          uri
+        else
+          uri = RDF::URI.new(item)
+          begin
+            uri.validate!
+          rescue
+            nil
+          end
+        end
+      end
+    end
+  end
+end
--- lib/seek/rdf/rdf_generation.rb
+++ lib/seek/rdf/rdf_generation.rb
@@ -1,11 +1,11 @@
 require 'csv'
 
-
 module Seek
   module Rdf
     module RdfGeneration
       include RdfRepositoryStorage
       include RightField
+      include CSVMappingsHandling
 
       def self.included(base)
         base.after_save :create_rdf_generation_job
@@ -14,7 +14,7 @@ module Seek
 
       def to_rdf
         rdf_graph = to_rdf_graph
-        RDF::Writer.for(:rdfxml).buffer(:prefixes=>ns_prefixes) do |writer|
+        RDF::Writer.for(:rdfxml).buffer(prefixes: ns_prefixes) do |writer|
           rdf_graph.each_statement do |statement|
             writer << statement
           end
@@ -29,12 +29,12 @@ module Seek
         rdf_graph
       end
 
-      def handle_rightfield_contents object
+      def handle_rightfield_contents(object)
         graph = nil
-        if (object.respond_to?(:contains_extractable_spreadsheet?) && object.contains_extractable_spreadsheet?)
+        if object.respond_to?(:contains_extractable_spreadsheet?) && object.contains_extractable_spreadsheet?
           begin
             graph = generate_rightfield_rdf_graph(self)
-          rescue Exception=>e
+          rescue Exception => e
             Rails.logger.error "Error generating RightField part of rdf for #{object} - #{e.message}"
           end
         end
@@ -42,109 +42,48 @@ module Seek
       end
 
       def rdf_resource
-        uri = Seek::Config.site_base_host+"/#{self.class.name.tableize}/#{self.id}"
+        uri = Seek::Config.site_base_host + "/#{self.class.name.tableize}/#{id}"
         RDF::Resource.new(uri)
       end
 
-
-      #extra steps that cannot be easily handled by the csv template
-      def additional_triples rdf_graph
+      # extra steps that cannot be easily handled by the csv template
+      def additional_triples(rdf_graph)
         if self.is_a?(Model) && self.contains_sbml?
-          rdf_graph << [self.rdf_resource,JERMVocab.hasFormat,JERMVocab.SBML_format]
+          rdf_graph << [rdf_resource, JERMVocab.hasFormat, JERMVocab.SBML_format]
         end
         rdf_graph
       end
 
-      def generate_from_csv_definitions rdf_graph
-        #load template
-        path_to_template=File.join(File.dirname(__FILE__), "rdf_mappings.csv")
-        rows = Rails.cache.fetch("rdf_definitions",:expires_in=>1.hour) do
-          CSV.read(path_to_template)
-        end
-        rows.each do |row|
-          unless row[0].downcase=="class"
-            klass=row[0].strip
-            method=row[1]
-            property=row[2]
-            uri_or_literal=row[3].downcase
-            transform=row[4]
-            collection_transform=row[5]
-            if (klass=="*" || self.class.name==klass) && self.respond_to?(method)
-              rdf_graph = generate_triples(self,method,property,uri_or_literal,transform,collection_transform,rdf_graph)
-            elsif self.class.name==klass #matched the class but the method isnt found
-              puts "WARNING: Expected to find method #{method} for class #{klass}"
-            end
-          end
+      def describe_type(rdf_graph)
+        it_is = JERMVocab.for_type self
+        unless it_is.nil?
+          resource = rdf_resource
+          rdf_graph << [resource, RDF.type, it_is]
         end
         rdf_graph
       end
 
-      def generate_triples subject, method, property,uri_or_literal,transformation,collection_transform,rdf_graph
-        resource = subject.rdf_resource
-        transform = transformation.strip unless transformation.nil?
-        collection_transform = collection_transform.strip unless collection_transform.nil?
-        items = subject.send(method)
-        items = [items] unless items.kind_of?(Array) #may be an array of items or a single item. Cant use Array(item) here cos it screws up timezones and strips out nils
-        unless collection_transform.blank?
-          items = eval("items.#{collection_transform}")
-        end
-        items.each do |item|
-          property_uri = eval(property)
-          if !transformation.blank?
-            item = eval(transformation)
-          end
-          o = if uri_or_literal.downcase=="u"
-                if item.respond_to?(:rdf_resource)
-                  item.rdf_resource
-                else
-                  uri = RDF::URI.new(item)
-                  begin
-                    uri.validate!
-                  rescue
-                    nil
-                  end
-                end
-          else
-            item.nil? ? "" : item
-          end
-          unless o.nil?
-            rdf_graph << [resource,property_uri,o]
-          end
-
-        end
-        rdf_graph
-      end
-
-      def describe_type rdf_graph
-       it_is = JERMVocab.for_type self
-       unless it_is.nil?
-         resource = self.rdf_resource
-         rdf_graph <<  [resource,RDF.type,it_is]
-       end
-       rdf_graph
-      end
-
-      #the hash of namespace prefixes to pass to the RDF::Writer when generating the RDF
+      # the hash of namespace prefixes to pass to the RDF::Writer when generating the RDF
       def ns_prefixes
         {
-            "jerm"=>JERMVocab.to_uri.to_s,
-            "dcterms"=>RDF::DC.to_uri.to_s,
-            "owl"=>RDF::OWL.to_uri.to_s,
-            "foaf"=>RDF::FOAF.to_uri.to_s,
-            "sioc"=>RDF::SIOC.to_uri.to_s,
-            "owl"=>RDF::OWL.to_uri.to_s,
+          'jerm' => JERMVocab.to_uri.to_s,
+          'dcterms' => RDF::DC.to_uri.to_s,
+          'owl' => RDF::OWL.to_uri.to_s,
+          'foaf' => RDF::FOAF.to_uri.to_s,
+          'sioc' => RDF::SIOC.to_uri.to_s,
+          'owl' => RDF::OWL.to_uri.to_s
         }
       end
 
-      def create_rdf_generation_job force=false,refresh_dependents=true
-        unless !force && (self.changed - ["updated_at","last_used_at"]).empty?
-          RdfGenerationJob.new(self,refresh_dependents).queue_job
+      def create_rdf_generation_job(force = false, refresh_dependents = true)
+        unless !force && (changed - %w(updated_at last_used_at)).empty?
+          RdfGenerationJob.new(self, refresh_dependents).queue_job
         end
       end
 
       def remove_rdf
-        self.remove_rdf_from_repository if self.rdf_repository_configured?
-        self.delete_rdf_file
+        remove_rdf_from_repository if self.rdf_repository_configured?
+        delete_rdf_file
         refresh_dependents_rdf
       end
 
@@ -156,28 +95,26 @@ module Seek
 
       def dependent_items
         items = []
-        #FIXME: this should go into a seperate mixin for active-record
-        methods=[:data_files,:sops,:models,:publications,
-                 :data_file_masters, :sop_masters, :model_masters,
-                 :assets,
-                 :assays, :studies, :investigations,
-                 :institutions, :creators, :owners,:owner, :contributors, :contributor,:projects, :events, :presentations,
-                 :samples, :specimens, :compounds, :organisms, :strains,
-                ]
+        # FIXME: this should go into a seperate mixin for active-record
+        methods = [:data_files, :sops, :models, :publications,
+                   :data_file_masters, :sop_masters, :model_masters,
+                   :assets,
+                   :assays, :studies, :investigations,
+                   :institutions, :creators, :owners, :owner, :contributors, :contributor, :projects, :events, :presentations,
+                   :samples, :specimens, :compounds, :organisms, :strains
+                  ]
         methods.each do |method|
           if self.respond_to?(method)
-            deps = Array(self.send(method))
-            #resolve User back to Person
-            deps = deps.collect{|dep| dep.is_a?(User) ? [dep, dep.person] : dep }.flatten.compact
-            items = items | deps
+            deps = Array(send(method))
+            # resolve User back to Person
+            deps = deps.collect { |dep| dep.is_a?(User) ? [dep, dep.person] : dep }.flatten.compact
+            items |= deps
           end
         end
 
         items.compact.uniq
 
-        if (self.rdf_repository_configured?)
-          items = items | related_items_from_sparql
-        end
+        items |= related_items_from_sparql if self.rdf_repository_configured?
 
         items.compact.uniq
       end
@@ -185,7 +122,6 @@ module Seek
       def refresh_rdf
         create_rdf_generation_job(true, false)
       end
-
     end
   end
 end
