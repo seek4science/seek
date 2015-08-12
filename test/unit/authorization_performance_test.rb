2b1df489c39c1c9205eb4cbc0ad7df5ac16ab22d
quyen <thucquyendn@gmail.com>
2015-08-12 15:16:19 +0200

    use ruby-prof to profile authorizing datafiles

---
+++ test/unit/authorization_performance_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+require 'ruby-prof'
+
+class AuthorizationPerformanceTest < ActiveSupport::TestCase
+
+  test 'profile authorizing data file' do
+    user = Factory(:user)
+    data_files = []
+    i = 0
+    while i < 10 do
+      data_files << Factory(:data_file, :title => "data file #{i}")
+      i+=1
+    end
+
+    result = RubyProf.profile do
+      data_files.each do |data_file|
+        data_file.can_view?(user)
+      end
+    end
+
+    printer = RubyProf::FlatPrinter.new(result)
+    printer.print(STDOUT, :min_percent => 1)
+  end
+end
