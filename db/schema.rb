2c529842f8897a3ec8762f74bd40f6e51289fd5f
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-08-17 15:51:48 +0100

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150804121500) do
+ActiveRecord::Schema.define(:version => 20150817133253) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
@@ -1265,7 +1265,8 @@ ActiveRecord::Schema.define(:version => 20150804121500) do
     t.integer  "snapshot_number"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
-    t.string   "zenodo_deposition_id"
+    t.integer  "zenodo_deposition_id"
+    t.string   "zenodo_record_url"
   end
 
   create_table "sop_auth_lookup", :id => false, :force => true do |t|
