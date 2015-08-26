fa75908fbc4ef29310a443ac1251c6ea0376b452
Eilidh Troup <e.troup@epcc.ed.ac.uk>
2015-08-26 13:14:02 +0100

    Added Accessable as a default option. If the item is not downloadable, accessible will change to visible as the default option.

---
+++ test/unit/helpers/policy_helper_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+class PolicyHelperTest < ActionView::TestCase
+
+  test "policy selection options accessible removed if not downloadable" do
+    policies = [Policy::NO_ACCESS, Policy::VISIBLE, Policy::ACCESSIBLE, Policy::EDITING]
+    resource = nil # not downloadable
+    selected_access_type = Policy::NO_ACCESS
+    actual_options = policy_selection_options policies, resource, selected_access_type
+
+    expected_options = "<option value='0' selected='selected'>No access </option><option value='1' >View summary </option><option value='3' >View and edit summary </option>"
+
+    assert_equal expected_options, actual_options
+  end
+
+  #  handle access_type = ACCESSIBLE, and !resource.is_downloadable?
+  #  Seek::Config.default :default_projects_access_type = @resource.is_asset? ? Policy::ACCESSIBLE : Policy::VISIBLE
+  test "policy selection options handles access type accessible selected but item not downloadable" do
+    policies = [Policy::NO_ACCESS, Policy::VISIBLE, Policy::ACCESSIBLE, Policy::EDITING]
+    resource = nil # not downloadable
+    selected_access_type = Policy::ACCESSIBLE
+    actual_options = policy_selection_options policies, resource, selected_access_type
+
+    expected_options = "<option value='0' >No access </option><option value='1' selected='selected'>View summary </option><option value='3' >View and edit summary </option>"
+
+    assert_equal expected_options, actual_options, "default selected access type should change from accessible
+to visible if item is not downloadable and the accessible option is removed."
+  end
+
+  test "policy selection options keeps access type accessible selected when item is downloadable" do
+    policies = [Policy::NO_ACCESS, Policy::VISIBLE, Policy::ACCESSIBLE, Policy::EDITING]
+    resource = DataFile.new # is downloadable
+    selected_access_type = Policy::ACCESSIBLE
+    actual_options = policy_selection_options policies, resource, selected_access_type
+
+    expected_options = "<option value='0' >No access </option><option value='1' >View summary only </option><option value='2' selected='selected'>View summary and get contents </option><option value='3' >View and edit summary and contents </option>"
+
+    assert_equal expected_options, actual_options, "default selected access type should change from accessible
+to visible if item is not downloadable and the accessible option is removed."
+  end
+
+end
