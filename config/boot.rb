81a001a8e87a09eb62aa2bce6b5ae5bd0ed6c462
Eilidh Troup <e.troup@epcc.ed.ac.uk>
2015-09-04 14:52:37 +0100

    Changed default port back to 3000.

--- config/boot.rb
+++ config/boot.rb
@@ -15,7 +15,7 @@ module Rails
     alias :default_options_alias :default_options
 
     def default_options
-      default_options_alias.merge!(:Port => 8550)
+      default_options_alias.merge!(:Port => 3000)
     end
   end
 end
