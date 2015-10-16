a4d879e078630fa6f05bb5e5635518d1aac1dd1e
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-10-16 22:06:32 +0100

    Split up big method in Streamer

--- lib/seek/download_handling/streamer.rb
+++ lib/seek/download_handling/streamer.rb
@@ -16,31 +16,41 @@ module Seek
       def stream(&block)
         get_uri(URI(@url), &block)
       end
-\
+
       private
 
       def get_uri(uri, redirect_count = 0, &block)
-        max_size = Seek::Config.hard_max_cachable_size
         Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
-          http.request(Net::HTTP::Get.new(uri)) do |res|
-            if res.code == '200'
-              total_size = 0
-              res.read_body do |chunk|
-                total_size += chunk.size
-                raise SizeLimitExceededException.new(total_size) if total_size > max_size
-                block.call(chunk)
-              end
-              total_size
-            elsif res.code == '301' || res.code == '302'
-              if redirect_count >= REDIRECT_LIMIT
-                raise RedirectLimitExceededException.new(redirect_count)
-              else
-                get_uri(URI(res.header['location']), redirect_count+1, &block)
-              end
+          http.request(Net::HTTP::Get.new(uri)) do |response|
+            if response.code == '200'
+              begin_stream(response, block)
+            elsif response.code == '301' || response.code == '302'
+              follow_redirect(response, redirect_count)
             else
-              raise BadResponseCodeException.new(res)
+              raise BadResponseCodeException.new(response)
             end
           end
+        end
+      end
+
+      def begin_stream(response, block)
+        max_size = Seek::Config.hard_max_cachable_size
+        total_size = 0
+
+        response.read_body do |chunk|
+          total_size += chunk.size
+          raise SizeLimitExceededException.new(total_size) if total_size > max_size
+          block.call(chunk)
+        end
+
+        total_size
+      end
+
+      def follow_redirect(response, redirect_count)
+        if redirect_count >= REDIRECT_LIMIT
+          raise RedirectLimitExceededException.new(redirect_count)
+        else
+          get_uri(URI(response.header['location']), redirect_count + 1, &block)
         end
       end
 
