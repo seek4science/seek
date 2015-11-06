e42bc8e3aceae4b0c93fb421256eaa1a1982813c
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-11-06 15:48:45 +0000

    Remove a user's project roles when they are removed from a project

--- app/models/group_membership.rb
+++ app/models/group_membership.rb
@@ -3,7 +3,7 @@ class GroupMembership < ActiveRecord::Base
   belongs_to :work_group
   has_one :project, :through=>:work_group
 
-  has_many :group_memberships_project_roles
+  has_many :group_memberships_project_roles, :dependent => :destroy
   has_many :project_roles, :through => :group_memberships_project_roles
 
   after_save :remember_previous_person
