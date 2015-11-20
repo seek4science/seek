ff09e205728e53cdf8b1e4bf16b0bee6203bdffc
Alan R Williams <alan.r.williams@manchester.ac.uk>
2015-11-20 13:49:23 +0100

    Updated for internal/external help

--- test/functional/help_documents_controller_test.rb
+++ test/functional/help_documents_controller_test.rb
@@ -10,21 +10,33 @@ class HelpDocumentsControllerTest < ActionController::TestCase
     login_as(:quentin)
   end
 
-  test "should be available to non logged in users" do
+  test "should be available to non logged in users for internal help" do
     logout
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:help_documents)
+    with_config_value :internal_help_enabled, true do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:help_documents)
 
-    get :show, :id => help_documents(:one).to_param
-    assert_response :success
-    assert_not_nil assigns(:help_document)
+      get :show, :id => help_documents(:one).to_param
+      assert_response :success
+      assert_not_nil assigns(:help_document)
+    end
+  end
+  
+  test "should be available to non logged in users for external help" do
+    logout
+    with_config_value :internal_help_enabled, false do
+      get :index
+      assert_response :redirected
+    end
   end
   
   test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:help_documents)
+    with_config_value :internal_help_enabled, true do
+      get :index
+      assert_response :success
+      assert_not_nil assigns(:help_documents)
+    end
   end
 
   test "should get new" do
@@ -33,71 +45,91 @@ class HelpDocumentsControllerTest < ActionController::TestCase
   end
 
   test "should create help_document" do
-    assert_difference('HelpDocument.count') do
-      post :create, :help_document => { :identifier => "test", :title => "new" }
-    end
+    with_config_value :internal_help_enabled, true do
+      assert_difference('HelpDocument.count') do
+        post :create, :help_document => { :identifier => "test", :title => "new" }
+      end
 
-    assert_redirected_to help_document_path(assigns(:help_document))
+      assert_redirected_to help_document_path(assigns(:help_document))
+    end
   end
 
   test "should show help_document" do
-    get :show, :id => help_documents(:one).to_param
-    assert_response :success
+    with_config_value :internal_help_enabled, true do
+      get :show, :id => help_documents(:one).to_param
+      assert_response :success
+    end
   end
 
   test "should get edit" do
-    get :edit, :id => help_documents(:one).to_param
-    assert_response :success
+    with_config_value :internal_help_enabled, true do
+      get :edit, :id => help_documents(:one).to_param
+      assert_response :success
+    end
   end
 
   test "should update help_document" do
-    put :update, :id => help_documents(:one).to_param, :help_document => {:title => "fish"}
-    assert_redirected_to help_document_path(assigns(:help_document))
+    with_config_value :internal_help_enabled, true do
+      put :update, :id => help_documents(:one).to_param, :help_document => {:title => "fish"}
+      assert_redirected_to help_document_path(assigns(:help_document))
+    end
   end
 
   test "should destroy help_document" do
-    assert_difference('HelpDocument.count', -1) do
-      delete :destroy, :id => help_documents(:one).to_param
-    end
+    with_config_value :internal_help_enabled, true do
+      assert_difference('HelpDocument.count', -1) do
+        delete :destroy, :id => help_documents(:one).to_param
+      end
 
-    assert_redirected_to help_documents_path
+      assert_redirected_to help_documents_path
+    end
   end
-  
+
   test "shouldn't allow non-admins to create" do
-    login_as(:aaron)
-    get :new
-    assert_response :redirect
-    assert_not_nil flash[:error]
+    with_config_value :internal_help_enabled, true do
+      login_as(:aaron)
+      get :new
+      assert_response :redirect
+      assert_not_nil flash[:error]
+    end
   end
 
   test "shouldn't allow anonymous users to create" do
-    logout
-    get :new
-    assert_response :redirect
-    assert_not_nil flash[:error]
+    with_config_value :internal_help_enabled, true do
+      logout
+      get :new
+      assert_response :redirect
+      assert_not_nil flash[:error]
+    end
   end
   
   test "should redirect to index page if available" do
-    assert_difference('HelpDocument.count') do
-      post :create, :help_document => { :identifier => "index", :title => "Index page" }
-      get :index
-      assert_response :redirect
-      assert_nil assigns(:help_documents) #no collection set (not on index page)
-      assert_not_nil assigns(:help_document) #doc set (on show page)
-    end    
+    with_config_value :internal_help_enabled, true do
+      assert_difference('HelpDocument.count') do
+        post :create, :help_document => { :identifier => "index", :title => "Index page" }
+        get :index
+        assert_response :redirect
+        assert_nil assigns(:help_documents) #no collection set (not on index page)
+        assert_not_nil assigns(:help_document) #doc set (on show page)
+      end    
+    end
   end
   
   test "can't change identifier" do
-    assert_no_difference('help_documents(:one).identifier.hash') do
-      put :update, :id => help_documents(:one).to_param, :help_document => {:identifier => "fish"}
-    end
-  end 
-  
+    with_config_value :internal_help_enabled, true do
+      assert_no_difference('help_documents(:one).identifier.hash') do
+        put :update, :id => help_documents(:one).to_param, :help_document => {:identifier => "fish"}
+      end
+    end 
+  end
+
   test "shouldn't create docs with invalid identifiers" do
-    assert_no_difference('HelpDocument.count') do
-      post :create, :help_document => { :identifier => "//#[][]a", :title => "invalid1" }
-      post :create, :help_document => { :identifier => "hello/hello", :title => "invalid2" }
-      post :create, :help_document => { :identifier => "-hello", :title => "invalid3" }
-    end
-  end 
+    with_config_value :internal_help_enabled, true do
+      assert_no_difference('HelpDocument.count') do
+        post :create, :help_document => { :identifier => "//#[][]a", :title => "invalid1" }
+        post :create, :help_document => { :identifier => "hello/hello", :title => "invalid2" }
+        post :create, :help_document => { :identifier => "-hello", :title => "invalid3" }
+      end
+    end 
+  end
 end
