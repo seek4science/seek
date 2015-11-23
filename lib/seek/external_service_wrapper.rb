c7da7eb18c1bb74b9c6201db1f89e7b0da5712ae
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-11-23 16:31:27 +0000

    Notify admins of external service exceptions

--- lib/seek/external_service_wrapper.rb
+++ lib/seek/external_service_wrapper.rb
@@ -1,28 +1,37 @@
 module Seek
   module ExternalServiceWrapper
-    
-    def wrap_service(service_name, redirect_path, &block)
+
+    def wrap_service(service_name, redirect_path, opts = {}, &block)
       begin
         block.call
-      rescue RestClient::ResourceNotFound, SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH
-        external_service_error "#{service_name} server unreachable", redirect_path
-      rescue RestClient::InternalServerError
-        external_service_error "#{service_name} server internal error", redirect_path
-      rescue RestClient::Forbidden, RestClient::Unauthorized
-        external_service_error "#{service_name} server inaccessible", redirect_path
-      rescue RestClient::RequestTimeout
-        external_service_error "#{service_name} server timed out", redirect_path
+      rescue RestClient::ResourceNotFound, SocketError, Errno::ECONNREFUSED, Errno::EHOSTUNREACH => e
+        external_service_error e, "#{service_name} error: service unreachable", redirect_path
+      rescue RestClient::InternalServerError => e
+        external_service_error e, "#{service_name} error: internal server error", redirect_path
+      rescue RestClient::Forbidden, RestClient::Unauthorized => e
+        external_service_error e, "#{service_name} error: service inaccessible", redirect_path
+      rescue RestClient::RequestTimeout => e
+        external_service_error e, "#{service_name} error: service timed out", redirect_path
       rescue RestClient::Exception => e
-        external_service_error "#{service_name} server error: #{e.http_code} #{e.response}", redirect_path
+        external_service_error e, "#{service_name} error: #{e.http_code} #{e.response}", redirect_path
+      rescue Exception => e
+        if opts[:rescue_all]
+          external_service_error e, "#{service_name} error: Unhandled error", redirect_path
+        else
+          raise e
+        end
       end
     end
 
     private
 
-    def external_service_error(message, redirect_path)
+    def external_service_error(exception, message, redirect_path)
+      if Seek::Config.exception_notification_enabled
+        ExceptionNotifier.notify_exception(exception, data: { message: "Error interacting with #{service_name}" })
+      end
       flash[:error] = message
       redirect_to redirect_path
     end
-    
+
   end
 end
