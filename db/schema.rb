ec97155019bc9b16b2c4dcc326dffb786c4fc081
Stuart Owen <stuart.owen@manchester.ac.uk>
2015-11-30 11:37:50 +0000

    OPSK-571 - nil roles mask was being treated as admin
    added migration to avoid nil roles_mask, and also protect against misinterpreting roles mask when detecting if has_role?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151119154010) do
+ActiveRecord::Schema.define(:version => 20151130111940) do
 
   create_table "activity_logs", :force => true do |t|
     t.string   "action"
