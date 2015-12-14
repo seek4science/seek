18f6c63d821c76f821d33bbfafa98b9e2f6ffa58
Finn Bacall <finn.bacall@manchester.ac.uk>
2015-12-14 11:16:22 +0000

    Fixed news feed not showing recent updates above old creates

--- app/models/person.rb
+++ app/models/person.rb
@@ -396,12 +396,22 @@ class Person < ActiveRecord::Base
   end
 
   def recent_activity(limit = 10)
+    # TODO: Need to find a better way of doing this
+    results = ActivityLog.group(:activity_loggable_type, :activity_loggable_id).
+        where(culprit_type: 'User', culprit_id: user, action: 'update').
+        where("`activity_logs`.`controller_name` != 'sessions'").
+        where("`activity_logs`.`controller_name` != 'people'").
+        order("`activity_logs`.`created_at` DESC").
+        limit(limit).
+        uniq +
     ActivityLog.group(:activity_loggable_type, :activity_loggable_id).
-        where(culprit_type: 'User', culprit_id: user, action: ['update', 'create']).
-        where("'controller_name' != 'sessions'").
-        order('created_at DESC').
+        where(culprit_type: 'User', culprit_id: user, action: 'create').
+        where("`activity_logs`.`controller_name` != 'sessions'").
+        where("`activity_logs`.`controller_name` != 'people'").
+        order("`activity_logs`.`created_at` DESC").
         limit(limit).
         uniq
+    results.sort_by { |r| r.created_at }.reverse.uniq { |r| "#{r.activity_loggable_type}#{r.activity_loggable_id}" }[0...limit]
   end
 
   def recent_items(limit = 10)
