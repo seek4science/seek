946953bc73757f3df8bebe73bf39f27607fcdec4
Stuart Owen <stuart.owen@manchester.ac.uk>
2015-12-14 16:27:51 +0000

    OPSK-605 - help links dictionary
    added a configurable dictionary of help links, and view helper to display them. This is to help start adding links off to our help and userguides from within SEEK

--- app/helpers/help_helper.rb
+++ app/helpers/help_helper.rb
@@ -1,2 +1,17 @@
 module HelpHelper
+
+  #provides a link to a help page in our documentation, defined by the key and in lib/seek/help/help_links.yml
+  #options include:
+  # - link_text - the text of the link - default help
+  def help_link key, options={}
+    options = default_help_options.merge(options)
+    link_to options[:link_text],Seek::Help::HelpDictionary.instance.help_link(key),:target=>"blank"
+  end
+
+  def default_help_options
+    {
+        link_text: 'help'
+    }
+  end
+
 end
