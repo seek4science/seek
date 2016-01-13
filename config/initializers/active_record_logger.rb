b1723ca6dcfbd6aacb61d699ad21650960c4a13c
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2016-01-13 18:04:37 +0000

    Some code cleaning for active record logger replacement

--- config/initializers/active_record_logger.rb
+++ config/initializers/active_record_logger.rb
@@ -1,10 +1,11 @@
 # Helper code to remove annoying CACHE (0.0ms) messages from active logger.
 # Based on http://heliom.ca/blog/posts/disable-rails-cache-logging
 
-# Create logger that ignores messages containing “CACHE” and "Settings Load"
+# Implementation of logger that ignores messages containing forbidden words
+# here “CACHE” and "Settings Load"
 class CacheFreeLogger < ActiveSupport::TaggedLogging
 
-  @@excluded = ["Settings Load","CACHE"]
+  @@excluded = ['Settings Load','CACHE']
 
   def add(severity, message = nil, progname = nil, &block)
     if message.nil?
@@ -15,16 +16,11 @@ class CacheFreeLogger < ActiveSupport::TaggedLogging
         progname = nil #No instance variable for this like Logger
       end
     end
-    if severity == Logger::DEBUG
-      if !(@@excluded.map{|e| message.include? e}.include?(true))
+    if severity > Logger::DEBUG ||  !(@@excluded.map{|e| message.include? e}.include?(true))
         @logger.add(severity, "#{tags_text}#{message}", progname)
-      end
-    else
-      @logger.add(severity, "#{tags_text}#{message}", progname)
     end
   end
 end
 
-
-#ActiveRecord::Base.logger = CacheFreeLogger.new(ActiveRecord::Base.logger) unless Rails.env.test?
+#Replace the existing logger with the filtering one
 ActiveRecord::Base.logger = CacheFreeLogger.new(ActiveRecord::Base.logger) if Rails.env.development?
