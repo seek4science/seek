c61ed22d3e89e39fb77139294e4909cab715fc65
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2016-01-13 12:14:38 +0000

    Removing configuration file to reinsert it again, and potentially solve git problems

---
+++ db/seeds.b.rb
@@ -0,0 +1,31 @@
+# Seeds workflow input port types
+workflow_input_port_types = [WorkflowInputPortType::DATA, WorkflowInputPortType::PARAMETER]
+
+count = 0
+workflow_input_port_types.each do |type|
+  unless WorkflowInputPortType.find_by_name(type)
+    WorkflowInputPortType.create!(name: type)
+    count += 1
+  end
+end
+
+puts "Seeded #{count} workflow input port types."
+
+# Seeds workflow output port types
+workflow_output_port_types = [WorkflowOutputPortType::RESULT, WorkflowOutputPortType::ERROR_LOG]
+count = 0
+workflow_output_port_types.each do |type|
+  unless WorkflowOutputPortType.find_by_name(type)
+    WorkflowOutputPortType.create!(name: type)
+    count += 1
+  end
+end
+
+puts "Seeded #{count} workflow output port types."
+
+#default project and institution
+
+project = Project.create(:title=>"Default Project")
+institution = Institution.create(:title=>"Default Institution",:country=>"United Kingdom")
+project.institutions=[institution]
+project.save!
