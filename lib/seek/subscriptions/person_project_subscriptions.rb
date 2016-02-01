adf560d42098a6a113ae62347d7772fc897e3cbc
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-02-01 09:35:25 +0000

    Test fix?

--- lib/seek/subscriptions/person_project_subscriptions.rb
+++ lib/seek/subscriptions/person_project_subscriptions.rb
@@ -10,11 +10,6 @@ module Seek
         after_remove_for_group_memberships << :unsubscribe_from_project_subscription
         after_remove_for_group_memberships << :touch_project_for_membership
 
-        after_add_for_work_groups << :subscribe_to_project_subscription
-        after_add_for_work_groups << :touch_project_for_membership
-        after_remove_for_work_groups << :unsubscribe_from_project_subscription
-        after_remove_for_work_groups << :touch_project_for_membership
-
         has_many :project_subscriptions, before_add: proc { |person, ps| ps.person = person }, uniq: true, dependent: :destroy
         accepts_nested_attributes_for :project_subscriptions, allow_destroy: true
 
@@ -32,7 +27,13 @@ module Seek
         if work_groups.empty?
           project_subscriptions.delete_all
           subscriptions.delete_all
-        elsif ps = project_subscriptions.find { |ps| ps.project_id == workgroup_or_membership.project.id }
+        else
+          if workgroup_or_membership.is_a?(WorkGroup)
+            pid =workgroup_or_membership.project_id_was
+          else
+            pid = workgroup_or_membership.work_group.project_id_was
+          end
+          ps = project_subscriptions.find { |ps| ps.project_id == pid }
           # unsunscribe direct project subscriptions
           project_subscriptions.delete ps
         end
