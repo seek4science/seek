a7766b148189c47018dd9d0e60f8958a8cb3fbe7
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-02-03 11:52:04 +0000

    Only show data licenses

--- app/helpers/license_helper.rb
+++ app/helpers/license_helper.rb
@@ -3,9 +3,11 @@ require 'json'
 module LicenseHelper
 
   LICENSE_ARRAY = JSON.parse(File.read(File.join(Rails.root, 'public', 'licenses.json')))
+  DATA_LICENSE_ARRAY = LICENSE_ARRAY.select { |l| l['domain_data'] }
 
   def license_select(name, selected = nil, opts = {})
-    option_pairs = LICENSE_ARRAY.map { |value| [value['title'], value['id'], {'data-url' => value['url']}] }
+    licenses = opts.delete(:data_only) ? DATA_LICENSE_ARRAY : LICENSE_ARRAY
+    option_pairs = licenses.map { |value| [value['title'], value['id'], {'data-url' => value['url']}] }
 
     select_tag(name, options_for_select(option_pairs, selected), opts)
   end
