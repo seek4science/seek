36536588dd703c982c8ed90d1250fd23579ba9b7
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-02-11 11:16:48 +0000

    set the default attribute position
    defaults to the next pos, or starts with 1

--- app/models/sample_type_sample_attribute.rb
+++ app/models/sample_type_sample_attribute.rb
@@ -1,6 +1,15 @@
 class SampleTypeSampleAttribute < ActiveRecord::Base
-  attr_accessible :pos, :sample_attribute, :sample_type_id
+  attr_accessible :pos, :sample_attribute, :sample_type, :sample_type_id
+
+  before_save :default_pos
 
   belongs_to :sample_type
   belongs_to :sample_attribute
+
+  private
+
+  #if not set, takes the next value for that sample type
+  def default_pos
+    self.pos ||= (SampleTypeSampleAttribute.where(sample_type_id:sample_type_id).maximum(:pos) || 0) + 1
+  end
 end
