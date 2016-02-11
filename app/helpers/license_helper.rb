6135f55d9a77c176fcd760a9175ff5552869cde0
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-02-11 09:57:44 +0000

    Group licenses in selector

--- app/helpers/license_helper.rb
+++ app/helpers/license_helper.rb
@@ -4,10 +4,11 @@ require 'seek/license'
 module LicenseHelper
 
   def license_select(name, selected = nil, opts = {})
-    licenses = opts.delete(:data_only) ? Seek::License::DATA_LICENSE_ARRAY : Seek::License::LICENSE_ARRAY
-    option_pairs = licenses.map { |value| [value['title'], value['id'], {'data-url' => value['url']}] }
+    select_tag(name, options_for_select(license_options(opts), selected), opts)
+  end
 
-    select_tag(name, options_for_select(option_pairs, selected), opts)
+  def grouped_license_select(name, selected = nil, opts = {})
+    select_tag(name, grouped_options_for_select(grouped_license_options(opts), selected), opts)
   end
 
   def describe_license(id)
@@ -22,5 +23,43 @@ module LicenseHelper
       content_tag(:span, 'No license specified', :class => 'none_text')
     end
   end
+
+  private
+
+  def license_values(opts = {})
+    opts.delete(:data_only) ? Seek::License::DATA_LICENSE_ARRAY : Seek::License::LICENSE_ARRAY
+  end
+
+  def license_options(opts = {})
+    license_values(opts).map { |value| [value['title'], value['id'], {'data-url' => value['url']}] }
+  end
+
+  def grouped_license_options(opts = {})
+    grouped_licenses = license_values(opts).group_by do |l|
+      if l.has_key?('is_generic') && l['is_generic']
+        'Generic'
+      elsif l.has_key?('od_recommended') && l['od_recommended']
+        'Recommended'
+      else
+        'Other'
+      end
+    end.to_a.sort_by do |pair|
+      case pair[0]
+        when 'Recommended'
+          0
+        when 'Generic'
+          1
+        else
+          2
+      end
+    end
+
+    grouped_licenses.each do |_, licenses|
+      licenses.map! { |value| [value['title'], value['id'], {'data-url' => value['url']}] }
+    end
+
+    grouped_licenses
+  end
+
 
 end
