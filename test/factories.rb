5b6a53203b92e35a2a26fc635354aeadb72b292f
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-02-11 10:34:18 +0000

    some initial tests for adding attribute to sample type

--- test/factories.rb
+++ test/factories.rb
@@ -1037,3 +1037,10 @@ end
     f.sequence(:title) {|n| "String attribute type #{n}"}
     f.base_type 'String'
   end
+
+  #a string that must contain 'xxx'
+  Factory.define(:simple_string_sample_attribute, :class=>SampleAttribute) do |f|
+    f.sequence(:title) {|n| "Simple sample attribute #{n}"}
+    f.sample_attribute_type Factory.build(:string_sample_attribute_type,regexp:".*xxx.*")
+    f.required true
+  end
