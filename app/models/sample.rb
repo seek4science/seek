bfc1e1cf7face7082ab8aeada1662788dc88b9f0
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-02-15 14:09:01 +0000

    OPSK-628 - cleaner validation of attributes
    using a custom ActiveRecord::Validator rather than injected validator methods

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -6,15 +6,16 @@ class Sample < ActiveRecord::Base
   belongs_to :sample_type
 
   validates :title, :sample_type, presence: true
+  include ActiveModel::Validations
+  validates_with SampleAttributeValidator
 
-  after_initialize :setup_accessor_methods, :setup_validations,:read_json_metadata, unless: 'sample_type.nil?'
+  after_initialize :setup_accessor_methods, :read_json_metadata, unless: 'sample_type.nil?'
 
   before_save :set_json_metadata
 
   def sample_type=(type)
     remove_accessor_methods if sample_type
     super(type)
-    setup_validations if type
     setup_accessor_methods if type
   end
 
@@ -37,34 +38,18 @@ class Sample < ActiveRecord::Base
     end
   end
 
-  def setup_validations
-    sample_type.sample_attributes.each do |attribute|
-      class_eval <<-END_EVAL
-          if #{attribute.required?}
-            validates '#{attribute.accessor_name}', presence:true
-          end
-          validate '#{attribute.accessor_name}', '#{attribute.accessor_name}_validator'
-          def #{attribute.accessor_name}_validator
-            unless sample_type.validate_value?('#{attribute.title}',#{attribute.accessor_name})
-              errors.add('#{attribute.accessor_name}','is not a valid #{attribute.sample_attribute_type.title}')
-            end
-          end
-      END_EVAL
-    end
-  end
-
   def set_json_metadata
     hash = Hash[sample_type.sample_attributes.map do |attribute|
-      [attribute.accessor_name,self.send(attribute.accessor_name)]
+      [attribute.accessor_name, send(attribute.accessor_name)]
     end]
     self.json_metadata = hash.to_json
   end
 
   def read_json_metadata
-    if sample_type && self.json_metadata
-      json = JSON.parse(self.json_metadata)
+    if sample_type && json_metadata
+      json = JSON.parse(json_metadata)
       sample_type.sample_attributes.collect(&:accessor_name).each do |accessor_name|
-        send("#{accessor_name}=",json[accessor_name])
+        send("#{accessor_name}=", json[accessor_name])
       end
     end
   end
---
+++ app/models/sample_attribute_validator.rb
@@ -0,0 +1,15 @@
+class SampleAttributeValidator < ActiveModel::Validator
+  def validate(record)
+    return unless record.sample_type
+    record.sample_type.sample_attributes.each do |attribute|
+      val = record.send(attribute.accessor_name)
+      if val.blank?
+        record.errors[attribute.accessor_name] << 'is required' if attribute.required?
+      else
+        unless attribute.validate_value?(val)
+          record.errors[attribute.accessor_name] << "is not a valid #{attribute.sample_attribute_type.title}"
+        end
+      end
+    end
+  end
+end
