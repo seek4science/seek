cbdcb1c7a2a54b2e4706beba0ca9fe40419d9301
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-03-07 16:55:49 +0000

    OPSK-708 - Title? on sample type form
    added to form, and sets the value. Updated tests accordingly.
    Some javascript to control that there is only 1 attribute set, and required auto sets to true is still outstanding.

--- test/functional/sample_types_controller_test.rb
+++ test/functional/sample_types_controller_test.rb
@@ -7,7 +7,7 @@ class SampleTypesControllerTest < ActionController::TestCase
   setup do
     @person = Factory(:person)
     login_as(@person)
-    @sample_type = Factory(:sample_type)
+    @sample_type = Factory(:simple_sample_type)
     @string_type = Factory(:string_sample_attribute_type)
     @int_type = Factory(:integer_sample_attribute_type)
   end
@@ -28,7 +28,7 @@ class SampleTypesControllerTest < ActionController::TestCase
       post :create, sample_type: { title: "Hello!",
                                    sample_attributes_attributes: {
                                        "0" => {
-                                           pos: "1", title: "a string", required: "1",
+                                           pos: "1", title: "a string", required: "1",is_title: "1",
                                            sample_attribute_type_id: @string_type.id, _destroy: "0"},
                                        "1" => {
                                            pos: "2", title: "a number", required: "1",
@@ -40,6 +40,7 @@ class SampleTypesControllerTest < ActionController::TestCase
 
     assert_redirected_to sample_type_path(assigns(:sample_type))
     assert_equal 2, assigns(:sample_type).sample_attributes.size
+    assert_equal "a string",assigns(:sample_type).sample_attributes.title_attributes.first.title
   end
 
   test "should show sample_type" do
@@ -64,7 +65,9 @@ class SampleTypesControllerTest < ActionController::TestCase
       }
     end
 
+    sample_attributes_fields[0][:is_title] = '0'
     sample_attributes_fields[1][:title] = 'hello'
+    sample_attributes_fields[1][:is_title] = '1'
     sample_attributes_fields[2][:_destroy] = '1'
     sample_attributes_fields = Hash[sample_attributes_fields.each_with_index.map { |f, i| [i, f] }]
 
@@ -77,6 +80,8 @@ class SampleTypesControllerTest < ActionController::TestCase
 
     assert_equal sample_attributes_fields.keys.size - 1, assigns(:sample_type).sample_attributes.size
     assert_includes assigns(:sample_type).sample_attributes.map(&:title), 'hello'
+    refute assigns(:sample_type).sample_attributes[0].is_title?
+    assert assigns(:sample_type).sample_attributes[1].is_title?
   end
 
   test "should destroy sample_type" do
