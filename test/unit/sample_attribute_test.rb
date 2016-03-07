9e729196122e83aa29b1a350eedc60e9dbcb58f4
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-03-07 16:35:08 +0000

    unit test fixes

--- test/unit/sample_attribute_test.rb
+++ test/unit/sample_attribute_test.rb
@@ -1,16 +1,17 @@
 require 'test_helper'
 
 class SampleAttributeTest < ActiveSupport::TestCase
+
   test 'sample_attribute initialize' do
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:integer_sample_attribute_type),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert_equal 'fish', attribute.title
     assert_equal 'Integer', attribute.sample_attribute_type.base_type
     refute attribute.required?
     refute attribute.is_title?
 
     attribute = SampleAttribute.new title: 'fish', required: true, is_title:true, sample_attribute_type: Factory(:string_sample_attribute_type),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert_equal 'fish', attribute.title
     assert_equal 'String', attribute.sample_attribute_type.base_type
     assert attribute.required?
@@ -22,7 +23,7 @@ class SampleAttributeTest < ActiveSupport::TestCase
     #if is_title? then required? is always true
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:integer_sample_attribute_type),
                                     required: false, is_title:true,
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.required?
     assert attribute.is_title?
 
@@ -34,7 +35,7 @@ class SampleAttributeTest < ActiveSupport::TestCase
     assert attribute[:required]
 
     attribute = SampleAttribute.new title: 'fish', is_title:false, required:false, sample_attribute_type: Factory(:string_sample_attribute_type),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     attribute.save!
     attribute.reload
     refute attribute.required?
@@ -45,18 +46,18 @@ class SampleAttributeTest < ActiveSupport::TestCase
   test 'valid?' do
     attribute = SampleAttribute.new title: 'fish',
                                     sample_attribute_type: Factory(:integer_sample_attribute_type),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.valid?
 
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:string_sample_attribute_type, regexp: 'xxx'),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.valid?
 
     attribute = SampleAttribute.new title: 'fish',
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     refute attribute.valid?
     attribute = SampleAttribute.new sample_attribute_type: Factory(:integer_sample_attribute_type),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     refute attribute.valid?
     attribute = SampleAttribute.new title: 'fish',
         sample_attribute_type: Factory(:integer_sample_attribute_type)
@@ -68,7 +69,7 @@ class SampleAttributeTest < ActiveSupport::TestCase
 
   test 'validate value - without required' do
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:integer_sample_attribute_type),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.validate_value?(1)
     assert attribute.validate_value?('1')
     refute attribute.validate_value?('frog')
@@ -78,7 +79,7 @@ class SampleAttributeTest < ActiveSupport::TestCase
     assert attribute.validate_value?('')
 
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:string_sample_attribute_type),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.validate_value?('funky fish 123')
     assert attribute.validate_value?(nil)
     assert attribute.validate_value?('')
@@ -86,7 +87,7 @@ class SampleAttributeTest < ActiveSupport::TestCase
     refute attribute.validate_value?(1)
 
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:string_sample_attribute_type, regexp: 'yyy'),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.validate_value?('yyy')
     assert attribute.validate_value?('')
     assert attribute.validate_value?(nil)
@@ -95,7 +96,7 @@ class SampleAttributeTest < ActiveSupport::TestCase
     refute attribute.validate_value?('xxx')
 
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:float_sample_attribute_type),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.validate_value?(1.0)
     assert attribute.validate_value?(1.2)
     assert attribute.validate_value?(0.78)
@@ -111,7 +112,7 @@ class SampleAttributeTest < ActiveSupport::TestCase
     refute attribute.validate_value?('1')
 
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:datetime_sample_attribute_type),
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.validate_value?('2 Feb 2015')
     assert attribute.validate_value?('Thu, 11 Feb 2016 15:39:55 +0000')
     assert attribute.validate_value?('2016-02-11T15:40:14+00:00')
@@ -124,25 +125,25 @@ class SampleAttributeTest < ActiveSupport::TestCase
 
   test 'validate value with required' do
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:integer_sample_attribute_type), required: true,
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.validate_value?(1)
     refute attribute.validate_value?(nil)
     refute attribute.validate_value?('')
 
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:string_sample_attribute_type), required: true,
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.validate_value?('string')
     refute attribute.validate_value?(nil)
     refute attribute.validate_value?('')
 
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:float_sample_attribute_type), required: true,
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.validate_value?(1.2)
     refute attribute.validate_value?(nil)
     refute attribute.validate_value?('')
 
     attribute = SampleAttribute.new title: 'fish', sample_attribute_type: Factory(:datetime_sample_attribute_type), required: true,
-                                    sample_type: Factory(:sample_type)
+                                    sample_type: Factory(:simple_sample_type)
     assert attribute.validate_value?('9 Feb 2015')
     refute attribute.validate_value?(nil)
     refute attribute.validate_value?('')
@@ -164,8 +165,8 @@ class SampleAttributeTest < ActiveSupport::TestCase
   end
 
   test 'title_attributes scope' do
-    title = Factory(:sample_attribute,is_title:true,required:true,:sample_attribute_type=>Factory(:string_sample_attribute_type))
-    not_title = Factory(:sample_attribute,is_title:false,required:true,:sample_attribute_type=>Factory(:string_sample_attribute_type))
+    title = Factory(:sample_attribute,is_title:true,required:true,:sample_attribute_type=>Factory(:string_sample_attribute_type),sample_type: Factory(:simple_sample_type))
+    not_title = Factory(:sample_attribute,is_title:false,required:true,:sample_attribute_type=>Factory(:string_sample_attribute_type),sample_type: Factory(:simple_sample_type))
 
     assert_includes SampleAttribute.title_attributes,title
     refute_includes SampleAttribute.title_attributes,not_title
