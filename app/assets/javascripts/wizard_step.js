3df4dce42c3b47793856b055a7242d052416c021
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-03-08 15:56:22 +0000

    Sample selector wizard

--- app/assets/javascripts/wizard_step.js
+++ app/assets/javascripts/wizard_step.js
@@ -1,28 +1,46 @@
 $j(document).ready(function () {
     $j('[data-role="seek-wizard"]').each(function () {
-        console.log("hi");
-        var wizard = $j(this);
+        this.currentStep = 1;
+        this.gotoStep = function (stepNo) {
+            this.currentStep = stepNo;
+            this.steps.hide();
+            this.steps[this.currentStep - 1].show();
+            $j('li', this.stepNav).removeClass('active');
+            $j('li:eq('+(this.currentStep - 1)+')', this.stepNav).addClass('active');
+        };
+        this.nextStep = function () {
+            if(++this.currentStep > this.steps.length)
+                this.currentStep = this.steps.length;
+            this.gotoStep(this.currentStep);
+        };
+        this.lastStep = function () {
+            if(--this.currentStep < 1)
+                this.currentStep = 1;
+
+            this.gotoStep(this.currentStep);
+        };
+
+        var wizard = this;
         var steps = $j('[data-role="seek-wizard-step"]', $j(this));
         var stepNav = $j('<ul class="seek-step-nav pagination">');
+        this.steps = steps;
+        this.stepNav = stepNav;
 
         steps.each(function (number, step) {
-            var stepEl = $j('<li><a href="#">Step ' + (number+1) +'</a></li>');
-            if(number == 0) {
-                stepEl.addClass('active');
-                step.show();
-            } else {
-                step.hide();
-            }
+            var stepName = '';
+            if($j(step).data('stepName'))
+                stepName = ' - ' + $j(step).data('stepName');
+
+            var stepEl = $j('<li><a href="#">Step ' + (number + 1) + stepName + '</a></li>');
 
             stepEl.click(function () {
-                $j(this).siblings().removeClass('active');
-                $j(this).addClass('active');
-                steps.hide();
-                $j(step).show();
+                wizard.gotoStep(number + 1);
                 return false;
             });
             stepNav.append(stepEl);
         });
+
+        this.gotoStep(1);
 
         $j(this).prepend(stepNav);
     });
