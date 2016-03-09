99e9f414955456174a4f9533bb4d3415781ca332
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-03-09 13:22:59 +0000

    Disable 2nd and 3rd steps of wizard until ready

--- app/assets/javascripts/wizard_step.js
+++ app/assets/javascripts/wizard_step.js
@@ -12,15 +12,21 @@ Wizards.Wizard = function (element) {
     this.element.prepend(HandlebarsTemplates['wizard/nav']({ steps: wizard.steps }));
     this.element.append(HandlebarsTemplates['wizard/buttons']());
     $j('[data-role="seek-wizard-nav"] li a', this.element).click(function () {
-        wizard.gotoStep($j(this).data('step'));
+        var stepNo = parseInt($j(this).data('step'));
+
+        if(wizard.step(stepNo).unlocked)
+            wizard.gotoStep(stepNo);
+
         return false;
     });
     $j('[data-role="seek-wizard-prev-btn"]', this.element).click(function () {
         wizard.lastStep();
+        return false;
     });
 
     $j('[data-role="seek-wizard-next-btn"]', this.element).click(function () {
         wizard.nextStep();
+        return false;
     });
 
     this.gotoStep(1);
@@ -48,7 +54,7 @@ Wizards.Wizard.prototype.nextStep = function () {
     if(next > this.steps.length)
         return false;
 
-    this.gotoStep(next)
+    this.gotoStep(next);
 };
 Wizards.Wizard.prototype.lastStep = function () {
     var last = this.currentStep.number - 1;
@@ -56,23 +62,35 @@ Wizards.Wizard.prototype.lastStep = function () {
     if(last < 1)
         return false;
 
-    this.gotoStep(last)
+    this.gotoStep(last);
 };
 Wizards.Wizard.prototype.updateNav = function () {
     // Highlight breadcrumb
     $j('[data-role="seek-wizard-nav"] li', this.element).removeClass('active');
     $j('[data-role="seek-wizard-nav"] li a[data-step="'+this.currentStep.number+'"]', this.element).parent().addClass('active');
 
-    // Show hide next/prev buttons
-    if(this.currentStep.number == 1)
-        $j('[data-role="seek-wizard-prev-btn"]', this.element).hide();
-    else
-        $j('[data-role="seek-wizard-prev-btn"]', this.element).show();
+    for(var i = 0; i < this.steps.length; i++) {
+        if(!this.steps[i].unlocked) {
+            $j('[data-role="seek-wizard-nav"] li a[data-step="'+this.steps[i].number+'"]', this.element).parent().addClass('disabled');
+        } else {
+            $j('[data-role="seek-wizard-nav"] li a[data-step="'+this.steps[i].number+'"]', this.element).parent().removeClass('disabled');
+        }
+    }
 
-    if(this.currentStep.number == this.steps.length)
-        $j('[data-role="seek-wizard-next-btn"]', this.element).hide();
+    // Show hide next/prev buttons
+    var prevBtn =  $j('[data-role="seek-wizard-prev-btn"]', this.element);
+    var nextBtn =  $j('[data-role="seek-wizard-next-btn"]', this.element);
+    if(this.currentStep.number == 1 ||
+        (this.step(this.currentStep.number - 1) && !this.step(this.currentStep.number - 1).unlocked))
+        prevBtn.hide();
     else
-        $j('[data-role="seek-wizard-next-btn"]', this.element).show();
+        prevBtn.show();
+
+    if(this.currentStep.number == this.steps.length ||
+        (this.step(this.currentStep.number + 1) && !this.step(this.currentStep.number + 1).unlocked))
+        nextBtn.hide();
+    else
+        nextBtn.show();
 };
 
 Wizards.Step = function (number, element, wizard) {
@@ -83,8 +101,8 @@ Wizards.Step = function (number, element, wizard) {
     this.name = this.element.data('stepName');
 };
 
-Wizards.Step.prototype.unlock = function () { this.unlocked = false; };
-Wizards.Step.prototype.lock = function () { this.deactivate(); this.unlocked = true; };
+Wizards.Step.prototype.unlock = function () { this.unlocked = true; this.wizard.updateNav(); };
+Wizards.Step.prototype.lock = function () { this.deactivate(); this.unlocked = false; this.wizard.updateNav(); };
 Wizards.Step.prototype.activate = function () { this.unlock(); this.element.show(); };
 Wizards.Step.prototype.deactivate = function () { this.element.hide(); };
 
