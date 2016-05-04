278500a40daafada51340531e9f04a595d194746
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-05-04 15:48:54 +0100

    OPSK-686 added controlled vocab (CV) as a base type

---
+++ lib/seek/samples/attribute_type_handlers/cv_attribute_type_handler.rb
@@ -0,0 +1,9 @@
+module Seek
+  module Samples
+    module AttributeTypeHandlers
+      class CVAttributeTypeHandler < BaseAttributeHandler
+
+      end
+    end
+  end
+end
--- test/unit/sample_attribute_type_test.rb
+++ test/unit/sample_attribute_type_test.rb
@@ -143,7 +143,7 @@ class SampleAttributeTypeTest < ActiveSupport::TestCase
   end
 
   test 'allowed types' do
-    assert_equal %w(Boolean Date DateTime Float Integer SeekStrain String Text).sort, SampleAttributeType.allowed_base_types.sort
+    assert_equal %w(Boolean CV Date DateTime Float Integer SeekStrain String Text).sort, SampleAttributeType.allowed_base_types.sort
   end
 
   test 'chebi atribute' do
