001e4886c920cb5cab03a93009182bc65430f684
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-06-06 09:09:53 +0100

    More dev seed data

--- db/seeds/development/people_and_projects.seeds.rb
+++ db/seeds/development/people_and_projects.seeds.rb
@@ -13,13 +13,25 @@ uk_proj = Project.where(title: "iGrid").first_or_create!
 uk_inst = Institution.where(title: "University of Duckchester", country: 'United Kingdom').first_or_create!
 uk_workgroup = WorkGroup.where(project_id: uk_proj.id, institution_id: uk_inst.id).first_or_create!
 
-["Flynn McCall", "Norbert Morrissey" "Stewart Irwin", "Natasha Stratford", "Adam Willingham",
- "Anastasiya Nenasheva", "Daniel Meadows", "Neil Baird", "Stanley Zeeland-Rogers", "Rainer Schüttler",
- "Shaun Suffolk", "Coraline Galbraith"].each_with_index do |name, index|
-  create_person(name, "#{index}.dcuni", uk_workgroup)
+["Flynn McCall", "Norma Morrissey" "Stewart Irwin", "Natasha Stratford", "Adam Willingham",
+ "Anastasiya Nenasheva", "Daniel Meadows", "Neil Baird", "Rhianne Zeeland-Rogers", "Rainer Schüttler",
+ "Shaun Suffolk", "Coraline Galbraith"].each do |name|
+  create_person(name, ".dcuni", uk_workgroup)
 end
 
-# Project with two institutions
+# Another project with some of the above people also included, but also some people from a different institution
+fd_proj = Project.where(title: "FREEDOM").first_or_create!
+uk_fd_workgroup = WorkGroup.where(project_id: fd_proj.id, institution_id: uk_inst.id).first_or_create!
+other_fd_inst = Institution.where(title: "MTSI", country: 'Germany').first_or_create!
+other_fd_workgroup = WorkGroup.where(project_id: fd_proj.id, institution_id: other_fd_inst.id).first_or_create!
+["Flynn McCall", "Stewart Irwin", "Natasha Stratford", "Coraline Galbraith"].each do |name|
+  create_person(name, ".dcuni", uk_fd_workgroup)
+end
+["Maggy Neumann", "Wilhelm Meyer", "Olivia Klein"].each do |name|
+  create_person(name, ".mtsi", other_fd_workgroup)
+end
+
+# Another project with two new institutions
 aus_proj = Project.where(title: "StrayaBioDiv").first_or_create!
 aus_inst1 = Institution.where(title: "NSW Institute of Technology", country: 'Australia').first_or_create!
 aus_inst2 = Institution.where(title: "ACT Institute of Science", country: 'Australia').first_or_create!
