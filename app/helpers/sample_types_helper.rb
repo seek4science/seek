996c68e7802d52e81d03c255f645126aed3277c7
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-06-07 09:40:09 +0100

    OPSK-853 - Fixed XSS on sample type show/index pages

--- app/helpers/sample_types_helper.rb
+++ app/helpers/sample_types_helper.rb
@@ -1,11 +1,9 @@
 module SampleTypesHelper
   def sample_attribute_details(sample_type_attribute)
+    type = sample_type_attribute.sample_attribute_type.title
+    unit = sample_type_attribute.unit ? "( #{ sample_type_attribute. unit.symbol } )" : ''
+    req = sample_type_attribute.required? ? required_span : ''
 
-       type = sample_type_attribute.sample_attribute_type.title
-       unit = sample_type_attribute.unit ? "( #{ sample_type_attribute. unit.symbol } )" : ''
-       req = sample_type_attribute.required? ? required_span : ''
-       "#{sample_type_attribute.title} (#{type}) #{unit} #{req}".html_safe
-
-
+    "#{h sample_type_attribute.title} (#{type}) #{unit} #{req}".html_safe
   end
 end
