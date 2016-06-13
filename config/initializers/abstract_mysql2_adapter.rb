7403ea17122a8539160594db14ed516c69029d76
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-06-13 11:05:55 +0100

    OPSK-857 - fix for when using databasese other than mysql
    restrict the monkey patch that fixes the mysql issue, to only be applied of the mysql adaptor is being used. Not doing so leads to a not defined exception when running with other databases adaptors.

--- config/initializers/abstract_mysql2_adapter.rb
+++ config/initializers/abstract_mysql2_adapter.rb
@@ -2,4 +2,4 @@
 # https://jira-bsse.ethz.ch/browse/OPSK-857
 class ActiveRecord::ConnectionAdapters::Mysql2Adapter
   NATIVE_DATABASE_TYPES[:primary_key] = "int(11) auto_increment PRIMARY KEY"
-end
+end if defined?(ActiveRecord::ConnectionAdapters::Mysql2Adapter)
