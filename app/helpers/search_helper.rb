cd74488b840d8370ceaf15da7df6b58611683f82
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-06-17 13:35:06 +0100

    OPSK-863 - stripped out html from external search tooltip

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -8,10 +8,10 @@ module SearchHelper
   end
 
   def external_search_tooltip_text
-    text = "Checking this box allows external resources to be includes in the search.<br/>"
-    text << "External resources include: "
-    text << search_adaptor_names.collect{|name| "<b>#{name}</b>"}.join(",")
-    text << "<br/>"
+    text = "Checking this box allows external resources to be includes in the search. "
+    text << "External resources include:  "
+    text << search_adaptor_names.collect{|name| "#{name}"}.join(", ")
+    text << ". "
     text << "This means the search will take longer, but will include results from other sites"
     text.html_safe
   end
