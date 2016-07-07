2d2831eb4ea4d5de9642b26901bfbc9bc638d18a
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-07-07 14:25:06 +0100

    updated compose to v '2' and added mysql
    updated format, and interlinked mysql with settings passed in ENV.
    currently don't work well due to the way passenger image works and swaps users, so doesn't pass across environment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,16 +2,23 @@
 
 # Change secret token
 sed -i "s/secret_token = '.*'/key = '"`bundle exec rake secret`"'/" config/initializers/secret_token.rb
-chown app:app config/initializers/secret_token.rb
 
-# DB
+# DB config
+
+cp docker/database.docker.mysql.yml config/database.yml
+
+bundle exec rake db:setup
+
+
+# Soffice service
 soffice --headless --accept="socket,host=127.0.0.1,port=8100;urp;" --nofirststartwizard > /dev/null 2>&1 &
 
 # Workers
-sudo -Hu app bash -c "bundle exec rake seek:workers:start RAILS_ENV=production"
+bundle exec rake seek:workers:start RAILS_ENV=production
 
 # Search
-sudo -Hu app bash -c "bundle exec rake sunspot:solr:start RAILS_ENV=production"
+bundle exec rake sunspot:solr:start RAILS_ENV=production
 
+chown -R app:app .
 
 /sbin/my_init
