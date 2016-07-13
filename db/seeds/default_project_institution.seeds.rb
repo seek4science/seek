3932dc4d5692b451f2fb8362751a9051db471199
Finn Bacall <finn.bacall@manchester.ac.uk>
2016-07-13 14:22:37 +0100

    OPSK-862 - Fixed default institution not being associated with default project on seeding

--- db/seeds/default_project_institution.seeds.rb
+++ db/seeds/default_project_institution.seeds.rb
@@ -1,10 +1,9 @@
 #default project and institution
-project = Project.find_or_create_by_title('Default Project')
-institution = Institution.find_or_create_by_title('Default Institution', country: 'United Kingdom')
+unless Project.find_by_title('Default Project') && Institution.find_by_title('Default Institution')
+  project = Project.find_or_create_by_title('Default Project')
+  institution = Institution.find_or_create_by_title('Default Institution', country: 'United Kingdom')
 
-if project.new_record? || institution.new_record?
-  project.institutions=[institution]
+  project.institutions << institution
   project.save!
-  puts 'Created default project'
+  puts 'Seeded default project'
 end
-
