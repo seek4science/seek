697b39824fc206d535b87e71fb28538814a974b6
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-08-19 16:40:35 +0100

    OPSK-921 - project browser drag and drop
    rough sketch of getting the drag and drop working with the jstree and usign jquery draggables and droppables.
    mechanics now sorted out and working, but code needs major tidying up
    updating the node text with rename_node, as documented, currently isn't working

--- app/assets/javascripts/project_folders.js
+++ app/assets/javascripts/project_folders.js
@@ -2,16 +2,14 @@ var tree;
 var elementFolderIds = new Array();
 var displayed_folder_id = 0;
 
-function item_dropped_to_folder(element, droppable_element, project_id) {
-    var folder_id = elementFolderIds[droppable_element.id];
+function item_dropped_to_folder(folder_id,origin_folder_id,asset_id,asset_class,asset_element_id,project_id) {
     if (folder_id != displayed_folder_id) {
-        var dropped_id = element.id;
-        var split_ids = dropped_id.split("_");
-        var origin_folder_id = split_ids[2];
-        var origin_folder_element_id = element_id_for_folder_id(origin_folder_id);
+        var folder_element_id='folder_'+folder_id;
+        var origin_folder_element_id='folder_'+origin_folder_id;
         var path = "/projects/" + project_id + "/folders/" + origin_folder_id + "/move_asset_to";
-        path += "?asset_id=" + split_ids[1] + "&asset_type=" + split_ids[0] + "&dest_folder_id=" + folder_id;
-        path += "&dest_folder_element_id=" + droppable_element.id + "&origin_folder_element_id=" + origin_folder_element_id;
+        path += "?asset_id=" + asset_id + "&asset_type=" + asset_class + "&dest_folder_id=" + folder_id;
+        path += "&dest_folder_element_id=" + folder_element_id + "&origin_folder_element_id=" + origin_folder_element_id;
+        path += "&asset_element_id="+asset_element_id;
 
         new Ajax.Request(path, {
             asynchronous:true,
