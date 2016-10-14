781826e9f5ccbc7de1af1fe46e3afb03c4a866f0
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-10-14 15:07:37 +0100

    OPSK-964 - fix to incorrect case for SOP
    in the delete sop menu item

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -97,7 +97,7 @@ module ImagesHelper
   def delete_icon model_item, user
     item_name = text_for_resource model_item
     if model_item.can_delete?(user)
-      html = content_tag(:li) { image_tag_for_key('destroy',url_for(model_item),"Delete #{item_name.downcase}", {:confirm=>"Are you sure?",:method=>:delete },"Delete #{item_name.downcase}") }
+      html = content_tag(:li) { image_tag_for_key('destroy',url_for(model_item),"Delete #{item_name}", {:confirm=>"Are you sure?",:method=>:delete },"Delete #{item_name}") }
       return html.html_safe
     elsif model_item.can_manage?(user)
       explanation=unable_to_delete_text model_item
