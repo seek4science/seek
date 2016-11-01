0dcb4861fedcb89a5a1998da07c231505675ace9
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-11-01 11:16:43 +0000

    Revert "trying out using the minitest specreporter"
    This reverts commit fa893de3c8840c9344b05229c6b39f8e5a3ea515.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'rails/test_help'
 
 require "minitest/reporters"
-Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new()]
+MiniTest::Reporters.use! MiniTest::Reporters::DefaultReporter.new
 
 require 'rest_test_cases'
 require 'rdf_test_cases'
