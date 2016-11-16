d47a8dd024baa5318638a7f09426fdd4f5674c2e
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-11-16 13:57:18 +0000

    OPSK-1031 - updates to SampleDataExtractionJob
    updated to store just the data file id (rather than the whole thing), and sample_type_id and persist. Also updated to follow SeekJob hierarchy and structure to be consistent with other jobs

--- app/jobs/sample_data_extraction_job.rb
+++ app/jobs/sample_data_extraction_job.rb
@@ -1,12 +1,39 @@
-class SampleDataExtractionJob
+class SampleDataExtractionJob < SeekJob
+  attr_reader :data_file_id, :sample_type_id, :persist
 
-  include DefaultJobProperties
+  def initialize(data_file, sample_type, persist = false)
+    @data_file_id = data_file.id
+    @sample_type_id = sample_type.id
+    @persist = persist
+  end
 
-  attr_reader :data_file, :sample_type, :contributor
+  def gather_items
+    [data_file].compact
+  end
+
+  # either pending or running
+  def self.in_progress?(data_file)
+    [:pending, :running].include?(get_status(data_file))
+  end
+
+  def perform_job(item)
+    extractor = Seek::Samples::Extractor.new(item, sample_type)
+
+    if persist
+      extractor.persist
+    else
+      extractor.clear
+      extractor.extract
+    end
+  end
+
+  def queue_name
+    QueueNames::SAMPLES
+  end
 
   def self.get_status(data_file)
-    job = Delayed::Job.where("handler LIKE '%!ruby/object:SampleDataExtractionJob%'").
-        where("handler LIKE '%data_file_id: #{data_file.id}%'").last
+    job = Delayed::Job.where("handler LIKE '%!ruby/object:SampleDataExtractionJob%'")
+          .where("handler LIKE '%data_file_id: #{data_file.id}%'").last
     if job
       if job.locked_at
         if job.failed_at
@@ -22,39 +49,13 @@ class SampleDataExtractionJob
     end
   end
 
-  #either pending or running
-  def self.in_progress?(data_file)
-    [:pending,:running].include?(get_status(data_file))
+  private
+
+  def data_file
+    DataFile.find_by_id(data_file_id)
   end
 
-  def initialize(data_file, sample_type, persist = false)
-    @data_file_id = data_file.id
-    @data_file = data_file
-    @sample_type = sample_type
-    @persist = persist
+  def sample_type
+    SampleType.find_by_id(sample_type_id)
   end
-
-  def perform
-    extractor = Seek::Samples::Extractor.new(@data_file, @sample_type)
-
-    if @persist
-      extractor.persist
-    else
-      extractor.clear
-      extractor.extract
-    end
-  end
-
-  def queue_job(priority = default_priority, time = default_delay.from_now)
-    Delayed::Job.enqueue(self, priority: priority, queue: queue_name, run_at: time)
-  end
-
-  def default_priority
-    1
-  end
-
-  def queue_name
-    QueueNames::SAMPLES
-  end
-
 end
