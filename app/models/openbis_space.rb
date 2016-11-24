eb3fe4fb57aae1db2484b6038fda4465f34cadb6
Stuart Owen <stuart.owen@manchester.ac.uk>
2016-11-24 14:36:25 +0000

    OPSK-1021 can_create? and can_edit? controls for openbis space
    will be used to determine if it is accessible. currently controlled by the it being enabled and the current user being an administrator

--- app/models/openbis_space.rb
+++ app/models/openbis_space.rb
@@ -4,4 +4,13 @@ class OpenbisSpace < ActiveRecord::Base
 
   validates :url, url: { allow_nil: true, allow_blank: true }
   validates :project, :url, :username, :password, :space_name, presence: true
+
+  def self.can_create?
+    User.logged_in_and_member? && User.current_user.is_admin_or_project_administrator? && Seek::Config.openbis_enabled
+  end
+
+  def can_edit?(user=User.current_user)
+    user && project.can_be_administered_by?(user) && Seek::Config.openbis_enabled
+  end
+
 end
--- test/unit/openbis_space_test.rb
+++ test/unit/openbis_space_test.rb
@@ -37,11 +37,96 @@ class OpenbisSpaceTest < ActiveSupport::TestCase
   end
 
   test 'link to project' do
-    project=Factory(:project)
-    space = OpenbisSpace.create project:project,username:'fred',password:'12345',url:'http://my-openbis.org/openbis',space_name:'aaa'
-    space2 = OpenbisSpace.create project:project,username:'fred',password:'12345',url:'http://my-openbis.org/openbis',space_name:'bbb'
-    project.reload
-    assert_equal [space,space2].sort,project.openbis_spaces
+    pa=Factory(:project_administrator)
+    project=pa.projects.first
+    User.with_current_user(pa.user) do
+      with_config_value :openbis_enabled,true do
+        space = OpenbisSpace.create project:project,username:'fred',password:'12345',url:'http://my-openbis.org/openbis',space_name:'aaa'
+        space2 = OpenbisSpace.create project:project,username:'fred',password:'12345',url:'http://my-openbis.org/openbis',space_name:'bbb'
+        space.save!
+        space2.save!
+        project.reload
+        assert_equal [space,space2].sort,project.openbis_spaces.sort
+      end
+    end
+  end
+
+  test 'can_create' do
+    User.with_current_user(Factory(:project_administrator).user) do
+      with_config_value :openbis_enabled,true do
+        assert OpenbisSpace.can_create?
+      end
+
+      with_config_value :openbis_enabled,false do
+        refute OpenbisSpace.can_create?
+      end
+    end
+
+    User.with_current_user(Factory(:person).user) do
+      with_config_value :openbis_enabled,true do
+        refute OpenbisSpace.can_create?
+      end
+
+      with_config_value :openbis_enabled,false do
+        refute OpenbisSpace.can_create?
+      end
+    end
+
+    User.with_current_user(nil) do
+      with_config_value :openbis_enabled,true do
+        refute OpenbisSpace.can_create?
+      end
+
+      with_config_value :openbis_enabled,false do
+        refute OpenbisSpace.can_create?
+      end
+    end
+  end
+
+  test 'can edit?' do
+    pa=Factory(:project_administrator).user
+    user=Factory(:person).user
+    space = OpenbisSpace.create project:pa.person.projects.first,username:'fred',password:'12345',url:'http://my-openbis.org/openbis',space_name:'aaa'
+    User.with_current_user(pa) do
+      with_config_value :openbis_enabled,true do
+        assert space.can_edit?
+      end
+
+      with_config_value :openbis_enabled,false do
+        refute space.can_edit?
+      end
+    end
+
+    User.with_current_user(user) do
+      with_config_value :openbis_enabled,true do
+        refute space.can_edit?
+      end
+
+      with_config_value :openbis_enabled,false do
+        refute space.can_edit?
+      end
+    end
+
+    User.with_current_user(nil) do
+      with_config_value :openbis_enabled,true do
+        refute space.can_edit?
+      end
+
+      with_config_value :openbis_enabled,false do
+        refute space.can_edit?
+      end
+    end
+
+    with_config_value :openbis_enabled,true do
+      assert space.can_edit?(pa)
+      refute space.can_edit?(user)
+      refute space.can_edit?(nil)
+
+      #cannot edit if another project admin
+      pa2=Factory(:project_administrator).user
+      refute space.can_edit?(pa2)
+
+    end
   end
 
 end
