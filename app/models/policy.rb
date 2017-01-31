243ebd5275f237455b54295260a4053325190d0c
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-01-31 12:53:49 +0000

    Fixed very strange error in StudiesControllerTest: ActiveRecord::HasManyThroughCantAssociateThroughHasOneOrManyReflection: Cannot modify association 'Study#projects' because the source reflection class 'Project' is associated to 'Investigation' via :has_and_belongs_to_many

--- app/models/policy.rb
+++ app/models/policy.rb
@@ -131,7 +131,7 @@ class Policy < ActiveRecord::Base
 
         #if share with your project is chosen
 
-        if (sharing[:sharing_scope].to_i == Policy::ALL_USERS) and !projects.map(&:id).compact.blank?
+        if (sharing[:sharing_scope].to_i == Policy::ALL_USERS) && projects.any?
           #add Project to contributor_type
           contributor_types << "Project" if !contributor_types.include? "Project"
           #add one hash {project.id => {"access_type" => sharing[:your_proj_access_type].to_i}} to new_permission_data
