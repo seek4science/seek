976386b8c8037257393736dfdc56ce18bb2ca1bb
Stuart Owen <stuart.owen@manchester.ac.uk>
2017-02-03 14:19:37 +0000

    OPSK-1102 space id should be DEFAULT

--- db/seeds/openseek/default_openbis_endpoint.seeds.rb
+++ db/seeds/openseek/default_openbis_endpoint.seeds.rb
@@ -6,7 +6,7 @@ disable_authorization_checks do
       dss_endpoint: 'https://openbis/openbis/datastore_server',
       username: ENV["OPENBIS_USERNAME"],
       password: ENV["OPENBIS_PASSWORD"],
-      space_perm_id: 'API-SPACE',
+      space_perm_id: 'DEFAULT',
       project_id: Project.first.id
   )
 end
