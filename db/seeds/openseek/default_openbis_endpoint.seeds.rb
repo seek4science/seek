f7b706761478ebf1e897dcebcfa7a684926e0107
Stuart Owen <stuart.owen@manchester.ac.uk>
2017-02-03 11:30:07 +0000

    OPSK-1102 disable_authorization_checks needed to create endpoint

--- db/seeds/openseek/default_openbis_endpoint.seeds.rb
+++ db/seeds/openseek/default_openbis_endpoint.seeds.rb
@@ -1,11 +1,14 @@
-endpoint = OpenbisEndpoint.find_or_initialize_by_web_endpoint('https://openbis/openbis/')
-endpoint.update_attributes(
-    as_endpoint: 'https://openbis/openbis/openbis',
-    dss_endpoint: 'https://openbis/openbis/datastore_server',
-    username: ENV["OPENBIS_USERNAME"],
-    password: ENV["OPENBIS_PASSWORD"],
-    space_perm_id: 'API-SPACE',
-    project_id: Project.first.id
-)
+
+disable_authorization_checks do
+  endpoint = OpenbisEndpoint.find_or_initialize_by_web_endpoint('https://openbis/openbis/')
+  endpoint.update_attributes(
+      as_endpoint: 'https://openbis/openbis/openbis',
+      dss_endpoint: 'https://openbis/openbis/datastore_server',
+      username: ENV["OPENBIS_USERNAME"],
+      password: ENV["OPENBIS_PASSWORD"],
+      space_perm_id: 'API-SPACE',
+      project_id: Project.first.id
+  )
+end
 
 Seek::Config.openbis_enabled=true
