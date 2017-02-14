f00119fa203bec022afb33e68d3c5687b0ea86cc
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-02-14 09:52:10 +0000

    Skip old project selector/sharing form UI tests until they can be rewritten to work with dynamic UI

--- test/integration/project_associations_test.rb
+++ test/integration/project_associations_test.rb
@@ -9,6 +9,7 @@ class ProjectAssociationsTest < ActionController::IntegrationTest
   end
 
   test 'form allows setting project_ids' do
+    skip 'This test no longer works with the dynamic project selector'
     ASSETS_WITH_MULTIPLE_PROJECTS.each do |type_name|
       if type_name=="samples"
         get "/#{type_name}/new?sample_type_id=#{Factory(:simple_sample_type).id}"
@@ -24,6 +25,7 @@ class ProjectAssociationsTest < ActionController::IntegrationTest
   end
 
   test 'choosing my project in the sharing form adds permissions for each project' do
+    skip 'This test no longer works with the dynamic permissions form'
     #publications are skipped, because they don't have a sharing form
     ASSETS_WITH_MULTIPLE_PROJECTS.reject { |t| t=='publications' }.each do |type_name|
       item = Factory(type_name.singularize.to_sym, :contributor => User.current_user)
