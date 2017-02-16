10e8606cc7383821db6b9926643c4e98197f7810
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-02-16 09:49:36 +0000

    Re-wrote upload selection JS tests to use MagicLamp fixtures

--- spec/javascripts/fixtures/magic_lamp.rb
+++ spec/javascripts/fixtures/magic_lamp.rb
@@ -1,4 +1,4 @@
-MagicLamp.register_fixture(controller: SopsController, name: "sops/with_associations") do
+MagicLamp.register_fixture(controller: SopsController, name: 'sops/with_associations') do
   assay = Factory(:assay, :policy => Factory(:public_policy))
   @sop = Factory(:sop,
                  :assays => [assay],
@@ -6,4 +6,20 @@ MagicLamp.register_fixture(controller: SopsController, name: "sops/with_associat
   @sop.valid?
   @display_sop = @sop.latest_version
   render :show
+end
+
+MagicLamp.register_fixture(controller: SopsController, name: 'sops/new') do
+  @controller_name = 'sops'
+  @sop = Sop.new
+  User.current_user = Factory(:user)
+  session[:user_id] = User.current_user.id.to_s
+  render :new
+end
+
+MagicLamp.register_fixture(controller: SopsController, name: 'sops/edit') do
+  @sop = Factory(:sop, policy: Factory(:public_policy))
+  @sop.valid?
+  @display_sop = @sop.latest_version
+  User.current_user = @sop.contributor
+  render :edit
 end
