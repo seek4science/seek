3b8c40607ba6f1420b926000d4fd7e8dc6f26d58
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-02-17 15:41:59 +0000

    Set some request environment variables to avoid having to use different URL helpers in the templates being tested

--- spec/javascripts/fixtures/magic_lamp.rb
+++ spec/javascripts/fixtures/magic_lamp.rb
@@ -11,8 +11,14 @@ end
 MagicLamp.register_fixture(controller: SopsController, name: 'sops/new') do
   @controller_name = 'sops'
   @sop = Sop.new
+  # Set the current_user
   User.current_user = Factory(:user)
   session[:user_id] = User.current_user.id.to_s
+  # Some URL helpers in the template don't work without this:
+  request.env["action_dispatch.request.path_parameters"] = {
+      action: "new",
+      controller: "sops",
+  }
   render :new
 end
 
@@ -21,5 +27,11 @@ MagicLamp.register_fixture(controller: SopsController, name: 'sops/edit') do
   @sop.valid?
   @display_sop = @sop.latest_version
   User.current_user = @sop.contributor
+  session[:user_id] = User.current_user.id.to_s
+  request.env["action_dispatch.request.path_parameters"] = {
+      action: "edit",
+      controller: "sops",
+      id: @sop.id
+  }
   render :edit
 end
