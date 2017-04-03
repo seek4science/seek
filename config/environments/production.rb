c6a7692e49e6736586223900d536c741e63b20d6
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-04-03 15:39:16 +0100

    Updated environment configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,84 +1,90 @@
-# Settings specified here will take precedence over those in config/environment.rb
 SEEK::Application.configure do
-  # Settings specified here will take precedence over those in config/application.rb
+    # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests
-  config.cache_classes = true
+    # Code is not reloaded between requests.
+    config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+    # Eager load code on boot. This eager loads most of Rails and
+    # your application in memory, allowing both thread web servers
+    # and those relying on copy on write to perform better.
+    # Rake tasks automatically ignore this option for performance.
+    config.eager_load = true
 
-  config.action_controller.cache_store = [:file_store, "#{Rails.root}/tmp/cache"]
-  config.cache_store = [:file_store, "#{Rails.root}/tmp/cache"]
+    # Full error reports are disabled and caching is turned on.
+    config.consider_all_requests_local       = false
+    config.action_controller.perform_caching = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = true
+    # Enable Rack::Cache to put a simple HTTP cache in front of your application
+    # Add `rack-cache` to your Gemfile before enabling this.
+    # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+    # config.action_dispatch.rack_cache = true
+    config.action_controller.cache_store = [:file_store, "#{Rails.root}/tmp/cache"]
+    config.cache_store = [:file_store, "#{Rails.root}/tmp/cache"]
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+    # Disable Rails's static asset server (Apache or nginx will already do this).
+    config.serve_static_assets = true
 
-  # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+    # Compress JavaScripts and CSS.
+    config.assets.js_compressor  = :uglifier
+    # config.assets.css_compressor = :sass
 
-  # Generate digests for assets URLs
-  config.assets.digest = true
+    # Whether to fallback to assets pipeline if a precompiled asset is missed.
+    config.assets.compile = false
 
-  # this config is set in order to use assets:clean_expired, to remove expired assets
-  config.assets.handle_expiration = true
+    # Generate digests for assets URLs.
+    config.assets.digest = true
 
-  # Defaults to nil and saved in location specified by config.assets.prefix
-  # config.assets.manifest = YOUR_PATH
+    # Version of your assets, change this if you want to expire all your assets.
+    config.assets.version = '1.0'
 
-  # Specifies the header that your server uses for sending files
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+    # Specifies the header that your server uses for sending files.
+    # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+    # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+    # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+    # config.force_ssl = true
 
-  # See everything in the log (default is :info)
-  # config.log_level = :debug
+    # Set to :debug to see everything in the log.
+    config.log_level = :info
 
-  # Prepend all log lines with the following tags
-  # config.log_tags = [ :subdomain, :uuid ]
+    # Prepend all log lines with the following tags.
+    # config.log_tags = [ :subdomain, :uuid ]
 
-  # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+    # Use a different logger for distributed setups.
+    # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
+    # Use a different cache store in production.
+    # config.cache_store = :mem_cache_store
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
+    # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+    # config.action_controller.asset_host = "http://assets.example.com"
 
-  # Precompile additional assets (application.js, application.css.scss, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+    # Precompile additional assets.
+    # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+    # config.assets.precompile += %w( search.js )
 
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+    # Ignore bad email addresses and do not raise email delivery errors.
+    # Set this to true and configure the email server for immediate delivery to raise delivery errors.
+    # config.action_mailer.raise_delivery_errors = false
 
-  # Enable threaded mode
-  # config.threadsafe!
+    # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+    # the I18n.default_locale when a translation can not be found).
+    config.i18n.fallbacks = true
+    I18n.enforce_available_locales = true
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
-  I18n.enforce_available_locales = true
+    # Send deprecation notices to registered listeners.
+    config.active_support.deprecation = :notify
 
-  # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
+    # Disable automatic flushing of the log to improve performance.
+    # config.autoflush_log = false
 
-  # Log the query plan for queries taking more than this (works
-  # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
+    # Use default logging formatter so that PID and timestamp are not suppressed.
+    config.log_formatter = ::Logger::Formatter.new
 
-  config.lograge.enabled = true
+    config.lograge.enabled = true
 
-  # add time to lograge
-  config.lograge.custom_options = lambda do |event|
-    {:time => event.time, :user_agent => event.payload[:user_agent]}
-  end
-
+    # add time to lograge
+    config.lograge.custom_options = lambda do |event|
+      {:time => event.time, :user_agent => event.payload[:user_agent]}
+    end
 end
-
