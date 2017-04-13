6c348e608c102aadca6821fba254b4f89deb0e01
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-04-13 10:05:30 +0100

    Re-enabled strong parameters

--- config/application.rb
+++ config/application.rb
@@ -72,8 +72,6 @@ module SEEK
     # openbis_endpoints/26-20170404142724224014370...
     config.active_record.cache_timestamp_format = :usec
 
-    config.action_controller.permit_all_parameters = true # TODO: REMOVE ME!!! - Temp measure for testing Rails 3.2 -> 4.0 upgrade
-
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
