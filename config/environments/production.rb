9847a8b62d3c875fc4193b9e46c92012fe1f2f16
Stuart Owen <stuart.owen@manchester.ac.uk>
2017-04-21 13:39:55 +0100

    need to explicity set the log level to :info to avoid warning message

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ SEEK::Application.configure do
 
     # Use the lowest log level to ensure availability of diagnostic information
     # when problems arise.
-    # config.log_level = :debug
+    config.log_level = :info
 
     # Prepend all log lines with the following tags.
     # config.log_tags = [ :subdomain, :uuid ]
