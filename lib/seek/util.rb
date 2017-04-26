b5ae8be5591ed5dd946f0e4e7d4a97f80cadbebb
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-04-26 14:05:37 +0100

    Don't cache lists of classes in development mode

--- lib/seek/util.rb
+++ lib/seek/util.rb
@@ -12,23 +12,23 @@ module Seek
     end
 
     def self.clear_cached
-      class_variables.each do |v|
-        class_variable_set(v, nil)
-      end
+      @@cache = {}
     end
 
     def self.ensure_models_loaded
-      @@models_loaded ||= begin
+      cache('models_loaded') do
         Dir.glob("#{Rails.root}/app/models/**/*.rb").each do |file|
-          model_name = file.gsub('.rb', '').gsub(File::SEPARATOR, '/').gsub("#{Rails.root}/app/models/", '')
-          model_name.camelize.constantize
+          unless file.start_with?("#{Rails.root}/app/models/concerns")
+            model_name = file.gsub('.rb', '').gsub(File::SEPARATOR, '/').gsub("#{Rails.root}/app/models/", '')
+            model_name.camelize.constantize
+          end
         end
         true
       end
     end
 
     def self.persistent_classes
-      @@persistent_classes ||= begin
+      cache('persistent_classes') do
         ensure_models_loaded
         ActiveRecord::Base.descendants
       end
@@ -38,7 +38,7 @@ module Seek
     # returns a list of all types that respond_to and return true for user_creatable?
     def self.user_creatable_types
       # FIXME: the user_creatable? is a bit mis-leading since we now support creation of people, projects, programmes by certain people in certain roles.
-      @@creatable_model_classes ||= begin
+      cache('creatable_model_classes') do
         persistent_classes.select do |c|
           c.respond_to?('user_creatable?') && c.user_creatable?
         end.sort_by { |a| [a.is_asset? ? -1 : 1, a.is_isa? ? -1 : 1, a.name] }
@@ -50,7 +50,7 @@ module Seek
     end
 
     def self.authorized_types
-      @@policy_authorised_types ||= begin
+      cache('policy_authorised_types') do
         persistent_classes.select do |c|
           c.respond_to?(:authorization_supported?) && c.authorization_supported?
         end.sort_by(&:name)
@@ -63,11 +63,11 @@ module Seek
       #- could add a searchable? method
       extras = [Person, Programme, Project, Institution]
       extras.delete(Programme) unless Seek::Config.programmes_enabled
-      @@searchable_types ||= (user_creatable_types | extras).sort_by(&:name)
+      cache('searchable_types') { (user_creatable_types | extras).sort_by(&:name) }
     end
 
     def self.scalable_types
-      @@scalable_types ||= begin
+      cache('scalable_types') do
         persistent_classes.select do |c|
           c.included_modules.include?(Seek::Scalable::InstanceMethods)
         end.sort_by(&:name)
@@ -75,7 +75,7 @@ module Seek
     end
 
     def self.rdf_capable_types
-      @@rdf_capable_types ||= begin
+      cache('rdf_capable_types') do
         persistent_classes.select do |c|
           c.included_modules.include?(Seek::Rdf::RdfGeneration)
         end
@@ -83,7 +83,7 @@ module Seek
     end
 
     def self.breadcrumb_types
-      @@breadcrumb_types ||= begin
+      cache('breadcrumb_types') do
         persistent_classes.select do |c|
           c.is_isa? || c.is_asset? || c.is_yellow_pages? || c.name == 'Event'
         end.sort_by(&:name)
@@ -91,7 +91,7 @@ module Seek
     end
 
     def self.asset_types
-      @@asset_types ||= begin
+      cache('asset_types') do
         persistent_classes.select(&:is_asset?).sort_by(&:name)
       end
     end
@@ -112,9 +112,17 @@ module Seek
     end
 
     def self.doiable_asset_types
-      @@doiable_types ||= begin
+      cache('doiable_types') do
         persistent_classes.select(&:supports_doi?).sort_by(&:name)
       end
+    end
+
+    private
+
+    def self.cache(name, &block)
+      @@cache ||= {}
+      @@cache = {} if Rails.env.development? # Don't use caching in development mode
+      @@cache[name] ||= block.call
     end
   end
 end
