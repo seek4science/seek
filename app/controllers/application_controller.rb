61a04329fe1b31d6c80d181ef3bcab1fed5cd6f0
Hadas Leonov <blackbirdy@gmail.com>
2017-05-15 13:51:31 +0200

    disable last commit (un-unescape characters in JSON..) without losing code because it leads to problems with asset creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,7 +30,7 @@ class ApplicationController < ActionController::Base
   before_filter :restrict_guest_user, only: [:new, :edit, :batch_publishing_preview]
   before_filter :process_params, :only=>[:edit, :update, :destroy, :create, :new]
 
-  after_filter :unescape_response
+  # after_filter :unescape_response
 
   helper :all
 
