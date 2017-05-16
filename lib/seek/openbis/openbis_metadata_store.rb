1cc189e3b2d0e39837b06cdf0bc5536ed5ae46f7
Stuart Owen <stuart.owen@manchester.ac.uk>
2017-05-16 16:11:01 +0100

    OPSK-1250 cleaner filestore endpoint key for store location, and some unit tests

--- lib/seek/openbis/openbis_metadata_store.rb
+++ lib/seek/openbis/openbis_metadata_store.rb
@@ -1,16 +1,18 @@
 module Seek
   module Openbis
+    # Handles the storage and caching of the OpenBIS metadata. Delegates and operates around a rails filestore cache,
+    # configured to point to a non-temporary filestore location based upon the endpoint
     class OpenbisMetadataStore
-      attr_reader :cache
       delegate :fetch, :exist?, to: :cache
 
       def initialize(endpoint)
-        @endpoint=endpoint
+        @endpoint = endpoint
         @cache = ActiveSupport::Cache::FileStore.new(filestore_path)
       end
 
+      # clears all stored metadata for this endpoint
       def clear
-        #TODO: this is rather brutal, in the future a counter will form a new path which can be reverted to if things fail
+        # TODO: this is rather brutal, in future need to be able to revert
         Rails.logger.info("Clearing OpenbisMedataStore at #{filestore_path}")
         cache.delete_matched(/.*/)
       end
@@ -18,21 +20,21 @@ module Seek
       private
 
       attr_reader :endpoint
+      attr_reader :cache
 
       def filestore_path
-        File.join(Seek::Config.append_filestore_path('openbis-metadata'),endpoint_key)
+        File.join(Seek::Config.append_filestore_path('openbis-metadata'), endpoint_key)
       end
 
       def endpoint_key
-        if endpoint.new_record? #TODO: double check why previous code needed to handle new_record?
-          str="#{endpoint.cache_key}-#{endpoint.space_perm_id}-#{endpoint.as_endpoint}-#{endpoint.dss_endpoint}-#{endpoint.username}"
-          Digest::SHA2.hexdigest(str)
+        if endpoint.new_record? # required to handle fetching spaces during the endpoint creation
+          str="#{endpoint.cache_key}-#{endpoint.space_perm_id}"
+          str << "-#{endpoint.as_endpoint}-#{endpoint.dss_endpoint}-#{endpoint.username}"
+          "new/#{Digest::SHA2.hexdigest(str)}"
         else
-          endpoint.cache_key
+          "#{endpoint.id}/#{endpoint.updated_at.utc}"
         end
       end
-
-
     end
   end
 end
