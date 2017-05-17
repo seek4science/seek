c702000ea6117fe4e250cf8c8141b93ec2a51072
Stuart Owen <stuart.owen@manchester.ac.uk>
2017-05-17 10:39:47 +0100

    removed obselete secret_token.rb
    now superceded by secrets.yml

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-SEEK::Application.config.secret_token = '0d94b972e0a5024f4650c1b42e66fe17872b9bff3894d439fc5954685334913588228c57d3b8a73d3759c8179eaa1e09f788d90359150ffdee2c7fde99aedea6'
