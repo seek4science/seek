58c333555df17be728b92ae0c605babc0746f67d
Hadas Leonov <blackbirdy@gmail.com>
2017-06-01 16:18:16 +0200

    fix annotation_serializer, no need for a tag name, only value

--- app/serializers/annotation_serializer.rb
+++ app/serializers/annotation_serializer.rb
@@ -1,11 +1,8 @@
 # to make "tags" inclusion possible for objects in the json output format
 class AnnotationSerializer < SimpleBaseSerializer
-  #include JSONAPI::Serializer
-  attribute :name do
-    object.attribute.name
-  end
+  attribute :id
   attribute :value do
-    object.value.text
+     object.value.text
   end
   def self_link
   end
