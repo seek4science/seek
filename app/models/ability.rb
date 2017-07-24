c65266f42eec167bcca608b5963b7bcd2d87bf75
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-07-24 16:49:05 +0100

    OPSK-1316 Simplified Ability class and refactored PublishingPermissions a bit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,17 +10,9 @@ class Ability
     if [:manage, :delete, :edit, :download, :view].include?(action)
       @person.is_asset_housekeeper_of?(item) && item.asset_housekeeper_can_manage?
     elsif action == :publish
-      if @person.is_asset_gatekeeper_of?(item)
-        if item.can_manage?(@person.user) && !item.is_published?
-          true
-        elsif !item.is_published? && item.is_waiting_approval?(nil, 5.years.ago)
-          true
-        else
-          false
-        end
-      else
-        false
-      end
+      @person.is_asset_gatekeeper_of?(item) && item.asset_gatekeeper_can_publish?
+    else
+      false
     end
   end
 
