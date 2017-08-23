0cd99ccb1266bbd871a4ab1be6ea5e473dc51b36
alaninmcr <alan.r.williams@manchester.ac.uk>
2017-08-23 10:54:32 +0000

    Explicit use of underscore

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,4 +1,5 @@
 ActiveModelSerializers.config.adapter = :json_api
+ActiveModelSerializers.config.key_transform = :underscore
 # ActiveModel::Serializer.setup do |config|
 #       config.adapter == :json_api   # Default: :attributes
 #  end
