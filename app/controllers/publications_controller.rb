85facefb62d0798d08b119a2b486201d547c6565
Hadas Leonov <blackbirdy@gmail.com>
2017-09-12 17:43:39 +0200

    add CU to publication

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -146,9 +146,11 @@ class PublicationsController < ApplicationController
         flash[:notice] = 'Publication was successfully updated.'
         format.html { redirect_to(@publication) }
         format.xml  { head :ok }
+        format.json { render json: @publication, status: :ok}
       else
         format.html { render action: 'edit' }
         format.xml  { render xml: @publication.errors, status: :unprocessable_entity }
+        format.json { render json: @publication.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -394,6 +396,7 @@ class PublicationsController < ApplicationController
           flash[:notice] = 'Publication was successfully created.'
           format.html { redirect_to(edit_publication_url(@publication)) }
           format.xml  { render xml: @publication, status: :created, location: @publication }
+          format.json  { render json: @publication, status: :created, location: @publication }
         end
       end
     else # Publication save not successful
@@ -430,12 +433,14 @@ class PublicationsController < ApplicationController
           flash[:notice] = 'Publication was successfully created.'
           format.html { redirect_to(edit_publication_url(@publication)) }
           format.xml  { render xml: @publication, status: :created, location: @publication }
+          format.json { render json: @publication, status: :created, location: @publication }
         end
       end
     else # Publication save not successful
       respond_to do |format|
         format.html { render action: 'new' }
         format.xml  { render xml: @publication.errors, status: :unprocessable_entity }
+        format.json { render json: @publication.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -469,10 +474,13 @@ class PublicationsController < ApplicationController
       respond_to do |format|
         format.html { render action: 'new' }
         format.xml  { render xml: @publication.errors, status: :unprocessable_entity }
+        format.json { render json: @publication.errors, status: :unprocessable_entity }
+
       end
     else
       respond_to do |format|
         format.html { render action: 'new' }
+        format.json { render json: @publication, status: :ok }
       end
     end
   end
@@ -527,11 +535,13 @@ class PublicationsController < ApplicationController
       respond_to do |format|
         format.html { render action: 'new' }
         format.xml  { render xml: @publication.errors, status: :unprocessable_entity }
+        format.json  { render json: @publication.errors, status: :unprocessable_entity }
       end
     else
       respond_to do |format|
         format.html { redirect_to(action: :index) }
         format.xml  { render xml: publications, status: :created, location: @publication }
+        format.json  { render json: publications, status: :created, location: @publication }
       end
     end
   end
