e1707df93458b0453362b4a1bbe97e80e74470a0
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2017-09-15 20:40:32 +0100

    Reformating code

--- app/models/openbis_endpoint.rb
+++ app/models/openbis_endpoint.rb
@@ -72,7 +72,7 @@ class OpenbisEndpoint < ActiveRecord::Base
   def registered_datafiles
     # ugly will scan all content blobs from data files
     url = "openbis:#{id}"
-    DataFile.all.select { |df| df.content_blob && df.content_blob.url && df.content_blob.url.start_with?(url)}
+    DataFile.all.select { |df| df.content_blob && df.content_blob.url && df.content_blob.url.start_with?(url) }
   end
 
   def clear_metadata_store
