221b7dbcf54aed6d7247e69105282cbfc5fcae46
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2017-09-15 18:02:48 +0100

    commenting out debug statements

--- config/boot.rb
+++ config/boot.rb
@@ -7,12 +7,12 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-#To listen on all ports
-module Rails
-  class Server
-    alias :default_options_bk :default_options
-    def default_options
-      default_options_bk.merge!(Host: '0.0.0.0')
-    end
-  end
-end
+#To listen on all ports by TZ
+#module Rails
+#  class Server
+#    alias :default_options_bk :default_options
+#    def default_options
+#      default_options_bk.merge!(Host: '0.0.0.0')
+#    end
+#  end
+#end
