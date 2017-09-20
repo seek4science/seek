1d47532a3ab022c7eff228d49f7fe4cb61242c0b
Stuart Owen <stuart.owen@manchester.ac.uk>
2017-09-20 14:32:18 +0100

    fix key for validation data relationship type

--- db/seeds/relationship_types.seeds.rb
+++ db/seeds/relationship_types.seeds.rb
@@ -2,7 +2,7 @@ construction_data = RelationshipType.find_or_initialize_by(title:'Construction d
 construction_data.update_attributes(description:'Data used for model testing',key:RelationshipType::CONSTRUCTION)
 
 validation_data = RelationshipType.find_or_initialize_by(title:'Validation data')
-validation_data.update_attributes(description:'Data used for validating a model',key:RelationshipType::SIMULATION)
+validation_data.update_attributes(description:'Data used for validating a model',key:RelationshipType::VALIDATION)
 
 simulation_data = RelationshipType.find_or_initialize_by(title:'Simulation results')
 simulation_data.update_attributes(description:'Data resulting from running a model simulation',key:RelationshipType::SIMULATION)
