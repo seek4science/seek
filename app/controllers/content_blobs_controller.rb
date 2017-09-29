dc50a4b6a525f36dad7abe4b86195a611b4bfe71
Stuart Owen <stuart.owen@manchester.ac.uk>
2017-09-29 16:37:59 +0100

    rubocop fixes to content_blobs_controller

--- app/controllers/content_blobs_controller.rb
+++ app/controllers/content_blobs_controller.rb
@@ -1,8 +1,7 @@
-  class ContentBlobsController < ApplicationController
-
-  before_filter :find_and_authorize_associated_asset, :only=>[:get_pdf, :view_content,:view_pdf_content, :download]
-  before_filter :find_and_authorize_content_blob, :only=>[:get_pdf, :view_content,:view_pdf_content, :download]
-  before_filter :set_asset_version, :only=>[:get_pdf, :download]
+class ContentBlobsController < ApplicationController
+  before_filter :find_and_authorize_associated_asset, only: %i[get_pdf view_content view_pdf_content download]
+  before_filter :find_and_authorize_content_blob, only: %i[get_pdf view_content view_pdf_content download]
+  before_filter :set_asset_version, only: %i[get_pdf download]
 
   include Seek::AssetsCommon
   include Seek::UploadHandling::ExamineUrl
@@ -11,42 +10,42 @@
     if @content_blob.is_text?
       view_text_content
     else
-      @pdf_url=pdf_url
-      render action: :view_pdf_content,:layout=>false
+      @pdf_url = pdf_url
+      render action: :view_pdf_content, layout: false
     end
   end
 
   def view_text_content
-    render text: File.read(@content_blob.filepath,:encoding => 'iso-8859-1'), layout: false, content_type: 'text/plain'
+    render text: File.read(@content_blob.filepath, encoding: 'iso-8859-1'), layout: false, content_type: 'text/plain'
   end
 
   def view_pdf_content
-    @pdf_url=pdf_url
+    @pdf_url = pdf_url
     respond_to do |format|
-      format.html { render :layout=>false }
+      format.html { render layout: false }
     end
   end
 
   def examine_url
-    #check content type and size
+    # check content type and size
     url = params[:data_url]
     begin
       case scheme = URI(url).scheme
-        when 'ftp'
-          handler = Seek::DownloadHandling::FTPHandler.new(url)
-          info = handler.info
-          handle_good_ftp_response(url, info)
-        when 'http', 'https', nil
-          handler = Seek::DownloadHandling::HTTPHandler.new(url)
-          info = handler.info
-          if info[:code] == 200
-            handle_good_http_response(url, info)
-          else
-            handle_bad_http_response(info[:code])
-          end
+      when 'ftp'
+        handler = Seek::DownloadHandling::FTPHandler.new(url)
+        info = handler.info
+        handle_good_ftp_response(url, info)
+      when 'http', 'https', nil
+        handler = Seek::DownloadHandling::HTTPHandler.new(url)
+        info = handler.info
+        if info[:code] == 200
+          handle_good_http_response(url, info)
         else
-          @warning = true
-          @warning_msg = "Unhandled URL scheme: #{scheme}. The given URL will be presented as a clickable link."
+          handle_bad_http_response(info[:code])
+        end
+      else
+        @warning = true
+        @warning_msg = "Unhandled URL scheme: #{scheme}. The given URL will be presented as a clickable link."
       end
     rescue Exception => e
       handle_exception_response(e)
@@ -71,7 +70,7 @@
         raise(e)
       end
     else
-      raise("This remote file is too big to be displayed as PDF.")
+      raise('This remote file is too big to be displayed as PDF.')
     end
   end
 
@@ -80,7 +79,7 @@
 
     if @asset.respond_to?(:openbis?) && @asset.openbis?
       respond_to do |format|
-        format.html { handle_openbis_download(@asset,params[:perm_id]) }
+        format.html { handle_openbis_download(@asset, params[:perm_id]) }
       end
     else
       disposition = params[:disposition] || 'attachment'
@@ -97,43 +96,43 @@
   private
 
   def pdf_url
-    polymorphic_path([@asset, @content_blob], :action => 'download', :intent => :inline_view, :format => 'pdf', :code => params[:code])
+    polymorphic_path([@asset, @content_blob], action: 'download', intent: :inline_view, format: 'pdf', code: params[:code])
   end
 
-  #check whether the file is pdf, otherwise convert to pdf
-  #then return the pdf file
-  def pdf_or_convert filepath = @content_blob.filepath
+  # check whether the file is pdf, otherwise convert to pdf
+  # then return the pdf file
+  def pdf_or_convert(filepath = @content_blob.filepath)
     if @content_blob.is_pdf?
       pdf_filepath = @content_blob.filepath
       pdf_filename = @content_blob.original_filename
     else
-      pdf_filepath = @content_blob.filepath("pdf")
+      pdf_filepath = @content_blob.filepath('pdf')
       @content_blob.convert_to_pdf(filepath, pdf_filepath)
 
-      if File.exists?(pdf_filepath)
-        pdf_filename = File.basename(@content_blob.original_filename, File.extname(@content_blob.original_filename))+".pdf"
+      if File.exist?(pdf_filepath)
+        pdf_filename = File.basename(@content_blob.original_filename, File.extname(@content_blob.original_filename)) + '.pdf'
       else
-        raise Exception.new("Couldn't find converted PDF file.")
+        raise Exception, "Couldn't find converted PDF file."
       end
     end
 
     send_file pdf_filepath,
-              :filename => pdf_filename,
-              :type => "application/pdf",
-              :disposition => 'attachment'
+              filename: pdf_filename,
+              type: 'application/pdf',
+              disposition: 'attachment'
 
-    headers["Content-Length"] = File.size(pdf_filepath).to_s
+    headers['Content-Length'] = File.size(pdf_filepath).to_s
   end
 
   def get_file_from_jerm
-    project=@asset_version.projects.first
+    project = @asset_version.projects.first
     project.decrypt_credentials
-    downloader=Jerm::DownloaderFactory.create project.title
-    resource_type = @asset_version.class.name.split("::")[0] #need to handle versions, e.g. Sop::Version
+    downloader = Jerm::DownloaderFactory.create project.title
+    resource_type = @asset_version.class.name.split('::')[0] # need to handle versions, e.g. Sop::Version
     begin
-      data_hash = downloader.get_remote_data @content_blob.url,project.site_username,project.site_password, resource_type
+      data_hash = downloader.get_remote_data @content_blob.url, project.site_username, project.site_password, resource_type
       data_hash
-    rescue Seek::DownloadException=>de
+    rescue Seek::DownloadException => de
       nil
     end
   end
@@ -145,8 +144,8 @@
         @asset = asset
       else
         respond_to do |format|
-          flash[:error] = "You are not authorized to perform this action"
-          format.html { redirect_to asset}
+          flash[:error] = 'You are not authorized to perform this action'
+          format.html { redirect_to asset }
         end
         return false
       end
@@ -154,25 +153,22 @@
   end
 
   def asset_object
-    begin
-      case
-        when params[:data_file_id] then
-          DataFile.find(params[:data_file_id])
-        when params[:model_id] then
-          Model.find(params[:model_id])
-        when params[:sop_id] then
-          Sop.find(params[:sop_id])
-        when params[:presentation_id] then
-          Presentation.find(params[:presentation_id])
-        when params[:sample_type_id] then
-          SampleType.find(params[:sample_type_id])
-        else
-          ContentBlob.find(params[:id]).asset
-      end
-    rescue ActiveRecord::RecordNotFound
-      error("Unable to find the asset", "is invalid")
-      return false
+    if params[:data_file_id]
+      DataFile.find(params[:data_file_id])
+    elsif params[:model_id]
+      Model.find(params[:model_id])
+    elsif params[:sop_id]
+      Sop.find(params[:sop_id])
+    elsif params[:presentation_id]
+      Presentation.find(params[:presentation_id])
+    elsif params[:sample_type_id]
+      SampleType.find(params[:sample_type_id])
+    else
+      ContentBlob.find(params[:id]).asset
     end
+  rescue ActiveRecord::RecordNotFound
+    error('Unable to find the asset', 'is invalid')
+    return false
   end
 
   def find_and_authorize_content_blob
@@ -180,27 +176,24 @@
     if content_blob.asset.id == @asset.id
       @content_blob = content_blob
     else
-      error("You are not authorized to see this content blob", "is invalid")
+      error('You are not authorized to see this content blob', 'is invalid')
       return false
     end
-
   end
 
   def content_blob_object
-    begin
-      ContentBlob.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      error("Unable to find content blob", "is invalid")
-      return false
-    end
+    ContentBlob.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    error('Unable to find content blob', 'is invalid')
+    return false
   end
 
   def set_asset_version
     if @content_blob.asset_version
       begin
         @asset_version = @content_blob.asset.find_version(@content_blob.asset_version)
-      rescue Exception=>e
-        error("Unable to find asset version", "is invalid")
+      rescue Exception => e
+        error('Unable to find asset version', 'is invalid')
         return false
       end
     end
