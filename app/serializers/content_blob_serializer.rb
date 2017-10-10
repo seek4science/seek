897cb778227c6756ca2c170d2962d234458c0547
Stuart Owen <stuart.owen@manchester.ac.uk>
2017-10-10 14:07:50 +0100

    OPSK-1389 don't show the is_remote attribute, but do include the sha1sum

--- app/serializers/content_blob_serializer.rb
+++ app/serializers/content_blob_serializer.rb
@@ -1,11 +1,7 @@
 class ContentBlobSerializer < BaseSerializer
-   attributes :original_filename, :url, :md5sum, :content_type
-  attribute :is_remote do
-    object.file_exists?
+  attributes :original_filename, :url, :md5sum, :sha1sum, :content_type
+
+  def self_link
+    polymorphic_path([object.asset, object])
   end
-
-   def self_link
-     polymorphic_path([object.asset,object])
-   end
-
 end
