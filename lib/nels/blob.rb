281eb9992d6e41dbd1f4e02e33260ef952320e92
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-10-10 11:03:15 +0100

    Added method to content blob to retrieve sample data from NeLS

--- lib/nels/blob.rb
+++ lib/nels/blob.rb
@@ -11,5 +11,17 @@ module Nels
     def nels?
       url && valid_url?(url) && url.start_with?(NELS_BASE)
     end
+
+    def retrieve_from_nels(access_token)
+      client_class = Nels::Rest::Client
+      rest_client = client_class.new(access_token)
+      ref = url.scan(/ref=([^&]+)/).try(:first).try(:first)
+
+      self.original_filename = 'sample_metadata.xlsx'
+      self.content_type = mime_types_for_extension('xlsx').sort.first
+      self.tmp_io_object = StringIO.new(rest_client.sample_metadata(ref))
+
+      save
+    end
   end
 end
--- test/nels_test_helper.rb
+++ test/nels_test_helper.rb
@@ -1,14 +1,13 @@
 module NelsTestHelper
 
-  def setup_nels
+  def setup_nels_for_units
     person = Factory(:person)
     @user = person.user
     @project = person.projects.first
     @project.settings['nels_enabled'] = true
+    @nels_access_token = 'fake-access-token'
 
-    @user.oauth_sessions.where(provider: 'NeLS').create(access_token: 'fake-access-token', expires_at: 1.week.from_now)
-
-    login_as(@user)
+    @user.oauth_sessions.where(provider: 'NeLS').create(access_token: @nels_access_token, expires_at: 1.week.from_now)
 
     study = Factory(:study, investigation: Factory(:investigation, project_ids: [@project.id]))
     @assay = Factory(:assay, contributor: person, study: study)
@@ -17,6 +16,12 @@ module NelsTestHelper
     @dataset_id = 91123528
     @subtype = 'reads'
     @reference = 'xMTEyMzEyMjoxMTIzNTI4OnJlYWRz'
+  end
+
+  def setup_nels
+    setup_nels_basic
+
+    login_as(@user)
   end
 
 end
