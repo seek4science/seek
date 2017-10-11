293df63f83ce00b4b42c7aaeca8ee043361d1cd8
alaninmcr <alan.r.williams@manchester.ac.uk>
2017-10-11 14:49:22 +0000

    Changed serialization of versioned things

--- app/serializers/contributed_resource_serializer.rb
+++ app/serializers/contributed_resource_serializer.rb
@@ -12,7 +12,18 @@ class ContributedResourceSerializer < PCSSerializer
     versions_data
   end
 
-  attribute :requested_version do
+   attribute :version do
+     v = @scope[:requested_version]
+     if v.nil?
+       v = object.version
+     end
+
+     requested_version = object.find_version(v)
+
+     requested_version.version
+  end
+
+  attribute :revision_comments do
     v = @scope[:requested_version]
     if v.nil?
       v = object.version
@@ -20,15 +31,48 @@ class ContributedResourceSerializer < PCSSerializer
 
     requested_version = object.find_version(v)
 
-    requested = {version: requested_version.version,
-                 revision_comments: requested_version.revision_comments.presence,
-                 created_at: requested_version.created_at,
-                 updated_at: requested_version.updated_at
-    }
-
-
-    requested
+    requested_version.revision_comments.presence
   end
+
+  attribute :created_at do
+    v = @scope[:requested_version]
+    if v.nil?
+      v = object.version
+    end
+
+    requested_version = object.find_version(v)
+
+    requested_version.created_at
+  end
+
+  attribute :updated_at do
+    v = @scope[:requested_version]
+    if v.nil?
+      v = object.version
+    end
+
+    requested_version = object.find_version(v)
+
+    requested_version.updated_at
+  end
+
+  # attribute :requested_version do
+  #   v = @scope[:requested_version]
+  #   if v.nil?
+  #     v = object.version
+  #   end
+  #
+  #   requested_version = object.find_version(v)
+  #
+  #   requested = {version: requested_version.version,
+  #                revision_comments: requested_version.revision_comments.presence,
+  #                created_at: requested_version.created_at,
+  #                updated_at: requested_version.updated_at
+  #   }
+  #
+  #
+  #   requested
+  # end
 
  has_many :content_blobs do
 
@@ -48,6 +92,6 @@ class ContributedResourceSerializer < PCSSerializer
       blobs.append(requested_version.content_blob)
     end
     blobs
-  end
+ end
 
 end
