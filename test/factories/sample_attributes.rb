3534ee66ab2be07eeac3be7d845539ec78d45bfb
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-11-01 09:57:58 +0000

    Revert "Final test fix attempt"
    This reverts commit a3f0900487cfc60581fb1d17a5c0ad9dec148e50.

--- test/factories/sample_attributes.rb
+++ test/factories/sample_attributes.rb
@@ -6,25 +6,19 @@ end
 
 # a string that must contain 'xxx'
 Factory.define(:simple_string_sample_attribute, parent: :sample_attribute) do |f|
+  f.sample_attribute_type factory: :xxx_string_sample_attribute_type
   f.required true
-  f.after_build do |type|
-    type.sample_attribute_type = Factory(:xxx_string_sample_attribute_type)
-  end
 end
 
 Factory.define(:any_string_sample_attribute, parent: :sample_attribute) do |f|
+  f.sample_attribute_type factory: :string_sample_attribute_type
   f.required true
-  f.after_build do |type|
-    type.sample_attribute_type = Factory(:string_sample_attribute_type)
-  end
 end
 
 Factory.define(:sample_sample_attribute, parent: :sample_attribute) do |f|
   f.sequence(:title) { |n| "sample attribute #{n}" }
-  f.after_build do |type|
-    type.linked_sample_type = Factory(:simple_sample_type)
-    type.sample_attribute_type = Factory(:sample_sample_attribute_type)
-  end
+  f.linked_sample_type factory: :simple_sample_type
+  f.sample_attribute_type factory: :sample_sample_attribute_type
 end
 
 Factory.define(:apples_controlled_vocab_attribute, parent: :sample_attribute) do |f|
