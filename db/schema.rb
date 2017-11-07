a7d85b745b825f1d9a4885abb213e7cc2271504d
Finn Bacall <finn.bacall@manchester.ac.uk>
2017-11-07 12:56:02 +0000

    Migration to convert existing SEEK sample data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171011095056) do
+ActiveRecord::Schema.define(version: 20171107102053) do
 
   create_table "activity_logs", force: :cascade do |t|
     t.string   "action",                 limit: 255
