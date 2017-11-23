0bc0bfa632f075c04cc71d6d2eb7578f2af8762f
Hadas Leonov <blackbirdy@gmail.com>
2017-11-23 17:22:43 +0100

    add full relationships to max-presentation object testing

--- test/factories/presentations.rb
+++ test/factories/presentations.rb
@@ -26,6 +26,9 @@ Factory.define(:max_presentation, class: Presentation) do |f|
   f.title 'A Maximal Presentation'
   f.description 'Non-equilibrium Free Energy Calculations and their caveats'
   f.projects { [Factory.build(:max_project)] }
+  f.assays {[Factory.build(:max_assay, policy: Factory(:public_policy))]}
+  f.events {[Factory.build(:event, policy: Factory(:public_policy))]}
+  f.relationships {[Factory(:relationship, predicate: Relationship::RELATED_TO_PUBLICATION, other_object: Factory(:publication))]}
   f.after_create do |presentation|
     presentation.content_blob = Factory.create(:min_content_blob, original_filename: 'test.pdf', content_type: 'application/pdf', asset: presentation, asset_version: presentation.version)
   end
