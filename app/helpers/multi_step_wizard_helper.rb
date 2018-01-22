4930cb9498f84b4c75d4504f5d7b5fa0f0931f8d
Stuart Owen <stuart.owen@manchester.ac.uk>
2018-01-22 11:43:44 +0000

    OPSK-1483 put back the buttons and display in bootstrap panel

--- app/helpers/multi_step_wizard_helper.rb
+++ app/helpers/multi_step_wizard_helper.rb
@@ -1,14 +1,22 @@
 # helper methods to support a wizard like form with multiple steps (e.g. for data file uploads)
 module MultiStepWizardHelper
 
+  def multi_step_back_icons
+    content_tag(:button,' ',class:'multi-step-start-icon') +
+      content_tag(:button,' ',class:'multi-step-back-icon')
+  end
+
+  def multi_step_forward_icons
+    content_tag(:button,' ',class:'multi-step-next-icon') +
+      content_tag(:button,' ',class:'multi-step-end-icon')
+  end
+
   def multi_step_back_buttons
-    content_tag(:button,' ',class:'multi-step-start-button') +
-      content_tag(:button,' ',class:'multi-step-back-button')
+    content_tag(:button,'Back',class:'multi-step-back-button btn btn-default')
   end
 
   def multi_step_forward_buttons
-    content_tag(:button,' ',class:'multi-step-next-button') +
-      content_tag(:button,' ',class:'multi-step-end-button')
+    content_tag(:button,'Next',class:'multi-step-next-button btn btn-primary')
   end
 
 end
