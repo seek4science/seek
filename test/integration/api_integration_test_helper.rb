cd3a3ae5d837cf69244298b99f859c0a8a9eb3e0
Hadas Leonov <blackbirdy@gmail.com>
2018-01-22 14:57:30 +0100

    transfered common tests from person to api test helper

--- test/integration/api_integration_test_helper.rb
+++ test/integration/api_integration_test_helper.rb
@@ -46,7 +46,17 @@ module ApiIntegrationTestHelper
   #   @to_post['data']['relationships'].each do |key, value|
   #     assert_equal value, h['data']['relationships'][key]
   #   end
-  # end
+  # end ("#{m}_#{clz}").to_sym
+
+  def test_should_delete_object
+    obj = Factory(("#{@clz}").to_sym)
+    #get "/people/#{a_person.id}.json"
+    delete "/#{@plural_clz}/#{obj.id}.json"
+    assert_response :success
+
+    get "/#{@plural_clz}/#{obj.id}.json"
+    assert_response :not_found
+  end
 
   def test_create_should_error_on_given_id
     @json_mm["min"]["data"]["id"] = "100000000"
@@ -54,6 +64,54 @@ module ApiIntegrationTestHelper
     post "/#{@plural_clz}.json", @json_mm["min"]
     assert_response :unprocessable_entity
     assert_match "A POST request is not allowed to specify an id", response.body
+  end
+
+  def test_create_should_error_on_wrong_type
+    @json_mm["min"]["data"]["type"] = "wrong"
+    post "/#{@plural_clz}.json", @json_mm["min"]
+    assert_response :unprocessable_entity
+    assert_match "The specified data:type does not match the URL's object (#{@json_mm["min"]["data"]["type"]} vs. #{@plural_clz})", response.body
+  end
+
+
+  def test_create_should_error_on_missing_type
+    @json_mm["min"]["data"].delete("type")
+    post "/#{@plural_clz}.json", @json_mm["min"]
+    assert_response :unprocessable_entity
+    assert_match "A POST/PUT request must specify a data:type", response.body
+  end
+
+  def test_update_should_error_on_wrong_id
+    obj = Factory(("#{@clz}").to_sym)
+    @json_mm["min"]["data"]["id"] = "100000000"
+    @json_mm["min"]["data"]["type"] = "#{@plural_clz}"
+
+    #wrong id = failire
+    put "/#{@plural_clz}/#{obj.id}.json", @json_mm["min"]
+    assert_response :unprocessable_entity
+    assert_match "id specified by the PUT request does not match object-id in the JSON input", response.body
+
+    #correct id = success
+    @json_mm["min"]["data"]["id"] = obj.id
+    @json_mm["min"]["data"]["attributes"]["title"] = "Updated #{@clz}"
+    put "/#{@plural_clz}/#{obj.id}.json", @json_mm["min"]
+    assert_response :success
+  end
+
+  def test_update_should_error_wrong_type
+    obj = Factory(("#{@clz}").to_sym)
+    @json_mm["min"]["data"]["type"] = "wrong"
+    put "/#{@plural_clz}/#{obj.id}.json", @json_mm["min"]
+    assert_response :unprocessable_entity
+    assert_match "The specified data:type does not match the URL's object (#{@json_mm["min"]["data"]["type"]} vs. #{@plural_clz})", response.body
+  end
+
+  def test_update_should_error_on_missing_type
+    obj = Factory(("#{@clz}").to_sym)
+    @json_mm["min"]["data"].delete("type")
+    put "/#{@plural_clz}/#{obj.id}.json", @json_mm["min"]
+    assert_response :unprocessable_entity
+    assert_match "A POST/PUT request must specify a data:type", response.body
   end
 
 end
