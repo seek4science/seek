2859437cf97bd134ca4984dd55e0f7e62f8c8d27
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-01-23 11:15:41 +0000

    Document tests

--- test/factories/documents.rb
+++ test/factories/documents.rb
@@ -16,6 +16,14 @@ Factory.define(:document) do |f|
   end
 end
 
+Factory.define(:public_document, parent: :document) do |f|
+  f.policy { Factory(:downloadable_public_policy) }
+end
+
+Factory.define(:private_document, parent: :document) do |f|
+  f.policy { Factory(:private_policy) }
+end
+
 Factory.define(:min_document, class: Document) do |f|
   f.title 'A Minimal Document'
   f.projects { [Factory.build(:min_project)] }
@@ -33,5 +41,18 @@ Factory.define(:max_document, class: Document) do |f|
   f.relationships {[Factory(:relationship, predicate: Relationship::RELATED_TO_PUBLICATION, other_object: Factory(:publication))]}
   f.after_create do |document|
     document.content_blob = Factory.create(:min_content_blob, content_type: 'application/pdf', asset: document, asset_version: document.version)
+  end
+end
+
+# Factory::Version
+Factory.define(:document_version, class: Document::Version) do |f|
+  f.association :document
+  f.projects { document.projects }
+  f.after_create do |document_version|
+    document_version.document.version += 1
+    document_version.document.save
+    document_version.version = document_version.document.version
+    document_version.title = document_version.document.title
+    document_version.save
   end
 end
