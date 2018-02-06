6cb6507c050d450a1273720732f7b535054c1146
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-02-06 10:46:55 +0000

    OPSK-1580 Throttle login attempts

---
+++ config/initializers/rack_attack.rb
@@ -0,0 +1,15 @@
+class Rack::Attack
+  if Rails.env.production?
+    throttle('logins/username', limit: 10, period: 5.minutes) do |req|
+      if req.path == '/session' && req.post?
+        req.params['login']
+      end
+    end
+
+    throttle('logins/ip', limit: 5, period: 1.hour) do |req|
+      if req.path == '/session' && req.post?
+        req.ip
+      end
+    end
+  end
+end
