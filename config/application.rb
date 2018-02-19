37479510fff2766462785a6d304d4c87653124e1
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-02-19 14:56:19 +0000

    Change the queue adapter in test environment!

--- config/application.rb
+++ config/application.rb
@@ -76,6 +76,6 @@ module SEEK
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.active_job.queue_adapter = :delayed_job
+    config.active_job.queue_adapter = Rails.env.test? ? :test : :delayed_job
   end
 end
