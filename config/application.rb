d01fe59a6b7bc861bba0c15059b14d499e463d1a
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-02-19 11:05:14 +0000

    Use delayed_job for emails

--- config/application.rb
+++ config/application.rb
@@ -75,5 +75,7 @@ module SEEK
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.active_job.queue_adapter = :delayed_job
   end
 end
