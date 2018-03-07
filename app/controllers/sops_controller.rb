065cff10df3cad876cf60a514aca838735924b12
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-03-07 15:27:27 +0000

    Don't clear tags when no `tag_list` param is provided

--- app/controllers/sops_controller.rb
+++ app/controllers/sops_controller.rb
@@ -47,9 +47,7 @@ class SopsController < ApplicationController
 
   # PUT /sops/1
   def update
-    update_annotations(params[:tag_list], @sop)
-    pp @sop
-    update_scales @sop
+    update_annotations(params[:tag_list], @sop) if params.key?(:tag_list)
 
     @sop.attributes = sop_params
 
