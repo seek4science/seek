1e3c96a7f9852409371a2e66b0b70ae0c440c34c
Hadas Leonov <blackbirdy@gmail.com>
2018-03-13 15:47:21 +0100

    Added assay max tests + ISA max patch

--- test/integration/api/assay_CUD_test.rb
+++ test/integration/api/assay_CUD_test.rb
@@ -15,13 +15,11 @@ class AssayCUDTest < ActionDispatch::IntegrationTest
     # Populate the assay classes
     Factory(:modelling_assay_class)
     Factory(:experimental_assay_class)
-    assay = Factory(:experimental_assay, policy: Factory(:public_policy))
-    assay.contributor = @current_user.person
-    assay.save
-
+    @assay = Factory(:experimental_assay, policy: Factory(:public_policy))
+    @assay.contributor = @current_user.person
+    @assay.save
     hash = {study_id: @study.id, r: ApiTestHelper.method(:render_erb)}
     @to_post = load_template("post_min_#{@clz}.json.erb", hash)
-    @to_patch = load_template("patch_#{@clz}.json.erb", {id: assay.id})
   end
 
   def create_post_values
@@ -29,6 +27,14 @@ class AssayCUDTest < ActionDispatch::IntegrationTest
                       creator_ids: [@current_user.person.id],
                       project_id: Factory(:project).id,
                       r: ApiTestHelper.method(:render_erb) }
+  end
+
+  def create_patch_values
+    @patch_values = {id: @assay.id,
+                     study_id: @assay.study.id,
+                     project_id: Factory(:project).id,
+                     creator_ids: [@current_user.person.id],
+                     r: ApiTestHelper.method(:render_erb) }
   end
 
   def populate_extra_relationships
