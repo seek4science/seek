b8cda7a550788ab71a2d31d44f30344cae8124b3
Hadas Leonov <blackbirdy@gmail.com>
2018-03-13 11:22:22 +0100

    Added deletion test to assay CUD tests

--- test/integration/api/assay_CUD_test.rb
+++ test/integration/api/assay_CUD_test.rb
@@ -44,4 +44,48 @@ class AssayCUDTest < ActionDispatch::IntegrationTest
     extra_relationships[:investigation] = JSON.parse "{\"data\" : {\"id\" : \"#{investigation_id}\", \"type\" : \"investigations\"}}"
     extra_relationships.with_indifferent_access
   end
+
+  test 'should not delete assay when not project member' do
+    a = Factory(:max_assay)
+    person = Factory(:person)
+    user_login(person)
+    assert_no_difference('ActivityLog.count') do
+      assert_no_difference('Assay.count') do
+        delete "/#{@plural_clz}/#{a.id}.json"
+        assert_response :forbidden
+      end
+    end
+  end
+
+  test 'project member can delete' do
+    person = Factory(:person)
+    user_login(person)
+    proj = person.projects.first
+    assay = Factory(:experimental_assay,
+                    policy: Factory(:policy,
+                                    access_type: Policy::NO_ACCESS,
+                                    permissions: [Factory(:permission, contributor: proj, access_type: Policy::MANAGING)]))
+
+    assert_difference('Assay.count', -1) do
+      delete "/#{@plural_clz}/#{assay.id}.json"
+      assert_response :success
+    end
+  end
+
+  test 'can delete an assay with subscriptions' do
+    assay = Factory(:assay, policy: Factory(:public_policy, access_type: Policy::VISIBLE))
+    p = Factory(:person)
+    Factory(:subscription, person: assay.contributor, subscribable: assay)
+    Factory(:subscription, person: p, subscribable: assay)
+
+    user_login(assay.contributor)
+
+    assert_difference('Subscription.count', -2) do
+      assert_difference('Assay.count', -1) do
+        delete "/#{@plural_clz}/#{assay.id}.json"
+        assert_response :success
+      end
+    end
+  end
+
 end
