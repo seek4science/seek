af5bf52278ad80d6cd5002c09463e6f4f0e30cbc
Stuart Owen <stuart.owen@manchester.ac.uk>
2018-03-22 15:24:37 +0000

    handle left and right keypress to step through wizard steps

--- app/assets/javascripts/multi_step_wizard.js
+++ app/assets/javascripts/multi_step_wizard.js
@@ -1,10 +1,14 @@
 var MultiStepWizard = {
     currentStep: 1,
     nextStep: function() {
-        this.jumpToStep(this.currentStep+1);
+        if (this.currentStep.toString()!=MultiStepWizard.lastStep()) {
+            this.jumpToStep(this.currentStep+1);
+        }
     },
     prevStep: function() {
-        this.jumpToStep(this.currentStep-1);
+        if (this.currentStep>1) {
+            this.jumpToStep(this.currentStep-1);
+        }
     },
     jumpToStep: function(step) {
         $j('#step-' + this.currentStep).hide();
@@ -44,5 +48,15 @@ $j(document).ready(function () {
     $j('.multi-step-start-button').click(function () {
         MultiStepWizard.jumpToStart();
         return false;
+    });
+
+    $j(document).keydown(function(e) {
+        if (e.keyCode==39) {
+            MultiStepWizard.nextStep();
+        }
+
+        if (e.keyCode==37) {
+            MultiStepWizard.prevStep();
+        }
     });
 });
