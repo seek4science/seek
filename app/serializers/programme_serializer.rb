194ee29eff7e8a45d332fa718bd29af3c360aceb
Hadas Leonov <blackbirdy@gmail.com>
2018-03-28 17:06:58 +0200

    added max relationships to programme CUD tests + administrators relationship to readAPI

--- app/serializers/programme_serializer.rb
+++ app/serializers/programme_serializer.rb
@@ -6,7 +6,7 @@ class ProgrammeSerializer < AvatarObjSerializer
   attribute :tags, key: :funding_codes do
     serialize_annotations(object, context = 'funding_code')
   end
-
+  has_many :administrators
   has_many :people
   has_many :projects
   has_many :institutions
@@ -20,4 +20,8 @@ class ProgrammeSerializer < AvatarObjSerializer
   has_many :presentations
   has_many :events
   has_many :documents
+
+  def administrators
+    object.programme_administrators
+  end
 end
