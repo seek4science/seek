db26218826ebf06c037f28dc9bc0e1d8633fa409
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-04-18 15:49:50 +0100

    OPSK-1577 Re-enabled and fixed `rest_test_cases` for ContentBlobs

--- app/serializers/content_blob_serializer.rb
+++ app/serializers/content_blob_serializer.rb
@@ -1,6 +1,12 @@
 class ContentBlobSerializer < BaseSerializer
   attributes :original_filename, :url, :md5sum, :sha1sum, :content_type
 
+  attribute :size do
+    object.file_size
+  end
+
+  has_one :asset, polymorphic: true
+
   def self_link
     polymorphic_path([object.asset, object])
   end
