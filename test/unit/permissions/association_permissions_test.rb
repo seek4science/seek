4d8cf6ec0eaa0e59ee52896a50b02c791cd691dd
Stuart Owen <stuart.owen@manchester.ac.uk>
2018-05-02 11:34:13 +0100

    rubocop on tests

--- test/unit/permissions/association_permissions_test.rb
+++ test/unit/permissions/association_permissions_test.rb
@@ -4,21 +4,19 @@ require 'test_helper'
 #
 # ... for example it shouldn't be possible to link an Assay to a Study if the Study isn't visible and in the same project
 class AssociationPermissionsTest < ActiveSupport::TestCase
-
   def setup
     @person = Factory(:person)
     @user = @person.user
     @project = @person.projects.first
-
   end
 
   # Assay->Study - study must be viewable and belong to the same project as the current user
   test 'assay linked to study' do
     User.with_current_user(@user) do
-      assay = Factory(:assay,contributor:@person)
-      good_study = Factory(:study, contributor:@person,investigation:Factory(:investigation,projects:[@project]))
-      bad_study1 = Factory(:study, contributor: Factory(:person),investigation:Factory(:investigation,projects:[@project]))
-      bad_study2 = Factory(:study, contributor: Factory(:person),policy:Factory(:public_policy))
+      assay = Factory(:assay, contributor: @person)
+      good_study = Factory(:study, contributor: @person, investigation: Factory(:investigation, projects: [@project]))
+      bad_study1 = Factory(:study, contributor: Factory(:person), investigation: Factory(:investigation, projects: [@project]))
+      bad_study2 = Factory(:study, contributor: Factory(:person), policy: Factory(:public_policy))
 
       assert good_study.can_view?
       assert good_study.projects.include?(@project)
@@ -40,8 +38,8 @@ class AssociationPermissionsTest < ActiveSupport::TestCase
       refute assay.save
 
       # check it saves with the study already linked
-      assay = Factory(:assay,study:bad_study1,contributor:@person)
-      assay.title='fish'
+      assay = Factory(:assay, study: bad_study1, contributor: @person)
+      assay.title = 'fish'
       assert assay.save
     end
   end
@@ -49,10 +47,10 @@ class AssociationPermissionsTest < ActiveSupport::TestCase
   # Study->Investigation investigation must be viewable and belong to the same project as the current user
   test 'study linked to investigation' do
     User.with_current_user(@user) do
-      study = Factory(:study,contributor:@person)
-      good_inv = Factory(:investigation, contributor:@person,projects:[@project])
-      bad_inv1 = Factory(:investigation, contributor: Factory(:person),projects:[@project])
-      bad_inv2 = Factory(:investigation, contributor: Factory(:person),policy:Factory(:public_policy))
+      study = Factory(:study, contributor: @person)
+      good_inv = Factory(:investigation, contributor: @person, projects: [@project])
+      bad_inv1 = Factory(:investigation, contributor: Factory(:person), projects: [@project])
+      bad_inv2 = Factory(:investigation, contributor: Factory(:person), policy: Factory(:public_policy))
 
       assert good_inv.can_view?
       assert good_inv.projects.include?(@project)
@@ -74,8 +72,8 @@ class AssociationPermissionsTest < ActiveSupport::TestCase
       refute study.save
 
       # check it saves with the study already linked
-      study = Factory(:study,contributor:@person, investigation: bad_inv1)
-      study.title='fish'
+      study = Factory(:study, contributor: @person, investigation: bad_inv1)
+      study.title = 'fish'
       assert study.save
     end
   end
@@ -83,84 +81,82 @@ class AssociationPermissionsTest < ActiveSupport::TestCase
   # Asset->Assay - assay must be editable
   test 'assets linked to assay' do
     User.with_current_user(@user) do
-
-      [:data_file,:model,:sop, :sample, :document].each do |asset_type|
-        good_assay = Factory(:assay,contributor:@person)
-        bad_assay = Factory(:assay,policy:Factory(:publicly_viewable_policy))
+      %i[data_file model sop sample document].each do |asset_type|
+        good_assay = Factory(:assay, contributor: @person)
+        bad_assay = Factory(:assay, policy: Factory(:publicly_viewable_policy))
 
         assert good_assay.can_edit?
         refute bad_assay.can_edit?
-        assert bad_assay.can_view? #check is can actually be viewed
+        assert bad_assay.can_view? # check is can actually be viewed
 
-        asset = Factory(asset_type,contributor:@person)
+        asset = Factory(asset_type, contributor: @person)
         assert asset.can_edit?
         assert_empty asset.assays
 
-        asset.assay_assets.create(assay:good_assay)
+        asset.assay_assets.create(assay: good_assay)
         assert asset.save
         asset.reload
-        assert_equal [good_assay],asset.assays
+        assert_equal [good_assay], asset.assays
 
-        asset = Factory(asset_type,contributor:@person)
+        asset = Factory(asset_type, contributor: @person)
         assert asset.can_edit?
         assert_empty asset.assays
 
-        asset.assay_assets.create(assay:bad_assay)
+        asset.assay_assets.create(assay: bad_assay)
         refute asset.save
         asset.reload
         assert_empty asset.assays
 
-        #check it only checks new links
+        # check it only checks new links
         disable_authorization_checks do
-          asset = Factory(asset_type,contributor:@person)
-          asset.assay_assets.create(assay:bad_assay)
+          asset = Factory(asset_type, contributor: @person)
+          asset.assay_assets.create(assay: bad_assay)
           assert asset.save
         end
 
-        assert_equal [bad_assay],asset.assays
-        asset.assay_assets.build(assay:good_assay)
+        assert_equal [bad_assay], asset.assays
+        asset.assay_assets.build(assay: good_assay)
         assert asset.save
         asset.reload
-        assert_equal [good_assay, bad_assay].sort,asset.assays.sort
+        assert_equal [good_assay, bad_assay].sort, asset.assays.sort
       end
-
     end
   end
 
   # Assay->Asset - asset must be viewable
   test 'assays linked to assets' do
     User.with_current_user(@user) do
-      [:data_file,:model,:sop, :sample, :document].each do |asset_type|
-        good_asset = Factory(asset_type,policy:Factory(:publicly_viewable_policy), contributor:Factory(:person))
-        bad_asset = Factory(asset_type,policy:Factory(:private_policy), contributor:Factory(:person))
+      %i[data_file model sop sample document].each do |asset_type|
+        good_asset = Factory(asset_type, policy: Factory(:publicly_viewable_policy), contributor: Factory(:person))
+        bad_asset = Factory(asset_type, policy: Factory(:private_policy), contributor: Factory(:person))
 
         assert good_asset.can_view?
         refute bad_asset.can_view?
 
-        assay = Factory(:assay,contributor:@person)
+        assay = Factory(:assay, contributor: @person)
         assert assay.can_edit?
 
-        assay.assay_assets.create(asset:good_asset)
+        assay.assay_assets.create(asset: good_asset)
         assert assay.save
         assay.reload
-        assert_equal [good_asset],assay.assets
+        assert_equal [good_asset], assay.assets
 
-        assay.assay_assets.create(asset:bad_asset)
+        assay.assay_assets.create(asset: bad_asset)
         refute assay.save
         assay.reload
         assert_empty assay.assets
 
-        #check it only checks new links
+        # check it only checks new links
         disable_authorization_checks do
-          assay = Factory(:assay,contributor:@person)
-          assay.assay_assets.create(asset:bad_asset)
+          assay = Factory(:assay, contributor: @person)
+          assay.assay_assets.create(asset: bad_asset)
           assert assay.save
         end
 
-        assay.assay_assets.create(asset:good_asset)
+        assay.assay_assets.create(asset: good_asset)
         assert assay.save
         assay.reload
-        assert_equal [good_asset, bad_asset].sort,assay.assets.sort
+        assert_equal [good_asset, bad_asset].sort, assay.assets.sort
       end
     end
   end
@@ -168,9 +164,9 @@ class AssociationPermissionsTest < ActiveSupport::TestCase
   test 'must be in project when creating an asset' do
     User.with_current_user(@user) do
       other_project = Factory(:project)
-      [:data_file,:model,:sop, :sample, :document].each do |asset_type|
-        good_asset = Factory.build(asset_type,projects:[@project])
-        bad_asset = Factory.build(asset_type,projects:[other_project])
+      %i[data_file model sop sample document].each do |asset_type|
+        good_asset = Factory.build(asset_type, projects: [@project])
+        bad_asset = Factory.build(asset_type, projects: [other_project])
 
         assert good_asset.save
         refute bad_asset.save
@@ -182,13 +178,11 @@ class AssociationPermissionsTest < ActiveSupport::TestCase
     User.with_current_user(@user) do
       other_project = Factory(:project)
 
-      good_inv = Factory.build(:investigation,projects:[@project])
-      bad_inv = Factory.build(:investigation,projects:[other_project])
+      good_inv = Factory.build(:investigation, projects: [@project])
+      bad_inv = Factory.build(:investigation, projects: [other_project])
 
       assert good_inv.save
       refute bad_inv.save
     end
   end
-
-
 end
