8868bfcc48177338fe3515a09f8249b83d51498f
Stuart Owen <stuart.owen@manchester.ac.uk>
2018-05-03 13:47:13 +0100

    fix to assay helper tests

--- test/unit/helpers/assays_helper_test.rb
+++ test/unit/helpers/assays_helper_test.rb
@@ -5,31 +5,30 @@ class AssaysHelperTest < ActionView::TestCase
   include AssetsHelper
 
   test 'authorised_assays' do
-    p1 = Factory :person
-    p2 = Factory :person
+    project = Factory(:project)
+    other_project = Factory(:project)
+    p1 = Factory :person, project: project
+    p2 = Factory :person, project: project
 
     # 2 assays of the same project, but different contributors
     a1 = Factory :assay, contributor: p1, policy: Factory(:downloadable_public_policy)
     a2 = Factory :assay, study: a1.study, contributor: p2, policy: Factory(:downloadable_public_policy)
 
-    a3 = Factory :assay, contributor: p1, policy: Factory(:downloadable_public_policy)
-    a4 = Factory :assay, study: a3.study, contributor: p2, policy: Factory(:downloadable_public_policy)
-
     assert_equal a1.projects, a2.projects
-    assert_equal a3.projects, a4.projects
-    refute_equal a1.projects, a3.projects
 
     User.with_current_user(p1.user) do
       assays = authorised_assays(nil, 'download').sort_by(&:id)
-      assert_equal [a1, a2, a3, a4], assays
-
-      assays = authorised_assays.sort_by(&:id)
-      assert_equal [a1, a3], assays
-
-      assays = authorised_assays(a1.projects, 'download').sort_by(&:id)
       assert_equal [a1, a2], assays
 
+      # nothing matches the project
+      assays = authorised_assays(other_project, 'download').sort_by(&:id)
+      assert_empty assays
+
       assays = authorised_assays(a1.projects, 'edit').sort_by(&:id)
+      assert_equal [a1], assays
+
+      # edit is the default
+      assays = authorised_assays.sort_by(&:id)
       assert_equal [a1], assays
     end
   end
