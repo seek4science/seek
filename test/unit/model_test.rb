f73a2ba7663b9f7dfc9369b411d1fa36449e75cd
Stuart Owen <stuart.owen@manchester.ac.uk>
2018-05-03 13:50:49 +0100

    fix to Model test
    required contributor to be able to edit the assay

--- test/unit/model_test.rb
+++ test/unit/model_test.rb
@@ -116,18 +116,22 @@ class ModelTest < ActiveSupport::TestCase
   end
 
   test 'assay association' do
-    model = models(:teusink)
-    assay = assays(:modelling_assay_with_data_and_relationship)
-    assay_asset = assay_assets(:metabolomics_assay_asset1)
-    assert_not_equal assay_asset.asset, model
-    assert_not_equal assay_asset.assay, assay
-    assay_asset.asset = model
-    assay_asset.assay = assay
-    User.with_current_user(model.contributor) { assay_asset.save! }
-    assay_asset.reload
-    assert assay_asset.valid?
-    assert_equal assay_asset.asset, model
-    assert_equal assay_asset.assay, assay
+    person = Factory(:person)
+    User.with_current_user(person.user) do
+      model = Factory(:model, contributor:person)
+      assay = Factory(:assay, contributor:person)
+      assay_asset = AssayAsset.new
+      assert_not_equal assay_asset.asset, model
+      assert_not_equal assay_asset.assay, assay
+      assay_asset.asset = model
+      assay_asset.assay = assay
+      assay_asset.save!
+      assay_asset.reload
+      assert assay_asset.valid?
+      assert_equal assay_asset.asset, model
+      assert_equal assay_asset.assay, assay
+    end
+
   end
 
   test 'sort by updated_at' do
