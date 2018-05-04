e0fd4fb9db81af919d838a7d916b2d57970b3581
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-05-04 14:53:41 +0100

    Fixed projects issue with investigation factory

--- test/factories/investigations.rb
+++ test/factories/investigations.rb
@@ -2,17 +2,17 @@
 Factory.define(:investigation, class: Investigation) do |f|
   f.sequence(:title) { |n| "Investigation#{n}" }
   f.association :contributor, factory: :person
-  f.after_build do |p|
-    p.projects = [p.contributor.person.projects.first] if p.projects.empty?
+  f.after_build do |i|
+    i.projects = [i.contributor.person.projects.first] if i.projects.empty?
   end
 end
 
 Factory.define(:min_investigation, class: Investigation) do |f|
   f.title "A Minimal Investigation"
-  f.after_build do |p|
-    project = Factory.build(:min_project)
-    p.contributor ||= Factory(:person, project: project)
-    p.projects = [project] if p.projects.empty?
+  f.after_build do |i|
+    project = Factory(:min_project)
+    i.contributor ||= Factory(:person, project: project)
+    i.projects = [project] if i.projects.empty?
   end
 end
 
@@ -20,7 +20,7 @@ Factory.define(:max_investigation, parent: :min_investigation) do |f|
   f.title "A Maximal Investigation"
   f.other_creators "Max Blumenthal, Ed Snowden"
   f.description "Investigation of the Human Genome"
-  f.after_build do |p|
-    p.studies = [Factory(:max_study, contributor: p.contributor, policy: Factory(:public_policy))]
+  f.after_build do |i|
+    i.studies = [Factory(:max_study, contributor: i.contributor, policy: Factory(:public_policy))]
   end
 end
