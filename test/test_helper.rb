653855c2abd821dc401ba3f40a640b6027fda815
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-07-06 13:43:01 +0100

    Try random-ordered tests again

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,8 +24,6 @@ require 'ostruct'
 
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new]
 
-Minitest::Test.i_suck_and_my_tests_are_order_dependent!
-
 module ActionView
   class Renderer
     def self.get_alternative(key)
