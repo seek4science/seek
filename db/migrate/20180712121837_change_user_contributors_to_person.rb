ea3d0eecee3f7e38bbefe3ae0c094670c1d27cc0
Stuart Owen <stuart.owen@manchester.ac.uk>
2018-07-12 14:09:11 +0100

    OPSK-696 db migration to swap User contributors to Person

---
+++ db/migrate/20180712121837_change_user_contributors_to_person.rb
@@ -0,0 +1,20 @@
+class ChangeUserContributorsToPerson < ActiveRecord::Migration
+  def up
+    types = Seek::Util.authorized_types.select{|t| t.attribute_names.include?('contributor_type')}
+    types.each do |type|
+      # the update could potentially be done in one update & join, but would be different for each database type
+      sql = "SELECT #{type.table_name}.id, users.person_id FROM #{type.table_name} LEFT JOIN users ON users.id = contributor_id WHERE #{type.table_name}.contributor_type = 'User';"
+      ActiveRecord::Base.connection.select_all(sql).each do |record|
+        id = record['id']
+        person_id = record['person_id']
+        sql = "UPDATE #{type.table_name} SET contributor_id = #{person_id}, contributor_type='Person' WHERE id = #{id}"
+        ActiveRecord::Base.connection.execute(sql)
+      end
+
+    end
+  end
+
+  def down
+
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410093814) do
+ActiveRecord::Schema.define(version: 20180712121837) do
 
   create_table "activity_logs", force: :cascade do |t|
     t.string   "action",                 limit: 255
