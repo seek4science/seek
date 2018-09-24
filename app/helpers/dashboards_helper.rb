f15f1afddcaa10844372fe314ca6cb4eaf65ad61
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-09-24 10:41:56 +0100

    Improved appearance of "top x" lists

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -15,4 +15,14 @@ module DashboardsHelper
     end
   end
 
+  def resource_with_count(asset, count)
+    avatar = resource_avatar_path(asset) || icon_filename_for_key("#{asset.class.name.downcase}_avatar")
+    link_to(asset, title: "#{asset.class.name.humanize} - #{asset.title}", target: :_blank, class: 'mini-resource-list-item') do
+      content_tag(:span, class: 'mini-resource-list-text') do
+        image_tag(asset_path(avatar), class: 'mini-resource-list-avatar') +
+            content_tag(:span, asset.title, class: 'mini-resource-list-title')
+      end +
+      content_tag(:span, count, class: 'mini-resource-list-count')
+    end
+  end
 end
