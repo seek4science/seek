4ad8b7ba47cb0987a5fa8da6f98396f59f6a796e
alaninmcr <alan.r.williams@manchester.ac.uk>
2018-09-27 11:07:42 +0100

    Some ordering

--- test/unit/util_test.rb
+++ test/unit/util_test.rb
@@ -7,15 +7,15 @@ class UtilTest < ActiveSupport::TestCase
     expected = [DataFile, Document, Model, Presentation, Publication, Sample, Sop, Assay, Investigation, Study, Event, SampleType, Strain, Workflow]
 
     # first as strings for more readable failed assertion message
-    assert_equal expected.map(&:to_s), types.map(&:to_s)
+    assert_equal expected.map(&:to_s).sort, types.map(&:to_s).sort
 
     # double check they are actual types
-    assert_equal expected, types
+    assert_equal expected.sort_by(&:to_s), types.sort_by(&:to_s)
   end
 
   test 'authorized types' do
-    expected = [Assay, DataFile, Document, Event, Investigation, Model, Presentation, Publication, Sample, Sop, Strain, Study, Workflow].map(&:name)
-    actual = Seek::Util.authorized_types.map(&:name)
+    expected = [Assay, DataFile, Document, Event, Investigation, Model, Presentation, Publication, Sample, Sop, Strain, Study, Workflow].map(&:name).sort
+    actual = Seek::Util.authorized_types.map(&:name).sort
     assert_equal expected, actual
   end
 
@@ -30,23 +30,23 @@ class UtilTest < ActiveSupport::TestCase
     expected = [Assay, DataFile, Document, Event, Institution, Investigation, Model, Person, Presentation, Programme, Project, Publication, Sample, SampleType, Sop, Strain, Study, Workflow]
 
     # first as strings for more readable failed assertion message
-    assert_equal expected.map(&:to_s), types.map(&:to_s)
+    assert_equal expected.map(&:to_s).sort, types.map(&:to_s).sort
 
     # double check they are actual types
-    assert_equal expected, types
+    assert_equal expected.sort_by(&:to_s), types.sort_by(&:to_s)
 
     with_config_value :events_enabled, false do
       Seek::Util.clear_cached
       types = Seek::Util.searchable_types
       expected = [Assay, DataFile, Document, Institution, Investigation, Model, Person, Presentation, Programme, Project, Publication, Sample, SampleType, Sop, Strain, Study, Workflow]
-      assert_equal expected.map(&:to_s), types.map(&:to_s)
+      assert_equal expected.map(&:to_s).sort, types.map(&:to_s).sort
     end
 
     with_config_value :programmes_enabled, false do
       Seek::Util.clear_cached
       types = Seek::Util.searchable_types
       expected = [Assay, DataFile, Document, Event, Institution, Investigation, Model, Person, Presentation, Project, Publication, Sample, SampleType, Sop, Strain, Study, Workflow]
-      assert_equal expected.map(&:to_s), types.map(&:to_s)
+      assert_equal expected.map(&:to_s).sort, types.map(&:to_s).sort
     end
   end
 
@@ -54,10 +54,10 @@ class UtilTest < ActiveSupport::TestCase
     expected = [Model]
     types = Seek::Util.multi_files_asset_types
     # first as strings for more readable failed assertion message
-    assert_equal expected.map(&:to_s), types.map(&:to_s)
+    assert_equal expected.map(&:to_s).sort, types.map(&:to_s).sort
 
     # double check they are actual types
-    assert_equal expected, types
+    assert_equal expected.sort_by(&:to_s), types.sort_by(&:to_s)
     expected.each do |type|
       assert Seek::Util.is_multi_file_asset_type?(type)
     end
