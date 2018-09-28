ee0ac364840b06be356940f3e9f3e0524247b4c6
Stuart Owen <stuart.owen@manchester.ac.uk>
2018-09-28 15:14:28 +0100

    use the external host and port when setting up the openbis connection for docker-compose
    use localhost:4000, rather than the internal 'openbis' hostname. This
    is because the openbis docker now requires SERVER_HOST_PORT and needs to
    be the same for internal and external access

--- db/seeds/openseek/default_openbis_endpoint.seeds.rb
+++ db/seeds/openseek/default_openbis_endpoint.seeds.rb
@@ -2,8 +2,8 @@
 disable_authorization_checks do
   endpoint = OpenbisEndpoint.find_or_initialize_by(web_endpoint:'https://openbis/openbis/')
   endpoint.update_attributes(
-      as_endpoint: 'https://openbis/openbis/openbis',
-      dss_endpoint: 'https://openbis/openbis/datastore_server',
+      as_endpoint: 'https://localhost:4000/openbis/openbis',
+      dss_endpoint: 'https://localhost:4000/openbis/datastore_server',
       username: ENV["OPENBIS_USERNAME"],
       password: ENV["OPENBIS_PASSWORD"],
       space_perm_id: 'DEFAULT',
