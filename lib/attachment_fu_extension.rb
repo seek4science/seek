6cc40fbf120750ee3237f37f7be07cfa192aff10
Hadas Leonov <blackbirdy@gmail.com>
2018-10-04 15:54:22 +0200

    use strong parameters for help_attachments and help_images

--- lib/attachment_fu_extension.rb
+++ lib/attachment_fu_extension.rb
@@ -1,7 +1,7 @@
 Technoweenie::AttachmentFu::InstanceMethods.module_eval do
 
   def uploaded_data_with_extension=(file_data)
-    unless self.class == ForumAttachment || self.class == HelpAttachment || self.class == HelpImage #FIXME: This check is an indication that this extension is applied too broadly, I think.
+    unless self.class == HelpAttachment || self.class == HelpImage #FIXME: This check is an indication that this extension is applied too broadly, I think.
       upload_results = self.uploaded_data_without_extension=file_data
       self.original_filename = file_data.original_filename
 
