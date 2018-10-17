173cd47245cd50c0624d56279b2ba08711ec6f4f
alaninmcr <alan.r.williams@manchester.ac.uk>
2018-10-17 16:38:12 +0100

    Attempt to allow workflows in related UI

--- app/helpers/related_items_helper.rb
+++ app/helpers/related_items_helper.rb
@@ -101,9 +101,7 @@ module RelatedItemsHelper
     { 'Person' => {}, 'Project' => {}, 'Institution' => {}, 'Investigation' => {},
       'Study' => {}, 'Assay' => {}, 'DataFile' => {}, 'Document' => {},
       'Model' => {}, 'Sop' => {}, 'Publication' => {}, 'Presentation' => {}, 'Event' => {}, 'Organism' => {},
-      'Strain' => {}, 'Sample' => {} }
-    # ,
-    #   'Workflow' => {} }
+      'Strain' => {}, 'Sample' => {}, 'Workflow' => {} }
   end
 
   def related_items_method(resource, item_type)
