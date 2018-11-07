caca0b53d59a59e3f191a61cbe94c6059c0d8443
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-11-07 15:38:16 +0000

    Added `fallback_location` to `:back` redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -160,11 +160,7 @@ class ApplicationController < ActionController::Base
   def restrict_guest_user
     if current_user && current_user.guest?
       flash[:error] = 'You cannot perform this action as a Guest User. Please sign in or register for an account first.'
-      if !request.env['HTTP_REFERER'].nil?
-        redirect_to :back
-      else
-        redirect_to main_app.root_path
-      end
+      redirect_to :back, fallback_location: main_app.root_path
     end
   end
 
