576315a1ff8cf7d0a0d7bd9724c864bb6c243674
Stuart Owen <stuart.owen@manchester.ac.uk>
2018-11-13 13:49:15 +0000

    email exception otification if json-ld goes wrong

--- app/helpers/rdf_helper.rb
+++ app/helpers/rdf_helper.rb
@@ -10,7 +10,12 @@ module RdfHelper
         content_tag :script,type:'application/ld+json' do
           json_ld(resource)
         end
-      rescue
+      rescue Exception => exception
+        if Seek::Config.exception_notification_enabled
+          data[:message] = "Error embedding JSON-LD into page HEAD"
+          data[:item] = resource.inspect
+          ExceptionNotifier.notify_exception(exception, data: data)
+        end
         ''
       end
     end
