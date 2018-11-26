b734dbbb3ee048e5f490a5d7ab297237f53dd50b
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-11-26 16:16:05 +0000

    Should use current IDs here

--- app/models/group_membership.rb
+++ app/models/group_membership.rb
@@ -50,8 +50,8 @@ class GroupMembership < ApplicationRecord
   private
 
   def remove_admin_defined_role_projects
-    project = Project.find_by_id(WorkGroup.find(work_group_id_before_last_save).project_id)
-    person = Person.find_by_id(person_id_before_last_save)
+    project = Project.find_by_id(WorkGroup.find(work_group_id).project_id)
+    person = Person.find_by_id(person_id)
     if project && person
       Seek::Roles::ProjectRelatedRoles.role_names.each do |role_name|
         person.send("is_#{role_name}=", [false, project])
@@ -61,7 +61,7 @@ class GroupMembership < ApplicationRecord
   end
 
   def destroy_empty_work_group
-    wg = WorkGroup.find_by_id(work_group_id_before_last_save)
+    wg = WorkGroup.find_by_id(work_group_id)
 
     wg.destroy if wg && wg.people.empty?
   end
