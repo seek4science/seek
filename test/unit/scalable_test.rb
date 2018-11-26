67597c209d489820d50f32f3f75bff7adf848b71
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-11-26 13:56:29 +0000

    Don't use same image names as in fixtures!

--- test/unit/scalable_test.rb
+++ test/unit/scalable_test.rb
@@ -4,9 +4,9 @@ class ScalableTest < ActiveSupport::TestCase
   def setup
     User.current_user = Factory(:user)
     @model = Factory :model
-    @small_scale = Factory :scale, title: 'small', pos: 1, image_name: 'vl-scales/cell.png'
-    @medium_scale = Factory :scale, title: 'medium', pos: 2, image_name: 'vl-scales/liver.png'
-    @large_scale = Factory :scale, title: 'large', pos: 3, image_name: 'vl-scales/organism.png'
+    @small_scale = Factory :scale, title: 'small', pos: 1, image_name: 'airprom/sub-cellular.jpg'
+    @medium_scale = Factory :scale, title: 'medium', pos: 2, image_name: 'airprom/lung.jpg'
+    @large_scale = Factory :scale, title: 'large', pos: 3, image_name: 'airprom/organism.jpg'
   end
 
   test 'models have scales' do
