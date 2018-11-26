69848af7b5ad8bd56c2665d8768c56c5e657e1a6
Finn Bacall <finn.bacall@manchester.ac.uk>
2018-11-26 15:53:43 +0000

    Another `ActiveRecord::Dirty` fix

--- lib/seek/search/background_reindexing.rb
+++ lib/seek/search/background_reindexing.rb
@@ -7,7 +7,7 @@ module Seek
 
       module InstanceMethods
         def queue_background_reindexing
-          unless (changed - %w(updated_at last_used_at)).empty?
+          unless (saved_changes.keys - %w(updated_at last_used_at)).empty?
             Rails.logger.info("About to reindex #{self.class.name} #{id}")
             ReindexingJob.new.add_items_to_queue self
           end
