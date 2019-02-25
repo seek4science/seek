e3e3aa44dd5ed326b17273b7630264bf937228e0
Finn Bacall <finn.bacall@manchester.ac.uk>
2019-02-25 10:21:08 +0000

    Better

--- app/assets/javascripts/models.js
+++ app/assets/javascripts/models.js
@@ -1,10 +1,10 @@
 function detectFileFormat(image_id) {
     var image = $j('#' + image_id);
     var parts = image.val().split('.');
-    var extension = parts[parts.length - 1];
-    if (extension.toLowerCase() !== 'jpg' &&
-        extension.toLowerCase() !== 'gif' &&
-        extension.toLowerCase() !== 'png') {
+    var extension = parts[parts.length - 1].toLowerCase();
+    if (extension !== 'jpg' &&
+        extension !== 'gif' &&
+        extension !== 'png') {
         image.val('');
         alert('Only jpg, gif and png formats are supported.');
     }
