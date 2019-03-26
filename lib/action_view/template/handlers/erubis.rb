2dd2956ca41ce30dea5510c7c41a2a46d4dff296
Stuart Owen <stuart.owen@manchester.ac.uk>
2019-03-26 16:06:01 +0000

    OPSK-1895 updated linkeddata and rdf gems, virtuoso currently disabled
    commented out the virtuoso gem, and also removed the erubis monkey patch
    work around.

--- lib/action_view/template/handlers/erubis.rb
+++
@@ -1,8 +0,0 @@
-# This is a stupid hack needed for haml to work.
-#   haml is used by rdf-rdfa, which is a dependency of linkeddata.
-#   linkeddata is locked to an old version because of rdf-virtuoso
-class ActionView::Template::Handlers::Erubis
-  def initialize(*args, &blk)
-
-  end
-end
