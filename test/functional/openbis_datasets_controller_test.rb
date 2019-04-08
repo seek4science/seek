0e6550f15e0fa4fd02ff07337f00b358b868263d
Stuart Owen <stuart.owen@manchester.ac.uk>
2019-04-08 13:44:41 +0100

    fix to authorization based random test failure

--- test/functional/openbis_datasets_controller_test.rb
+++ test/functional/openbis_datasets_controller_test.rb
@@ -10,31 +10,31 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
     mock_openbis_calls
     @project_administrator = Factory(:project_administrator)
     @project = @project_administrator.projects.first
-    @user = Factory(:person)
-    @user.add_to_project_and_institution(@project, @user.institutions.first)
-    assert @user.save
+    @person = Factory(:person)
+    @person.add_to_project_and_institution(@project, @person.institutions.first)
+    assert @person.save
     @endpoint = Factory(:openbis_endpoint, project: @project)
     @dataset = Seek::Openbis::Dataset.new(@endpoint, '20160210130454955-23')
   end
 
   test 'test setup works' do
-    assert @user
+    assert @person
     assert @project_administrator
     assert @project
-    assert_includes @user.projects, @project
+    assert_includes @person.projects, @project
     assert @endpoint
     assert @dataset
   end
 
   test 'index gives index view' do
-    login_as(@user)
+    login_as(@person)
     get :index, params: { openbis_endpoint_id: @endpoint.id }
 
     assert_response :success
   end
 
   test 'index renders parents details' do
-    login_as(@user)
+    login_as(@person)
     get :index, params: { openbis_endpoint_id: @endpoint.id }
 
     assert_response :success
@@ -46,7 +46,7 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
   end
 
   test 'index filters by dataset types' do
-    login_as(@user)
+    login_as(@person)
     get :index, params: { openbis_endpoint_id: @endpoint.id, entity_type: 'TZ_FAIR_TEST' }
 
     assert_response :success
@@ -59,7 +59,7 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
   end
 
   test 'edit renders edit view' do
-    login_as(@user)
+    login_as(@person)
     get :edit, params: { openbis_endpoint_id: @endpoint.id, id: @dataset.perm_id }
 
     assert_response :success
@@ -69,7 +69,7 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
   end
 
   test 'refresh updates content with fetched version and redirects' do
-    login_as(@user)
+    login_as(@person)
 
     fake = Seek::Openbis::Dataset.new(@endpoint, '20160215111736723-31')
     assert_not_equal fake.perm_id, @dataset.perm_id
@@ -100,7 +100,7 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
   ## Register ##
 
   test 'register registers new DataFile' do
-    login_as(@user)
+    login_as(@person)
 
     post :register, params: { openbis_endpoint_id: @endpoint.id, id: @dataset.perm_id }
 
@@ -121,9 +121,9 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
   end
 
   test 'register registers new DataFile under Assay if passed' do
-    login_as(@user)
+    login_as(@person)
 
-    assay = Factory :assay, contributor: @user
+    assay = Factory :assay, contributor: @person
 
     post :register, params: { openbis_endpoint_id: @endpoint.id, id: @dataset.perm_id, data_file: { assay_ids: assay.id } }
 
@@ -140,8 +140,8 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
   end
 
   test 'register does not create datafile if dataset already registered but redirects to it' do
-    login_as(@user)
-    existing = Factory :data_file
+    login_as(@person)
+    existing = Factory :data_file, contributor:@person
 
     external = OpenbisExternalAsset.build(@dataset)
     assert external.save
@@ -157,9 +157,9 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
 
   ## Update ###
   test 'update updates content and redirects' do
-    login_as(@user)
+    login_as(@person)
 
-    exdatafile = Factory :data_file
+    exdatafile = Factory :data_file, contributor:@person
     asset = OpenbisExternalAsset.build(@dataset)
     asset.synchronized_at = 2.days.ago
     asset.created_at = 2.days.ago
@@ -196,8 +196,8 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
   ## Batch register ##
 
   test 'batch register multiple DataSets' do
-    login_as(@user)
-    assay = Factory :assay, contributor: @user
+    login_as(@person)
+    assay = Factory :assay, contributor: @person
 
     sync_options = {}
     batch_ids = ['20160210130454955-23', '20160215111736723-31']
@@ -219,8 +219,8 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
 
   # there was a bug and all were named same, lets have test for it
   test 'batch register independently names them' do
-    login_as(@user)
-    assay = Factory :assay, contributor: @user
+    login_as(@person)
+    assay = Factory :assay, contributor: @person
 
     sync_options = {}
     batch_ids = ['20160210130454955-23', '20160215111736723-31']
@@ -260,7 +260,7 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
     assert_response :redirect
     assert_redirected_to :root
 
-    login_as(@user)
+    login_as(@person)
 
     get :index, params: { openbis_endpoint_id: @endpoint.id }
     assert_response :success
@@ -279,7 +279,7 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
   end
 
   test 'show dataset files' do
-    login_as(@user)
+    login_as(@person)
     get :show_dataset_files, params: { openbis_endpoint_id: @endpoint.id, id: @dataset.perm_id }
 
     assert_response :success
@@ -294,78 +294,7 @@ class OpenbisDatasetsControllerTest < ActionController::TestCase
     assert_select 'td.filename', text: 'original/autumn.jpg', count: 0
   end
 
-  # Original stuart test with access tests
-  #   test 'show dataset files' do
-  #
-  #     # without a datafile, only project member can view
-  #     person = Factory(:person)
-  #     another_person = Factory(:person)
-  #
-  #     login_as(person)
-  #
-  #     project = person.projects.first
-  #     endpoint = Factory(:openbis_endpoint, project: project)
-  #     get :show_dataset_files, id: endpoint.id, project_id: project.id, perm_id: '20160210130454955-23'
-  #     assert_response :success
-  #     assert_select 'td.filename', text: 'original/autumn.jpg', count: 1
-  #
-  #     logout
-  #     login_as(another_person)
-  #     get :show_dataset_files, id: endpoint.id, project_id: project.id, perm_id: '20160210130454955-23'
-  #     assert_response :redirect
-  #     assert_select 'td.filename', text: 'original/autumn.jpg', count: 0
-  #
-  #     logout
-  #     login_as(person)
-  #
-  #     # now with a datafile, accessible to all
-  #     df = openbis_linked_data_file(User.current_user, endpoint)
-  #     disable_authorization_checks do
-  #       df.policy = Factory(:public_policy)
-  #       df.save!
-  #     end
-  #     assert df.can_download?
-  #     get :show_dataset_files, id: endpoint.id, project_id: project.id, data_file_id: df.id
-  #     assert_response :success
-  #     assert_select 'td.filename', text: 'original/autumn.jpg', count: 1
-  #
-  #     logout
-  #     login_as(another_person)
-  #     assert df.can_download?
-  #     get :show_dataset_files, id: endpoint.id, project_id: project.id, data_file_id: df.id
-  #     assert_response :success
-  #     assert_select 'td.filename', text: 'original/autumn.jpg', count: 1
-  #
-  #     logout
-  #     assert df.can_download?
-  #     get :show_dataset_files, id: endpoint.id, project_id: project.id, data_file_id: df.id
-  #     assert_response :success
-  #     assert_select 'td.filename', text: 'original/autumn.jpg', count: 1
-  #
-  #     # not accessible if df is not downloadable
-  #     disable_authorization_checks do
-  #       df.policy = Factory(:private_policy)
-  #       df.save!
-  #     end
-  #
-  #     refute df.can_download?
-  #     get :show_dataset_files, id: endpoint.id, project_id: project.id, data_file_id: df.id
-  #     assert_response :redirect
-  #     assert_select 'td.filename', text: 'original/autumn.jpg', count: 0
-  #
-  #     logout
-  #     login_as(another_person)
-  #     get :show_dataset_files, id: endpoint.id, project_id: project.id, data_file_id: df.id
-  #     assert_response :redirect
-  #     assert_select 'td.filename', text: 'original/autumn.jpg', count: 0
-  #
-  #     logout
-  #     login_as(person)
-  #     assert df.can_download?
-  #     get :show_dataset_files, id: endpoint.id, project_id: project.id, data_file_id: df.id
-  #     assert_response :success
-  #     assert_select 'td.filename', text: 'original/autumn.jpg', count: 1
-  #   end
+
 
   # unit like tests
 end
