cebd9c70b5eb2dc66f4794c956b5cf42c6ada16a
Stuart Owen <stuart.owen@manchester.ac.uk>
2019-04-10 13:24:33 +0100

    removed deprecated sample/specimen models

--- app/models/deprecated/treatment.rb
+++
@@ -1,8 +0,0 @@
-module Deprecated
-  class Treatment < ApplicationRecord
-    self.table_name = 'deprecated_treatments'
-
-    belongs_to :deprecated_sample, class_name: 'Deprecated::Sample', foreign_key: 'deprecated_sample_id'
-    belongs_to :deprecated_specimen, class_name: 'Deprecated::Specimen', foreign_key: 'deprecated_specimen_id'
-  end
-end
