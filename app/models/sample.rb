ccc81418ece104060bd5a8c36b2eee2eda37973f
Stuart Owen <stuart.owen@manchester.ac.uk>
2019-05-14 14:51:51 +0100

    OPSK-1978 fix to gettng the attribute value
    fix to get teh value when the attribute title is capitalized. Need to
    use the hash_key

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,7 +1,5 @@
 class Sample < ActiveRecord::Base
-  # attr_accessible :contributor_id, :contributor_type, :json_metadata,
-  #                :policy_id, :sample_type_id, :sample_type, :title, :uuid, :project_ids, :policy, :contributor,
-  #                :other_creators, :data
+
 
   include Seek::Rdf::RdfGeneration
 
@@ -159,7 +157,7 @@ class Sample < ActiveRecord::Base
     Rails.cache.fetch("sample-organisms-#{cache_key}-#{Organism.order('updated_at DESC').first.try(:cache_key)}") do
       sample_type.sample_attributes.collect do |attribute|
         next unless attribute.sample_attribute_type.title == 'NCBI ID'
-        value = get_attribute(attribute.title.to_sym)
+        value = get_attribute(attribute.hash_key)
         if value
           Organism.all.select { |o| o.ncbi_id && o.ncbi_id.to_s == value }
         end
