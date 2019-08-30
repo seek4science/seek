1b1a778965416ba8ec358ee693dc4d4f81aaa6dd
Finn <finn.bacall@manchester.ac.uk>
2019-08-30 11:57:11 +0100

    Schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_122522) do
+ActiveRecord::Schema.define(version: 2019_08_29_144713) do
 
   create_table "activity_logs", id: :integer,  force: :cascade do |t|
     t.string "action"
