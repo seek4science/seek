3d059c4ea178f7f64eb10a04564a7d07b45910c5
Finn <finn.bacall@manchester.ac.uk>
2019-09-02 13:57:15 +0100

    Don't eager load associations in API

--- app/serializers/base_serializer.rb
+++ app/serializers/base_serializer.rb
@@ -10,66 +10,6 @@ class BaseSerializer < SimpleBaseSerializer
     BaseSerializer.convert_policy object.policy
   end
 
-  def associated(name)
-    unless @associated[name].blank?
-      items = @associated[name][:items]
-      items = items.sort_by(&:id) unless items.blank?
-      items
-    end
-  end
-
-  def people
-    associated('Person')
-   end
-
-  def projects
-    associated('Project')
-  end
-
-  def institutions
-    associated('Institution')
-  end
-
-  def investigations
-    associated('Investigation')
-  end
-
-  def studies
-    associated('Study')
-   end
-
-  def assays
-    associated('Assay')
-  end
-
-  def data_files
-    associated('DataFile')
-  end
-
-  def models
-    associated('Model')
-  end
-
-  def sops
-    associated('Sop')
-  end
-
-  def publications
-    associated('Publication')
-  end
-
-  def presentations
-    associated('Presentation')
-  end
-
-  def events
-    associated('Event')
-  end
-
-  def documents
-    associated('Document')
-  end
-
   def self_link
     polymorphic_path(object)
   end
@@ -88,17 +28,6 @@ class BaseSerializer < SimpleBaseSerializer
     meta[:uuid] = object.uuid if object.respond_to?('uuid')
     meta[:base_url] = base_url
     meta
-  end
-
-  def initialize(object, options = {})
-    super
-
-    # access related resources with proper authorization & ignore version subclass
-    @associated = if object.class.to_s.include?('::Version')
-                    associated_resources(object.parent)
-                  else
-                    associated_resources(object)
-                  end
   end
 
   def BaseSerializer.convert_policy policy
