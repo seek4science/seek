80802a3f31e08439f2599008db9fbaa2f0584e2b
Finn <finn.bacall@manchester.ac.uk>
2019-09-18 10:50:44 +0100

    Added "active filters" section

--- app/helpers/filtering_helper.rb
+++ app/helpers/filtering_helper.rb
@@ -1,18 +1,16 @@
 module FilteringHelper
-  def sorted_filters(key, filters)
+  def sorted_filters(filters)
     filters.sort_by do |filter|
       (filter[:active] ? -1000 : 0) - filter[:count]
     end
   end
 
   def filter_link(key, filter)
-    content_tag(:div, class: "filter-option#{' filter-active' if filter[:active]}") do
-      link_to(page_and_sort_params.merge({ filter: filter[:active] ? without_filter(key, filter[:value]) : with_filter(key, filter[:value]) }),
-              title: filter[:title],
-              class: 'filter-link') do
-        content_tag(:span, filter[:title], class: 'filter-title') +
-            content_tag(:span, filter[:count], class: 'filter-count')
-      end
+    link_to(page_and_sort_params.merge({ page: nil, filter: filter[:active] ? without_filter(key, filter[:value]) : with_filter(key, filter[:value]) }),
+            title: filter[:title],
+            class: "filter-option#{' filter-active' if filter[:active]}") do
+      content_tag(:span, filter[:title], class: 'filter-title') +
+          content_tag(:span, filter[:count], class: 'filter-count')
     end
   end
 
