f51bdbc89043370490be2897796f11580a7a08c6
Finn <finn.bacall@manchester.ac.uk>
2019-09-19 09:02:13 +0100

    Fixed mismatched filter titles

--- lib/seek/filtering/filter.rb
+++ lib/seek/filtering/filter.rb
@@ -23,14 +23,14 @@ module Seek
         collection = collection.select(*@select_fields)
         collection = collection.joins(joins) if joins
         collection = collection.includes(includes) if includes
-        groups = collection.group(field).order("#{@count_field} DESC").pluck(*@select_fields).reject { |g| g[1].zero? } # Remove 0 count results
+        groups = collection.group(field).pluck(*@select_fields).reject { |g| g[1].zero? } # Remove 0 count results
         if title_mapping
           title_mapping.call(groups.map(&:first)).each.with_index do |title, index|
             groups[index][2] = title
           end
         end
 
-        groups
+        groups.sort_by { |g| -g[1] }
       end
     end
   end
