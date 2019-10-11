e1e6762d059f3dd6edcf302153aa54ffe84de379
Finn <finn.bacall@manchester.ac.uk>
2019-10-11 14:44:57 +0100

    Making date filter more comprehensible

--- lib/seek/filtering/date_filter.rb
+++ lib/seek/filtering/date_filter.rb
@@ -9,13 +9,13 @@ module Seek
       end
 
       def apply(collection, values)
-        values = values.map do |value|
-          value = parse_value(value)
+        date_ranges = values.map do |value|
+          value = parse(value)
           date_range(value) if value
         end.compact
 
-        if values.any?
-          query(collection, values)
+        if date_ranges.any?
+          query(collection, date_ranges)
         else
           collection
         end
@@ -28,7 +28,7 @@ module Seek
         # Add each preset option, and calculate counts.
         presets.each do |value|
           date_like = value
-          string_value = string_value(date_like)
+          string_value = serialize(date_like)
           date_range = date_range(date_like)
           count = query(collection, date_range).count
           next if count.zero?
@@ -42,7 +42,7 @@ module Seek
 
         # Try and parse any custom, user-supplied active filter values so they appear in the active filters section.
         other_options.each do |value|
-          date_like = parse_value(value)
+          date_like = parse(value)
           next unless date_like
           opts << Seek::Filtering::Option.new(label(date_like), value, nil, true, preset: false, date_range: date_range(date_like))
         end
@@ -56,13 +56,9 @@ module Seek
         collection.where("#{collection.table_name}.#{field}" => date_ranges)
       end
 
-      # Produces a Date Range which can be supplied to `where(field: ...)` to query between the two dates.
-      # If the end of the range is Float::INFINITY, it will not add a maximum date to the query.
-      #
-      # The string can be:
-      # * an ISO8601 duration e.g. P1W for 1 week
-      # * a single ISO8601 date e.g. 2016-06-02
-      # * an ISO8601 date range e.g. 2016-06-02--2019-01-01
+      # Produces a Date Range from a "date-like" object.
+      # This date range can be supplied to `where(field: the_date_range)` to query between the two dates.
+      # If the end of the range is a Date::Infinity, it will not add a maximum date to the query.
       def date_range(date_like)
         case date_like
         when ActiveSupport::Duration
@@ -78,27 +74,22 @@ module Seek
         case object
         when ActiveSupport::Duration
           "in the last #{object.inspect}"
-        when Date
-          "since #{object.iso8601}"
         when Range
           "between #{object.begin} and #{object.end}"
+        when Date
+          "since #{object.iso8601}"
         else
           object.to_s
         end
       end
 
-      def string_value(object)
-        case object
-        when ActiveSupport::Duration, Date
-          object.iso8601
-        when Range
-          "#{object.begin.iso8601}/#{object.end.iso8601}"
-        else
-          object.to_s
-        end
-      end
-
-      def parse_value(value)
+      # Parses the string value from the parameters into a "date-like" object (Date, Duration, or Range of two dates).
+      #
+      # The string can be:
+      # * an ISO8601 duration e.g. "P1W" for 1 week
+      # * an ISO8601 date range e.g. "2016-06-02/2019-01-01"
+      # * a single ISO8601 date e.g. "2016-06-02"
+      def parse(value)
         begin
           if value.start_with?('P')
             ActiveSupport::Duration.parse(value)
@@ -110,6 +101,17 @@ module Seek
           end
         rescue ArgumentError
           nil
+        end
+      end
+
+      # Does the opposite of `parse`.
+      def serialize(date_like)
+        if date_like.respond_to?(:iso8601)
+          date_like.iso8601
+        elsif date_like.is_a?(Range)
+          "#{serialize(date_like.begin)}/#{serialize(date_like.end)}"
+        else
+          date_like.to_s
         end
       end
     end
