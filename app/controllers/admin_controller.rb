ef878b79dac6e544b45f190f358b48285bbb8e3b
Stuart Owen <stuart.owen@manchester.ac.uk>
2019-10-25 15:21:56 +0100

    admin options to change elixir aai secret and client id #91

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -58,6 +58,8 @@ class AdminController < ApplicationController
 
     Seek::Config.omniauth_enabled = string_to_boolean params[:omniauth_enabled]
     Seek::Config.omniauth_user_create = string_to_boolean params[:omniauth_user_create]
+    Seek::Config.elixir_aai_client_id = params[:elixir_aai_client_id]
+    Seek::Config.elixir_aai_secret = params[:elixir_aai_secret]
 
     Seek::Config.solr_enabled = string_to_boolean params[:solr_enabled]
     Seek::Config.jws_enabled = string_to_boolean params[:jws_enabled]
