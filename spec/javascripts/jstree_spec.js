b596dbde944f3563a93f624f2ca6234725f2a55e
Vahid Kiani <vahidkiani88@gmail.com>
2019-11-03 16:54:47 +0330

    added jstree test

--- spec/javascripts/jstree_spec.js
+++ spec/javascripts/jstree_spec.js
@@ -1,32 +1,41 @@
-describe('jstree function', function() {
-    beforeEach(function() {
+describe('jstree function', function () {
+    before(function (done) {
         let mock_data = [{
             "text": "Project",
-            "state": { "separate": { 'label': 'Projects' }, },
+            "state": { "separate": { 'label': 'Projects' },'opened':true },
             "children": [{ "text": "Presentations", "count": '0', "children": [{ "text": "Proposals", "count": '0', }] }, {
                 "text": "Investigation",
-                "state": { "separate": { 'label': 'Investigations', 'action': '#' } },
+                "state": { "separate": { 'label': 'Investigations', 'action': '#' },'opened':true },
                 "children": [{ "text": "Presentations ", "count": '0', "children": [{ "text": "Articles", "count": 4, }] }]
-            }, ]
+            },]
         }]
 
-        this.tree = $j('<div id="treeview"><div id="html" ></div></div>');
+        this.tree = $j('<div id="treeview"><div id="jstree" ></div></div>');
         $j(document.body).append(this.tree);
-        $j('#html').jstree({
+        $j('#jstree').jstree({
             'core': {
                 'data': mock_data
             }
         });
-        console.log(document)
+
+        var interval_id = setInterval(function () {
+            if ($j("li#j1_1").length != 0) {
+                clearInterval(interval_id)
+                done();
+            }
+        }, 1000);
+
     });
 
 
-    it('should insert the separators based on input data', function() {
+    it('should insert the separators based on input data', function () {
+        expect($j('#jstree ul')).to.have.$class('jstree-container-ul');
+        expect($j('#jstree ul li p')).to.have.$class('separator');
+    });
 
+    it('should insert the badges', function () {
+        expect($j('#jstree ul li ul li:first a span')).to.have.$class('badge');
 
-        //  expect($j('#upload_type_selection ul.nav-tabs li.active a')).to.have.$text('Local file');
-        // expect($j('#local-file')).to.have.$class('active');
-        // expect($j('#remote-url')).to.not.have.$class('active');
     });
 
 });
