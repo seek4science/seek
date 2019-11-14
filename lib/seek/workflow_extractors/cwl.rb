1fc34edc1160c45c3195259936e2fc12d4a7de0d
Finn Bacall <finn.bacall@manchester.ac.uk>
2019-11-14 14:03:22 +0000

    Don't try and show CWL diagram if viewer not available

--- lib/seek/workflow_extractors/cwl.rb
+++ lib/seek/workflow_extractors/cwl.rb
@@ -8,6 +8,7 @@ module Seek
       available_diagram_formats(png: 'image/png', svg: 'image/svg+xml', default: :svg)
 
       def diagram(format = self.class.default_digram_format)
+        return nil unless Seek::Config.cwl_viewer_url.present?
         content_type = self.class.diagram_formats[format]
         url = URI.join(Seek::Config.cwl_viewer_url, DIAGRAM_PATH % { format: format }).to_s
         RestClient.post(url, @io.read, content_type: 'text/plain', accept: content_type)
