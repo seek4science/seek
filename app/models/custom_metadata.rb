e78fbe2aacd2e23299bcba2f15570aeb39d54b95
Stuart Owen <stuart.owen@manchester.ac.uk>
2019-11-21 11:21:06 +0000

    setting, storing and getting the custom metadata, via JSON #99 #103

--- app/models/custom_metadata.rb
+++ app/models/custom_metadata.rb
@@ -1,3 +1,38 @@
 class CustomMetadata < ApplicationRecord
 
+  belongs_to :item, polymorphic: true
+  has_many :custom_metadata_attributes
+
+  before_validation :update_json_metadata
+
+  def get_attribute_value(attr)
+    attr = attr.accessor_name if attr.is_a?(CustomMetadataAttribute)
+
+    data[attr]
+  end
+
+  def set_attribute_value(attr, value)
+    attr = attr.accessor_name if attr.is_a?(CustomMetadataAttribute)
+
+    data[attr] = value
+  end
+
+  def data
+    @data ||= build_json_hash
+  end
+
+  private
+
+  def build_json_hash
+    if json_metadata
+      JSON.parse(json_metadata)
+    else
+      Hash[custom_metadata_attributes.map{|attr| [attr.title,nil]}]
+    end
+  end
+
+  def update_json_metadata
+    self.json_metadata = data.to_json
+  end
+
 end
--- app/models/custom_metadata_attribute.rb
+++ app/models/custom_metadata_attribute.rb
@@ -7,4 +7,10 @@ class CustomMetadataAttribute < ApplicationRecord
     (value.blank? && !required?) || sample_attribute_type.validate_value?(value, required: required?)
   end
 
+  def hash_key
+    title.parameterize.underscore
+  end
+
+  alias_method :accessor_name, :hash_key
+
 end
