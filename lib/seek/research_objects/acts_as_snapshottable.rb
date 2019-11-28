b5e535263c8dc86035f52a83c8f3d41cecf38428
Finn <finn.bacall@manchester.ac.uk>
2019-11-28 11:00:36 +0000

    Ensure files are cleaned up after snapshotting

--- lib/seek/research_objects/acts_as_snapshottable.rb
+++ lib/seek/research_objects/acts_as_snapshottable.rb
@@ -33,13 +33,27 @@ module Seek #:nodoc:
           filename = "#{self.class.name.underscore}-#{id}-#{snapshot.snapshot_number}.ro.zip"
           blob = snapshot.build_content_blob(content_type: Mime::Type.lookup_by_extension('ro').to_s,
                                              original_filename: filename)
-          Rails.logger.debug("Generating RO...")
-          ro_file = Seek::ResearchObjects::Generator.new(self).generate
-          Rails.logger.debug("Writing zip file to content blob (and fixing)...")
-          `zip -FF #{ro_file.path} --out #{blob.filepath}`
-          blob.save!
-          Rails.logger.debug("Done!")
-          snapshot
+          ro_file = nil
+          begin
+            Rails.logger.debug("Generating RO...")
+            ro_file = Seek::ResearchObjects::Generator.new(self).generate
+            Rails.logger.debug("Writing zip file to content blob (and fixing)...")
+            `zip -FF #{ro_file.path} --out #{blob.filepath}`
+            blob.save!
+            Rails.logger.debug("Done!")
+            snapshot
+          rescue StandardError => e
+            # Clean up
+            snapshot.destroy
+            blob.destroy
+            File.delete(blob.filepath) if File.exist?(blob.filepath)
+            raise e
+          ensure
+            if ro_file
+              ro_file.close unless ro_file.closed?
+              File.delete(ro_file.path) if File.exist?(ro_file.path)
+            end
+          end
         end
 
         def snapshot(number)
