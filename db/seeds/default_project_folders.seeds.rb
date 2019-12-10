5c160c7ed33708a05dea96662d943d4903e0280f
vahid <vdkkia2004@yahoo.com>
2019-12-10 11:02:29 +0100

    Added project default folders seed

---
+++ db/seeds/default_project_folders.seeds.rb
@@ -0,0 +1,10 @@
+
+titles = YAML.load_file(File.join(Rails.root, 'config/default_data/default_project_folders_v2.yml')).values.collect { |x| x['title'] }
+
+titles.each do |title|
+  unless DefaultProjectFolder.find_by(title: title)
+    DefaultProjectFolder.find_or_create_by(title: title)
+  end
+end
+
+
