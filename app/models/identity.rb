0a6a3ef37b0e445d02cecb58123b4bdbf302090a
Finn <finn.bacall@manchester.ac.uk>
2019-12-19 14:00:28 +0000

    Removed unused fields from `identities` and changed indexes

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -5,11 +5,7 @@ class Identity < ActiveRecord::Base
     # TODO: Decide what to do about users who have an account but authenticate later on via Elixir AAI.
     # TODO: The code below will update their account to note the Elixir auth. but leave their password intact;
     # TODO: is this what we should be doing?
-
-    auth_info = { provider: auth.provider,
-                  uid: auth.uid }
-    auth_info[:email] = auth.info.email if auth.info.email
-
-    Identity.where(provider: auth.provider, uid: auth.uid).first_or_initialize(auth_info)
+    Identity.where(provider: auth.provider, uid: auth.uid).first_or_initialize(provider: auth.provider,
+                                                                               uid: auth.uid)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_122522) do
+ActiveRecord::Schema.define(version: 2019_12_19_134954) do
 
   create_table "activity_logs", id: :integer,  force: :cascade do |t|
     t.string "action"
@@ -625,22 +625,13 @@ ActiveRecord::Schema.define(version: 2019_04_10_122522) do
   end
 
   create_table "identities", id: :integer,  force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "provider"
     t.string "uid"
-    t.string "token"
-    t.integer "expires_at"
-    t.boolean "expires"
     t.integer "user_id"
-    t.index ["email"], name: "index_identities_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_identities_on_reset_password_token", unique: true
-    t.index ["user_id"], name: "index_user_id"
+    t.index ["provider", "uid"], name: "index_identities_on_provider_and_uid"
+    t.index ["user_id"], name: "index_identities_on_user_id"
   end
 
   create_table "institutions", id: :integer,  force: :cascade do |t|
