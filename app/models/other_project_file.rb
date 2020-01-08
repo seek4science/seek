e34b6e8a8332ab1be2942746ce0c940900a1dc9b
vahid <vdkkia2004@yahoo.com>
2020-01-08 14:42:05 +0100

    corrected a typo

--- app/models/other_project_file.rb
+++ app/models/other_project_file.rb
@@ -1,4 +1,4 @@
 class OtherProjectFile < ApplicationRecord
-    has_and_belongs_to_many :project
-    belongs_to :default_project_folders
+  has_and_belongs_to_many :projects
+  belongs_to :default_project_folders
 end
