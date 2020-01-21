f2b1ed9bc0ed2e2afae680f6f5737e1bb5d3cc3b
Stuart Owen <stuart.owen@manchester.ac.uk>
2020-01-21 15:39:20 +0000

    updated tests to start using factories rather manually constructed test custom metadata objects

---
+++ test/factories/custom_metadata_types.rb
@@ -0,0 +1,24 @@
+Factory.define(:age_custom_metadata_attribute,class:CustomMetadataAttribute) do |f|
+  f.title 'age'
+  f.association :sample_attribute_type, factory: :integer_sample_attribute_type
+end
+
+Factory.define(:name_custom_metadata_attribute,class:CustomMetadataAttribute) do |f|
+  f.title 'name'
+  f.association :sample_attribute_type, factory: :string_sample_attribute_type
+end
+
+Factory.define(:datetime_custom_metadata_attribute,class:CustomMetadataAttribute) do |f|
+  f.title 'date'
+  f.association :sample_attribute_type, factory: :datetime_sample_attribute_type
+end
+
+Factory.define(:simple_investigation_custom_metadata_type,class: CustomMetadataType) do |f|
+  f.title 'simple investigation custom metadata type'
+  f.supported_type 'Investigation'
+  f.after_build do |a|
+    a.custom_metadata_attributes << Factory(:age_custom_metadata_attribute)
+    a.custom_metadata_attributes << Factory(:name_custom_metadata_attribute, required: true)
+    a.custom_metadata_attributes << Factory(:datetime_custom_metadata_attribute)
+  end
+end
