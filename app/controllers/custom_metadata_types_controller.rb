db87e81d8f8f5d1b96c1234a098bca48c81b1059
Stuart Owen <stuart.owen@manchester.ac.uk>
2020-01-24 16:04:07 +0000

    updated form to handle and display validation errors without losing metadata

--- app/controllers/custom_metadata_types_controller.rb
+++ app/controllers/custom_metadata_types_controller.rb
@@ -1,5 +1,4 @@
 class CustomMetadataTypesController < ApplicationController
-
   # generated for form, to display fields for selected metadata type
   def form_fields
     id = params[:id]
@@ -8,7 +7,12 @@ class CustomMetadataTypesController < ApplicationController
         format.html { render html: '' }
       else
         cm = CustomMetadataType.find(id)
-        format.html { render partial: 'custom_metadata/custom_metadata_fields', locals: { custom_metadata_type: cm } }
+        resource = cm.supported_type.constantize.new
+        resource.custom_metadata = CustomMetadata.new(custom_metadata_type: cm)
+        format.html do
+          render partial: 'custom_metadata/custom_metadata_fields',
+                 locals: { custom_metadata_type: cm, resource: resource }
+        end
       end
     end
   end
