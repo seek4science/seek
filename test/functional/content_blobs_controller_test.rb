e6906ce79edac5d4acd84e490bf56bfb07b5be34
Finn Bacall <finn.bacall@manchester.ac.uk>
2020-04-16 15:23:28 +0100

    Debug broken test

--- test/functional/content_blobs_controller_test.rb
+++ test/functional/content_blobs_controller_test.rb
@@ -193,6 +193,8 @@ class ContentBlobsControllerTest < ActionController::TestCase
       assert PrivateAddressCheck.resolves_to_private_address?('192.168.0.1')
       VCR.turned_off do
         get :examine_url, xhr: true, params: { data_url: 'http://192.168.0.1/config' }
+        puts @response.body
+        pp assigns(:info)
         assert_response 400
         assert_equal 490, assigns(:info)[:code]
         assert @response.body.include?('URL is inaccessible')
