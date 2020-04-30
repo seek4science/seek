64d01c7566d2b70248c6bd295c3577812fcde211
Stuart Owen <stuart.owen@manchester.ac.uk>
2020-04-30 15:48:29 +0100

    change asset_link url from string to text #222

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_095637) do
+ActiveRecord::Schema.define(version: 2020_04_30_144615) do
 
   create_table "activity_logs", id: :integer,  force: :cascade do |t|
     t.string "action"
@@ -190,7 +190,7 @@ ActiveRecord::Schema.define(version: 2020_04_30_095637) do
   create_table "asset_links",  force: :cascade do |t|
     t.integer "asset_id"
     t.string "asset_type"
-    t.string "url"
+    t.text "url"
     t.string "link_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
