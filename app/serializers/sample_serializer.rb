52fb00cabaa59b519d7c4459f302f6e310ca1cf7
Stuart Owen <stuart.owen@mancheser.ac.uk>
2020-05-07 15:09:09 +0100

    updated Sample serialzier to use the attrbute names in the attribute_map
    using :data used the internal format for the attribute names

--- app/serializers/sample_serializer.rb
+++ app/serializers/sample_serializer.rb
@@ -1,14 +1,21 @@
 class SampleSerializer < PCSSerializer
+    attribute :title
+    attribute :attribute_map
+    attribute :tags do
+      serialize_annotations(object)
+    end
+    has_many :projects
+    has_one :sample_type
+    has_many :submitter
+    has_many :projects
+    has_many :data_files
+    has_many :creators
+    has_one :policy
 
-  attribute :title
-  
-  attribute :data, key: :attribute_map
-  
-  attribute :tags do
-    serialize_annotations(object)
-  end
-
-  has_many :projects
-  has_one :sample_type
+    def attribute_map
+      Hash[object.sample_type.sample_attributes.collect do |attribute|
+        [attribute.title, object.get_attribute_value(attribute)]
+      end]
+    end
 end
  
