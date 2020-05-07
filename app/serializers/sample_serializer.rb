5224012bd74fd8a66fdd43c6fceee5173ed74baa
Xiaoming Hu <xiaoming.hu@h-its.org>
2020-05-07 15:47:40 +0200

    add submitter,creators,data_files to Sample API

--- app/serializers/sample_serializer.rb
+++ app/serializers/sample_serializer.rb
@@ -1,7 +1,5 @@
 class SampleSerializer < PCSSerializer
 
-  attribute :title
-  
   attribute :data, key: :attribute_map
   
   attribute :tags do
@@ -10,5 +8,9 @@ class SampleSerializer < PCSSerializer
 
   has_many :projects
   has_one :sample_type
+  has_many :submitter
+  has_many :creators
+  has_many :data_files
+
 end
  
