91f8315a51a38be7425518fab386d65452876864
Xiaoming Hu <xiaoming.hu@h-its.org>
2020-05-12 15:22:29 +0200

    modify sample type serializer according to Alan's doc

--- app/serializers/sample_type_serializer.rb
+++ app/serializers/sample_type_serializer.rb
@@ -1,18 +1,32 @@
 class SampleTypeSerializer < BaseSerializer
-  attributes :title, :description
+  attributes :title, :description, :template_link
   attribute :attribute_map
 
   attribute :tags do
     serialize_annotations(object)
   end
 
-  def attribute_map
-    Hash[ object.sample_attributes.collect do |attribute|
-       [ attribute.title, SampleAttributeType.find(attribute.sample_attribute_type_id).title]
-     end]
+  def template_link
+    "#{base_url}#{download_sample_type_content_blob_path(object,object.template)}"
+  end
 
+
+  def attribute_map
+     object.sample_attributes.collect do |attribute|
+      {attribute.accessor_name => get_sample_attribute_type(attribute)}
+    end
   end
 
   has_many :projects
   has_many :samples
+
+
+  def get_sample_attribute_type(attribute)
+    {
+        "required": attribute.required,
+        "title": attribute.is_title,
+        "type": SampleAttributeType.find(attribute.sample_attribute_type_id).title
+    }
+  end
 end
+
