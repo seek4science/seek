018f55aadfabf299bbe482b483fb9a7b35033e33
Xiaoming Hu <xiaoming.hu@h-its.org>
2020-05-14 12:00:13 +0200

    modify sample type serializer according to the finalized json response format

--- app/serializers/sample_type_serializer.rb
+++ app/serializers/sample_type_serializer.rb
@@ -1,32 +1,46 @@
 class SampleTypeSerializer < BaseSerializer
-  attributes :title, :description, :template_link
+  attributes :title, :description
   attribute :attribute_map
 
   attribute :tags do
     serialize_annotations(object)
   end
 
-  def template_link
-    "#{base_url}#{download_sample_type_content_blob_path(object,object.template)}"
-  end
-
-
   def attribute_map
      object.sample_attributes.collect do |attribute|
-      {attribute.accessor_name => get_sample_attribute_type(attribute)}
+       get_sample_attribute(attribute)
+         #attribute
     end
   end
 
   has_many :projects
   has_many :samples
 
+  def get_sample_attribute(attribute)
+    {
+        "id": attribute.id,
+        "title": attribute.title,
+        "sample_attibute_type": get_sample_attribute_type(attribute),
+        "required": attribute.required,
+        "pos": attribute.pos,
+        "unit_id": attribute.unit_id.nil? ? nil : Unit.find(attribute.unit_id).symbol,
+        "is_title": attribute.is_title,
+        "accessor_name": attribute.accessor_name,
+        "sample_controlled_vocab_id": attribute.sample_controlled_vocab_id,
+        "linked_sample_type_id": attribute.linked_sample_type_id
+
+    }
+  end
 
   def get_sample_attribute_type(attribute)
+    attribute_type= SampleAttributeType.find(attribute.sample_attribute_type_id)
     {
-        "required": attribute.required,
-        "title": attribute.is_title,
-        "type": SampleAttributeType.find(attribute.sample_attribute_type_id).title,
-        "unit": attribute.unit_id.nil? ? nil : Unit.find(attribute.unit_id).symbol
+        "id": attribute_type.id,
+        "title": attribute_type.title,
+        "base_type": attribute_type.base_type,
+        "regular_expression": attribute_type.regexp,
+        "placeholder":attribute_type.placeholder,
+        "resolution": attribute_type.resolution
     }
   end
 end
