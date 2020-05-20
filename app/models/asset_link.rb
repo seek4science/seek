4765d7780daada04929c77a503c18e281ba6be70
Stuart Owen <stuart.owen@manchester.ac.uk>
2020-05-20 16:02:47 +0100

    slight reordering of the form position for discussion links
    should come after the license, possibly lower down

--- app/models/asset_link.rb
+++ app/models/asset_link.rb
@@ -1,11 +1,9 @@
 class AssetLink < ApplicationRecord
+  DISCUSSION = 'discussion'.freeze
 
-  DISCUSSION = 'discussion'
-
-  scope :discussion, -> { where(link_type:AssetLink::DISCUSSION)}
+  scope :discussion, -> { where(link_type: AssetLink::DISCUSSION) }
 
   belongs_to :asset, polymorphic: true, inverse_of: :asset_links
   validates :url, url: { allow_nil: false }
   validates :asset, presence: true
-
 end
