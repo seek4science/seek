5e71202c7404a31049f0fc0ab511ed9e68945d72
Xiaoming Hu <xiaoming.hu@h-its.org>
2020-06-16 13:25:15 +0200

    #280 add text box "details" to the contact request add tests

--- lib/seek/assets_common.rb
+++ lib/seek/assets_common.rb
@@ -41,9 +41,9 @@ module Seek
 
     def request_contact
       resource = class_for_controller_name.find(params[:id])
-      mail = Mailer.request_contact(current_user, resource)
-      mail.deliver_later
       details = params[:details]
+      mail = Mailer.request_contact(current_user, resource, details)
+      mail.deliver_later
       MessageLog.log_contact_request(current_user.person, resource, details)
       @resource = resource
       respond_to do |format|
