29094117e4bb267ebfa38b7cc949d56c192befe7
jdestin <jeremy.destin@inra.fr>
2020-06-29 17:14:48 +0200

    Fix and add test on studies batch extractor, and minor fixes.

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -121,9 +121,13 @@ class StudiesController < ApplicationController
   def batch_uploader; end
 
   def preview_content
-    user_uuid = "#{User.current_user.attributes["uuid"]}"
+    user_uuid = if User.current_user
+                  User.current_user.attributes['uuid'].to_s
+                else
+                  'user_uuid'
+                end
     tempzip_path = params[:content_blobs][0][:data].tempfile.path
-    data_files, studies = Study.unzip_batch(tempzip_path)
+    data_files, studies = Study.unzip_batch(tempzip_path, user_uuid)
     study_filename = studies.first
     studies_file = ContentBlob.new
     studies_file.tmp_io_object = File.open("#{Rails.root}/tmp/#{user_uuid}_studies_upload/#{study_filename}")
@@ -140,7 +144,11 @@ class StudiesController < ApplicationController
     # e.g: Study.new(title: 'title', investigation: investigations(:metabolomics_investigation), policy: Factory(:private_policy))
     # study.policy = Policy.create(name: 'default policy', access_type: 1)
     # render plain: params[:studies].inspect
-    user_uuid = "#{User.current_user.attributes["uuid"]}"
+    user_uuid = if User.current_user
+                  User.current_user.attributes['uuid'].to_s
+                else
+                  'user_uuid'
+                end
     metadata_types = CustomMetadataType.where(title: 'MIAPPE metadata', supported_type:'Study')
     studies_length = params[:studies][:title].length
     studies_uploaded = false
@@ -156,26 +164,26 @@ class StudiesController < ApplicationController
         custom_metadata: CustomMetadata.new(
           custom_metadata_type: metadata_types.first,
           data: metadata
-      )
+        )
       }
       @study = Study.new(study_params)
       Study.check_study_is_valid(@study, metadata)
-      if @study.valid? && @study.save! &&  @study.custom_metadata.valid?
+      if @study.valid? && @study.save! && @study.custom_metadata.valid?
         studies_uploaded = true if @study.save
       end
 
 
-      data_file_names = params[:studies][:data_files][index].split(", ")
+      data_file_names = params[:studies][:data_files][index].split(', ')
       data_file_names.length.times do |data_file_index|
         study_metadata_id = '"id":"' + params[:studies][:id][index] + '"'
-        study_id = CustomMetadata.where("json_metadata LIKE ?", "%#{study_metadata_id}%").last.item_id
-        assay_class_id = AssayClass.where(title: "Experimental assay").first.id
-        data_file_description = params[:studies][:data_file_description][index].split(", ")
+        study_id = CustomMetadata.where('json_metadata LIKE ?', "%#{study_metadata_id}%").last.item_id
+        assay_class_id = AssayClass.where(title: 'Experimental assay').first.id
+        data_file_description = params[:studies][:data_file_description][index].split(', ')
         assay_params = {
-            title: 'Assay for ' + params[:studies][:id][index] + '-' + (data_file_index+1).to_s,
-            description: data_file_description[data_file_index],
-            study_id: study_id,
-            assay_class_id: assay_class_id
+          title: 'Assay for ' + params[:studies][:id][index] + '-' + (data_file_index + 1).to_s,
+          description: data_file_description[data_file_index],
+          study_id: study_id,
+          assay_class_id: assay_class_id
         }
 
         data_file_name = "#{data_file_names[data_file_index]}.csv"
@@ -187,16 +195,16 @@ class StudiesController < ApplicationController
         #TODO Check and use the right license
 
         data_file_params = {
-            title: data_file_names[data_file_index],
-            description: data_file_description[data_file_index],
-            license: "CC-BY-4.0",
-            projects: Project.where(title: "Default Project"),
-            content_blob: data_file_content_blob
+          title: data_file_names[data_file_index],
+          description: data_file_description[data_file_index],
+          license: 'CC-BY-4.0',
+          projects: Project.where(title: 'Default Project'),
+          content_blob: data_file_content_blob
         }
 
         assay_asset_params = {
-            assay: Assay.new(assay_params),
-            asset: DataFile.new(data_file_params)
+          assay: Assay.new(assay_params),
+          asset: DataFile.new(data_file_params)
         }
         @assay_asset = AssayAsset.new(assay_asset_params)
 
@@ -212,7 +220,11 @@ class StudiesController < ApplicationController
     batch_uploaded = studies_uploaded && data_file_uploaded
 
     if batch_uploaded
-      user_uuid = "#{User.current_user.attributes["uuid"]}"
+      user_uuid = if User.current_user
+                    User.current_user.attributes['uuid'].to_s
+                  else
+                    'user_uuid'
+                  end
       FileUtils.rm_r("#{Rails.root}/tmp/#{user_uuid}_studies_upload/")
       respond_to do |format|
         flash[:notice] = "The #{t('studies')} were successfully created.<br/>".html_safe
@@ -227,22 +239,22 @@ class StudiesController < ApplicationController
 
   def generate_metadata(studies_meta_data, index)
     metadata = {
-        id: studies_meta_data[:id][index],
-        study_start_date: studies_meta_data[:startDate][index],
-        study_end_date: studies_meta_data[:endDate][index] || "",
-        contact_institution: studies_meta_data[:contactInstitution][index],
-        geographic_location_country: studies_meta_data[:geographicLocationCountry][index],
-        experimental_site_name: studies_meta_data[:experimentalSiteName][index],
-        latitude: studies_meta_data[:latitude][index],
-        longitude: studies_meta_data[:longitude][index],
-        altitude: studies_meta_data[:altitude][index],
-        description_of_the_experimental_design: studies_meta_data[:descriptionOfTheExperimentalDesign][index],
-        type_of_experimental_design: studies_meta_data[:typeOfExperimentalDesign][index],
-        observation_unit_level_hierarchy: studies_meta_data[:observationUnitLevelHierarchy][index],
-        observation_unit_description: studies_meta_data[:observationUnitDescription][index],
-        description_of_growth_facility: studies_meta_data[:descriptionOfGrowthFacility][index],
-        type_of_growth_facility: studies_meta_data[:typeOfGrowthFacility][index],
-        cultural_practices: studies_meta_data[:culturalPractices][index],
+      id: studies_meta_data[:id][index],
+      study_start_date: studies_meta_data[:startDate][index],
+      study_end_date: studies_meta_data[:endDate][index] || '',
+      contact_institution: studies_meta_data[:contactInstitution][index],
+      geographic_location_country: studies_meta_data[:geographicLocationCountry][index],
+      experimental_site_name: studies_meta_data[:experimentalSiteName][index],
+      latitude: studies_meta_data[:latitude][index],
+      longitude: studies_meta_data[:longitude][index],
+      altitude: studies_meta_data[:altitude][index],
+      description_of_the_experimental_design: studies_meta_data[:descriptionOfTheExperimentalDesign][index],
+      type_of_experimental_design: studies_meta_data[:typeOfExperimentalDesign][index],
+      observation_unit_level_hierarchy: studies_meta_data[:observationUnitLevelHierarchy][index],
+      observation_unit_description: studies_meta_data[:observationUnitDescription][index],
+      description_of_growth_facility: studies_meta_data[:descriptionOfGrowthFacility][index],
+      type_of_growth_facility: studies_meta_data[:typeOfGrowthFacility][index],
+      cultural_practices: studies_meta_data[:culturalPractices][index],
     }
     metadata
   end
