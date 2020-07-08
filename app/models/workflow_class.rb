bbf5b9b8e08e7cc1efd33673cbfe9a192ade3ce5
Finn Bacall <finn.bacall@manchester.ac.uk>
2020-07-08 12:29:38 +0100

    Initial hack for workflow versioning

--- app/models/workflow_class.rb
+++ app/models/workflow_class.rb
@@ -1,5 +1,18 @@
 class WorkflowClass < ApplicationRecord
   def extractor_class
-    self.class.const_get("Seek::WorkflowExtractors::#{key}")
+    class_name = "Seek::WorkflowExtractors::#{key}"
+    if const_defined?(class_name)
+      self.class.const_get(class_name)
+    else
+      Seek::WorkflowExtractors::Base
+    end
+  end
+
+  def self.extractor_class_for(id)
+    (find_by_id(id)&.extractor_class || Seek::WorkflowExtractors::Base)
+  end
+
+  def self.extractor_for(id, content_blob)
+    extractor_class_for(id).new(content_blob)
   end
 end
