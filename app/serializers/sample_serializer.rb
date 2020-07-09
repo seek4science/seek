08ea07c94d31a90ed6435b86f54f0433bb8a40af
Vahid Kiani <vdkkia2004@yahoo.com>
2020-07-09 17:47:45 +0200

    Remove  duplicate code

--- app/serializers/sample_serializer.rb
+++ app/serializers/sample_serializer.rb
@@ -1,27 +1,17 @@
 class SampleSerializer < PCSSerializer
-    attribute :title
-    attribute :attribute_map
-    attribute :tags do
-      serialize_annotations(object)
-    end
-    has_many :projects
-    has_one :sample_type
-    has_many :submitter
-    has_many :projects
-    has_many :data_files
-    has_many :creators
-    has_one :policy
-
   attribute :title
+  attribute :attribute_map
   attribute :data, key: :attribute_map
-  
   attribute :tags do
     serialize_annotations(object)
   end
-
   has_many :projects
   has_one :sample_type
+  has_many :submitter
+  has_many :projects
   has_many :data_files
-
+  has_many :creators
+  has_one :policy
+  has_many :people
 end
  
