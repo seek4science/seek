3ea36df60704fec6b85361c2d036c48b0a615a10
Stuart Owen <stuart.owen@manchester.ac.uk>
2020-09-04 17:01:53 +0100

    readded the caching, but of just the json response #422

--- lib/seek/biomodels_search/search_biomodels_adaptor.rb
+++ lib/seek/biomodels_search/search_biomodels_adaptor.rb
@@ -5,15 +5,16 @@ module Seek
     class SearchBiomodelsAdaptor < AbstractSearchAdaptor
       NUMRESULTS = 25
       def perform_search(query)
-        results = RestClient.get("https://www.ebi.ac.uk/biomodels/search?query=#{CGI.escape(query)}&numResults=#{NUMRESULTS}", accept: 'application/json') do |resp|
-          json = JSON.parse(resp.body)
-          json['models'].collect do |result|
-            BiomodelsSearchResult.new result['id']
-          end.compact.reject do |biomodels_result|
-            biomodels_result.title.blank?
-          end
+        json = Rails.cache.fetch("biomodels_search_json_#{CGI.escape(query)}", expires_in: 1.day) do
+          RestClient.get("https://www.ebi.ac.uk/biomodels/search?query=#{CGI.escape(query)}&numResults=#{NUMRESULTS}", accept: 'application/json').body
         end
-        results
+
+        json = JSON.parse(json)
+        json['models'].collect do |result|
+          BiomodelsSearchResult.new result['id']
+        end.compact.reject do |biomodels_result|
+          biomodels_result.title.blank?
+        end
       end
 
       def fetch_item(item_id)
@@ -40,17 +41,19 @@ module Seek
       private
 
       def populate
-        RestClient.get("https://www.ebi.ac.uk/biomodels/#{model_id}", accept: 'application/json') do |resp|
-          json = JSON.parse(resp.body)
-          self.title = json['name']
-          self.publication_title = json['publication']['title']
-          self.abstract = json['description']
-          self.authors = json['publication']['authors'].collect { |author| author['name'] }
-          self.published_date = Time.at(json['firstPublished'] / 1000)
-          latest_version = json['history']['revisions'].sort { |rev| rev['version'] }.first
-          self.last_modification_date = Time.at(latest_version['submitted'] / 1000)
-          self.main_filename = json['files']['main'].first['name']
+        json = Rails.cache.fetch("biomodels_item_json_#{model_id}") do
+          RestClient.get("https://www.ebi.ac.uk/biomodels/#{model_id}", accept: 'application/json').body
         end
+
+        json = JSON.parse(json)
+        self.title = json['name']
+        self.publication_title = json['publication']['title']
+        self.abstract = json['description']
+        self.authors = json['publication']['authors'].collect { |author| author['name'] }
+        self.published_date = Time.at(json['firstPublished'] / 1000)
+        latest_version = json['history']['revisions'].sort { |rev| rev['version'] }.first
+        self.last_modification_date = Time.at(latest_version['submitted'] / 1000)
+        self.main_filename = json['files']['main'].first['name']
       end
     end
   end
