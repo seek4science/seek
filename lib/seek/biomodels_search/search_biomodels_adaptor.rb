1ba5ad58f32e944ab0ba126e6f8ed5d0d06c2898
Stuart Owen <stuart.owen@manchester.ac.uk>
2020-09-04 15:35:08 +0100

    fix to the download link, and link back to source #422

--- lib/seek/biomodels_search/search_biomodels_adaptor.rb
+++ lib/seek/biomodels_search/search_biomodels_adaptor.rb
@@ -26,7 +26,7 @@ module Seek
       end
     end
 
-    class BiomodelsSearchResult < Struct.new(:authors, :abstract, :title, :published_date, :publication_id, :publication_title, :model_id, :last_modification_date)
+    class BiomodelsSearchResult < Struct.new(:authors, :abstract, :title, :published_date, :publication_id, :publication_title, :model_id, :last_modification_date, :main_filename)
       include Seek::ExternalSearchResult
 
       alias_attribute :id, :model_id
@@ -35,6 +35,10 @@ module Seek
         self.authors = []
         self.model_id = model_id
         populate
+      end
+
+      def download_link
+        "https://www.ebi.ac.uk/biomodels/model/download/#{self.model_id}?filename=#{self.main_filename}"
       end
 
       private
@@ -49,6 +53,8 @@ module Seek
           self.published_date = Time.at(json['firstPublished']/1000)
           latest_version = json['history']['revisions'].sort{|rev| rev['version']}.first
           self.last_modification_date = Time.at(latest_version['submitted']/1000)
+          self.main_filename = json['files']['main'].first['name']
+
         end
       end
 
