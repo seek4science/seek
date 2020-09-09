44528497eda571bd53b804092e39fb1153d98d11
Finn Bacall <finn.bacall@manchester.ac.uk>
2020-09-09 12:42:48 +0100

    Content type needs to be `application/json`

--- app/controllers/ga4gh/trs/v2/trs_base_controller.rb
+++ app/controllers/ga4gh/trs/v2/trs_base_controller.rb
@@ -3,6 +3,12 @@ module Ga4gh
     module V2
       class TrsBaseController < ::ApplicationController
         respond_to :json, :plain
+
+        after_action :set_content_type
+
+        def set_content_type
+          self.content_type = "application/json"
+        end
       end
     end
   end
