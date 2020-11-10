6cf6427c151b672a9e460f630a6658beaa22d247
Vahid Kiani <vdkkia2004@yahoo.com>
2020-11-10 19:14:41 +0100

    Add new fileds to SampleControlledVocabTerm

--- app/models/sample_controlled_vocab_term.rb
+++ app/models/sample_controlled_vocab_term.rb
@@ -2,10 +2,6 @@ class SampleControlledVocabTerm < ApplicationRecord
   # attr_accessible :label, :sample_controlled_vocab_id, :sample_controlled_vocab, :_destroy
 
   belongs_to :sample_controlled_vocab, inverse_of: :sample_controlled_vocab_terms
-  has_many :ontology_labels, dependent: :destroy
-
-  accepts_nested_attributes_for :ontology_labels, allow_destroy: true
-
 
   validates :label, presence: true
 end
