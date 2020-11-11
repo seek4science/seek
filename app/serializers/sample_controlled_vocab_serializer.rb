2c2a4777f410c472545b56e9eddf5c3354ed9fc8
Vahid Kiani <vdkkia2004@yahoo.com>
2020-11-11 13:55:32 +0100

    Update serializer to show repository_standard_attributes

--- app/serializers/sample_controlled_vocab_serializer.rb
+++ app/serializers/sample_controlled_vocab_serializer.rb
@@ -1,9 +1,23 @@
 class SampleControlledVocabSerializer < BaseSerializer
   attributes :title, :description, :source_ontology, :ols_root_term_uri, :required, :short_name
   attributes :sample_controlled_vocab_terms_attributes
-  attributes :repository_standard
+  attributes :repository_standard_attributes
   
   has_many :sample_controlled_vocab_terms
+
+  def repository_standard_attributes
+    repo = object.repository_standard
+     if repo
+      {
+        id: repo.id,
+        title: repo.title,
+        url: repo.url,
+        group_tag: repo.group_tag,
+        repo_type: repo.repo_type,
+        description: repo.description
+      }
+    end
+  end
 
   def sample_controlled_vocab_terms_attributes
     object.sample_controlled_vocab_terms.collect do |term|
