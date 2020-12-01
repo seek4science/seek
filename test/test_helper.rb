d48fbf88235b9131e26901c1d5ffe51ce3609776
Finn Bacall <finn.bacall@manchester.ac.uk>
2020-12-01 10:25:09 +0000

    Better way of setting colour

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,7 +24,7 @@ require 'ostruct'
 require 'pry'
 require 'json_test_helper'
 
-Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(fast_fail: true)]
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(fast_fail: true, color: true)]
 
 Minitest::Test.i_suck_and_my_tests_are_order_dependent!
 
