de425dfd2ae13aca0971475ddaa987096a6a82a6
Finn Bacall <finn.bacall@manchester.ac.uk>
2020-12-01 10:59:47 +0000

    Don't show detailed "skip" messages

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -24,7 +24,9 @@ require 'ostruct'
 require 'pry'
 require 'json_test_helper'
 
-Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(fast_fail: true, color: true)]
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(fast_fail: true,
+                                                                   color: true,
+                                                                   detailed_skip: false)]
 
 Minitest::Test.i_suck_and_my_tests_are_order_dependent!
 
