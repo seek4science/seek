483656becd92ab883ea7efd76783410d9515ee07
Stuart Owen <stuart.owen@manchester.ac.uk>
2020-12-09 16:54:46 +0000

    removed unecessary call to white_list institution_id in request_all ajax method, fixed #481
    the call to white_list was causing an error, but isn't actually needed at all

--- app/controllers/institutions_controller.rb
+++ app/controllers/institutions_controller.rb
@@ -95,12 +95,10 @@ class InstitutionsController < ApplicationController
   def request_all
     # listing all institutions is public data, but still
     # we require login to protect from unwanted requests
-    institution_id = white_list(params[:id])
     institution_list = Institution.get_all_institutions_listing
     respond_to do |format|
        format.json do
          render json: institution_list
-         #render json: { status: 200, institution_list: institution_list }
        end
     end
   end
