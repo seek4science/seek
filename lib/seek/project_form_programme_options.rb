ef727655fbba3548e330f81bdb80d7d83458614f
Stuart Owen <stuart.owen@manchester.ac.uk>
2020-12-15 17:06:26 +0000

    some minor rubocop fixes on the project form programme options

--- lib/seek/project_form_programme_options.rb
+++ lib/seek/project_form_programme_options.rb
@@ -1,13 +1,15 @@
 module Seek
-
   # the behaviour of the project creation form Programme options is driven by several configuration options
   # this class bundles the behaviour together into one logical place
   class ProjectFormProgrammeOptions
-
     module ClassMethods
       # whether the Programme box should be shown at all
       def show_programme_box?
-        Seek::Config.programmes_enabled && ( programme_administrator_logged_in? || Programme.site_managed_programme.present? || Seek::Config.programme_user_creation_enabled )
+        Seek::Config.programmes_enabled && (
+          programme_administrator_logged_in? ||
+            Programme.site_managed_programme.present? ||
+            Seek::Config.programme_user_creation_enabled
+        )
       end
 
       # whether the programmes should be selected from a drop down box
@@ -17,12 +19,16 @@ module Seek
 
       # whether there should be a checkbox to select a managed programme
       def managed_checkbox?
-        !programme_dropdown? && Programme.site_managed_programme.present? && Seek::Config.programme_user_creation_enabled
+        !programme_dropdown? &&
+          Programme.site_managed_programme.present? &&
+          Seek::Config.programme_user_creation_enabled
       end
 
       # whether managed programmes are forced and the only option
       def managed_only?
-        !programme_dropdown? && Programme.site_managed_programme.present? && !Seek::Config.programme_user_creation_enabled
+        !programme_dropdown? &&
+          Programme.site_managed_programme.present? &&
+          !Seek::Config.programme_user_creation_enabled
       end
 
       # whether programme creation is an allowed option
@@ -32,7 +38,10 @@ module Seek
 
       # whether creating a new programme is the only option
       def creation_allowed_only?
-        creation_allowed? && !(programme_administrator_logged_in? || Programme.site_managed_programme.present?)
+        creation_allowed? && !(
+          programme_administrator_logged_in? ||
+            Programme.site_managed_programme.present?
+        )
       end
 
       private
@@ -44,6 +53,5 @@ module Seek
     end
 
     extend ClassMethods
-
   end
 end
