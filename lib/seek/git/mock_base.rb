74d2af2d73bbad9636ac83ba182e8799551fd505
Finn Bacall <finn.bacall@manchester.ac.uk>
2021-01-08 14:14:43 +0000

    Don't stub git remote

--- lib/seek/git/mock_base.rb
+++ lib/seek/git/mock_base.rb
@@ -8,15 +8,6 @@ module Seek
         'abcdef12345'
       end
 
-      def add_remote(key, val)
-        @remotes ||= {}
-        @remotes[key] = val
-      end
-
-      def remotes
-        @remotes
-      end
-
       def fetch
 
       end
