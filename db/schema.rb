b643d8f0c50d1721fdd1cad27eb2dba32c53a702
Finn Bacall <finn.bacall@manchester.ac.uk>
2021-03-01 15:19:50 +0000

    Fixed wrong schema version after merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_113905) do
+ActiveRecord::Schema.define(version: 2021_02_22_180839) do
 
   create_table "activity_logs", id: :integer,  force: :cascade do |t|
     t.string "action"
