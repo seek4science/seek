ed87ad31216ede7ec22093f6239f6cd2a0a4d32d
Stuart Owen <stuart.owen@manchester.ac.uk>
2021-03-18 12:07:19 +0000

    allow a longer label, with max of 500 and automatically truncate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_125310) do
+ActiveRecord::Schema.define(version: 2021_03_18_115608) do
 
   create_table "activity_logs", id: :integer,  force: :cascade do |t|
     t.string "action"
@@ -1479,7 +1479,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_125310) do
     t.datetime "updated_at"
   end
 
-  create_table "repository_standards", id: :integer,  force: :cascade do |t|
+  create_table "repository_standards",  force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.string "group_tag"
@@ -1543,7 +1543,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_125310) do
   end
 
   create_table "sample_controlled_vocab_terms", id: :integer,  force: :cascade do |t|
-    t.string "label"
+    t.text "label"
     t.integer "sample_controlled_vocab_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -1960,7 +1960,7 @@ ActiveRecord::Schema.define(version: 2021_02_22_125310) do
     t.index ["user_id", "can_view"], name: "index_w_auth_lookup_on_user_id_and_can_view"
   end
 
-  create_table "workflow_classes", id: :integer,  force: :cascade do |t|
+  create_table "workflow_classes",  force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.string "key"
