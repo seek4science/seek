50e59b3a39c6938b1bfdd5cf4f1dbf94d98e3e8f
Finn Bacall <finn.bacall@manchester.ac.uk>
2021-03-30 16:33:23 +0100

    Experiment with copying .git folder on test run

--- test/factories/git.rb
+++ test/factories/git.rb
@@ -1,8 +1,13 @@
 Factory.define(:local_repository, class: GitRepository) do |f|
   f.resource { Factory(:workflow) }
+  f.after_create do |r|
+    FileUtils.cp_r(File.join(Rails.root, 'test', 'fixtures', 'git', 'nf-core-rnaseq', '_git'), File.join(r.local_path, '.git'))
+  end
 end
 
 Factory.define(:remote_repository, class: GitRepository) do |f|
-  f.resource { Factory(:workflow) }
   f.remote File.join(Rails.root, 'test', 'fixtures', 'files', 'git', 'nf-core', 'chipseq')
+  f.after_create do |r|
+    FileUtils.cp_r(File.join(Rails.root, 'test', 'fixtures', 'git', 'nf-core-rnaseq', '_git'), File.join(r.local_path, '.git'))
+  end
 end
--- test/unit/git_repository_test.rb
+++ test/unit/git_repository_test.rb
@@ -1,11 +1,6 @@
 require 'test_helper'
 
 class GitRepositoryTest < ActiveSupport::TestCase
-  setup do
-    WebMock.disable_net_connect!
-    FileUtils.rm_r(Seek::Config.git_filestore_path)
-    FileUtils.rm_r(Seek::Config.git_temporary_filestore_path)
-  end
 
   test 'init local repo' do
     repo = Factory(:local_repository)
@@ -15,7 +10,6 @@ class GitRepositoryTest < ActiveSupport::TestCase
   test 'fetch remote' do # Remote in this case is a local path, because we don't want to use the network
     repo = Factory(:remote_repository)
     # Could use rubyzip for this
-    `unzip -qq #{repo.remote}.zip -d #{Pathname.new(repo.remote).parent}`
     RemoteGitFetchJob.perform_now(repo)
     remote = repo.git_base.remotes.first
 
