51f95dc4a25ccae3c322ca2f62f451f4362dbfb7
Vahid Kiani <vdkkia2004@yahoo.com>
2021-05-25 16:30:19 +0200

    Add seek_sample_multi AttributeHandler

--- lib/seek/samples/attribute_type_handlers/seek_sample_multi_attribute_type_handler.rb
+++ lib/seek/samples/attribute_type_handlers/seek_sample_multi_attribute_type_handler.rb
@@ -1,7 +1,59 @@
 module Seek
   module Samples
     module AttributeTypeHandlers
-      class SeekSampleMultiAttributeTypeHandler < SeekSampleAttributeTypeHandler
+      class SeekSampleMultiAttributeTypeHandler < SeekResourceAttributeTypeHandler
+        class MissingLinkedSampleTypeException < AttributeHandlerException; end
+
+        def type
+          'Sample'
+        end
+
+        def test_value(value)
+          if value.kind_of?(Array)
+            value.each {|v| test_value_item v}
+          else
+            test_value_item value
+          end
+        end
+
+        def convert(value)
+          if value.kind_of?(Array)
+            value.uniq.map {|v| get_conversion v}
+          else
+            if value.include? ','
+              value.split(',').map(&:strip).uniq.map {|v| get_conversion v}
+            else
+              get_conversion value
+            end
+          end
+        end
+
+        private
+
+        def get_conversion(value)
+          resource = find_resource(value)
+          hash = { id: resource ? resource.id : value, type: type }.with_indifferent_access
+          hash[:title] = resource.title if resource
+          hash
+        end
+
+        def test_value_item(value)
+          if additional_options[:required]
+            sample = find_resource(value['id'])
+            fail 'Unable to find Sample in database' unless sample
+            fail 'Sample type does not match' unless sample.sample_type == linked_sample_type
+          end
+        end
+
+        def find_resource(value)
+          super(value) || linked_sample_type.samples.find_by_title(value)
+        end
+
+        def linked_sample_type
+          sample_type = additional_options[:linked_sample_type]
+          fail MissingLinkedSampleTypeException.new unless sample_type
+          sample_type
+        end
       end
     end
   end
