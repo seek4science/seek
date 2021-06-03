8bf150a7c83a0b7cb7ac128f81d197fec8e7b964
Finn Bacall <finn.bacall@manchester.ac.uk>
2021-06-03 11:08:55 +0100

    Added handler that understands Galaxy workflow URLs. Fixes #206

--- lib/seek/workflow_extractors/ro_crate.rb
+++ lib/seek/workflow_extractors/ro_crate.rb
@@ -77,6 +77,8 @@ module Seek
             handler = ContentBlob.remote_content_handler_for(source_url)
             if handler.respond_to?(:repository_url)
               source_url = handler.repository_url
+            elsif handler.respond_to?(:display_url)
+              source_url = handler.display_url
             end
             m[:source_link_url] = source_url
           end
