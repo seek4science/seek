b3dfe599e668441c662c8d0984d197fc9941c71d
Stuart Owen <stuart.owen@manchester.ac.uk>
2021-06-07 15:33:10 +0100

    control the order of the database seeds
    the miappe needs to run after the sample_attribute_types, so applied a numeric prefix

--- db/seeds/008_miappe_custom_metadata.seeds.rb
+++ db/seeds/008_miappe_custom_metadata.seeds.rb
@@ -6,6 +6,7 @@ unless CustomMetadataType.where(title:'MIAPPE metadata v1.1', supported_type:'In
   cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'license', sample_attribute_type: SampleAttributeType.where(title:'String').first)
   cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'miappe_version', required:true, sample_attribute_type: SampleAttributeType.where(title:'Integer').first)
   cmt.save!
+  puts 'Seeded Investigation extended metadata for MIAPPE v1.1'
 end
 
 unless CustomMetadataType.where(title:'MIAPPE metadata v1.1', supported_type:'Study').any?
@@ -28,10 +29,12 @@ unless CustomMetadataType.where(title:'MIAPPE metadata v1.1', supported_type:'St
   cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'cultural_practices', sample_attribute_type: SampleAttributeType.where(title:'String').first)
 
   cmt.save!
+  puts 'Seeded Study extended metadata for MIAPPE v1.1'
 end
 
 unless CustomMetadataType.where(title:'MIAPPE metadata v1.1', supported_type:'Assay').any?
   cmt = CustomMetadataType.new(title: 'MIAPPE metadata v1.1', supported_type: 'Assay')
   cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'level', sample_attribute_type: SampleAttributeType.where(title:'String').first)
   cmt.save!
+  puts 'Seeded Assay extended metadata for MIAPPE v1.1'
 end
