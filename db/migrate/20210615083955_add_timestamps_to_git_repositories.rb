d5cbc17e4a1638cb81677d9113a17f20e9f7ec77
Finn Bacall <finn.bacall@manchester.ac.uk>
2021-06-15 10:52:51 +0100

    Fixed migration to set timestamps for existing GitRepositories

--- db/migrate/20210615083955_add_timestamps_to_git_repositories.rb
+++ db/migrate/20210615083955_add_timestamps_to_git_repositories.rb
@@ -1,5 +1,8 @@
 class AddTimestampsToGitRepositories < ActiveRecord::Migration[5.2]
   def change
-    add_timestamps(:git_repositories)
+    add_timestamps(:git_repositories, null: true)
+    GitRepository.update_all(created_at: Time.now, updated_at: Time.now)
+    change_column_null(:git_repositories, :created_at, false)
+    change_column_null(:git_repositories, :updated_at, false)
   end
 end
