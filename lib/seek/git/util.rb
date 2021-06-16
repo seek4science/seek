a7c8137be68c694b6ce171224c8d464832b4f2d0
Finn Bacall <finn.bacall@manchester.ac.uk>
2021-06-16 12:00:41 +0100

    Hopefully fixed git author override not working

--- lib/seek/git/util.rb
+++ lib/seek/git/util.rb
@@ -1,10 +1,10 @@
 module Seek
   module Git
     module Util
-      mattr_accessor :git_author
+      mattr_accessor :custom_git_author
 
       def git_author
-        @@git_author || default_git_author
+        custom_git_author || default_git_author
       end
 
       def default_git_author
@@ -20,11 +20,11 @@ module Seek
       end
 
       def with_git_author(name: git_user_name, email: git_user_email, time: Time.now)
-        orig_git_author = git_author
-        self.git_author = { email: email, name: name, time: time }
+        orig_git_author = custom_git_author
+        self.custom_git_author = { email: email, name: name, time: time }
         yield
       ensure
-        self.git_author = orig_git_author
+        self.custom_git_author = orig_git_author
       end
     end
   end
