93a1827ec1eaf9448e0f1bf605fb09df93748bf9
Finn Bacall <finn.bacall@manchester.ac.uk>
2021-06-30 15:05:40 +0100

    Test fix

--- test/unit/workflow_extraction/cwl_extraction_test.rb
+++ test/unit/workflow_extraction/cwl_extraction_test.rb
@@ -14,7 +14,7 @@ class CWLExtractionTest < ActiveSupport::TestCase
     assert_equal 5, internals[:inputs].length
     assert_equal 3, internals[:outputs].length
     input = internals[:inputs].detect { |i| i[:id] == '#main/max-steps' }
-    assert_equal ['null', 'int'].sort, input[:type].sort
+    assert_equal [{ 'type' => 'int' }, { 'type' => 'null' }], input[:type].sort_by { |t| t['type'] }
   end
 
   test 'extracts metadata from CWL in workflow RO-Crate' do
@@ -26,7 +26,7 @@ class CWLExtractionTest < ActiveSupport::TestCase
     assert_equal 5, internals[:inputs].length
     assert_equal 3, internals[:outputs].length
     input = internals[:inputs].detect { |i| i[:id] == '#main/max-steps' }
-    assert_equal ['null', 'int'].sort, input[:type].sort
+    assert_equal [{ 'type' => 'int' }, { 'type' => 'null' }], input[:type].sort_by { |t| t['type'] }
     assert_equal 'RetroPath2.0 IBISBA workflow node', metadata[:title]
     assert_equal "RetroPath2.0 builds a reaction network from a set of source compounds to a set of sink compounds. When applied in a retrosynthetic fashion, the source is composed of the target compounds and the sink is composed of the available reactants (for instance in the context of metabolic engineering the sink is the set of native metabolites of a chassis strain). From amongst all the chemical reactions generated using RetroPath2.0 (in the retrosynthetic way), only a subset may effectively link a source to a subset of sink compounds. This sub-network is considered as a scope and is output in dedicated files.", metadata[:description]
     assert_equal ['workflow', 'knime', 'CWL', 'reaction'].sort, metadata[:tags].sort
