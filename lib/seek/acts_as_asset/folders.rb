a8ab31f025ca0b2eec2135b540c496874a805727
Aitor <apaolaza@hotmail.com>
2021-08-11 12:59:12 +0100

    Moved folder update logic to library

--- lib/seek/acts_as_asset/folders.rb
+++ lib/seek/acts_as_asset/folders.rb
@@ -13,6 +13,18 @@ module Seek
         def folders
           project_folder_assets.map(&:project_folder)
         end
+
+        def register_folder_tree
+          object = object_for_request
+          # assign each folder id
+          folder_params.each do |folder_id|
+            folder_id = Integer(folder_id) rescue nil
+            unless folder_id.nil?
+              dest_folder = ProjectFolder.find(folder_id)
+              dest_folder.assign_folder(object)
+            end
+          end
+        end
       end
 
       module Associations
@@ -20,6 +32,8 @@ module Seek
         included do
           has_many :project_folder_assets, as: :asset, dependent: :destroy
           after_create :add_new_to_folder
+            # TODO: Review actions that can affect folder_trees
+          after_save :register_folder_tree, only: [:create, :create_metadata, :manage_update]
         end
       end
     end
