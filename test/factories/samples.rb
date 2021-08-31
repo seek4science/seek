fc344113c4c536d7a606647abc9b7db18de35243
Stuart Owen <stuart.owen@manchester.ac.uk>
2021-08-31 15:24:14 +0100

    updates the sample json api serialization
    fixed to validate against jsonapi schema. Updated hierarchy to use ContributedResourceSerializer to get the correct policy definition and time stamps, and avoid the duplication of defining other attributes.

--- test/factories/samples.rb
+++ test/factories/samples.rb
@@ -39,6 +39,7 @@ end
 
 Factory.define(:min_sample, parent: :sample) do |f|
   f.association :sample_type, factory: :min_sample_type
+  f.association :contributor, factory: :person
   f.after_build do |sample|
     sample.set_attribute_value(:full_name, 'Fred Bloggs')
   end
@@ -47,7 +48,7 @@ end
 
 Factory.define(:max_sample, parent: :sample) do |f|
   f.association :sample_type, factory: :max_sample_type
-  f.association :policy, factory: :public_policy
+  f.association :contributor, factory: :person
   f.after_build do |sample|
     sample.set_attribute_value(:full_name, 'Fred Bloggs')
     sample.set_attribute_value(:address, "HD")
