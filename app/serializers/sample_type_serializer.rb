510bca7b0c5a3f5b2493e5bc6598469057dca48e
Stuart Owen <stuart.owen@manchester.ac.uk>
2021-09-10 13:32:10 +0100

    minor change to the sample type api
    changed attribute_map to sample_attributes, and use the SampleAttributeTypeSerializer do describe the attribute type

--- app/serializers/sample_type_serializer.rb
+++ app/serializers/sample_type_serializer.rb
@@ -1,12 +1,12 @@
 class SampleTypeSerializer < BaseSerializer
   attributes :title, :description
-  attribute :attribute_map
+  attribute :sample_attributes
 
   attribute :tags do
     serialize_annotations(object)
   end
 
-  def attribute_map
+  def sample_attributes
      object.sample_attributes.collect do |attribute|
        get_sample_attribute(attribute)
     end
@@ -24,7 +24,7 @@ class SampleTypeSerializer < BaseSerializer
         "sample_attribute_type": get_sample_attribute_type(attribute),
         "required": attribute.required,
         "pos": attribute.pos.to_s,
-        "unit": attribute.unit_id.nil? ? nil : Unit.find(attribute.unit_id).symbol,
+        "unit": attribute.unit.nil? ? nil : attribute.unit.symbol,
         "is_title": attribute.is_title,
         "sample_controlled_vocab_id": attribute.sample_controlled_vocab_id.nil? ? nil : attribute.sample_controlled_vocab_id.to_s,
         "linked_sample_type_id": attribute.linked_sample_type_id.nil? ? nil : attribute.linked_sample_type_id.to_s
@@ -32,15 +32,7 @@ class SampleTypeSerializer < BaseSerializer
   end
 
   def get_sample_attribute_type(attribute)
-    attribute_type= SampleAttributeType.find(attribute.sample_attribute_type_id)
-    {
-        "id": attribute_type.id.to_s,
-        "title": attribute_type.title,
-        "base_type": attribute_type.base_type,
-        "regular_expression": attribute_type.regexp,
-        "placeholder":attribute_type.placeholder,
-        "resolution": attribute_type.resolution
-    }
+    JSON.parse(SampleAttributeTypeSerializer.new(attribute.sample_attribute_type).to_json)
   end
 end
 
