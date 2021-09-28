b83d96df41e59c1302e211633c4b8b6ad88aab5e
Stuart Owen <stuart.owen@manchester.ac.uk>
2021-09-28 10:43:32 +0100

    use radio checkboxes for sample booleans instead #707
    seems a better bit than a dropdown

--- app/helpers/samples_helper.rb
+++ app/helpers/samples_helper.rb
@@ -5,7 +5,6 @@ module SamplesHelper
     element_name = "sample[data][#{attribute.title}]"
     value = @sample.get_attribute_value(attribute.title)
     placeholder = "e.g. #{attribute.sample_attribute_type.placeholder}" unless attribute.sample_attribute_type.placeholder.blank?
-    include_blank = attribute.required? ? false : 'No value'
 
     case base_type
     when Seek::Samples::BaseType::TEXT
@@ -19,25 +18,34 @@ module SamplesHelper
         text_field_tag element_name, value, data: { calendar: true }, class: "calendar form-control #{clz}", placeholder: placeholder
       end
     when Seek::Samples::BaseType::BOOLEAN
-      content_tag :div, style:'width: 10em; position:relative' do
-        select_tag(element_name, options_for_select(["true","false"],value&.to_s), include_blank: include_blank, class: "form-control #{clz}")
+      content_tag :div, class: 'form-check' do
+        unless attribute.required?
+          concat(text_field_tag(element_name, '', class: 'form-check-input', type: :radio, checked: value != true && value != false))
+          concat(label_tag(nil, "Unset", class: 'form-check-label', style:'padding-left:0.25em;padding-right:1em;'))
+        end
+
+        concat(text_field_tag(element_name, 'true', class: 'form-check-input', type: :radio, checked: value == true))
+        concat(label_tag(nil, "true", class: 'form-check-label', style:'padding-left:0.25em;padding-right:1em;'))
+
+        concat(text_field_tag(element_name, 'false', class: 'form-check-input', type: :radio, checked: value == false))
+        concat(label_tag(nil, "false", class: 'form-check-label', style:'padding-left:0.25em;padding-right:1em;'))
       end
     when Seek::Samples::BaseType::SEEK_STRAIN
       options = option_groups_from_collection_for_select(Organism.all, :strains,
                                                          :title, :id,
                                                          :title, value.try(:[],'id'))
-      select_tag(element_name, options, include_blank: include_blank, class: "form-control #{clz}")
+      select_tag(element_name, options, include_blank: !attribute.required?, class: "form-control #{clz}")
     when Seek::Samples::BaseType::SEEK_DATA_FILE
       options = options_from_collection_for_select(DataFile.authorized_for(:view), :id,
                                                          :title, value.try(:[],'id'))
-      select_tag(element_name, options, include_blank: include_blank, class: "form-control #{clz}")
+      select_tag(element_name, options, include_blank: !attribute.required?, class: "form-control #{clz}")
     when Seek::Samples::BaseType::CV
       controlled_vocab_form_field attribute, element_name, value
     when Seek::Samples::BaseType::SEEK_SAMPLE
       terms = attribute.linked_sample_type.samples.authorized_for('view').to_a
       options = options_from_collection_for_select(terms, :id, :title, value.try(:[], 'id'))
       select_tag element_name, options,
-                 include_blank: include_blank , class: "form-control #{clz}"
+                 include_blank: !attribute.required? , class: "form-control #{clz}"
     else
       text_field_tag element_name,value, class: "form-control #{clz}", placeholder: placeholder
     end
