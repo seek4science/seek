76952d8e77401746681703d111c9c64e15585835
Aitor <apaolaza@hotmail.com>
2021-10-04 14:18:31 +0100

    Add admin panel option for isa tree in folder view

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -133,6 +133,7 @@ class AdminController < ApplicationController
     Seek::Config.copasi_enabled = string_to_boolean(params[:copasi_enabled])
     Seek::Config.project_single_page_enabled = string_to_boolean(params[:project_single_page_enabled])
     Seek::Config.project_browser_enabled = string_to_boolean (params[:project_browser_enabled])
+    Seek::Config.project_browser_show_isa = string_to_boolean (params[:project_browser_show_isa])
 
     Seek::Config.nels_enabled = string_to_boolean(params[:nels_enabled])
     Seek::Config.nels_client_id = params[:nels_client_id]&.strip
