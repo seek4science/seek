c8027978b21e94d38016e8827184efe608819beb
Alain Becam <abecam@gmail.com>
2021-10-21 14:28:57 +0200

    Update schema.rb
    Merged with the oldest date, reverted to the latest one.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_17_094247) do
+ActiveRecord::Schema.define(version: 2021_10_15_152343) do
 
   create_table "activity_logs", id: :integer,  force: :cascade do |t|
     t.string "action"
