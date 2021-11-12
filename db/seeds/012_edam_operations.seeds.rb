3c7393e3de26b58c9cd15d9b2da1feadd1038b3d
Stuart Owen <stuart.owen@manchester.ac.uk>
2021-11-12 10:13:23 +0000

    ignore short_name for edam seeds
    it's an unused attribute that I think can can be removed

--- db/seeds/012_edam_operations.seeds.rb
+++ db/seeds/012_edam_operations.seeds.rb
@@ -6,8 +6,7 @@ unless SampleControlledVocab.find_by_key(SampleControlledVocab::SystemVocabs::KE
                                     key: SampleControlledVocab::SystemVocabs::KEYS[:edam_operations],
                                     description: data[:description],
                                     source_ontology: data[:source_ontology],
-                                    ols_root_term_uri: data[:ols_root_term_uri],
-                                    short_name: data[:short_name])
+                                    ols_root_term_uri: data[:ols_root_term_uri])
   data[:terms].each do |term|
     vocab.sample_controlled_vocab_terms << SampleControlledVocabTerm.new(label: term[:label], iri: term[:iri], parent_iri: term[:parent_iri])
   end
