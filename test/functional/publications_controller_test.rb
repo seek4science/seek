1abc40045293fab7c31732106df7c00d41cd3981
Stuart Owen <stuzart@gmail.com>
2022-01-17 10:34:03 +0000

    text should be existing author, not new author

--- test/functional/publications_controller_test.rb
+++ test/functional/publications_controller_test.rb
@@ -1045,9 +1045,9 @@ class PublicationsControllerTest < ActionController::TestCase
 
   test 'query authors for initialization' do
     FactoryGirl.create_list(:publication_with_author, 5)
-    Factory.create(:publication_with_author, publication_authors:[Factory(:publication_author, first_name:'New', last_name:'Author')])
+    Factory.create(:publication_with_author, publication_authors:[Factory(:publication_author, first_name:'Existing', last_name:'Author')])
     query_authors = {
-      '0' => { full_name: 'New Author' }, # Existing author-> should return 1
+      '0' => { full_name: 'Existing Author' }, # Existing author-> should return 1
       '1' => { full_name: 'NewAuthor ShouldBeCreated' } # New author (i.e. not found)
     }
     get :query_authors, format: :json, as: :json, params: { authors: query_authors }
@@ -1058,7 +1058,7 @@ class PublicationsControllerTest < ActionController::TestCase
     assert authors[0].key?('first_name'), 'missing author first name'
     assert authors[0].key?('last_name'), 'missing author last name'
     assert authors[0].key?('count'), 'missing author publication count'
-    assert_equal 'New', authors[0]['first_name']
+    assert_equal 'Existing', authors[0]['first_name']
     assert_equal 'Author', authors[0]['last_name']
     assert_nil authors[0]['person_id']
     assert_equal 1, authors[0]['count']
