e8583b91c384eb6fe8622f24a27bccceac07ca29
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-01-27 15:47:59 +0000

    minor refactor, more conistent variable name for requester between methods #827

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -89,7 +89,7 @@ class ProjectsController < ApplicationController
   end
 
   def respond_join_request
-    sender = @message_log.sender
+    requester = @message_log.sender
     validation_error_msg=nil;
 
     if params[:accept_request]=='1'
@@ -107,19 +107,19 @@ class ProjectsController < ApplicationController
       end
 
       unless validation_error_msg
-        sender.add_to_project_and_institution(@project,@institution)
-        sender.save!
-        Mailer.notify_user_projects_assigned(sender,[@project]).deliver_later
-        Mailer.notify_admins_project_join_accepted(current_person, sender, @project).deliver_later
-        flash[:notice]="Request accepted and #{sender.name} added to #{t('project')} and notified"
+        requester.add_to_project_and_institution(@project,@institution)
+        requester.save!
+        Mailer.notify_user_projects_assigned(requester,[@project]).deliver_later
+        Mailer.notify_admins_project_join_accepted(current_person, requester, @project).deliver_later
+        flash[:notice]="Request accepted and #{requester.name} added to #{t('project')} and notified"
         @message_log.respond('Accepted')
       end
     else
       comments = params['reject_details']
       @message_log.respond(comments)
-      Mailer.join_project_rejected(sender,@project,comments).deliver_later
-      Mailer.notify_admins_project_join_rejected(current_person, sender, @project, comments).deliver_later
-      flash[:notice]="Request rejected and #{sender.name} has been notified"
+      Mailer.join_project_rejected(requester,@project,comments).deliver_later
+      Mailer.notify_admins_project_join_rejected(current_person, requester, @project, comments).deliver_later
+      flash[:notice]="Request rejected and #{requester.name} has been notified"
     end
 
     if validation_error_msg
