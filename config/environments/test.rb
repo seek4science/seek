72fbb3a5ec8b8e5b535711632f4823b204d3a543
Finn Bacall <finn.bacall@manchester.ac.uk>
2022-02-03 16:29:01 +0000

    Make sure tests are sorted again

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
   config.assets.digest = false
+
+  # TODO: Change this to: `:random` when tests are all passing
+  config.active_support.test_order = :sorted
 end
