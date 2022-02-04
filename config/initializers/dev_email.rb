820d7cc84a2b01e41065ce08fb2b1512ffbab261
Finn Bacall <finn.bacall@manchester.ac.uk>
2022-02-04 09:53:56 +0000

    Accidentally committed

--- config/initializers/dev_email.rb
+++
@@ -1,9 +0,0 @@
-if (Rails.env.development? || Rails.env.test?) # && false
-  class DevEmail
-    def self.delivering_email(mail)
-      mail.to = 'test@localhost'
-      pp mail
-    end
-  end
-  ActionMailer::Base.register_interceptor(DevEmail)
-end
