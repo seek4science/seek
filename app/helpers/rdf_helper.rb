9e292f6668b595d5cf57e9594007ac9221d9d4f8
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-03-09 11:17:26 +0000

    no need to create a separate data hash for expection notification

--- app/helpers/rdf_helper.rb
+++ app/helpers/rdf_helper.rb
@@ -7,28 +7,29 @@ module RdfHelper
   def schema_ld_script_block
     resource = determine_resource_for_schema_ld
 
-    if resource && resource.respond_to?(:schema_org_supported?) && resource.schema_org_supported?
+    if resource.respond_to?(:schema_org_supported?) && resource&.schema_org_supported?
       begin
         content_tag :script, type: 'application/ld+json' do
           resource.to_schema_ld.html_safe
         end
-      rescue StandardError => exception
-        raise exception if Rails.env.development?
-        data={}
-        data[:message] = 'Error embedding Schema JSON-LD into page HEAD'
-        data[:item] = resource.inspect
-        Seek::Errors::ExceptionForwarder.send_notification(exception, data:data)
+      rescue StandardError => e
+        raise e if Rails.env.development?
+
+        Seek::Errors::ExceptionForwarder.send_notification(e,
+                                                           data: {
+                                                             message: 'Error embedding Schema JSON-LD into page HEAD',
+                                                             item: resource.inspect
+                                                           })
         ''
       end
     end
   end
 
   def determine_resource_for_schema_ld
-    if controller_name=='homes' && action_name=='index'
+    if controller_name == 'homes' && action_name == 'index'
       Seek::BioSchema::DataCatalogMockModel.new
     elsif action_name == 'show'
       versioned_resource_for_controller || resource_for_controller
     end
   end
-
 end
