4d1ee6e232c42c720d54044c8257e5f87ba64594
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-03-09 10:56:09 +0000

    rescue StandardError when schema_ld generation fails

--- app/helpers/rdf_helper.rb
+++ app/helpers/rdf_helper.rb
@@ -4,24 +4,6 @@ module RdfHelper
     resource_for_controller.to_rdf
   end
 
-  # FIXME: currently experimental and may not be used.
-  def json_ld_script_block
-    resource = resource_for_controller
-    if resource && resource.rdf_supported?
-      begin
-        content_tag :script, type: 'application/ld+json' do
-          resource.to_json_ld.html_safe
-        end
-      rescue Exception => exception
-        data={}
-        data[:message] = 'Error embedding JSON-LD into page HEAD'
-        data[:item] = resource.inspect
-        Seek::Errors::ExceptionForwarder.send_notification(exception, data)
-        ''
-      end
-    end
-  end
-
   def schema_ld_script_block
     resource = determine_resource_for_schema_ld
 
@@ -30,7 +12,7 @@ module RdfHelper
         content_tag :script, type: 'application/ld+json' do
           resource.to_schema_ld.html_safe
         end
-      rescue Exception => exception
+      rescue StandardError => exception
         raise exception if Rails.env.development?
         data={}
         data[:message] = 'Error embedding Schema JSON-LD into page HEAD'
