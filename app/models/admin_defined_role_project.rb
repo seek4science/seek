38f8911fb62f03d5fdbecc1cd4013556567f337b
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-03-16 16:22:40 +0000

    add validation for person / project match admin role #881
    validation must have been a side effect of project hierarchy code

--- app/models/admin_defined_role_project.rb
+++ app/models/admin_defined_role_project.rb
@@ -4,6 +4,7 @@ class AdminDefinedRoleProject < ApplicationRecord
 
   validates :project,:person, presence:true
   validates :role_mask,numericality: {greater_than:0,less_than_or_equal_to:16}
+  validate :person_must_be_in_project
 
 
   after_commit :queue_update_auth_table, :if=>:persisted?
@@ -17,6 +18,12 @@ class AdminDefinedRoleProject < ApplicationRecord
 
   def queue_update_auth_table
     AuthLookupUpdateQueue.enqueue(person)
+  end
+
+  def person_must_be_in_project
+    unless person.projects.include?(project)
+      errors.add(:project, "The person must be a member of the project")
+    end
   end
 
 end
