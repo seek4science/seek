ffddf23dcdb811820162c5d0e4aed5ba0f383d07
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-04-12 14:38:44 +0100

    removed unused belongs_to spreadsheet from worksheet  #882

--- app/models/worksheet.rb
+++ app/models/worksheet.rb
@@ -1,6 +1,5 @@
 class Worksheet < ApplicationRecord
 
-  belongs_to :spreadsheet
   belongs_to :content_blob, inverse_of: :worksheets
   has_many :cell_ranges
   validates_numericality_of :last_column, :greater_than => 0
