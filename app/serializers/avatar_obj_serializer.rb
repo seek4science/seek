8e69ab8bf5bc53f9b34623f10c7c91863a417294
alaninmcr <alan.r.williams@manchester.ac.uk>
2022-04-20 11:58:00 +0100

    Change as suggested by Finn

--- app/serializers/avatar_obj_serializer.rb
+++ app/serializers/avatar_obj_serializer.rb
@@ -1,8 +1,5 @@
 class AvatarObjSerializer < BaseSerializer
   attribute :avatar do
-    uri = nil
-    # This will need to be replaced, possibly with polymorphic_path(object,avatar) once #946 issue is merged
-    uri = "/#{object.class.name.pluralize.underscore}/#{object.id}/avatars/#{object.avatar.id}" unless object.avatar.nil?
-    uri
+    polymorphic_path([object, object.avatar]) unless object.avatar.nil?
   end
 end
