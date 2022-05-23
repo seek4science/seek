1968ef2cd4c0a8fd6e0715c0b2b8ed6c9b9f7a55
Finn Bacall <finn.bacall@manchester.ac.uk>
2022-05-23 14:22:55 +0100

    Better cache key for git blobs. Remove redundant code

--- app/models/git/blob.rb
+++ app/models/git/blob.rb
@@ -99,17 +99,7 @@ module Git
     end
 
     def cache_key
-      "#{git_repository.id}-#{oid}"
-    end
-
-    def notebook
-      Rails.cache.fetch("notebook-#{cache_key}") do
-        f = Tempfile.new('ipynb')
-        f.binmode
-        f.write(file_contents)
-        f.rewind
-        `jupyter nbconvert --to html #{f.path} --stdout`
-      end
+      "#{git_repository.cache_key}/blobs/#{oid}"
     end
 
     def content_path(opts = {})
