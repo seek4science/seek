6680cff1b5b18aafc5a999d881a2a10ba977d006
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-06-22 15:36:53 +0100

    updated defaults #862
    made the defaults more in line with the originals, whilst retaining some consistency. The first 3 are what are used for the condensed view

--- lib/seek/index_table_column_definitions.rb
+++ lib/seek/index_table_column_definitions.rb
@@ -2,18 +2,18 @@ module Seek
   class IndexTableColumnDefinitions
 
     def self.allowed_columns(resource)
-      columns = required_columns(resource) | default_columns(resource) | definitions[:general][:additional_allowed] | (definitions[resource.model_name.name.underscore]&.fetch(:additional_allowed) || [])
-      check(columns, resource)
+      columns = required_columns(resource) | default_columns(resource) | (definitions[resource.model_name.name.underscore]&.fetch(:additional_allowed) || []) | definitions[:general][:additional_allowed]
+      check(columns, resource).uniq
     end
 
     def self.required_columns(resource)
-      columns = definitions[:general][:required] | (definitions[resource.model_name.name.underscore]&.fetch(:required) || [])
-      check(columns, resource)
+      columns = (definitions[resource.model_name.name.underscore]&.fetch(:required) || []) | definitions[:general][:required]
+      check(columns, resource).uniq
     end
 
     def self.default_columns(resource)
-      columns = definitions[:general][:default] | (definitions[resource.model_name.name.underscore]&.fetch(:default) || [])
-      check(columns, resource)
+      columns = (definitions[resource.model_name.name.underscore]&.fetch(:default) || []) | definitions[:general][:default]
+      check(columns, resource).uniq
     end
 
     private
