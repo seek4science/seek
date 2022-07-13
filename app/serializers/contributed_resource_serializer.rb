03f81ca6f3bf31296e24dd28ea545ab735748c9c
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-07-13 11:05:14 +0100

    some minor refactoring of api
    removed unncessary intermediate superclass. cleaned up method to determine whether to show policy

--- app/serializers/contributed_resource_serializer.rb
+++ app/serializers/contributed_resource_serializer.rb
@@ -1,4 +1,8 @@
-class ContributedResourceSerializer < PCSSerializer
+class ContributedResourceSerializer < BaseSerializer
+
+  has_many :creators
+  has_many :submitter
+
   attributes :title
   attribute :license, if: -> { object.respond_to?(:license) && !object.is_a?(Publication) }
   attribute :description, if: -> { object.respond_to?(:description) && !object.is_a?(Publication) }
