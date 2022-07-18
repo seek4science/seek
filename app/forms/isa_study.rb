6334da7d18af21ea9df6a19d3b7c0eeb5b545a5c
Vahid Kiani <vdkkia2004@yahoo.com>
2022-07-18 16:20:11 +0200

    Rename the input attribute name with linkes attribute title #1091

--- app/forms/isa_study.rb
+++ app/forms/isa_study.rb
@@ -1,6 +1,6 @@
 class IsaStudy
   include ActiveModel::Model
-  
+
   attr_accessor :study, :source_sample_type, :sample_collection_sample_type
 
   delegate :source_sample_type, to: :sample_type, prefix: true
@@ -9,24 +9,30 @@ class IsaStudy
   validates_presence_of :study, :source_sample_type, :sample_collection_sample_type
   validate :validate_objects
 
-  def initialize (params = {})
-    @study = Study.new((params[:study]||{}))
+  def initialize(params = {})
+    @study = Study.new((params[:study] || {}))
 
-    @source_sample_type = SampleType.new((params[:source_sample_type]||{}).merge({project_ids: @study.project_ids}))
-    @sample_collection_sample_type = SampleType.new((params[:sample_collection_sample_type]||{}).merge({project_ids: @study.project_ids}))
+    @source_sample_type = SampleType.new((params[:source_sample_type] || {}).merge({ project_ids: @study.project_ids }))
+    @sample_collection_sample_type = SampleType.new((params[:sample_collection_sample_type] || {}).merge({ project_ids: @study.project_ids }))
 
-    @source_sample_type.sample_attributes.build(is_title: true, required: true) if !params[:source_sample_type] # Initial attribute
-    @sample_collection_sample_type.sample_attributes.build(is_title: true, required: true) if !params[:sample_collection_sample_type] # Initial attribute
+    unless params[:source_sample_type]
+      @source_sample_type.sample_attributes.build(is_title: true, required: true)
+    end
+    unless params[:sample_collection_sample_type]
+      @sample_collection_sample_type.sample_attributes.build(is_title: true, required: true)
+    end
   end
-
 
   def save
     if valid?
-      @sample_collection_sample_type.sample_attributes.detect {|a| a.seek_sample_multi?}.linked_sample_type = @source_sample_type
+      input_attribute = @sample_collection_sample_type.sample_attributes.detect(&:seek_sample_multi?)
+      input_attribute.linked_sample_type = @source_sample_type
+			title = @source_sample_type.sample_attributes.detect(&:is_title).title
+      input_attribute.title = "Input (#{title})"
+
       @study.save
       @source_sample_type.save
       @sample_collection_sample_type.save
-      
       @study.sample_types = [@source_sample_type, @sample_collection_sample_type]
       @study.save
     else
@@ -34,9 +40,7 @@ class IsaStudy
     end
   end
 
-  def study
-    @study
-  end
+  attr_reader :study
 
   def source
     @source_sample_type
@@ -57,30 +61,29 @@ class IsaStudy
   private
 
   def validate_objects
-    @study.errors.each {|e| errors[:base] << "[Study]: #{e}" } unless @study.valid?
-    errors[:base] << "SOP is required" unless @study.sop_id
+    @study.errors.each { |e| errors[:base] << "[Study]: #{e}" } unless @study.valid?
+    errors[:base] << 'SOP is required' unless @study.sop_id
 
     unless @source_sample_type.valid?
-      @source_sample_type.errors.full_messages.each {|e| errors[:base] << "[Source sample type]: #{e}"} 
+      @source_sample_type.errors.full_messages.each { |e| errors[:base] << "[Source sample type]: #{e}" }
     end
 
     unless @sample_collection_sample_type.valid?
       @sample_collection_sample_type.errors.full_messages.each do |e|
-         errors[:base] << "[Sample collection sample type]: #{e}" 
+        errors[:base] << "[Sample collection sample type]: #{e}"
       end
     end
 
-    unless @source_sample_type.sample_attributes.select {|a| a.isa_tag&.isa_source? }.one?
-      errors[:base] << "[Sample type]: An attribute with source ISA tag is not provided"
+    unless @source_sample_type.sample_attributes.select { |a| a.isa_tag&.isa_source? }.one?
+      errors[:base] << '[Sample type]: An attribute with source ISA tag is not provided'
     end
 
-    unless @sample_collection_sample_type.sample_attributes.select {|a| a.isa_tag&.isa_sample? }.one?
-      errors[:base] << "[Sample type]: An attribute with sample ISA tag is not provided"
+    unless @sample_collection_sample_type.sample_attributes.select { |a| a.isa_tag&.isa_sample? }.one?
+      errors[:base] << '[Sample type]: An attribute with sample ISA tag is not provided'
     end
 
-    unless @sample_collection_sample_type.sample_attributes.any? {|a| a.seek_sample_multi?}
-      errors[:base] << "[Sample Collection sample type]: SEEK Sample Multi attribute is not provided"
+    unless @sample_collection_sample_type.sample_attributes.any?(&:seek_sample_multi?)
+      errors[:base] << '[Sample Collection sample type]: SEEK Sample Multi attribute is not provided'
     end
   end
-
 end
