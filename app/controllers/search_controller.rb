d6698f0c8abc8e85037cfe0624d052d577f1f728
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-07-25 14:48:20 +0100

    refactor the search logic flow #972

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,7 +9,10 @@ class SearchController < ApplicationController
 
     if Seek::Config.solr_enabled
       begin
-        perform_search
+        determine_query
+        sources = determine_sources
+        perform_search(sources)
+        respond_with_search_results
       rescue InvalidSearchException => e
         flash.now[:error] = e.message
       rescue RSolr::Error::ConnectionRefused => e
@@ -20,7 +23,52 @@ class SearchController < ApplicationController
                                                               data: { message: 'An error with search occurred, SOLR connection refused.' })
       end
     end
+  end
 
+  def perform_search(sources)
+    downcase_query = @search_query.downcase
+
+    sources.each do |source|
+      @results[source.to_s] = source.with_search_query(downcase_query).authorized_for('view')
+    end
+
+    if search_params[:include_external_search] == '1'
+      @external_results = Seek::ExternalSearch.instance.external_search(downcase_query, @search_type&.downcase)
+    end
+
+    @results
+  end
+
+  private
+
+  def determine_query
+    @search_query = ActionController::Base.helpers.sanitize(search_params[:q] || search_params[:search_query])
+    @search = @search_query # used for logging, and logs the origin search query - see ApplicationController#log_event
+    @search_query ||= ''
+    @search_query = Seek::Search::SearchTermFilter.filter @search_query
+    raise InvalidSearchException, 'Query string is empty or blank' if @search_query.blank?
+  end
+
+  def determine_sources
+    @search_type = search_params[:search_type] || 'all'
+    type = @search_type&.downcase
+    searchable_types = Seek::Util.searchable_types
+
+    if type == 'all'
+      sources = searchable_types
+      sources -= [Strain, Sample] if request.format.json?
+    else
+      type_name = type.singularize.camelize
+      unless searchable_types.map(&:to_s).include?(type_name)
+        raise InvalidSearchException, "#{type} is not a valid search type"
+      end
+
+      sources = [type_name.constantize]
+    end
+    sources
+  end
+
+  def respond_with_search_results
     matches = @results.values.sum(&:count) + @external_results.count
     if matches.zero?
       flash.now[:notice] = "No matches found for '<b>#{@search_query}</b>'.".html_safe
@@ -42,48 +90,6 @@ class SearchController < ApplicationController
       end
     end
   end
-
-  def perform_search
-    @search_query = ActionController::Base.helpers.sanitize(search_params[:q] || search_params[:search_query])
-    @search = @search_query # used for logging, and logs the origin search query - see ApplicationController#log_event
-    @search_query ||= ''
-    @search_type = search_params[:search_type] || 'all'
-    type = @search_type&.downcase
-
-    @search_query = Seek::Search::SearchTermFilter.filter @search_query
-
-    downcase_query = @search_query.downcase
-
-    searchable_types = Seek::Util.searchable_types
-
-    raise InvalidSearchException, 'Query string is empty or blank' if downcase_query.blank?
-
-    if Seek::Config.solr_enabled
-      if type == 'all'
-        sources = searchable_types
-        sources -= [Strain, Sample] if request.format.json?
-      else
-        type_name = type.singularize.camelize
-        unless searchable_types.map(&:to_s).include?(type_name)
-          raise InvalidSearchException, "#{type} is not a valid search type"
-        end
-
-        sources = [type_name.constantize]
-      end
-
-      sources.each do |source|
-        @results[source.to_s] = source.with_search_query(downcase_query).authorized_for('view')
-      end
-
-      if search_params[:include_external_search] == '1'
-        @external_results = Seek::ExternalSearch.instance.external_search(downcase_query, type)
-      end
-
-      @results
-    end
-  end
-
-  private
 
   def search_params
     params.permit(:search_type, :q, :search_query, :include_external_search)
