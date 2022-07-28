192155f9067b7b6e8c9ce969958a5a5f175bf7f5
alaninmcr <alan.r.williams@manchester.ac.uk>
2022-07-28 10:10:00 +0100

    Seeding

---
+++ db/seeds/016_ibisba_custom_metadata.seeds.rb
@@ -0,0 +1,28 @@
+
+unless CustomMetadataType.where(title:'IBISBA project 0.1', supported_type:'Project').any?
+  cmt = CustomMetadataType.new(title: 'IBISBA project 0.1', supported_type:'Project')
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'ibisba_contact', sample_attribute_type: SampleAttributeType.where(title:'String').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'scheduled_start_date', sample_attribute_type: SampleAttributeType.where(title:'Date').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'scheduled_finish_date', sample_attribute_type: SampleAttributeType.where(title:'Date').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'actual_start_date', sample_attribute_type: SampleAttributeType.where(title:'Date').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'actual_finish_date', sample_attribute_type: SampleAttributeType.where(title:'Date').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'status', sample_attribute_type: SampleAttributeType.where(title:'String').first)
+  cmt.save!
+  puts 'Seeded Project extended metadata for IBISBA project 0.1'
+end
+
+unless CustomMetadataType.where(title:'IBISBA service 0.1', supported_type:'Project').any?
+  cmt = CustomMetadataType.new(title: 'IBISBA service 0.1', supported_type:'Project')
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'institution_name', sample_attribute_type: SampleAttributeType.where(title:'String').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'facility_name', sample_attribute_type: SampleAttributeType.where(title:'String').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'service_name', sample_attribute_type: SampleAttributeType.where(title:'String').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'service_contact', sample_attribute_type: SampleAttributeType.where(title:'String').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'scheduled_start_date', sample_attribute_type: SampleAttributeType.where(title:'Date').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'scheduled_finish_date', sample_attribute_type: SampleAttributeType.where(title:'Date').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'actual_start_date', sample_attribute_type: SampleAttributeType.where(title:'Date').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'actual_finish_date', sample_attribute_type: SampleAttributeType.where(title:'Date').first)
+  cmt.custom_metadata_attributes << CustomMetadataAttribute.new(title: 'status', sample_attribute_type: SampleAttributeType.where(title:'String').first)
+  cmt.save!
+  puts 'Seeded Project extended metadata for IBISBA service 0.1'
+end
+
