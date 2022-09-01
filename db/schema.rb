d55376c975ad8e4fb4873b8c1fed1673a4c270ac
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-09-01 14:37:36 +0100

    migration to explicity rename the git table index names to the shorter form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_07_145032) do
+ActiveRecord::Schema.define(version: 2022_09_01_133247) do
 
   create_table "activity_logs", id: :integer, force: :cascade do |t|
     t.string "action"
