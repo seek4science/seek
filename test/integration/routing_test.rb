cd9acf3488f838fa4d66dbdb6d1ac8e28ea15a17
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-09-08 15:04:34 +0100

    tests to check nested routes exist for all possible related items, along with fixes for missing routes #1165

---
+++ test/integration/routing_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+# tests related to general routes, that are not tied to a particular controller
+class ScheduleTest < ActionDispatch::IntegrationTest
+
+  test 'related items routes' do
+    # searchable_types covers all creatable types, plus some others that can be displayed and my have related items
+    Seek::Util.searchable_types.each do |type|
+      related_type_methods = type.related_type_methods
+      related_type_methods.each_key do |related_type|
+        controller = related_type.tableize
+        parent = type.name.tableize
+        parent_key = "#{parent.singularize}_id".to_sym
+
+        path = "#{parent}/2/#{controller}"
+
+        assert_routing path, controller: controller, action: 'index', parent_key => '2'
+      end
+    end
+  end
+
+end
