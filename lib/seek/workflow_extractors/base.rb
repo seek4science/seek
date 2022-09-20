6d52f90e5d93018978486607f65acf5de3256f0d
Finn Bacall <finn.bacall@manchester.ac.uk>
2022-09-20 11:52:18 +0100

    Extract workflow authors as `AssetsCreator`s where possible. #1176

--- lib/seek/workflow_extractors/base.rb
+++ lib/seek/workflow_extractors/base.rb
@@ -38,6 +38,39 @@ module Seek
       def self.file_extensions
         []
       end
+
+      private
+
+      # Extract author from a string or a Hash complying to schema.org's `Person`
+      def extract_author(obj)
+        author = {}
+        if obj.is_a?(String)
+          if obj.present?
+            given_name, family_name = obj.split(' ', 2)
+            author[:given_name] = given_name if given_name.present?
+            author[:family_name] = family_name if family_name.present?
+          end
+        elsif obj.is_a?(Hash)
+          name = obj['name'] || obj['@id'] || ''
+          given_name, family_name = name.split(' ', 2)
+          family_name = obj['familyName'] if obj['familyName'].present?
+          given_name = obj['givenName'] if obj['givenName'].present?
+          affiliation = obj['affiliation']
+          if affiliation.present?
+            if affiliation.is_a?(String)
+              obj[:affiliation] = affiliation
+            else
+              obj[:affiliation] = affiliation['name'] if affiliation['name'].present?
+            end
+          end
+          orcid = obj['identifier'] || obj['@id']
+          author[:orcid] = orcid if orcid.present? && orcid.include?('orcid.org')
+          author[:given_name] = given_name if given_name.present?
+          author[:family_name] = family_name if family_name.present?
+        end
+
+        author
+      end
     end
   end
 end
