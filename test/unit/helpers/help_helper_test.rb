2b957f23f3ae6b06e067f0947ec267922583d288
Alain Becam <abecam@gmail.com>
2022-10-07 18:51:49 +0200

    Added tests

--- test/unit/helpers/help_helper_test.rb
+++ test/unit/helpers/help_helper_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class HelpHelperTest < ActionView::TestCase
+  include ApplicationHelper
+
   test 'help link' do
     link = help_link :investigation_checksum
     tag = Nokogiri::HTML::DocumentFragment.parse(link).children.first
@@ -37,4 +39,32 @@ class HelpHelperTest < ActionView::TestCase
     assert_equal 1,tag.children.count
     assert_equal 'Help', tag.children.first.content
   end
+
+  test 'help icon' do
+    link = index_and_new_help_icon :Collection
+    tag = Nokogiri::HTML::DocumentFragment.parse(link).children.first
+    assert_equal 'https://docs.seek4science.org/help/user-guide/collections.html', tag['href']
+    assert_equal '_blank', tag['target']
+    assert_equal t("info_text.collection"), tag['data-tooltip']
+    assert_equal 'What is a Collection?', tag.children[1].content
+  end
+
+  test 'help icon with text' do
+    link = help_icon_with_link 'Collection', 'chicken soup'
+    tag = Nokogiri::HTML::DocumentFragment.parse(link).children.first
+    pp tag
+    pp tag.children[1]
+    pp tag.children[1].content
+    assert_equal 'https://docs.seek4science.org/help/user-guide/collections.html', tag['href']
+    assert_equal '_blank', tag['target']
+    assert_equal 'What is a Collection?', tag.children[1].content
+    assert_equal 'chicken soup', tag['data-tooltip']
+  end
+
+  test 'help icon no key' do
+    span_tag = index_and_new_help_icon :Mailer
+    tag = Nokogiri::HTML::DocumentFragment.parse(span_tag).children.first
+    assert_nil tag
+  end
+
 end
