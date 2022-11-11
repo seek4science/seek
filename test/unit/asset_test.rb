e9d38db5c29a638029094cc2994e12bb62951303
fherreazcue <francisco.herreazcue@gmail.com>
2022-11-11 14:06:20 +0000

    Added unit test for updated_last_by

--- test/unit/asset_test.rb
+++ test/unit/asset_test.rb
@@ -499,4 +499,24 @@ class AssetTest < ActiveSupport::TestCase
     refute ActiveRecord::Base.cache_versioning
   end
 
+  test 'last updated by' do
+    assets = %i[data_file sop model presentation document event assay investigation study]
+    assets.each do |asset_key|
+      person1 = Factory(:person)
+      person2 = Factory(:person)
+      User.with_current_user(person1.user) do
+        asset = Factory(asset_key, contributor: person1)
+        Factory :activity_log, activity_loggable: asset, action: 'create', created_at: 15.minute.ago, culprit: person1
+        assert_nil asset.updated_last_by
+        Factory :activity_log, activity_loggable: asset, action: 'update', created_at: 10.minute.ago, culprit: person1
+        assert_equal person1, asset.updated_last_by
+        Factory :activity_log, activity_loggable: asset, action: 'update', created_at: 5.minute.ago, culprit: person1.user
+        assert_equal person1, asset.updated_last_by
+        Factory :activity_log, activity_loggable: asset, action: 'update', created_at: 1.minute.ago, culprit: person2
+        assert_equal person2, asset.updated_last_by
+        person2.delete
+        assert_nil asset.updated_last_by
+      end
+    end
+  end
 end
