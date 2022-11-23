4bf58a465de5cfcf4b9c9519a4df4017c4d1ffd4
fherreazcue <francisco.herreazcue@gmail.com>
2022-11-23 10:01:26 +0000

    Updated db/schema timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_04_140342) do
+ActiveRecord::Schema.define(version: 2022_11_22_154144) do
 
   create_table "activity_logs", id: :integer, force: :cascade do |t|
     t.string "action"
