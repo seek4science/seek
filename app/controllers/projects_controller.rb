045747d9d6b5ffa5f7d623362df5ba9eedaa2638
Stuart Owen <stuart.owen@manchester.ac.uk>
2022-12-09 13:38:21 +0000

    refactor, reverting some changes and making use of administerable_by_user callback instead #1288

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,9 +14,8 @@ class ProjectsController < ApplicationController
                                         :administer_create_project_request, :respond_create_project_request,
                                         :project_join_requests, :project_creation_requests, :typeahead]
 
-  before_action :find_requested_item, only: %i[asset_report]
-  before_action :find_and_authorize_requested_item, only: %i[show admin edit update destroy admin_members
-                                               populate populate_from_spreadsheet
+  before_action :find_requested_item, only: %i[show admin edit update destroy admin_members
+                                               asset_report populate populate_from_spreadsheet
                                                admin_member_roles update_members storage_report
                                                overview administer_join_request respond_join_request]
 
@@ -26,7 +25,7 @@ class ProjectsController < ApplicationController
   before_action :auth_to_create, only: %i[new create,:administer_create_project_request, :respond_create_project_request]
   before_action :editable_by_user, only: %i[edit update]
   before_action :check_investigations_are_for_this_project, only: %i[update]
-  before_action :administerable_by_user, only: %i[admin admin_members admin_member_roles update_members storage_report administer_join_request respond_join_request populate populate_from_spreadsheet]
+  before_action :administerable_by_user, only: %i[admin admin_members admin_member_roles destroy update_members storage_report administer_join_request respond_join_request populate populate_from_spreadsheet]
 
   before_action :member_of_this_project, only: [:asset_report], unless: :admin_logged_in?
 
