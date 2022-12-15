25a82aa850018f77a2761aa51ca41765c2a98f62
Xiaoming Hu <xiaoming.hu@h-its.org>
2022-12-15 22:50:43 +0100

    update ListAttributeTypeHandler to raise more exceptions add tests for ListAttributeTypeHandler

--- lib/seek/samples/attribute_type_handlers/list_attribute_type_handler.rb
+++ lib/seek/samples/attribute_type_handlers/list_attribute_type_handler.rb
@@ -3,14 +3,16 @@ module Seek
     module AttributeTypeHandlers
       class ListAttributeTypeHandler < BaseAttributeHandler
         class MissingControlledVocabularyException < AttributeHandlerException; end
+        class NotArrayException < AttributeHandlerException; end
+        class InvalidControlledVocabularyException < AttributeHandlerException; end
 
         def test_value(array_value)
 
           if !array_value.is_a?(Array)
-            fail "'#{array_value}' is not an array" unless array_value.is_a?(Array)
+            fail NotArrayException.new unless array_value.is_a?(Array)
           else
             array_value.each do |value|
-              fail "'#{value}' is not included in the controlled vocabulary" unless controlled_vocab.includes_term?(value) || controlled_vocab.custom_input
+              fail InvalidControlledVocabularyException.new(value) unless controlled_vocab.includes_term?(value) || controlled_vocab.custom_input
             end
           end
         end
