8e6d80fc3e9047b28d206671b0fa987903c23fd6
Xiaoming Hu <xiaoming.hu@h-its.org>
2023-01-16 11:46:21 +0100

    issue-1314

--- app/serializers/base_serializer.rb
+++ app/serializers/base_serializer.rb
@@ -121,6 +121,12 @@ class BaseSerializer < SimpleBaseSerializer
     end
   end
 
+  attribute :custom_metadata_attributes, if: -> { object.respond_to?(:custom_metadata) && !object.custom_metadata.blank? } do
+    { custom_metadata_type_id: object.custom_metadata.custom_metadata_type_id.to_s,
+      attribute_map: object.custom_metadata.data.to_hash }
+  end
+
+
   def show_policy?
     return false unless object.respond_to?('can_manage?')
     return false unless object.respond_to?('policy')
