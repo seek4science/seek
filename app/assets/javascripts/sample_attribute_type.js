641dabf59a55b34680ecc7fa75ef08f9212592bc
Xiaoming Hu <xiaoming.hu@h-its.org>
2023-01-23 16:17:58 +0100

    add CVList for the sample type

---
+++ app/assets/javascripts/sample_attribute_type.js
@@ -0,0 +1,10 @@
+var SampleAttributeType = {
+    initCVList: function () {
+        $j('.cv_list_attribute').select2(
+            {
+                placeholder: 'Search ...',
+                theme: "bootstrap"
+            }
+        );
+    }
+}
--- app/assets/javascripts/sample_types.js
+++ app/assets/javascripts/sample_types.js
@@ -71,10 +71,10 @@ var SampleTypes = {
 
     attributeTypeChanged: function (e, resetSelection=true) {
         //check if it is a controlled vocab, and change the state of the controlled vocab selector if need be
-        var is_cv = $j(this).find(':selected').data('is-cv');
+        var use_cv = $j(this).find(':selected').data('use-cv');
 				var is_ontology = $j(this).find(':selected').data('is-ontology');
         var cv_element = $j(this).siblings('.controlled-vocab-block');
-        if (is_cv) {
+        if (use_cv) {
 					var cv_selection = cv_element.find('.controlled-vocab-selection')
 					cv_selection.find('option').show()
 					cv_selection.find(`option[data-is-ontology="${!is_ontology}"]`).hide()
