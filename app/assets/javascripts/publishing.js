85455adac797ed9fa0452c022d9b26711d637698
fherreazcue <francisco.herreazcue@gmail.com>
2023-02-23 13:59:33 +0000

    Clickable images in dropdown menu - improved.
    - Does not use predefined wrappers, not quite right yet.

--- app/assets/javascripts/publishing.js
+++ app/assets/javascripts/publishing.js
@@ -10,21 +10,15 @@ function checkRepeatedItems(checkbox_element) {
 
 function selectChildren(select_all_element){
     var scrollPosition = $j(window).scrollTop();
-    console.log(scrollPosition)
-
-    var children_checkboxes = $j(':checkbox', $j(select_all_element).parent().parent().parent().parent().parent())
+    var children_checkboxes = $j(':checkbox', $j(select_all_element).parent().parent().parent().parent().parent().parent())
     for(var i=0;i<children_checkboxes.length;i++){
         var checkbox_element = { className: children_checkboxes[i].className, checked: true }
         checkRepeatedItems(checkbox_element)
     }
-
-    console.log($j(window).scrollTop())
-    $j(window).scrollTop(scrollPosition);
-    console.log('No scrolling please.')
 }
 
 function deselectChildren(deselect_all_element){
-    var children_checkboxes = $j(':checkbox', $j(deselect_all_element).parent().parent().parent().parent().parent())
+    var children_checkboxes = $j(':checkbox', $j(deselect_all_element).parent().parent().parent().parent().parent().parent())
     for(var i=0;i<children_checkboxes.length;i++){
         var checkbox_element = { className: children_checkboxes[i].className, checked: false }
         checkRepeatedItems(checkbox_element)
