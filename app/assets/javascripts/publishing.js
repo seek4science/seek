0d801bb88a7d107bec9e97f27947c8534357277f
fherreazcue <francisco.herreazcue@gmail.com>
2023-02-23 13:49:38 +0000

    Clickable images for recursive selection of subitems

--- app/assets/javascripts/publishing.js
+++ app/assets/javascripts/publishing.js
@@ -7,3 +7,27 @@ function checkRepeatedItems(checkbox_element) {
         repeated_elements[i].checked = check;
     }
 }
+
+function selectChildren(select_all_element){
+    var scrollPosition = $j(window).scrollTop();
+    console.log(scrollPosition)
+
+    var children_checkboxes = $j(':checkbox', $j(select_all_element).parent().parent().parent())
+    for(var i=0;i<children_checkboxes.length;i++){
+        var checkbox_element = { className: children_checkboxes[i].className, checked: true }
+        checkRepeatedItems(checkbox_element)
+    }
+
+    console.log($j(window).scrollTop())
+    $j(window).scrollTop(scrollPosition);
+    console.log('No scrolling please.')
+}
+
+function deselectChildren(deselect_all_element){
+    var children_checkboxes = $j(':checkbox', $j(deselect_all_element).parent().parent().parent())
+    for(var i=0;i<children_checkboxes.length;i++){
+        var checkbox_element = { className: children_checkboxes[i].className, checked: false }
+        checkRepeatedItems(checkbox_element)
+    }
+
+}
