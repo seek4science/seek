c566c031a80cc3ba473ed62fe970ecd6e7295074
Stuart Owen <stuart.owen@manchester.ac.uk>
2023-02-27 14:30:50 +0000

    update to tagging query tests #1371

--- test/functional/tags_controller_test.rb
+++ test/functional/tags_controller_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class TagsControllerTest < ActionController::TestCase
-
   include AuthenticatedTestHelper
 
   fixtures :all
@@ -17,7 +16,6 @@ class TagsControllerTest < ActionController::TestCase
     assert_not_nil flash[:error]
     assert_redirected_to all_anns_path
   end
-
 
   test 'show for sample_type_tag' do
     st = Factory(:simple_sample_type, contributor: User.current_user.person, tags: 'fish, peas')
@@ -135,9 +133,10 @@ class TagsControllerTest < ActionController::TestCase
     df = Factory :data_file, contributor: p
     tag = Factory :tag, value: 'twinkle', source: p.user, annotatable: df
 
-    get :query, params: { format: 'json', query: 'twi' }
+    get :query, params: { format: 'json', q: 'twi' }
     assert_response :success
-    assert_includes JSON.parse(@response.body), 'twinkle'
+    expected = { results: [{ id: 'twinkle', text: 'twinkle' }] }.with_indifferent_access
+    assert_equal expected, JSON.parse(@response.body)
   end
 
   test 'can handle empty response from query' do
@@ -146,8 +145,8 @@ class TagsControllerTest < ActionController::TestCase
     df = Factory :data_file, contributor: p
     tag = Factory :tag, value: 'twinkle', source: p.user, annotatable: df
 
-    get :query, params: { format: 'json', query: 'zzzxxxyyyqqq' }
+    get :query, params: { format: 'json', q: 'zzzxxxyyyqqq' }
     assert_response :success
-    assert_empty JSON.parse(@response.body)
+    assert_empty JSON.parse(@response.body)['results']
   end
 end
