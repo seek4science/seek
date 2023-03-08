ce6236e993d191b6bcb3327244e970593eb4b55f
Stuart Owen <stuart.owen@manchester.ac.uk>
2023-03-08 15:59:58 +0000

    added ObjectsInput.init() which can be called when new content is dynamically added #1371
    can either pass an element, or will loop and detect unitialized

--- app/assets/javascripts/objects_input.js
+++ app/assets/javascripts/objects_input.js
@@ -1,45 +1,57 @@
-$j(document).ready(function () {
-    $j('[data-role="seek-objectsinput"]').each(function () {
+var ObjectsInput = {
 
-        var opts = {
-            placeholder: 'Search ...',
-            theme: "bootstrap",
-            width: '100%'
-        };
+    init: function(element) {
+        (element?.item || $j('[data-role="seek-objectsinput"]')).each(function () {
 
-        if ($j(this).data('placeholder')) {
-            opts.placeholder = $j(this).data('placeholder');
-        }
-
-        if ($j(this).data('tags-limit')) {
-            opts.maximumSelectionLength = $j(this).data('tags-limit');
-        }
-
-        if ($j(this).data('allow-new-items')) {
-            opts.tags = $j(this).data('allow-new-items')
-        }
-
-        if ($j(this).data('typeahead-local-values')) {
-            opts.data = $j(this).data('typeahead-local-values');
-        }
-
-        var template = $j(this).data('typeahead-template') || 'typeahead/hint';
-        opts.templateResult = HandlebarsTemplates[template];
-        opts.escapeMarkup = function (m) {
-            return m;
-        }
-
-        if ($j(this).data('typeahead-query-url')) {
-            opts.ajax={
-                url: $j(this).data('typeahead-query-url'),
-                dataType: 'json'
+            //skip if already initialised
+            if ($j(this).data('select2')) {
+                return true;
             }
-        }
 
-        $j(this).select2(
-            opts
-        );
-    });
+            var opts = {
+                placeholder: 'Search ...',
+                theme: "bootstrap",
+                width: '100%'
+            };
+
+            if ($j(this).data('placeholder')) {
+                opts.placeholder = $j(this).data('placeholder');
+            }
+
+            if ($j(this).data('tags-limit')) {
+                opts.maximumSelectionLength = $j(this).data('tags-limit');
+            }
+
+            if ($j(this).data('allow-new-items')) {
+                opts.tags = $j(this).data('allow-new-items')
+            }
+
+            if ($j(this).data('typeahead-local-values')) {
+                opts.data = $j(this).data('typeahead-local-values');
+            }
+
+            var template = $j(this).data('typeahead-template') || 'typeahead/hint';
+            opts.templateResult = HandlebarsTemplates[template];
+            opts.escapeMarkup = function (m) {
+                return m;
+            }
+
+            if ($j(this).data('typeahead-query-url')) {
+                opts.ajax={
+                    url: $j(this).data('typeahead-query-url'),
+                    dataType: 'json'
+                }
+            }
+
+            $j(this).select2(
+                opts
+            );
+        });
+    }
+}
+$j(document).ready(function () {
+
+    ObjectsInput.init();
 
     $j('[data-role="seek-suggested-tags"]').on('click', function () {
         let selectName = $j(this).data('tag-input');
