fbc5e2d9b6d6d7192464922f8eea4a91270c8cd1
fherreazcue <francisco.herreazcue@gmail.com>
2023-03-08 10:02:54 +0000

    Moved split_button_checkbox to partial.

---
+++ app/assets/javascripts/checkbox.js
@@ -0,0 +1,23 @@
+function selectChildren(select_all_element,nesting_level){
+    let children_checkboxes = $j(':checkbox', $j(select_all_element).parents(":eq("+nesting_level+")"))
+    for(let checkbox of children_checkboxes){
+        let checkbox_element = { className: checkbox.className, checked: true }
+        checkRepeatedItems(checkbox_element)
+    }
+}
+
+function deselectChildren(deselect_all_element,nesting_level){
+    let children_checkboxes = $j(':checkbox', $j(deselect_all_element).parents(":eq("+nesting_level+")"))
+    for(let checkbox of children_checkboxes){
+        let checkbox_element = { className: checkbox.className, checked: false }
+        checkRepeatedItems(checkbox_element)
+    }
+}
+
+function checkRepeatedItems(checkbox_element) {
+    let repeated_elements = document.getElementsByClassName(checkbox_element.className)
+    let check = checkbox_element.checked
+    for(let element of repeated_elements){
+        element.checked = check
+    }
+}
