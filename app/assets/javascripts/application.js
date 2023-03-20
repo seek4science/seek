ea1e9598ceb4f8c0ccad8e3166308bfa0a3fdfc1
fherreazcue <francisco.herreazcue@gmail.com>
2023-03-20 11:52:20 +0000

    Addresses pull-request comments: - Removed ".js" extension from "checkbox.js" in applications.js - Uses pluralize instead of s - _options_for_publishing takes a boolean instead of children array

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,7 +58,7 @@
 //= require calendar
 //= require inplace
 //= require strain
-//= require checkbox.js
+//= require checkbox
 //= require cytoscape.js-2.5.0/cytoscape
 //= require cytoscape_isa_graph
 //= require bives
