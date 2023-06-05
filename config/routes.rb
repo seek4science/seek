3e8fc4ada1b0684bb1a75adc7a554c67514f0fbe
Finn Bacall <fbacall@users.noreply.github.com>
2023-06-05 12:58:51 +0100

    Remove unused route

--- config/routes.rb
+++ config/routes.rb
@@ -132,7 +132,6 @@ SEEK::Application.routes.draw do
         get 'freeze' => 'git#freeze_preview', as: :git_freeze_preview
         post 'freeze' => 'git#freeze', as: :git_freeze
         patch '' => 'git#update', as: :git_update_version
-        #delete '' => 'git#destroy', as: :git_destroy_version
       end
     end
   end
