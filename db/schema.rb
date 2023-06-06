de1481c7e6c73f053cd7cdc0ea8908492f3f6b4b
Finn Bacall <finn.bacall@manchester.ac.uk>
2023-06-06 11:28:45 +0100

    Bump `resource_attributes` column length
    It was overflowing for long descriptions
    
    # Conflicts:
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_16_123819) do
+ActiveRecord::Schema.define(version: 2023_06_06_102509) do
 
   create_table "activity_logs", id: :integer, force: :cascade do |t|
     t.string "action"
@@ -815,7 +815,7 @@ ActiveRecord::Schema.define(version: 2023_05_16_123819) do
     t.text "root_path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "resource_attributes"
+    t.text "resource_attributes", size: :medium
     t.bigint "git_repository_id"
     t.integer "visibility"
     t.string "doi"
