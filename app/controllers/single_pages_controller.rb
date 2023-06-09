62e52c4d215ed1003cc10dec4762977d5cab26ad
Kevin De Pelseneer <82407142+kdp-cloud@users.noreply.github.com>
2023-06-09 17:33:16 +0200

    Issue 1405 fix samples dynamic table (#1460)
    * Changed the `query` parameter to `q`.
    
    * Added the select2 ObjectsInput to the dynamic table in single page view
    
    * Fix objects input display and query on editable "dynamic table"
    
    * Move select2 creation from createdCell function to mRender function.
    
    * Cleanup
    
    * Required modifications from PR comments
    
    * Fix select2 not working when adding new row. Various other tweaks:
    * Ensure hidden field has a different ID to select field
    * Remove redundant map call
    * Ensure ID only contains valid characters
    * Listen for select2 events on the table element instead of each individual td
    
    * Reformat document using 2 spaces instead of tabs
    
    ---------
    
    Co-authored-by: Finn Bacall <finn.bacall@manchester.ac.uk>

--- app/controllers/single_pages_controller.rb
+++ app/controllers/single_pages_controller.rb
@@ -22,7 +22,8 @@ class SinglePagesController < ApplicationController
   def index; end
 
   def project_folders
-    return if !Seek::Config.project_single_page_folders_enabled
+    return unless Seek::Config.project_single_page_folders_enabled
+
     project_folders = ProjectFolder.root_folders(@project)
     if project_folders.empty?
       project_folders = ProjectFolder.initialize_default_folders(@project)
@@ -42,7 +43,7 @@ class SinglePagesController < ApplicationController
       data = helpers.dt_aggregated(study, assay)[:rows]
     end
     data = data.map { |row| row.unshift('') } if params[:rows_pad]
-    render json: { data: data }
+    render json: { data: }
   rescue Exception => e
     render json: { status: :unprocessable_entity, error: e.message }
   end
@@ -75,5 +76,4 @@ class SinglePagesController < ApplicationController
       render json: { status: :unprocessable_entity, error: 'You must be logged in to access batch sharing permission.' }
     end
   end
-
 end
