30e1618ef0854f5421d8f07fdfabbb8c449de973
Finn Bacall <finn.bacall@manchester.ac.uk>
2023-06-16 12:07:45 +0100

    Faster class lookup
    Change class variable to instance variable (it's already on the Module itself)

--- lib/seek/util.rb
+++ lib/seek/util.rb
@@ -14,7 +14,7 @@ module Seek
     end
 
     def self.clear_cached
-      @@cache = {}
+      @cache = {}
     end
 
     def self.ensure_models_loaded
@@ -150,19 +150,29 @@ module Seek
     end
 
     def self.lookup_class(class_name, raise: true)
-      c = Seek::Util.persistent_classes.detect { |klass| klass.name == class_name }
+      c = persistent_class_lookup[class_name]
       raise NameError "#{class_name} not an appropriate class" if c.nil? && raise
       c
     end
 
     private
 
+    def self.persistent_class_lookup
+      cache('persistent_class_lookup') do
+        lookup = {}
+        persistent_classes.each do |klass|
+          lookup[klass.name] = klass
+        end
+        lookup
+      end
+    end
+
     def self.cache(name, &block)
-      @@cache ||= {}
+      @cache ||= {}
       if Rails.env.development? # Don't use caching in development mode
         block.call
       else
-        @@cache[name] ||= block.call
+        @cache[name] ||= block.call
       end
     end
 
