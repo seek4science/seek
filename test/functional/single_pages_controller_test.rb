edfab228aea3211f01246310102b4c7dc52b7499
Kevin De Pelseneer <kevin.depelseneer@psb.ugent.be>
2023-06-22 15:19:59 +0200

    Writing tests for exporting to excel
    - Made source sample table test instance-agnostic
    
    - New test for exporting samples from a sample collection table or assay sample table
    
    - format document

--- test/functional/single_pages_controller_test.rb
+++ test/functional/single_pages_controller_test.rb
@@ -6,6 +6,7 @@ class SinglePagesControllerTest < ActionController::TestCase
   fixtures :isa_tags, :templates
 
   def setup
+    @instance_name = Seek::Config::instance_name
     @member = FactoryBot.create :user
     login_as @member
   end
@@ -41,27 +42,29 @@ class SinglePagesControllerTest < ActionController::TestCase
       project = FactoryBot.create(:project)
       investigation = FactoryBot.create(:investigation, policy: FactoryBot.create(:private_policy), projects: [project])
       get :export_isa, params: { id: project.id, investigation_id: investigation.id }
-      assert_equal flash[:error], "The investigation cannot be found!"
+      assert_equal flash[:error], 'The investigation cannot be found!'
       assert_redirected_to action: :show
     end
   end
 
-  test "genertates a valid export of source samples in single page" do
+  test 'generates a valid export of sources in single page' do
     with_config_value(:project_single_page_enabled, true) do
       # Generate the excel data
-      person = User.current_user.person
+      person = @member.person
       project = FactoryBot.create(:project)
       study = FactoryBot.create(:study)
-      source_sample_type = FactoryBot.create(:isa_source_sample_type,
-                                   contributor: person,
-                                   project_ids: [project.id],
-                                   isa_template: Template.find_by_title('ISA Source'),
-                                   studies: [study])
+      id_label = "#{Seek::Config::instance_name} id"
 
-      source_samples = (1..5).map do |n|
+      source_sample_type = FactoryBot.create(:isa_source_sample_type,
+                                             contributor: person,
+                                             project_ids: [project.id],
+                                             isa_template: Template.find_by_title('ISA Source'),
+                                             studies: [study])
+
+      sources = (1..5).map do |n|
         FactoryBot.create(
           :sample,
-          title: "sample_#{n}",
+          title: "source#{n}",
           sample_type: source_sample_type,
           project_ids: [project.id],
           contributor: person,
@@ -80,14 +83,98 @@ class SinglePagesControllerTest < ActionController::TestCase
         )
       end
 
-      sample_ids = source_samples.map { |ss| { 'FAIRDOM-SEEK id' => ss.id } }
+      source_ids = sources.map { |s| { id_label => s.id } }
       sample_type_id = source_sample_type.id
       study_id = study.id
+      assay_id = nil
 
-      post_params = { source_sample_data: sample_ids.to_json,
+      post_params = { sample_data: source_ids.to_json,
                       sample_type_id: sample_type_id.to_json,
                       study_id: study_id.to_json,
-                      assay_id: nil.to_json }
+                      assay_id: assay_id.to_json }
+
+      post :export_to_excel, params: post_params, xhr: true
+
+      assert_response :ok, msg = "Couldn't reach the server"
+
+      response_body = JSON.parse(response.body)
+      assert response_body.key?('uuid'), msg = "Response body is expected to have a 'uuid' key"
+      cache_uuid = response_body['uuid']
+
+      get :download_samples_excel, params: { uuid: cache_uuid }
+      assert_response :ok, msg = 'Unable to generate the excel'
+    end
+  end
+
+  test 'generates a valid export of source samples in single page' do
+    with_config_value(:project_single_page_enabled, true) do
+      person = @member.person
+      project = FactoryBot.create(:project)
+      study = FactoryBot.create(:study)
+      assay = FactoryBot.create(:assay)
+      id_label = "#{Seek::Config::instance_name} id"
+
+      source_sample_type = FactoryBot.create(:isa_source_sample_type,
+                                             contributor: person,
+                                             project_ids: [project.id],
+                                             isa_template: Template.find_by_title('ISA Source'),
+                                             studies: [study])
+
+      sample_collection_sample_type = FactoryBot.create(:isa_sample_collection_sample_type,
+                                                        contributor: person,
+                                                        project_ids: [project.id],
+                                                        isa_template: Template.find_by_title('ISA sample collection'),
+                                                        studies: [study],
+                                                        linked_sample_type: source_sample_type)
+
+      sources = (1..5).map do |n|
+        FactoryBot.create(
+          :sample,
+          title: "source #{n}",
+          sample_type: source_sample_type,
+          project_ids: [project.id],
+          contributor: person,
+          data: {
+            'Source Name': 'Source Name',
+            'Source Characteristic 1': 'Source Characteristic 1',
+            'Source Characteristic 2':
+              source_sample_type
+                .sample_attributes
+                .find_by_title('Source Characteristic 2')
+                .sample_controlled_vocab
+                .sample_controlled_vocab_terms
+                .first
+                .label
+          }
+        )
+      end
+
+      source_samples = (1..4).map do |n|
+        FactoryBot.create(
+          :sample,
+          title: "Sample collection #{n}",
+          sample_type: sample_collection_sample_type,
+          project_ids: [project.id],
+          contributor: person,
+          data: {
+            Input: [sources[n - 1].id, sources[n].id],
+            'sample collection': 'sample collection',
+            'sample collection parameter value 1': 'sample collection parameter value 1',
+            'Sample Name': 'sample name',
+            'sample characteristic 1': 'sample characteristic 1'
+          }
+        )
+      end
+
+      source_sample_ids = source_samples.map { |ss| { id_label => ss.id } }
+      sample_type_id = sample_collection_sample_type.id
+      study_id = study.id
+      assay_id = assay.id
+
+      post_params = { sample_data: source_sample_ids.to_json,
+                      sample_type_id: sample_type_id.to_json,
+                      study_id: study_id.to_json,
+                      assay_id: assay_id.to_json }
 
       post :export_to_excel, params: post_params, xhr: true
 
