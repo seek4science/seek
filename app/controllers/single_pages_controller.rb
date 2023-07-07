4c4c8c0be2bfdd9c6c7a594e7bd10b76851ff0b4
Kevin De Pelseneer <kevin.depelseneer@psb.ugent.be>
2023-07-07 13:29:32 +0200

    Modified controller to handle multi-sample inputs

--- app/controllers/single_pages_controller.rb
+++ app/controllers/single_pages_controller.rb
@@ -126,24 +126,69 @@ class SinglePagesController < ApplicationController
 
   def upload_samples
     wb = Roo::Excelx.new(params[:file].path)
-    puts params
-    # Extract Samples metadata
-    study_id = wb.cell(2, 2, sheet = 'Metadata').to_i
-    sample_type_id = wb.cell(5, 2, sheet = 'Metadata').to_i
-    template_id = wb.cell(8, 2, sheet = 'Metadata').to_i
+    sample_type_id_ui = params[:sample_type_id].to_i
 
+    # Extract Samples metadata from spreadsheet
+    study_id = wb.cell(2, 2, sheet = 'Metadata').to_i
     @study = Study.find(study_id)
+    sample_type_id = wb.cell(5, 2, sheet = 'Metadata').to_i
     @sample_type = SampleType.find(sample_type_id)
+
+    # Sample Type validation rules
+    unless sample_type_id_ui == @sample_type&.id
+      raise "Sample Type #{@sample_type&.id} from spreadsheet doesn't match Sample Type #{sample_type_id_ui} from the table. Please upload in the correct table."
+    end
+    unless @study.sample_types.include?(@sample_type)
+      raise "Sample Type '#{@sample_type.id}' doesn't belong to Study #{@study.id}. Sample Upload aborted."
+    end
+
+    is_assay = @sample_type.assays.any?
+    @assay = @sample_type.assays.first
+
+    multiple_input_fields = @sample_type.sample_attributes.map do |sa_attr|
+      sa_attr.title if sa_attr.sample_attribute_type.base_type == 'SeekSampleMulti'
+    end
+
+    if is_assay
+      assay_id = wb.cell(8, 2, sheet = 'Metadata').to_i
+      template_id = wb.cell(11, 2, sheet = 'Metadata').to_i
+    else
+      template_id = wb.cell(8, 2, sheet = 'Metadata').to_i
+    end
+
+    @assay = Assay.find(assay_id) unless assay_id.nil?
     @template = Template.find(template_id)
     @project = @study.projects.first # In Single Page a study can only belong to one project
+
 
     sample_fields = wb.row(1, sheet = 'Samples').map { |field| field.sub(' *', '') }
     samples_data = (2..wb.last_row(sheet = 'Samples')).map { |i| wb.row(i, sheet = 'Samples') }
 
+    # Compare Excel header row to Sample Type Sample Attributes
+    # Should raise an error if they don't match
+    sample_type_attributes = %w[id uuid].concat(@sample_type.sample_attributes.map(&:title))
+    has_unmapped_sample_attributes = sample_type_attributes.map { |sa| sample_fields.include?(sa) }.include?(false)
+    if has_unmapped_sample_attributes
+      raise "The Sample Attributes from the excel sheet don't match those of the Sample Type in the database. Sample upload was aborted!"
+    end
+
+    # Construct Samples objects from Excel data
     @excel_samples = samples_data.map do |excel_sample|
       obj = {}
       (0..sample_fields.size - 1).map do |i|
-        obj.merge!(sample_fields[i] => excel_sample[i])
+        if multiple_input_fields.include?(sample_fields[i])
+          parsed_excel_input_samples = JSON.parse(excel_sample[i].gsub('=>', ':')).map do |subsample|
+            # Uploader should at least have viewing permissions for the inputs he's using
+            unless Sample.find(subsample['id'])&.authorized_for_view?
+              raise "Unauthorized Sample was detected in spreadsheet: #{subsample.inspect}"
+            end
+
+            subsample
+          end
+          obj.merge!(sample_fields[i] => parsed_excel_input_samples)
+        else
+          obj.merge!(sample_fields[i] => excel_sample[i])
+        end
       end
       obj
     end
@@ -162,6 +207,7 @@ class SinglePagesController < ApplicationController
       db_sample = @db_samples.select { |s| s['id'] == ees['id'] }.first
       # An exception is raised if the ID of an existing Sample cannot be found in the DB
       raise "Sample with id '#{ees['id']}' does not exist in the database. Sample upload was aborted!" if db_sample.nil?
+
       is_changed = false
 
       db_sample.map do |k, v|
@@ -198,6 +244,7 @@ class SinglePagesController < ApplicationController
     end
 
     upload_data = { study: @study,
+                    assay: @assay,
                     sampleType: @sample_type,
                     template: @template,
                     excelSamples: @excel_samples,
