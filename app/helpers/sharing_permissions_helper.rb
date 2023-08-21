ea2e182157ee7b7c7306dae42fc14dafd349d85a
fherreazcue <francisco.herreazcue@gmail.com>
2023-08-21 14:08:15 +0100

    Improves switch statement in get_permission_list

--- app/helpers/sharing_permissions_helper.rb
+++ app/helpers/sharing_permissions_helper.rb
@@ -17,30 +17,16 @@ module SharingPermissionsHelper
     option = { target: :_blank }
     item.policy.permissions.map do |permission|
       sharing_policy_changed = @batch_sharing_permission_changed && (@items_for_sharing.include? item) ? PolicyHelper::permission_changed_item_class(permission, @policy_params) : ""
-      div_id = 'case-not-found'
+      m = permission.contributor
+      div_id = "#{item_id}-Permission-#{permission.contributor_type}-#{m.id}:"
+      policy_text = "<strong>#{permission.contributor_type}  #{link_to(h(m.title), m, option)}:</strong>"
       case permission.contributor_type
-      when Permission::PROJECT
-        m = Project.find(permission.contributor_id)
-        policy_text = "<strong>Project  #{link_to(h(m.title), m, option)}:</strong>"
-        div_id = "#{item_id}-Permission-Project-#{m.id}:<strong>"
       when Permission::WORKGROUP
-        m = WorkGroup.find(permission.contributor_id)
         institution = Institution.find(m.institution_id)
         project = Project.find(m.project_id)
         policy_text = "<strong> #{link_to(h(project.title), project, option)}  @  #{link_to(h(institution.title), institution, option)}:</strong>"
-        div_id = "#{item_id}-Permission-Workgroup-#{m.id}"
-      when Permission::INSTITUTION
-        m = Institution.find(permission.contributor_id)
-        policy_text = "<strong>Institution  #{link_to(h(m.title), m, option)}:</strong>"
-        div_id = "#{item_id}-Permission-Institution-#{m.id}"
       when Permission::PERSON
-        m = Person.find(permission.contributor_id)
         policy_text = "<strong>#{link_to(h(m.title), m, option)}:</strong>"
-        div_id = "#{item_id}-Permission-Person-#{m.id}"
-      when Permission::PROGRAMME
-        m = Programme.find(permission.contributor_id)
-        policy_text = "<strong>Programme  #{link_to(h(m.title), m, option)}:</strong>"
-        div_id = "#{item_id}-Permission-Programme-#{m.id}"
       end
       policy_text += " #{Policy.get_access_type_wording(permission.access_type, downloadable)}."
       policy.append("<div id=\"#{div_id}\" class=\"permission-node #{sharing_policy_changed}\">#{policy_text}</div>".html_safe)
