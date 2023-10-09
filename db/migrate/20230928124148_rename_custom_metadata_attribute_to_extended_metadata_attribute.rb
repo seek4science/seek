b41ce0236a9566be9db3a78a7e4e11a1879e97e7
Xiaoming Hu <xiaoming.hu@h-its.org>
2023-10-09 15:21:19 +0200

    fix migrations

--- db/migrate/20230928124148_rename_custom_metadata_attribute_to_extended_metadata_attribute.rb
+++ db/migrate/20230928124148_rename_custom_metadata_attribute_to_extended_metadata_attribute.rb
@@ -2,7 +2,7 @@ class RenameCustomMetadataAttributeToExtendedMetadataAttribute < ActiveRecord::M
     def change
       rename_table :extended_metadata_attributes, :extended_metadata_attributes
 
-      rename_column :extended_metadata_attributes, :extended_metadata_type_id, :extended_metadata_type_id
-      rename_column :extended_metadata_attributes, :linked_extended_metadata_type_id, :linked_extended_metadata_type_id
+      rename_column :extended_metadata_attributes, :custom_metadata_type_id, :extended_metadata_type_id
+      rename_column :extended_metadata_attributes, :linked_custom_metadata_type_id, :linked_extended_metadata_type_id
     end
 end
