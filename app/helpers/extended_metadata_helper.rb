a0aaf2486c6ea1e3136be6b0c4e00125711cf5e2
Kevin De Pelseneer <kevin.depelseneer@psb.ugent.be>
2023-10-10 17:58:54 +0200

    fix merge errors

--- app/helpers/extended_metadata_helper.rb
+++ app/helpers/extended_metadata_helper.rb
@@ -3,7 +3,12 @@ module ExtendedMetadataHelper
 
   def extended_metadata_form_field_for_attribute(attribute, resource)
     element_class = "extended_metadata_attribute_#{attribute.sample_attribute_type.base_type.downcase}"
-    element_name = "#{resource.class.name.underscore}[extended_metadata_attributes][data][#{attribute.title}]"
+
+    if parent_resource
+      element_name = "#{parent_resource}[#{resource.class.name.underscore}][extended_metadata_attributes][data][#{attribute.title}]"
+    else
+      element_name = "#{resource.class.name.underscore}[extended_metadata_attributes][data][#{attribute.title}]"
+    end
 
     if attribute.linked_extended_metadata? || attribute.linked_extended_metadata_multi?
       content_tag(:span, class: 'linked_extended_metdata') do
