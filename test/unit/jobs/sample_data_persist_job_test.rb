272aaacca002da10f165489a726c04ac86f2cfdd
Stuart Owen <stuart.owen@manchester.ac.uk>
2023-10-18 15:03:16 +0100

    fixed SampleDataPersistJob tests to run without a logged in user #1625

--- test/unit/jobs/sample_data_persist_job_test.rb
+++ test/unit/jobs/sample_data_persist_job_test.rb
@@ -1,24 +1,26 @@
 require 'test_helper'
 
 class SampleDataPersistJobTest < ActiveSupport::TestCase
+
   def setup
     create_sample_attribute_type
     @person = FactoryBot.create(:project_administrator)
-    User.current_user = @person.user
-
-    @data_file = FactoryBot.create :data_file, content_blob: FactoryBot.create(:sample_type_populated_template_content_blob),
+    User.with_current_user(@person.user) do
+      @data_file = FactoryBot.create :data_file, content_blob: FactoryBot.create(:sample_type_populated_template_content_blob),
                                      policy: FactoryBot.create(:private_policy), contributor: @person
-    refute @data_file.sample_template?
-    assert_empty @data_file.possible_sample_types
+      refute @data_file.sample_template?
+      assert_empty @data_file.possible_sample_types
 
-    @sample_type = SampleType.new title: 'from template', uploaded_template: true,
-                                  project_ids: [@person.projects.first.id], contributor: @person
-    @sample_type.content_blob = FactoryBot.create(:sample_type_template_content_blob)
-    @sample_type.build_attributes_from_template
-    # this is to force the full name to be 2 words, so that one row fails
-    @sample_type.sample_attributes.first.sample_attribute_type = FactoryBot.create(:full_name_sample_attribute_type)
-    @sample_type.sample_attributes[1].sample_attribute_type = FactoryBot.create(:datetime_sample_attribute_type)
-    @sample_type.save!
+      @sample_type = SampleType.new title: 'from template', uploaded_template: true,
+                                    project_ids: [@person.projects.first.id], contributor: @person
+      @sample_type.content_blob = FactoryBot.create(:sample_type_template_content_blob)
+      @sample_type.build_attributes_from_template
+      # this is to force the full name to be 2 words, so that one row fails
+      @sample_type.sample_attributes.first.sample_attribute_type = FactoryBot.create(:full_name_sample_attribute_type)
+      @sample_type.sample_attributes[1].sample_attribute_type = FactoryBot.create(:datetime_sample_attribute_type)
+      @sample_type.save!
+    end
+
   end
 
   test 'queue job' do
