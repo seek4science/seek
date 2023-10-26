c58e5b37562e7f40ea4c18e92e79112c4be8436a
Finn Bacall <finn.bacall@manchester.ac.uk>
2023-10-26 13:11:52 +0100

    Delete auth lookup records asynchronously. Fixes #1628

---
+++ app/jobs/auth_lookup_delete_job.rb
@@ -0,0 +1,15 @@
+class AuthLookupDeleteJob < ApplicationJob
+  BATCH_SIZE = 1000
+  queue_as QueueNames::AUTH_LOOKUP
+  queue_with_priority 2
+
+  def perform(item_class, item_id)
+    if item_class == 'User'
+      Seek::Util.authorized_types.each do |type|
+        type.lookup_class.where(user: item_id).in_batches(of: BATCH_SIZE).delete_all
+      end
+    else
+      item_class.constantize.lookup_class.where(asset_id: item_id).in_batches(of: BATCH_SIZE).delete_all
+    end
+  end
+end
