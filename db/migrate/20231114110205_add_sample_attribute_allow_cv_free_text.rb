10729006498062b6f9a86ae9e67999546015b0d0
Stuart Owen <stuart.owen@manchester.ac.uk>
2023-11-14 16:19:09 +0000

    also need to deal with extended metadata and template attributes #1648

--- db/migrate/20231114110205_add_sample_attribute_allow_cv_free_text.rb
+++
@@ -1,5 +0,0 @@
-class AddSampleAttributeAllowCvFreeText < ActiveRecord::Migration[6.1]
-  def change
-    add_column :sample_attributes, :allow_cv_free_text, :boolean, default: false
-  end
-end
--- db/migrate/20231114110917_remove_custom_input_from_cv.rb
+++ db/migrate/20231114110917_remove_custom_input_from_cv.rb
@@ -1,9 +1,11 @@
 class RemoveCustomInputFromCv < ActiveRecord::Migration[6.1]
   def up
-    # Move to SampleAttribute first, for CV's without associated attributes the information will be lost
-    controlled_vocabs = SampleControlledVocab.where(custom_input: true).includes(:sample_attributes).select{|cv| cv.sample_attributes.any?}
+    # Move to Attribute first, for CV's without associated attributes the information will be lost
+    controlled_vocabs = SampleControlledVocab.where(custom_input: true).includes(:sample_attributes, :extended_metadata_attributes, :template_attributes)
     controlled_vocabs.each do |controlled_vocab|
       controlled_vocab.sample_attributes.update_all(allow_cv_free_text: true)
+      controlled_vocab.extended_metadata_attributes.update_all(allow_cv_free_text: true)
+      controlled_vocab.template_attributes.update_all(allow_cv_free_text: true)
     end
     remove_column :sample_controlled_vocabs, :custom_input
   end
@@ -12,7 +14,17 @@ class RemoveCustomInputFromCv < ActiveRecord::Migration[6.1]
     add_column :sample_controlled_vocabs, :custom_input, :boolean, default: false
 
     # will only restore CV's that had attributes associated
-    attributes = SampleAttribute.where(allow_cv_free_text: true).includes(:sample_controlled_vocab)
+    attributes = SampleAttribute.where(allow_cv_free_text: true)
+    attributes.each do |attr|
+      attr.sample_controlled_vocab&.update_column(:custom_input, :true)
+    end
+
+    attributes = ExtendedMetadataAttribute.where(allow_cv_free_text: true)
+    attributes.each do |attr|
+      attr.sample_controlled_vocab&.update_column(:custom_input, :true)
+    end
+
+    attributes = TemplateAttribute.where(allow_cv_free_text: true)
     attributes.each do |attr|
       attr.sample_controlled_vocab&.update_column(:custom_input, :true)
     end
