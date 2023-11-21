68b2bfb6fd271069079d455c3ade25b4dadbba94
Stuart Owen <stuart.owen@manchester.ac.uk>
2023-11-21 15:50:20 +0000

    made attribute required when calling attribute_type_handler_factory #1648

--- lib/seek/samples/attribute_type_handlers/attribute_type_handler_factory.rb
+++ lib/seek/samples/attribute_type_handlers/attribute_type_handler_factory.rb
@@ -10,7 +10,7 @@ module Seek
           @handlers = {}
         end
 
-        def for_base_type(base_type, attribute=nil)
+        def for_base_type(base_type, attribute)
           "Seek::Samples::AttributeTypeHandlers::#{base_type}AttributeTypeHandler".constantize.new(attribute)
         rescue NameError
           raise UnrecognisedAttributeHandlerType, "unrecognised attribute base type '#{base_type}'"
--- test/unit/samples/attribute_type_handler_factory_test.rb
+++ test/unit/samples/attribute_type_handler_factory_test.rb
@@ -6,11 +6,13 @@ class AttributeTypeHandlerFactoryTest < ActiveSupport::TestCase
   end
 
   test 'handlers to for base type' do
+    st = FactoryBot.create(:simple_sample_type)
+    attr = FactoryBot.create(:simple_string_sample_attribute, sample_type: st)
     # they are repeated twice to check for any caching issues
     types = Seek::Samples::BaseType::ALL_TYPES + Seek::Samples::BaseType::ALL_TYPES
     types.each do |type|
       expected = "Seek::Samples::AttributeTypeHandlers::#{type}AttributeTypeHandler".constantize
-      assert_kind_of expected, @factory.for_base_type(type), "Expected #{expected.name} for #{type}"
+      assert_kind_of expected, @factory.for_base_type(type, attr), "Expected #{expected.name} for #{type}"
     end
   end
 
@@ -22,13 +24,15 @@ class AttributeTypeHandlerFactoryTest < ActiveSupport::TestCase
   end
 
   test 'exception for invalid type' do
+    st = FactoryBot.create(:simple_sample_type)
+    attr = FactoryBot.create(:simple_string_sample_attribute, sample_type: st)
     e = assert_raise(Seek::Samples::AttributeTypeHandlers::UnrecognisedAttributeHandlerType) do
-      @factory.for_base_type('fish')
+      @factory.for_base_type('fish', attr)
     end
     assert_equal "unrecognised attribute base type 'fish'", e.message
 
     e = assert_raise(Seek::Samples::AttributeTypeHandlers::UnrecognisedAttributeHandlerType) do
-      @factory.for_base_type('fish')
+      @factory.for_base_type('fish', attr)
     end
     assert_equal "unrecognised attribute base type 'fish'", e.message
   end
