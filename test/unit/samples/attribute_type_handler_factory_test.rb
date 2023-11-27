7330cea11ff71cfdea70aedc68a43e2be8a6d236
Stuart Owen <stuart.owen@manchester.ac.uk>
2023-11-27 13:18:02 +0000

    removed need to pass base_type to the attribute type handler factory #1648

--- test/unit/samples/attribute_type_handler_factory_test.rb
+++ test/unit/samples/attribute_type_handler_factory_test.rb
@@ -11,28 +11,18 @@ class AttributeTypeHandlerFactoryTest < ActiveSupport::TestCase
     # they are repeated twice to check for any caching issues
     types = Seek::Samples::BaseType::ALL_TYPES + Seek::Samples::BaseType::ALL_TYPES
     types.each do |type|
+      attr.sample_attribute_type.base_type = type
       expected = "Seek::Samples::AttributeTypeHandlers::#{type}AttributeTypeHandler".constantize
-      assert_kind_of expected, @factory.for_base_type(type, attr), "Expected #{expected.name} for #{type}"
+      assert_kind_of expected, @factory.for_base_type(attr), "Expected #{expected.name} for #{type}"
     end
-  end
-
-  test 'for_base_type passes attribute' do
-    st = FactoryBot.create(:simple_sample_type)
-    attr = FactoryBot.create(:simple_string_sample_attribute, sample_type: st)
-    type = @factory.for_base_type('SeekSample', attr)
-    assert_equal attr, type.send(:attribute)
   end
 
   test 'exception for invalid type' do
     st = FactoryBot.create(:simple_sample_type)
     attr = FactoryBot.create(:simple_string_sample_attribute, sample_type: st)
+    attr.sample_attribute_type.base_type = 'fish'
     e = assert_raise(Seek::Samples::AttributeTypeHandlers::UnrecognisedAttributeHandlerType) do
-      @factory.for_base_type('fish', attr)
-    end
-    assert_equal "unrecognised attribute base type 'fish'", e.message
-
-    e = assert_raise(Seek::Samples::AttributeTypeHandlers::UnrecognisedAttributeHandlerType) do
-      @factory.for_base_type('fish', attr)
+      @factory.for_base_type(attr)
     end
     assert_equal "unrecognised attribute base type 'fish'", e.message
   end
