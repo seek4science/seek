767d2afb7d08247d2f15569fa8f06ad7fe475cca
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-01-31 17:33:47 +0100

    Change the default value to an empty script tag

--- config/initializers/seek_configuration.rb
+++ config/initializers/seek_configuration.rb
@@ -35,7 +35,7 @@ def load_seek_config_defaults!
   Seek::Config.default :piwik_analytics_tracking_notice, true
   Seek::Config.default :custom_analytics_snippet_enabled, false
   Seek::Config.default :custom_analytics_name, 'Custom name'
-  Seek::Config.default :custom_analytics_snippet, ''
+  Seek::Config.default :custom_analytics_snippet, '<script id="custom-tracking-script"></script>'
   Seek::Config.default :custom_analytics_tracking_notice, true
   Seek::Config.default :bioportal_api_key,''
   Seek::Config.default :project_news_enabled,false
