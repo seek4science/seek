c29a34db283070157b3c3eb80df81b3303e92fde
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-02-12 07:56:29 +0100

    Run migrations after rebase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_06_084106) do
+ActiveRecord::Schema.define(version: 2024_02_06_132054) do
 
   create_table "activity_logs", id: :integer, force: :cascade do |t|
     t.string "action"
