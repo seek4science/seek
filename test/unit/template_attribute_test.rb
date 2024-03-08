f94e51a4a4db61213d9a37a990e9a82eea75c9f5
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-03-08 08:36:49 +0100

    Add sample_attribute_types to tests

--- test/unit/template_attribute_test.rb
+++ test/unit/template_attribute_test.rb
@@ -1,12 +1,18 @@
 require 'test_helper'
+
 class TemplateAttributeTest < ActiveSupport::TestCase
 
   test 'allow isa tag change' do
     # When template doesn't have child templates => editable
     # When template has child templates => disabled
-    parent_attribute = FactoryBot.create(:template_attribute)
-    child_attribute = FactoryBot.create(:template_attribute, parent_attribute: parent_attribute)
-    parentless_attribute = FactoryBot.create(:template_attribute)
+    string_type = FactoryBot.create(:string_sample_attribute_type)
+
+    parent_attribute = FactoryBot.create(:template_attribute,
+                                         sample_attribute_type: string_type)
+    child_attribute = FactoryBot.create(:template_attribute,
+                                        parent_attribute: parent_attribute,
+                                        sample_attribute_type: string_type)
+    parentless_attribute = FactoryBot.create(:template_attribute, sample_attribute_type: string_type)
     assert parentless_attribute.allow_isa_tag_change?
     refute child_attribute.allow_isa_tag_change?
   end
