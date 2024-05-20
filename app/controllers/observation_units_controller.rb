de12eb488fdb3d0328ebdc4cabb4325b55607ce3
Stuart Owen <stuart.owen@manchester.ac.uk>
2024-05-20 15:07:44 +0100

    tests for update and manage update

--- app/controllers/observation_units_controller.rb
+++ app/controllers/observation_units_controller.rb
@@ -19,4 +19,26 @@ class ObservationUnitsController < ApplicationController
     end
   end
 
+  def update
+    update_annotations(params[:tag_list], @observation_unit) if params.key?(:tag_list)
+    update_sharing_policies @observation_unit
+    update_relationships(@observation_unit,params)
+
+    respond_to do |format|
+      if @observation_unit.update(observation_unit_params)
+        flash[:notice] = "#{t('document')} metadata was successfully updated."
+        format.html { redirect_to observation_unit_path(@observation_unit) }
+        format.json { render json: @observation_unit, include: [params[:include]] }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: json_api_errors(@observation_unit), status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def observation_unit_params
+    params.require(:observation_unit).permit(:title, :description, *creator_related_params, { project_ids: [] },
+                                         { special_auth_codes_attributes: [:code, :expiration_date, :id, :_destroy] },
+                                         { extended_metadata_attributes: determine_extended_metadata_keys })
+  end
 end
--- test/functional/observation_units_controller_test.rb
+++ test/functional/observation_units_controller_test.rb
@@ -37,4 +37,67 @@ class ObservationUnitsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test 'manage update' do
+    proj1=FactoryBot.create(:project)
+    proj2=FactoryBot.create(:project)
+    person = FactoryBot.create(:person,project:proj1)
+    other_person = FactoryBot.create(:person)
+    person.add_to_project_and_institution(proj2,person.institutions.first)
+    person.save!
+    other_creator = FactoryBot.create(:person,project:proj1)
+    other_creator.add_to_project_and_institution(proj2,other_creator.institutions.first)
+    other_creator.save!
+
+    obs_unit = FactoryBot.create(:observation_unit, contributor:person, projects:[proj1], policy:FactoryBot.create(:private_policy))
+
+    login_as(person)
+    assert obs_unit.can_manage?
+
+    patch :manage_update, params: {id: obs_unit,
+                                   observation_unit: {
+                                     creator_ids: [other_creator.id],
+                                     project_ids: [proj1.id, proj2.id]
+                                   },
+                                   policy_attributes: {access_type: Policy::VISIBLE, permissions_attributes: {'1' => {contributor_type: 'Person', contributor_id: other_person.id, access_type: Policy::MANAGING}}
+                                   }}
+
+    assert_redirected_to obs_unit
+
+    obs_unit.reload
+    assert_equal [proj1,proj2],obs_unit.projects.sort_by(&:id)
+    assert_equal [other_creator],obs_unit.creators
+    assert_equal Policy::VISIBLE,obs_unit.policy.access_type
+    assert_equal 1,obs_unit.policy.permissions.count
+    assert_equal other_person,obs_unit.policy.permissions.first.contributor
+    assert_equal Policy::MANAGING,obs_unit.policy.permissions.first.access_type
+  end
+
+  test 'update' do
+    obs_unit = FactoryBot.create(:observation_unit)
+    emt = FactoryBot.create(:simple_observation_unit_extended_metadata_type)
+    login_as(obs_unit.contributor)
+
+    patch :update, params: { id: obs_unit,
+                             observation_unit:{
+                               title: 'updated title',
+                               description: 'updated description',
+                               extended_metadata_attributes: {
+                                 extended_metadata_type_id: emt.id,
+                                 data: {
+                                   name: 'updated name',
+                                   strain: 'updated strain'
+                                 }
+                               }
+                             }}
+
+    assert_redirected_to obs_unit
+
+    obs_unit.reload
+    assert_equal 'updated title', obs_unit.title
+    assert_equal 'updated description', obs_unit.description
+    assert_equal emt, obs_unit.extended_metadata.extended_metadata_type
+    assert_equal 'updated name', obs_unit.extended_metadata.get_attribute_value('name')
+    assert_equal 'updated strain', obs_unit.extended_metadata.get_attribute_value('strain')
+  end
+
 end
