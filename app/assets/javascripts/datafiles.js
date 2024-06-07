b346f17424ccafe8f8556e09b19df21ca2552cfe
Andrew Wood <andrew.wood@ed.ac.uk>
2024-06-07 10:10:14 +0100

    Setting up confirm unzip screen

--- app/assets/javascripts/datafiles.js
+++ app/assets/javascripts/datafiles.js
@@ -1,8 +1,7 @@
 var Datafiles = {};
 
-Datafiles.initTable = function (selector, enableRowSelection, opts) {
+Datafiles.initTable = function (selector, opts) {
     var table;
-    enableRowSelection = enableRowSelection || false;
     opts = opts || {};
 
     $j('table tfoot th', selector).each( function () {
@@ -13,148 +12,8 @@ Datafiles.initTable = function (selector, enableRowSelection, opts) {
     var options = $j.extend({}, opts, {
         "lengthMenu": [ 5, 10, 25, 50, 75, 100 ],
         "pageLength": 10,
-        dom: '<"row"<"col-sm-10"lr><"col-sm-2 text-right"B>><"datafiles-table-container"t>ip', // Needed to place the buttons
-        "columnDefs": [{
-            "targets": [ 0, 1 ],
-            "visible": false,
-            "searchable": false
-        }],
-				buttons: [
-					{
-							extend: 'csvHtml5',
-							text: 'Export table',
-							exportOptions: {
-									columns: [':visible']
-							}
-					}
-				],
-				initComplete: function () {
-					if(opts.hideEmptyColumns) hideEmptyColumns(this);
-				}
-        //"initComplete": function () {  // THIS IS TOO SLOW - CRASHES BROWSER
-        //    console.log("Hiding empty columns");
-        //    table.columns().flatten().each(function (columnIndex) {
-        //        console.log("Col " + columnIndex);
-        //        if(table.columns(columnIndex).data()[0].every(function(v) { return v === null; }))
-        //            table.column(columnIndex).visible(false);
-        //    });
-        //}
+        dom: '<"row"<"col-sm-10"lr>><"datafiles-table-container"t>ip' // Needed to place the buttons
     });
-
-    if($j('table', selector).data('sourceUrl'))
-        options.ajax = $j('table', selector).data('sourceUrl');
-
-    if(options.ajax) {
-        options.columns = [{ data: 'id'},{ data: 'title'}];
-        $j('table thead th', selector).each(function (index, column) {
-            if($j(column).data('accessorName'))
-                options.columns.push({ data: 'data.' + $j(column).data('accessorName') });
-        });
-    }
-
-    // Date Columns
-    var dateColumns = [];
-    $j('table thead th', selector).each(function (index, column) {
-        if($j(column).data('columnType') == 'Date' ||
-           $j(column).data('columnType') == 'DateTime') {
-            dateColumns.push(index);
-        }
-    });
-    if(dateColumns.length > 0) {
-        options["columnDefs"].push({
-            "targets": dateColumns,
-            "type": "date"
-        });
-    }
-    // The following only needed if we're loading the data from ajax
-
-    if($j('table', selector).data('sourceUrl')) {
-        // Strain columns
-        var strainColumns = [];
-        $j('table thead th', selector).each(function (index, column) {
-            if($j(column).data('columnType') == 'SeekStrain') {
-                strainColumns.push(index);
-            }
-        });
-        if(strainColumns.length > 0) {
-            options["columnDefs"].push({
-                "targets": strainColumns,
-                "render": function (data, type, row) {
-                    if(data && data.id) {
-                        if (data.title)
-                            return '<a href="/strains/' + data.id + '">' + data.title + '</a>';
-                        else
-                            return '<span class="none_text">' + data.id + '</span>';
-                    } else {
-                        return '<span class="none_text">Not specified</span>';
-                    }
-                }
-            });
-        }
-        // SEEK datafile columns
-        var seekDatafileColumns = [];
-        $j('table thead th', selector).each(function (index, column) {
-            if(['SeekDatafile','SeekDatafileMulti'].includes($j(column).data('columnType'))) {
-                seekDatafileColumns.push(index);
-            }
-        });
-        if(seekDatafileColumns.length > 0) {
-            options["columnDefs"].push({
-                "targets": seekDatafileColumns,
-                "render": function (data, type, row) {
-                    var values = Array.isArray(data) ? data : [data];
-                    var result = $j.map(values, function(value, i) {
-                        if(value && value.id) {
-                            if (value.title)
-                                return '<a href="/datafiles/' + value.id + '">' + value.title + '</a>';
-                            else
-                                return '<span class="none_text">' + (value.id || value.title) + '</span>';
-                        } else {
-                            return '<span class="none_text">Not specified</span>';
-                        }
-                    })
-                    return result.join(", ")
-                }
-            });
-        }
-
-        // Title column
-        $j('table thead th', selector).each(function (index, column) {
-            if($j(column).data('titleColumn')) {
-                options["columnDefs"].push({
-                    "targets": [index],
-                    "render": function (data, type, row) {
-                        return '<a href="/datafiles/' + row.id + '">' + row.title + '</a>';
-                    }
-                });
-            }
-        });
-    }
-
-    if(enableRowSelection) {
-        $j.extend(options, options, {
-            dom: '<"row"<"col-sm-6"l><"col-sm-6 text-right"B>>r<"datafiles-table-container"t>ip', // Needed to place the buttons
-            buttons: [
-            {
-                text: 'Select All',
-                action: function () {
-                    table.rows().deselect();
-                    table.rows( {search:'applied'} ).select();
-                }
-            },
-            'selectNone'
-        ],
-            language: {
-            buttons: {
-                selectNone: "Clear Selection"
-            }
-        },
-            "select": {
-            style: 'multi'
-        }});
-
-        $j('table tbody tr', selector).addClass('clickable');
-    }
 
     table = $j('table', selector).DataTable(options);
 
@@ -180,10 +39,3 @@ Datafiles.initTable = function (selector, enableRowSelection, opts) {
     });
     return table;
 };
-
-function hideEmptyColumns(selector) {
-	const table = $j(selector).DataTable()
-	table.columns().every(function(idx) {
-		if (!this.data().toArray().some(x=>x)) table.column(idx).visible(false)
-	});
-}
