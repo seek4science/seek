32ff6ad2f01dd105e93fbf0fca6784252d86740f
whomingbird <xiaoming.hu@h-its.org>
2024-06-27 11:07:59 +0200

    When the model is not publicly accessible but can be downloaded by the current user, use SpecialAuthCode to enable loading the model into CopasiUI (desktop application)

--- app/helpers/assets_helper.rb
+++ app/helpers/assets_helper.rb
@@ -221,10 +221,13 @@ module AssetsHelper
 
   end
 
-  def open_with_copasi_ui_button (asset)
+  def open_with_copasi_ui_button
 
-    file  =   asset.copasi_supported_content_blobs.first
-    download_path = polymorphic_path([file.asset, file], action: :download, code: params[:code])
+    blob  =   @display_model.copasi_supported_content_blobs.first
+
+    auth_code = @model.special_auth_codes.where('code LIKE ?', 'copasi_%').first.code
+
+    download_path = polymorphic_path([@model, blob], action: :download, code:auth_code)
 
     copasi_download_path =  "copasi://process?downloadUrl=http://"+request.host_with_port+download_path+"&activate=Time%20Course&createPlot=Concentrations%2C%20Volumes%2C%20and%20Global%20Quantity%20Values&runTask=Time-Course"
 
--- lib/seek/copasi/simulator.rb
+++ lib/seek/copasi/simulator.rb
@@ -4,16 +4,21 @@ module Seek
       extend ActiveSupport::Concern
 
       included do
-        before_action :find_display_asset_for_copasi, :find_model_file, only: [:copasi_simulate]
+        before_action :find_model, :find_display_asset_for_copasi, :select_model_file_for_simulation, only: [:copasi_simulate]
+        before_action :fetch_special_auth_code, if: -> { is_special_auth_code_required? }, only: [:copasi_simulate]
+
       end
 
       def copasi_simulate
-        @model = Model.find_by_id(params[:id])
-        @content_blob =  @model.content_blobs.first
         render 'copasi_simulate'
       end
 
-      def find_model_file
+      def find_model
+        @model = Model.find_by_id(params[:id])
+      end
+
+      # # If the content blob is not available locally, fetch a copy from the remote URL
+      def select_model_file_for_simulation
 
         @blob = nil
 
@@ -42,6 +47,7 @@ module Seek
         end
       end
 
+      # select the first COPASI-compatible content_blob when multiple items are associated with the display model.
       def select_copasi_content_blob
         blob = @display_model.copasi_supported_content_blobs.first
         raise 'Unable to find file to support Copasi Online' unless blob
@@ -49,7 +55,34 @@ module Seek
       end
 
       def find_display_asset_for_copasi
-        find_display_asset Model.find_by_id(params[:id])
+        find_display_asset
+      end
+
+
+      # fetches or generates a special auth code with a "copasi_" prefix, which is used by COPASI desk application to load the non public model 
+      def fetch_special_auth_code
+        copasi_codes = special_auth_codes_with_copasi_prefix
+
+        if copasi_codes.empty?
+          # Generate a special auth code for a day
+          auth_code = SpecialAuthCode.new(expiration_date: Time.now + 1.day)
+          auth_code.update(code: "copasi_#{auth_code.code}")
+          @model.special_auth_codes << auth_code
+          auth_code # Ensure the new auth code is returned
+        else
+          copasi_codes.first
+        end
+      end
+
+      private
+
+      def special_auth_codes_with_copasi_prefix
+        @model.special_auth_codes.where('code LIKE ?', 'copasi_%')
+      end
+
+      def is_special_auth_code_required?
+        # If the model is not publicly accessible but can be downloaded by the current user, the special auth code will be required.
+        Seek::Config.copasi_enabled && @display_model.is_copasi_supported? && @display_model.model.can_download? && !@model.public?
       end
 
     end
