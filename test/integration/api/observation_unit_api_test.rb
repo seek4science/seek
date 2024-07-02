7686c770ceb1059d43b8d563de9fc50b5e8af3cd
Stuart Owen <stuart.owen@manchester.ac.uk>
2024-07-02 16:38:11 +0100

    post max resource working

--- test/integration/api/observation_unit_api_test.rb
+++ test/integration/api/observation_unit_api_test.rb
@@ -2,10 +2,16 @@ require 'test_helper'
 class ObservationUnitApiTest < ActionDispatch::IntegrationTest
 
   include ReadApiTestSuite
-  #include WriteApiTestSuite
+  include WriteApiTestSuite
 
   def setup
+    user_login
     @observation_unit = FactoryBot.create(:observation_unit)
+    @creator = FactoryBot.create(:person)
+    @project = @current_user.person.projects.first
+    @study = FactoryBot.create(:study, contributor: @current_user.person)
+    @sample = FactoryBot.create(:sample, contributor: @current_user.person)
+    @data_file = FactoryBot.create(:data_file, contributor: @current_user.person)
   end
 
 end
