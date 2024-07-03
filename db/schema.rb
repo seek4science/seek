08424e21c5bf69dd5c7f6d6890ba1b8de7906450
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-07-03 12:22:34 +0200

    Apply migration to schema

--- db/schema.rb
+++ db/schema.rb
@@ -1811,6 +1811,32 @@ ActiveRecord::Schema.define(version: 2024_07_03_132257) do
     t.index ["sample_id"], name: "index_sample_resource_links_on_sample_id"
   end
 
+  create_table "sample_type_auth_lookup", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "asset_id"
+    t.boolean "can_view", default: false
+    t.boolean "can_manage", default: false
+    t.boolean "can_edit", default: false
+    t.boolean "can_download", default: false
+    t.boolean "can_delete", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "asset_id", "can_view"], name: "index_sample_type_user_id_asset_id_can_view"
+    t.index ["user_id", "can_view"], name: "index_sample_type_auth_lookup_on_user_id_and_can_view"
+  end
+
+  create_table "sample_type_auth_lookups", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "asset_id"
+    t.boolean "can_view", default: false
+    t.boolean "can_manage", default: false
+    t.boolean "can_edit", default: false
+    t.boolean "can_download", default: false
+    t.boolean "can_delete", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "sample_types", id: :integer, force: :cascade do |t|
     t.string "title"
     t.string "uuid"
@@ -1823,6 +1849,7 @@ ActiveRecord::Schema.define(version: 2024_07_03_132257) do
     t.string "deleted_contributor"
     t.integer "template_id"
     t.text "other_creators"
+    t.integer "policy_id"
   end
 
   create_table "sample_types_studies", force: :cascade do |t|
