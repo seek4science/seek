cfd7789f847882c750722702eaa3692216f49d9c
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-07-24 10:31:54 +0200

    Add sample metadata updater module

---
+++ lib/seek/samples/sample_metadata_updater.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module Seek
+  module Samples
+
+    class SampleMetadataUpdateException < StandardError; end
+
+    # Class to handle the updating of sample metadata after updating Sample type
+    class SampleMetadataUpdater
+      def initialize(sample_type)
+        @sample_type = sample_type
+      end
+
+      def update_metadata
+        samples = @sample_type.samples
+        # Should raise an exception if the user does not have permission to update all the samples in this sample type.
+        raise SampleMetadataUpdateException('Invalid permissions! You need editing permission to all samples in this sample type.') unless samples.all?(&:can_edit?)
+
+        sample_attributes = @sample_type.sample_attributes.map(&:title)
+
+        samples.each do |sample|
+          metadata = JSON.parse(sample.json_metadata)
+          missing_attributes = sample_attributes - metadata.keys
+          missing_attributes.map do |attr|
+            metadata[attr] = nil
+          end
+
+          removed_attributes = metadata.keys - sample_attributes
+          removed_attributes.map do |attr|
+            metadata.delete(attr)
+          end
+          # For now permission are skipped and all samples will be updated
+          Sample.find(sample.id).update_column(json_metadata, metadata.to_json)
+        end
+      end
+    end
+  end
+end
