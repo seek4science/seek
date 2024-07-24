133718f73bce70370a4a05f308f58ff0633ed8dc
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-07-24 10:33:02 +0200

    Add Sample Metadata upater job

---
+++ app/jobs/update_sample_metadata_job.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class UpdateSampleMetadataJob < ApplicationJob
+  queue_with_priority 1
+  queue_as :QueueNames::SAMPLES
+
+  def perform(sample_type)
+    Seek::Samples::SampleMetadataUpdater.new(sample_type).update_metadata
+  end
+end
