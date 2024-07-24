8ce597beee68bab6cf59b874ff2c56d91af0dc07
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2024-07-24 13:05:12 +0200

    typo

--- app/jobs/update_sample_metadata_job.rb
+++ app/jobs/update_sample_metadata_job.rb
@@ -2,7 +2,7 @@
 
 class UpdateSampleMetadataJob < ApplicationJob
   queue_with_priority 1
-  queue_as :QueueNames::SAMPLES
+  queue_as QueueNames::SAMPLES
 
   def perform(sample_type)
     Seek::Samples::SampleMetadataUpdater.new(sample_type).update_metadata
