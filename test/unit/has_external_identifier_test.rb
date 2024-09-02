aaa4a0043685166dc11bce4fc52918de7f64f0e2
Stuart Owen <stuart.owen@manchester.ac.uk>
2024-09-02 15:23:35 +0100

    add concern that validates identifier and adds search indexing #1984
    identifier needs to be unique within the scope of all associated projects

--- test/unit/has_external_identifier_test.rb
+++ test/unit/has_external_identifier_test.rb
@@ -4,10 +4,9 @@ class HasExternalIdentifierTest < ActiveSupport::TestCase
   include AuthenticatedTestHelper
 
   test 'field present on relevant types' do
-    types = [Investigation, Study, Assay, ObservationUnit, DataFile, Model, Sop, Presentation, Workflow, Document, Sample, Strain]
     contributor = FactoryBot.create(:person)
 
-    types.each do |type|
+    affected_types.each do |type|
       factory_name = type.name.underscore.to_sym
       obj = FactoryBot.create(factory_name, contributor: contributor)
       assert obj.respond_to?(:external_identifier)
@@ -19,5 +18,32 @@ class HasExternalIdentifierTest < ActiveSupport::TestCase
         assert_equal 'some identifier', obj.external_identifier
       end
     end
+  end
+
+  test 'validate uniq in scope of projects' do
+    contributor1 = FactoryBot.create(:person)
+    contributor2 = FactoryBot.create(:person)
+    project2 = contributor2.projects.first
+
+    affected_types.each do |type|
+      factory_name = type.name.underscore.to_sym
+      obj = FactoryBot.create(factory_name, contributor: contributor1, external_identifier: 'some id')
+      assert obj.valid?, "#{type.name} should be valid"
+      obj2 = FactoryBot.build(factory_name, contributor: contributor1, external_identifier: 'some id')
+      assert_equal obj.projects, obj2.projects, "#{type.name} projects should match"
+      refute obj2.valid?, "#{type.name} should not be valid"
+      obj2.external_identifier = 'some other id'
+      assert obj2.valid?, "#{type.name} should be valid"
+      # same id, different project. Skip Study and Assay as they inherit project from Investigation
+      unless [Study, Assay].include?(type)
+        obj3 = FactoryBot.build(factory_name, contributor: contributor2, external_identifier: 'some id', projects: [project2])
+        assert obj3.valid?, "#{type.name} should be valid"
+      end
+    end
+  end
+
+  private
+  def affected_types
+    [Investigation, Study, Assay, ObservationUnit, DataFile, Model, Sop, Presentation, Workflow, Document, Sample, Strain]
   end
 end
