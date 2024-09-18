37935900697de7a17e1b3bcf88c1dedd5d2c02d3
whomingbird <xiaoming.hu@h-its.org>
2024-09-18 09:30:27 +0200

    small javascript fix

--- app/assets/javascripts/extended_metadata_type.js
+++ app/assets/javascripts/extended_metadata_type.js
@@ -10,16 +10,17 @@ var ExtendedMetadataType = {
         noticeFlashDiv.show();
     },
 
-    addNewMarker: function(id, type) {
+    addNewMarker: function(new_id, type) {
         var isNested = (type === "ExtendedMetadata");
 
+        alert(new_id)
         $j("#nested-tab, #nested-tab").parent().toggleClass("active", isNested);
         $j("#nested-metadata-table").toggleClass("active in", isNested);
         $j("#top-level-tab, #top-level-tab").parent().toggleClass("active", !isNested);
         $j("#top-level-metadata-table").toggleClass("active in", !isNested);
 
         var table = isNested ? $j("#nested-metadata-table") : $j("#top-level-metadata-table");
-        table.find("tbody tr:first td:first").append(' <sup style="color:red; font-weight:bold; font-size: 12px">new</sup>');
+        table.find("tbody td#" + new_id).append(' <sup style="color:red; font-weight:bold; font-size: 12px">new</sup>');
     },
 
     initializeNoticeFlash: function() {
