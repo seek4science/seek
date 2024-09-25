742f155ff885743467b54a20811c021d8a4c9b3e
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-09-25 12:42:38 +0100

    Ensure all resource types are shown in programmes dashboard. Fixes #1995

--- test/unit/dashboard_stats_test.rb
+++ test/unit/dashboard_stats_test.rb
@@ -47,4 +47,53 @@ class DashboardStatsTest < ActiveSupport::TestCase
 
     assert Rails.cache.exist?(another_project_key)
   end
+
+  test 'contribution stats' do
+    Project.delete_all
+    Programme.delete_all
+
+    programme = FactoryBot.create(:min_programme)
+    project = FactoryBot.create(:project, programme: programme)
+    person = FactoryBot.create(:person, project: project)
+    programme.reload # To refresh the `projects` association
+
+    assets = [:data_file, :sop, :model, :publication, :presentation, :document, :workflow, :collection].map do |type|
+      FactoryBot.create(type, projects: [project], contributor: person)
+    end
+
+    investigation = FactoryBot.create(:investigation, projects: [project], contributor: person)
+    study = FactoryBot.create(:study, investigation: investigation, contributor: person)
+    assay = FactoryBot.create(:assay, study: study, contributor: person)
+
+    assets + [investigation, study, assay]
+
+    assets.each do |asset|
+      FactoryBot.create(:activity_log, action: 'create', activity_loggable: asset, culprit: person)
+    end
+
+    instance_stats = Seek::Stats::DashboardStats.new
+    project_stats = Seek::Stats::ProjectDashboardStats.new(project)
+    programme_stats = Seek::Stats::ProgrammeDashboardStats.new(programme)
+
+    opts = [2.days.ago, 2.days.from_now, 'month']
+    types = [DataFile, Sop, Model, Publication, Presentation, Document, Workflow, Collection, Investigation, Study, Assay]
+
+    c = instance_stats.contributions(*opts)
+    (types + [Project, Programme]).each do |t|
+      assert c[:datasets].key?(t), "#{t.name} missing from instance contributions stats"
+      assert_equal [1], c[:datasets][t], "#{t.name} unexpected number in instance contributions stats"
+    end
+
+    c = programme_stats.contributions(*opts)
+    (types + [Project]).each do |t|
+      assert c[:datasets].key?(t), "#{t.name} missing from programme contributions stats"
+      assert_equal [1], c[:datasets][t], "#{t.name} unexpected number in instance contributions stats"
+    end
+
+    c = project_stats.contributions(*opts)
+    types.each do |t|
+      assert c[:datasets].key?(t), "#{t.name} missing from project contributions stats"
+      assert_equal [1], c[:datasets][t], "#{t.name} unexpected number in instance contributions stats"
+    end
+  end
 end
