b9c42bfa7bedd80de5095af769bda855f4263ad9
whomingbird <xiaoming.hu@h-its.org>
2024-10-05 00:09:42 +0200

    add unit tests for emt_extractor add functional tests for extended_metadata_types_controller

--- test/functional/extended_metadata_types_controller_test.rb
+++ test/functional/extended_metadata_types_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ExtendedMetadataTypesControllerTest < ActionController::TestCase
 
   include AuthenticatedTestHelper
+  fixtures :sample_attribute_types
 
   test 'get form fields' do
     cmt = FactoryBot.create(:simple_investigation_extended_metadata_type)
@@ -52,8 +53,14 @@ class ExtendedMetadataTypesControllerTest < ActionController::TestCase
     get :administer
     assert_response :success
     refute flash[:error]
-    assert_select 'table tbody tr:not(.emt-partition-title)', count: 1
+    assert_select 'table tbody tr', count: 4
     assert_select 'table tbody tr:not(.emt-partition-title) td', text: emt.title
+    assert_select "a[href=?]", new_extended_metadata_type_path, text: 'Create Extended Metadata Type'
+    assert_select 'li.nav-item > a', count: 4
+    assert_select 'li.nav-item > a', text: 'Top Level'
+    assert_select 'li.nav-item > a', text: 'Nested Level'
+    assert_select 'li.nav-item > a', text: 'Controlled Vocabs'
+    assert_select 'li.nav-item > a', text: 'Extended Metadata Attribute Types'
   end
 
   test 'administer table - disabled as text-secondary' do
@@ -81,6 +88,91 @@ class ExtendedMetadataTypesControllerTest < ActionController::TestCase
     get :administer
     assert_redirected_to :root
     assert flash[:error]
+    assert_equal 'Admin rights required', flash[:error]
+  end
+
+  test 'can new as admin' do
+    person = FactoryBot.create(:admin)
+    login_as(person)
+    get :new
+    assert_response :success
+  end
+
+  test 'cannot new as non-admin' do
+    person = FactoryBot.create(:person)
+    login_as(person)
+    get :new
+    assert_redirected_to :root
+    assert_equal 'Admin rights required', flash[:error]
+  end
+
+  test 'should create successfully' do
+    person = FactoryBot.create(:admin)
+    login_as(person)
+
+    file = fixture_file_upload('extended_metadata_type/valid_simple_emt.json', 'application/json')
+
+    assert_difference('ExtendedMetadataType.count') do
+      post :create, params: { emt_json_file: file }
+    end
+
+    assert_redirected_to administer_extended_metadata_types_path
+    assert_equal 'Extended metadata type was successfully created.', flash[:notice]
+  end
+
+  test 'upload file with no file selected' do
+    person = FactoryBot.create(:admin)
+    login_as(person)
+
+    post :create, params: { emt_json_file: nil }
+
+    assert_redirected_to new_extended_metadata_type_path
+    assert_equal 'Please select a file to upload!', flash[:error]
+  end
+
+  test 'can display extended metadata types' do
+    person = FactoryBot.create(:admin)
+    login_as(person)
+
+    FactoryBot.create(:study_extended_metadata_type_with_cv_and_cv_list_type)
+    FactoryBot.create(:simple_investigation_extended_metadata_type)
+    FactoryBot.create(:role_extended_metadata_type )
+
+    get :administer
+    assert_response :success
+
+    abc = assigns(:administer)
+
+
+    assert_select 'ul.nav-tabs' do
+      assert_select 'li a[href=?]', '#top-level-metadata-table', text: 'Top Level'
+      assert_select 'li a[href=?]', '#nested-metadata-table', text: 'Nested Level'
+      assert_select 'li a[href=?]', '#sample-controlled-vocabs', text: 'Controlled Vocabs'
+      assert_select 'li a[href=?]', '#sample-attribute-types', text: 'Extended Metadata Attribute Types'
+    end
+
+
+    assert_select 'div#top-level-metadata-table tbody tr', count: 3
+    assert_select 'div#nested-metadata-table tbody tr', count: 1
+    assert_select 'div#sample-controlled-vocabs tbody tr', count: 2
+
+    assert_select 'div#nested-metadata-table tbody tr' do
+      assert_select 'td', text: 'role_name'
+      assert_select 'td', text: 'ExtendedMetadata'
+      assert_select 'td', text: '1'
+    end
+
+    assert_select 'div#top-level-metadata-table tbody tr' do
+      assert_select 'td', text: 'simple investigation extended metadata type'
+      assert_select 'td', text: 'Investigation'
+      assert_select 'td', text: '0'
+    end
+
+    assert_select 'div#sample-controlled-vocabs tbody tr' do
+      assert_select 'td', text: SampleControlledVocab.first.title
+      assert_select 'td', text: SampleControlledVocab.last.title
+    end
+
   end
 
 end
---
+++ test/unit/extended_metadatas/emt_extractor_test.rb
@@ -0,0 +1,152 @@
+require 'test_helper'
+
+class EmtExtractorTest < ActiveSupport::TestCase
+
+  fixtures :sample_attribute_types
+
+  test 'creates extended metadata type with simple attributes from valid json file' do
+
+    emt_file = fixture_file_upload('extended_metadata_type/valid_simple_emt.json', 'application/json')
+
+    assert_difference('ExtendedMetadataType.count') do
+      Seek::ExtendedMetadataType::EMTExtractor.extract_extended_metadata_type(emt_file)
+    end
+
+    emt = ExtendedMetadataType.find_by(title: 'person')
+    assert_not_nil emt
+    assert_equal 'ExtendedMetadata', emt.supported_type
+    assert emt.enabled
+    assert_equal 2, emt.extended_metadata_attributes.count
+
+    attr1 = emt.extended_metadata_attributes.find_by(title: 'first_name')
+    assert_not_nil attr1
+    assert_equal 'First name', attr1.label
+    assert_equal SampleAttributeType.find_by(title: 'String'), attr1.sample_attribute_type
+    assert_equal true, attr1.required
+
+    attr2 = emt.extended_metadata_attributes.find_by(title: 'last_name')
+    assert_not_nil attr2
+    assert_equal 'last name', attr2.label
+    # add for description
+    assert_nil attr2.description
+    assert_equal SampleAttributeType.find_by(title: 'String'), attr2.sample_attribute_type
+    assert_equal false, attr2.required
+
+  end
+
+
+
+  test 'creates extended metadata type with linked attributes from valid json file' do
+
+    person_emt = FactoryBot.create(:role_name_extended_metadata_type)
+
+    uploaded_file = update_id('valid_emt_with_linked_emt.json', person_emt, 'PERSON_EMT_ID')
+
+    assert_difference('ExtendedMetadataType.count') do
+      Seek::ExtendedMetadataType::EMTExtractor.extract_extended_metadata_type(uploaded_file)
+    end
+
+    emt = ::ExtendedMetadataType.find_by(title: 'family')
+    assert_not_nil emt
+    assert_equal 'Investigation', emt.supported_type
+    assert emt.enabled
+    assert_equal 3, emt.extended_metadata_attributes.count
+
+    dad_attr = emt.extended_metadata_attributes.find_by(title: 'dad')
+    assert_not_nil dad_attr
+    assert_equal 'Dad', dad_attr.label
+    assert_nil dad_attr.description
+    assert_equal SampleAttributeType.find_by(title: 'Linked Extended Metadata'), dad_attr.sample_attribute_type
+    refute dad_attr.required
+
+    mom_attr = emt.extended_metadata_attributes.find_by(title: 'mom')
+    assert_not_nil mom_attr
+    assert_equal 'Mom', mom_attr.label
+    assert_nil mom_attr.description
+    assert_equal SampleAttributeType.find_by(title: 'Linked Extended Metadata'), mom_attr.sample_attribute_type
+    refute mom_attr.required
+
+    child_attr = emt.extended_metadata_attributes.find_by(title: 'child')
+    assert_not_nil child_attr
+    assert_equal 'child', child_attr.label
+    assert_nil child_attr.description
+    assert_equal SampleAttributeType.find_by(title: 'Linked Extended Metadata (multiple)'), child_attr.sample_attribute_type
+    refute child_attr.required
+
+  end
+
+  test 'creates extended metadata type with controlled vocab attributes from valid json file' do
+
+    topic_cv = FactoryBot.create(:topics_controlled_vocab)
+
+    uploaded_file = update_id('valid_emt_with_cv_with_ontologies.json', topic_cv, 'CV_TOPICS_ID')
+
+    assert_difference('ExtendedMetadataType.count') do
+      Seek::ExtendedMetadataType::EMTExtractor.extract_extended_metadata_type(uploaded_file)
+    end
+
+
+    emt = ::ExtendedMetadataType.find_by(title: 'An example with attributes associated with ontology terms')
+    assert_not_nil emt
+    assert_equal 'Study', emt.supported_type
+    assert emt.enabled
+    assert_equal 1, emt.extended_metadata_attributes.count
+
+    topics_attr = emt.extended_metadata_attributes.find_by(title: 'Topics')
+    assert_not_nil topics_attr
+    assert_equal 'Topics', topics_attr.label
+    assert_equal "Topics, used for annotating. Describes the domain, field of interest, of study, application, work, data, or technology. Initially seeded from the EDAM ontology.", topics_attr.description
+    assert_equal SampleAttributeType.find_by(title: 'Controlled Vocabulary'), topics_attr.sample_attribute_type
+    assert topics_attr.required
+
+    assert_equal topic_cv, topics_attr.sample_controlled_vocab
+    assert_equal 4, topics_attr.sample_controlled_vocab.sample_controlled_vocab_terms.count
+
+
+  end
+
+
+  test 'handles invalid json file with wrong attr type' do
+    invalid_emt_file = fixture_file_upload('extended_metadata_type/invalid_emt_with_wrong_type.json')
+
+    assert_no_difference('ExtendedMetadataType.count') do
+
+      error = assert_raises(StandardError) do
+        Seek::ExtendedMetadataType::EMTExtractor.extract_extended_metadata_type(invalid_emt_file)
+      end
+
+      assert_match /Invalid JSON file: The property '#\/attributes\/0' of type object did not match any of the required schemas/, error.message
+    end
+  end
+
+  test 'handles invalid json file with wrong id' do
+    invalid_emt_file = fixture_file_upload('extended_metadata_type/invalid_emt_with_wrong_id.json')
+
+    assert_no_difference('ExtendedMetadataType.count') do
+
+      error = assert_raises(StandardError) do
+        Seek::ExtendedMetadataType::EMTExtractor.extract_extended_metadata_type(invalid_emt_file)
+      end
+
+      assert_match /Couldn't find SampleControlledVocab with 'id'=-1/, error.message
+    end
+  end
+
+
+  private
+  def update_id(emt_file_name, person_emt, replaced)
+    emt_file = fixture_file_upload("extended_metadata_type/#{emt_file_name}", 'application/json')
+    updated_content = emt_file.read.gsub(replaced, person_emt.id.to_s)
+    updated_emt_file = StringIO.new(updated_content)
+
+    ActionDispatch::Http::UploadedFile.new(
+      tempfile: updated_emt_file,
+      filename: emt_file_name,
+      type: 'application/json'
+    )
+  end
+
+
+end
+
+
