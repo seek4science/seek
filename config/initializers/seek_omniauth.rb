2bc90e035927cd99f238d01fb1ad91efd26b658d
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-10-16 10:27:26 +0100

    Don't try and access settings table until app is initialized

--- config/initializers/seek_omniauth.rb
+++ config/initializers/seek_omniauth.rb
@@ -1,23 +1,25 @@
 # use the rails logger for loggin OmniAuth; otherwise it will use std::out
 OmniAuth.config.logger = Rails.logger
 callme = -> {}
-if Seek::Config.omniauth_enabled
-  callme = -> {
-    # To add more providers, see the `omniauth_providers` definition in: `lib/seek/config.rb`
-    begin
-      providers = Seek::Config.omniauth_providers
-    rescue Settings::DecryptionError
-      providers = []
-    end
-
-    providers.each do |key, options|
-      if options.is_a?(Array)
-        provider key, *options
-      else
-        provider key, options
+Rails.configuration.after_initialize do
+  if Seek::Config.omniauth_enabled
+    callme = -> {
+      # To add more providers, see the `omniauth_providers` definition in: `lib/seek/config.rb`
+      begin
+        providers = Seek::Config.omniauth_providers
+      rescue Settings::DecryptionError
+        providers = []
       end
-    end
-  }
+
+      providers.each do |key, options|
+        if options.is_a?(Array)
+          provider key, *options
+        else
+          provider key, options
+        end
+      end
+    }
+  end
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
