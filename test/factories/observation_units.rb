cbf647d8c2954453ba106ac14a82bb0fcf294b04
Stuart Owen <stuart.owen@manchester.ac.uk>
2024-10-25 13:59:39 +0100

    change factory back #2020

--- test/factories/observation_units.rb
+++ test/factories/observation_units.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     policy { FactoryBot.create(:public_policy) }
     association :contributor, factory: :person, strategy: :create
     after(:build) do |a|
-      a.study = FactoryBot.create(:study, contributor: a.contributor) if a.study.nil?
+      a.study ||= FactoryBot.create(:study, contributor: a.contributor)
     end
   end
 
