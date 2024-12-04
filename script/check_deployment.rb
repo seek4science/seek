4806f82ce63762320329827265b0481e7bcc9f3e
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-12-04 18:27:40 +0000

    Fix copy & paste

--- script/check_deployment.rb
+++ script/check_deployment.rb
@@ -1,9 +1,9 @@
-output = `curl --verbose --silent http://localhost:3000/ 2>&1`
+output = `curl --verbose --silent http://localhost:3000/statistics/application_status 2>&1`
 if $?.success? && output.include?('FAIRDOM-SEEK is running | search is enabled | 7 delayed jobs running')
   exit 0
 else
   puts "::group::Docker logs"
-  puts `docker compose --file docker-compose-prod.yml logs`
+  puts `docker compose --file docker-compose.yml --file docker-compose.build.yml logs`
   puts "::endgroup::"
   puts "::group::curl output"
   puts output
