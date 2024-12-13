c41306061998fb01a39699269410fef0330d3bfd
Xiaoming Hu <xiaoming.hu@h-its.org>
2024-12-13 10:51:56 +0100

    display ROR id as a clickable link

--- app/helpers/institutions_helper.rb
+++ app/helpers/institutions_helper.rb
@@ -2,4 +2,8 @@ module InstitutionsHelper
   def can_create_institutions?
     Institution.can_create?
   end
+
+  def ror_link(ror_id)
+    text_or_not_specified("https://ror.org/#{ror_id}", :external_link => true)
+  end
 end
