a0ad570e44b75eb931d6c233755a202b723ec874
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-12-13 16:04:01 +0000

    Fix AMS deprecation warning

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -1,4 +1,4 @@
-ActiveModelSerializers.config.adapter = ActiveModel::Serializer::Adapter::JsonApi
+ActiveModelSerializers.config.adapter = ActiveModelSerializers::Adapter::JsonApi
 ActiveModelSerializers.config.key_transform = :unaltered
 ActiveModelSerializers.config.jsonapi_include_toplevel_object = true
 ActiveModelSerializers.config.api_version = '0.3'
