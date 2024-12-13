8b5105b3aea0fa9cbe3868c39767c4c6c2b390c1
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-12-13 16:42:05 +0000

    Update omniauth. Fix omniauth not actually being loaded

--- config/initializers/seek_omniauth.rb
+++ config/initializers/seek_omniauth.rb
@@ -1,27 +1,20 @@
 # use the rails logger for loggin OmniAuth; otherwise it will use std::out
 OmniAuth.config.logger = Rails.logger
-callme = -> {}
-Rails.configuration.after_initialize do
-  if Seek::Config.omniauth_enabled
-    callme = -> {
-      # To add more providers, see the `omniauth_providers` definition in: `lib/seek/config.rb`
-      begin
-        providers = Seek::Config.omniauth_providers
-      rescue Settings::DecryptionError
-        providers = []
-      end
-
-      providers.each do |key, options|
-        if options.is_a?(Array)
-          provider key, *options
-        else
-          provider key, options
-        end
-      end
-    }
-  end
-end
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  callme
+  if Seek::Config.omniauth_enabled
+    # To add more providers, see the `omniauth_providers` definition in: `lib/seek/config.rb`
+    begin
+      providers = Seek::Config.omniauth_providers
+    rescue Settings::DecryptionError
+      providers = []
+    end
+
+    providers.each do |key, options|
+      if options.is_a?(Array)
+        provider key, *options
+      else
+        provider key, options
+      end
+    end
+  end
 end
