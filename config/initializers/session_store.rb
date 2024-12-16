f7d4cd425692e3cf049182ca195c8cebc8c8ff81
Finn Bacall <finn.bacall@manchester.ac.uk>
2024-12-16 14:28:06 +0000

    Fix session store config not being applied and using defaults instead

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,8 +3,6 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Rails.configuration.after_initialize do
-  SEEK::Application.config.session_store(:active_record_store,
-                                         key: '_seek_session',
-                                         expire_after: Seek::Config.session_store_timeout)
-end
+# FIXME: Use Seek::Config.session_store_timeout somehow
+SEEK::Application.config.session_store(:active_record_store, key: '_seek_session',
+                                       expire_after: 30.minutes)
