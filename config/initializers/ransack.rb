b869a2362602635fb4fb6dcb1ae1f4ea746f81bd
Stuart Owen <stuart.owen@manchester.ac.uk>
2024-12-17 10:42:00 +0000

    updated ransack and removed the hack to remove the search method

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -4,7 +4,3 @@ Ransack.configure do |config|
   # Default key name is :q
   config.search_key = :query
 end
-
-# prevent conflict of search method in gem sunspot_solr
-# https://github.com/activerecord-hackery/ransack#ransack-search-method
-Ransack::Adapters::ActiveRecord::Base.class_eval('remove_method :search')
