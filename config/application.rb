45c24a85481d27c2712503527c13e73626d1ea8c
Stuart <stuart.owen@manchester.ac.uk>
2025-01-10 14:28:28 +0000

    config.load_defaults 7.2

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ end
 module SEEK
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
     config.active_record.default_column_serializer = YAML
     # Force all environments to use the same logger level
     # Configuration for the application, engines, and railties goes here.
