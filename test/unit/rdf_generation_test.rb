0116ac9613d420d7b48f074ec94f6f60b8646920
Stuart <stuart.owen@manchester.ac.uk>
2025-01-20 14:19:28 +0000

    change the rdf writer to use TTL #2116
    worked around an issue with the seek_id, and fixed a test case for data file. Other tests will need updating to read as ttl and rdfxml and load into a graph

--- test/unit/rdf_generation_test.rb
+++ test/unit/rdf_generation_test.rb
@@ -131,10 +131,13 @@ class RDFGenerationTest < ActiveSupport::TestCase
     rdf = df.to_rdf
     assert_not_nil rdf
     # just checks it is valid rdf/xml and contains some statements for now
-    RDF::Reader.for(:rdfxml).new(rdf) do |reader|
-      assert reader.statements.count > 0
-      assert_equal RDF::URI.new("http://localhost:3000/data_files/#{df.id}"), reader.statements.first.subject
+    graph = RDF::Graph.new do |graph|
+      RDF::Reader.for(:ttl).new(rdf) {|reader| graph << reader}
     end
+
+    assert graph.statements.count > 0
+    assert_equal RDF::URI.new("http://localhost:3000/data_files/#{df.id}"), graph.statements.first.subject
+
   end
 
   test 'non spreadsheet datafile to_rdf' do
