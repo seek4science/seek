2ec4fecd28594d86f3d6f6e537a09dc9ccd0089d
Stuart <stuart.owen@manchester.ac.uk>
2025-01-28 15:19:55 +0000

    refute instead of assert ! #2124

--- test/unit/permissions/publishing_permissions_test.rb
+++ test/unit/permissions/publishing_permissions_test.rb
@@ -5,7 +5,7 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
   test 'is_rejected?' do
     items_for_publishing_tests.each do |item|
       item = FactoryBot.create(:data_file)
-      assert !item.is_rejected?
+      refute item.is_rejected?
 
       ResourcePublishLog.add_log(ResourcePublishLog::REJECTED, item)
       assert item.is_rejected?
@@ -17,7 +17,7 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
 
     User.with_current_user person.user do
       items_for_publishing_tests(person).each do |item|
-        assert !item.is_rejected?
+        refute item.is_rejected?
 
         ResourcePublishLog.add_log(ResourcePublishLog::REJECTED, item)
         assert item.is_rejected?
@@ -35,8 +35,8 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
 
   test 'is_waiting_approval?' do
     items_for_publishing_tests.each do |item|
-      assert !item.is_waiting_approval?
-      assert !item.is_waiting_approval?(User.current_user)
+      refute item.is_waiting_approval?
+      refute item.is_waiting_approval?(User.current_user)
 
       log = ResourcePublishLog.add_log(ResourcePublishLog::WAITING_FOR_APPROVAL, item)
       assert item.is_waiting_approval?
@@ -46,7 +46,7 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
 
   test 'gatekeeper_required?' do
     items_for_publishing_tests.each do |item|
-      assert !item.gatekeeper_required?
+      refute item.gatekeeper_required?
 
       gatekeeper = FactoryBot.create(:asset_gatekeeper)
       refute item.gatekeeper_required?
@@ -73,7 +73,7 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
       public_obs_unit = FactoryBot.create(:observation_unit, policy: FactoryBot.create(:public_policy, access_type: Policy::VISIBLE))
 
       assert public_sop.is_published?
-      assert !not_public_model.is_published?
+      refute not_public_model.is_published?
       assert public_datafile.is_published?
       assert public_assay.is_published?
       assert public_obs_unit.is_published?
@@ -84,11 +84,11 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
     assert FactoryBot.create(:sop).is_in_isa_publishable?
     assert FactoryBot.create(:model).is_in_isa_publishable?
     assert FactoryBot.create(:data_file).is_in_isa_publishable?
-    assert !FactoryBot.create(:assay).is_in_isa_publishable?
-    assert !FactoryBot.create(:investigation).is_in_isa_publishable?
-    assert !FactoryBot.create(:study).is_in_isa_publishable?
-    assert !FactoryBot.create(:event).is_in_isa_publishable?
-    assert !FactoryBot.create(:publication).is_in_isa_publishable?
+    refute FactoryBot.create(:assay).is_in_isa_publishable?
+    refute FactoryBot.create(:investigation).is_in_isa_publishable?
+    refute FactoryBot.create(:study).is_in_isa_publishable?
+    refute FactoryBot.create(:event).is_in_isa_publishable?
+    refute FactoryBot.create(:publication).is_in_isa_publishable?
     refute FactoryBot.create(:observation_unit).is_in_isa_publishable?
   end
 
@@ -97,8 +97,8 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
     User.with_current_user user do
       items_for_publishing_tests(user.person).each do |item|
         assert item.can_manage?, 'This item must be manageable for the test to succeed'
-        assert !item.is_published?, 'This item must be not published for the test to succeed'
-        assert !item.gatekeeper_required?, 'This item must not require gatekeeper for the test to succeed'
+        refute item.is_published?, 'This item must be not published for the test to succeed'
+        refute item.gatekeeper_required?, 'This item must not require gatekeeper for the test to succeed'
 
         assert item.can_publish?, 'This item should be publishable'
       end
@@ -126,11 +126,11 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
       items_for_publishing_tests.each do |item|
         item.policy = FactoryBot.create(:all_sysmo_viewable_policy)
         disable_authorization_checks { item.save! }
-        assert !item.can_manage?, 'This item must be manageable for the test to succeed'
-        assert !item.is_published?, 'This item must be not published for the test to be meaningful'
-        assert !item.gatekeeper_required?, 'This item must require gatekeeper for the test to be meaningful'
+        refute item.can_manage?, 'This item must be manageable for the test to succeed'
+        refute item.is_published?, 'This item must be not published for the test to be meaningful'
+        refute item.gatekeeper_required?, 'This item must require gatekeeper for the test to be meaningful'
 
-        assert !item.can_publish?, 'This item should not be publishable'
+        refute item.can_publish?, 'This item should not be publishable'
       end
     end
   end
@@ -143,9 +143,9 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
         disable_authorization_checks { item.save! }
         assert item.can_manage?, 'This item must be manageable for the test to be meaningful'
         assert item.is_published?, 'This item must be not published for the test to succeed'
-        assert !item.gatekeeper_required?, 'This item must require gatekeeper for the test to be meaningful'
+        refute item.gatekeeper_required?, 'This item must require gatekeeper for the test to be meaningful'
 
-        assert !item.can_publish?, 'This item should not be publishable'
+        refute item.can_publish?, 'This item should not be publishable'
       end
 
     end
@@ -206,7 +206,7 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
 
       User.with_current_user gatekeeper.user do
         assert gatekeeper.is_asset_gatekeeper_of?(item), 'The gatekeeper must be the gatekeeper of the item for the test to succeed'
-        assert !item.can_manage?, 'The item must be manageable for the test to be meaningful'
+        refute item.can_manage?, 'The item must be manageable for the test to be meaningful'
         assert item.is_waiting_approval?, 'The item must be waiting for approval for the test to succeed'
 
         assert item.can_publish?, 'This item should be publishable'
@@ -220,10 +220,10 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
       item.resource_publish_logs.create(publish_state: ResourcePublishLog::WAITING_FOR_APPROVAL, user: item.contributor.user)
 
       User.with_current_user gatekeeper.user do
-        assert !gatekeeper.is_asset_gatekeeper_of?(item), 'The gatekeeper must not be the gatekeeper of the item for the test to be succeed'
+        refute gatekeeper.is_asset_gatekeeper_of?(item), 'The gatekeeper must not be the gatekeeper of the item for the test to be succeed'
         assert item.is_waiting_approval?, 'The item must be waiting for approval for the test to be meaningful'
 
-        assert !item.can_publish?, 'This item should not be publishable'
+        refute item.can_publish?, 'This item should not be publishable'
       end
     end
   end
@@ -254,7 +254,7 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
         assert gatekeeper.is_asset_gatekeeper_of?(item), 'The gatekeeper must be the gatekeeper of the item for the test to be meaningful'
         assert item.can_manage?, 'The item must not be manageable for the test to succeed'
 
-        assert !item.can_publish?, 'This item should not be publishable'
+        refute item.can_publish?, 'This item should not be publishable'
       end
     end
   end
@@ -267,8 +267,8 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
       assert df.publish!
     end
 
-    assert !df.can_publish?
-    assert !df.publish!
+    refute df.can_publish?
+    refute df.publish!
   end
 
   test 'publish! is performed when no gatekeeper is required' do
@@ -276,8 +276,8 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
     User.with_current_user user do
       items_for_publishing_tests(user.person).each do |item|
         assert item.can_publish?, 'The item must be publishable'
-        assert !item.gatekeeper_required?, 'The gatekeeper must not be required for the test to succeed'
-        assert !item.is_published?, 'This item must not be published yet for the test to be meaningful'
+        refute item.gatekeeper_required?, 'The gatekeeper must not be required for the test to succeed'
+        refute item.is_published?, 'This item must not be published yet for the test to be meaningful'
 
         assert item.publish!
         assert item.is_published?, 'This item should be published now'
@@ -337,7 +337,7 @@ class PublishingPermissionsTest < ActiveSupport::TestCase
       assert_equal Policy::NO_ACCESS, item.policy.access_type
 
       User.with_current_user user do
-        assert !item.can_publish?
+        refute item.can_publish?
       end
 
       disable_authorization_checks do
