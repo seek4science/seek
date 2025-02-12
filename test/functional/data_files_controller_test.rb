5fb9bfb4a2aaa1ac4c156b72b652c30e2e9f0d50
Stuart <stuart.owen@manchester.ac.uk>
2025-02-12 13:12:58 +0000

    fix to handle viewing a data file with no content blob #2139
    also test for edit

--- test/functional/data_files_controller_test.rb
+++ test/functional/data_files_controller_test.rb
@@ -4415,4 +4415,20 @@ class DataFilesControllerTest < ActionController::TestCase
       assert flash[:error].include?('disabled')
     end
   end
+
+  test 'view data file with nil content blob' do
+    df = FactoryBot.create(:data_file_with_no_content_blob)
+    assert_nil df.content_blob
+    login_as(df.contributor)
+    get :show, params: { id: df }
+    assert_response :success
+  end
+
+  test 'edit data file with nil content blob' do
+    df = FactoryBot.create(:data_file_with_no_content_blob)
+    assert_nil df.content_blob
+    login_as(df.contributor)
+    get :edit, params: { id: df }
+    assert_response :success
+  end
 end
