bdf464becde664b84d84626485d2f75d1e48c029
Finn Bacall <finn.bacall@manchester.ac.uk>
2025-02-20 13:37:02 +0000

    Revert `i18n-js` to version 3 to keep asset pipeline support

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,7 @@ module SEEK
     config.middleware.use Rack::Deflater,
                           include: %w(text/html application/xml application/json text/css application/javascript)
     config.middleware.use Rack::Attack
+    config.middleware.use I18n::JS::Middleware
     config.middleware.use Rack::SettingsCache
 
     config.exceptions_app = self.routes
