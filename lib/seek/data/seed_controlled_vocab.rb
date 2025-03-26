f9b5160ab577fef1c3df61974da6cc657adddc91
Stuart <stuart.owen@manchester.ac.uk>
2025-03-26 10:35:58 +0000

    refactor seed class, and also summarise the new terms

--- lib/seek/data/seed_controlled_vocab.rb
+++ lib/seek/data/seed_controlled_vocab.rb
@@ -2,39 +2,60 @@ module Seek
   module Data
     class SeedControlledVocab
       def self.seed(json_file, property)
-        json = File.read(File.join(Rails.root, 'config/default_data', json_file))
-        data = JSON.parse(json).with_indifferent_access
-        data[:key] = SampleControlledVocab::SystemVocabs.database_key_for_property(property)
+        data = read_data(json_file, property)
         name = property.to_s.humanize
 
         if (vocab = SampleControlledVocab::SystemVocabs.vocab_for_property(property))
-          puts "#{name} controlled vocabulary already exists, updating ..."
-          current_iris = vocab.sample_controlled_vocab_terms.collect(&:iri)
-          update_iris = data[:sample_controlled_vocab_terms_attributes].collect { |atr| atr[:iri] }
-          gone = current_iris - update_iris
-          new = update_iris - current_iris
-          puts "#{new.count} #{'term'.pluralize(new.count)} new terms"
-          puts "#{gone.count} #{'term'.pluralize(gone.count)} terms removed: "
-          gone.each do |iri|
-            term = vocab.sample_controlled_vocab_terms.where(iri: iri).first
-            puts "\t#{term.label}\t#{iri}"
-          end
-
-          disable_authorization_checks do
-            # vocab.update_from_json_dump(data)
-            # vocab.save!
-          end
+          update_existing(data, name, vocab)
         else
-          puts "Seeding #{name} controlled vocabulary ..."
-
-          vocab = SampleControlledVocab.new(data)
-
-          disable_authorization_checks do
-            vocab.save!
-          end
-
-          puts '... Done'
+          create_new(data, name)
         end
+        puts '... Done'
+      end
+
+      def self.create_new(data, name)
+        puts "Seeding #{name} controlled vocabulary ..."
+
+        vocab = SampleControlledVocab.new(data)
+
+        disable_authorization_checks do
+          vocab.save!
+        end
+      end
+
+      def self.update_existing(data, name, vocab)
+        puts "#{name} controlled vocabulary already exists, updating ..."
+        show_changes_summary(data, vocab)
+
+        disable_authorization_checks do
+          # vocab.update_from_json_dump(data)
+          # vocab.save!
+        end
+      end
+
+      def self.show_changes_summary(data, vocab)
+        current_iris = vocab.sample_controlled_vocab_terms.collect(&:iri)
+        terms = data[:sample_controlled_vocab_terms_attributes]
+        update_iris = terms.collect { |atr| atr[:iri] }
+        gone = current_iris - update_iris
+        new = update_iris - current_iris
+        puts "#{new.count} new #{'term'.pluralize(new.count)}#{':' if new.any?}"
+        new.each do |iri|
+          term = terms.detect { |t| iri == t[:iri] }
+          puts "\t#{term[:label]}\t#{iri}"
+        end
+        puts "#{gone.count} #{'term'.pluralize(gone.count)} removed#{':' if gone.any?}"
+        gone.each do |iri|
+          term = vocab.sample_controlled_vocab_terms.where(iri: iri).first
+          puts "\t#{term.label}\t#{iri}"
+        end
+      end
+
+      def self.read_data(json_file, property)
+        json = File.read(File.join(Rails.root, 'config/default_data', json_file))
+        data = JSON.parse(json).with_indifferent_access
+        data[:key] = SampleControlledVocab::SystemVocabs.database_key_for_property(property)
+        data
       end
     end
   end
