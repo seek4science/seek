0dd95050b2f2d2fd3022b79d7e326f76be21a3cd
Finn Bacall <finn.bacall@manchester.ac.uk>
2025-06-26 15:29:37 +0100

    Link disciplines to EDAM Topics where possible

--- db/seeds/018_discipline_vocab.seeds.rb
+++ db/seeds/018_discipline_vocab.seeds.rb
@@ -1,35 +1,32 @@
-DISCIPLINES = %(
-Chemistry
-Computer Science
-Earth Science
-Health Science
-Life Science
-Space Science
-Social Science
-Astronomy
-Physics
-Climate Science
-Humanities
-Library Science
-Cross-discipline
-Environmental Science
-Engineering
-Mathematics
-Neuroscience
-).split("\n").map(&:strip).compact_blank.freeze
+# Made up from a subset of EDAM Topics, plus some others
+DISCIPLINES = [
+  { label: 'Astronomy', iri: '' },
+  { label: 'Chemistry', iri: 'http://edamontology.org/topic_3314' },
+  { label: 'Climate Science', iri: '' },
+  { label: 'Computer Science', iri: 'http://edamontology.org/topic_3316' },
+  { label: 'Cross-discipline', iri: '' },
+  { label: 'Earth Science', iri: '' },
+  { label: 'Engineering', iri: '' },
+  { label: 'Environmental Science', iri: 'http://edamontology.org/topic_3855' },
+  { label: 'Humanities', iri: '' },
+  { label: 'Library Science', iri: '' },
+  { label: 'Life Science', iri: 'http://edamontology.org/topic_4019' },
+  { label: 'Machine Learning', iri: 'http://edamontology.org/topic_3474' },
+  { label: 'Mathematics', iri: 'http://edamontology.org/topic_3315' },
+  { label: 'Medicine', iri: 'http://edamontology.org/topic_3303' },
+  { label: 'Physics', iri: 'http://edamontology.org/topic_3318' },
+  { label: 'Social Science', iri: '' }
+].freeze
 
 data = {
   title: 'Scientific disciplines',
   description: 'A list of scientific disciplines.',
-  ols_root_term_uris: '',
-  source_ontology: '',
+  ols_root_term_uris: 'http://edamontology.org/topic_0003',
+  source_ontology: 'edam',
   sample_controlled_vocab_terms_attributes: DISCIPLINES.map do |d|
-    {
-      label: d,
-      iri: '',
-      parent_iri: ''
-    }
+    d[:parent_iri] = 'http://edamontology.org/topic_0003' if d[:iri].present?
+    d
   end
-}.with_indifferent_access
+}
 
-Seek::Data::SeedControlledVocab.seed(data, :disciplines)
+Seek::Data::SeedControlledVocab.seed(data.with_indifferent_access, :disciplines)
