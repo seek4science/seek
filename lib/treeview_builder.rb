2e1b5a30a2cd2238f229a4d659f4ef6397f7bc72
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2025-07-08 14:42:38 +0200

    Disable prettier

--- lib/treeview_builder.rb
+++ lib/treeview_builder.rb
@@ -1,66 +1,67 @@
 class TreeviewBuilder
-  include ImagesHelper
-  include ActionView::Helpers::SanitizeHelper
-  def initialize(project, folders)
-    @project = project
-    @folders = folders
-  end
+	include ImagesHelper
+	include ActionView::Helpers::SanitizeHelper
 
-  SP_ADVANCED_ENABLED = Seek::Config.isa_json_compliance_enabled
-  BOLD = { 'style': 'font-weight:bold' }.freeze
+	def initialize(project, folders)
+		@project = project
+		@folders = folders
+	end
 
-  def build_tree_data
-    study_items = []
-    investigation_items = []
+	SP_ADVANCED_ENABLED = Seek::Config.isa_json_compliance_enabled
+	BOLD = { 'style': 'font-weight:bold' }.freeze
 
-    @project.investigations.map do |investigation|
-      if investigation.is_isa_json_compliant?
-        investigation.studies.map do |study|
-          assay_stream_items = study.assay_streams.sort_by{ |stream| stream.position }.map do |assay_stream|
-            assay_items = assay_stream.child_assays.order(:position).map do |child_assay|
-              build_assay_item(child_assay)
-            end
-            build_assay_stream_item(assay_stream, assay_items)
-          end
+	def build_tree_data
+		study_items = []
+		investigation_items = []
 
-          study_items << build_study_item(study, assay_stream_items)
-        end
-      else
-        investigation.studies.map do |study|
-          assay_items = study.assays.map { |assay| build_assay_item(assay) }
-          study_items << build_study_item(study, assay_items)
-        end
-      end
-      investigation_items << build_investigation_item(investigation, study_items)
-      study_items = []
-    end
+		@project.investigations.map do |investigation|
+			if investigation.is_isa_json_compliant?
+				investigation.studies.map do |study|
+					assay_stream_items = study.assay_streams.sort_by { |stream| stream.position }.map do |assay_stream|
+						assay_items = assay_stream.child_assays.order(:position).map do |child_assay|
+							build_assay_item(child_assay)
+						end
+						build_assay_stream_item(assay_stream, assay_items)
+					end
 
-    # Documents folder
-    if Seek::Config.project_single_page_folders_enabled
-    @folders.reverse_each.map { |f| investigation_items.unshift(folder_node(f)) } if @folders.respond_to? :each
-    end
-    sanitize(JSON[[build_project_item(@project, investigation_items)]])
-  end
+					study_items << build_study_item(study, assay_stream_items)
+				end
+			else
+				investigation.studies.map do |study|
+					assay_items = study.assays.map { |assay| build_assay_item(assay) }
+					study_items << build_study_item(study, assay_items)
+				end
+			end
+			investigation_items << build_investigation_item(investigation, study_items)
+			study_items = []
+		end
 
-  private
+		# Documents folder
+		if Seek::Config.project_single_page_folders_enabled
+			@folders.reverse_each.map { |f| investigation_items.unshift(folder_node(f)) } if @folders.respond_to? :each
+		end
+		sanitize(JSON[[build_project_item(@project, investigation_items)]])
+	end
 
-  def folder_node(folder)
-    children = folder.children.map { |child| folder_node(child) }
-    obj = { id: "folder_#{folder.id}", text: folder.title, _type: 'folder', count: folder.count.to_s,
-            children: children, folder_id: folder.id, project_id: folder.project.id, resource: folder }
-    create_node(obj)
-  end
+	private
 
-  def create_node(obj)
-    can_view = obj[:resource].can_view?
-    unless can_view
-      obj[:text] = 'hidden item'
-      obj[:a_attr] = { 'style': 'font-style:italic;font-weight:bold;color:#ccc' }
-    end
+	def folder_node(folder)
+		children = folder.children.map { |child| folder_node(child) }
+		obj = { id: "folder_#{folder.id}", text: folder.title, _type: 'folder', count: folder.count.to_s,
+						children: children, folder_id: folder.id, project_id: folder.project.id, resource: folder }
+		create_node(obj)
+	end
+
+	def create_node(obj)
+		can_view = obj[:resource].can_view?
+		unless can_view
+			obj[:text] = 'hidden item'
+			obj[:a_attr] = { 'style': 'font-style:italic;font-weight:bold;color:#ccc' }
+		end
 
 		is_opened_node = %w[assay source_table study_samples_table assay_samples_table].none? obj[:_type]
-    node = { id: obj[:id], text: obj[:text], a_attr: obj[:a_attr], count: obj[:count],
-             data: { id: obj[:_id],
+		node = { id: obj[:id], text: obj[:text], a_attr: obj[:a_attr], count: obj[:count],
+						 data: { id: obj[:_id],
 										 type: obj[:_type] == 'assay_stream' ? 'assay' : obj[:_type],
 										 project_id: obj[:project_id],
 										 folder_id: obj[:folder_id] },
@@ -68,87 +69,87 @@ class TreeviewBuilder
 											separate: { label: obj[:label] } },
 						 children: obj[:children],
 						 icon: get_icon(obj[:resource]) }
-    deep_compact(node)
-  end
+		deep_compact(node)
+	end
 
-  def get_icon(resource)
-    ActionController::Base.helpers.asset_path(resource_avatar_path(resource) ||
-    icon_filename_for_key("#{resource.class.name.downcase}_avatar"))
-  end
+	def get_icon(resource)
+		ActionController::Base.helpers.asset_path(resource_avatar_path(resource) ||
+																							icon_filename_for_key("#{resource.class.name.downcase}_avatar"))
+	end
 
-  def deep_compact(hash)
-    hash.compact.transform_values do |value|
-      next value unless value.instance_of?(Hash)
+	def deep_compact(hash)
+		hash.compact.transform_values do |value|
+			next value unless value.instance_of?(Hash)
 
-      deep_compact(value)
-    end.reject { |_k, v| v.blank? }
-  end
+			deep_compact(value)
+		end.reject { |_k, v| v.blank? }
+	end
 
-  def create_sample_node(sample_type)
-    create_node({ text: 'samples', _type: 'sample', resource: Sample.new,	count: sample_type.samples.length,
-                  _id: sample_type.id })
-  end
+	def create_sample_node(sample_type)
+		create_node({ text: 'samples', _type: 'sample', resource: Sample.new, count: sample_type.samples.length,
+									_id: sample_type.id })
+	end
 
-  def isa_study_elements(study)
-    return [] unless SP_ADVANCED_ENABLED
+	def isa_study_elements(study)
+		return [] unless SP_ADVANCED_ENABLED
 
-    elements = []
-    if study.sample_types.any?
-      elements << create_node({ text: 'Sources table', _type: 'source_table', _id: study.sample_types.first.id,
-                                resource: study.sample_types.first, children: [create_sample_node(study.sample_types.first)] })
-      if study.sops.present?
-        elements << create_node({ text: 'Protocol', _type: 'study_protocol', _id: study.sops.first.id,
-                                  resource: study.sops.first })
-      end
-      elements << create_node({ text: 'Samples table', _type: 'study_samples_table', _id: study.sample_types.second.id,
-                                resource: study.sample_types.first, children: [create_sample_node(study.sample_types.second)] })
-      elements << create_node({ text: 'Experiment overview', _type: 'study_experiment_overview', _id: study.id,
-                                resource: study.sample_types.first })
-    end
+		elements = []
+		if study.sample_types.any?
+			elements << create_node({ text: 'Sources table', _type: 'source_table', _id: study.sample_types.first.id,
+																resource: study.sample_types.first, children: [create_sample_node(study.sample_types.first)] })
+			if study.sops.present?
+				elements << create_node({ text: 'Protocol', _type: 'study_protocol', _id: study.sops.first.id,
+																	resource: study.sops.first })
+			end
+			elements << create_node({ text: 'Samples table', _type: 'study_samples_table', _id: study.sample_types.second.id,
+																resource: study.sample_types.first, children: [create_sample_node(study.sample_types.second)] })
+			elements << create_node({ text: 'Experiment overview', _type: 'study_experiment_overview', _id: study.id,
+																resource: study.sample_types.first })
+		end
 
-    elements
-  end
+		elements
+	end
 
-  def isa_assay_elements(assay)
-    return [] unless SP_ADVANCED_ENABLED
+	def isa_assay_elements(assay)
+		return [] unless SP_ADVANCED_ENABLED
 
-    elements = []
-    if assay.sample_type.present?
-      if assay.sops.any?
-        elements << create_node({ text: 'Protocol', _type: 'assay_protocol', _id: assay.sops.first.id,
-                                  resource: assay.sops.first })
-      end
-      elements << create_node({ text: 'Samples table', _type: 'assay_samples_table', _id: assay.sample_type.id,
-                                resource: assay.sample_type, children: [create_sample_node(assay.sample_type)] })
-      elements << create_node({ text: 'Experiment overview', _type: 'assay_experiment_overview', _id: assay.id,
-                                resource: assay.sample_type })
-    end
+		elements = []
+		if assay.sample_type.present?
+			if assay.sops.any?
+				elements << create_node({ text: 'Protocol', _type: 'assay_protocol', _id: assay.sops.first.id,
+																	resource: assay.sops.first })
+			end
+			elements << create_node({ text: 'Samples table', _type: 'assay_samples_table', _id: assay.sample_type.id,
+																resource: assay.sample_type, children: [create_sample_node(assay.sample_type)] })
+			elements << create_node({ text: 'Experiment overview', _type: 'assay_experiment_overview', _id: assay.id,
+																resource: assay.sample_type })
+		end
 
-    elements
-  end
+		elements
+	end
 
-  def build_project_item(_project, investigation_items)
-    create_node({ text: @project.title, _type: 'project', _id: @project.id, a_attr: BOLD, label: 'Project',
-                  children: investigation_items, resource: @project })
-  end
+	def build_project_item(_project, investigation_items)
+		create_node({ text: @project.title, _type: 'project', _id: @project.id, a_attr: BOLD, label: 'Project',
+									children: investigation_items, resource: @project })
+	end
 
-  def build_investigation_item(investigation, study_items)
-    create_node({ text: investigation.title, _type: 'investigation', _id: investigation.id, a_attr: BOLD,
-                  label: 'Investigation', children: study_items, resource: investigation })
-  end
+	def build_investigation_item(investigation, study_items)
+		create_node({ text: investigation.title, _type: 'investigation', _id: investigation.id, a_attr: BOLD,
+									label: 'Investigation', children: study_items, resource: investigation })
+	end
 
-  def build_study_item(study, assay_items)
-    create_node({ text: study.title, _type: 'study', _id: study.id, a_attr: BOLD, label: 'Study',
-                  children: isa_study_elements(study) + assay_items, resource: study })
-  end
+	def build_study_item(study, assay_items)
+		create_node({ text: study.title, _type: 'study', _id: study.id, a_attr: BOLD, label: 'Study',
+									children: isa_study_elements(study) + assay_items, resource: study })
+	end
 
-  def build_assay_stream_item(assay_stream, child_assays)
-    create_node({ text: assay_stream.title, _type: 'assay_stream', label: 'Assay Stream', _id: assay_stream.id, a_attr: BOLD,
-                  children: isa_assay_elements(assay_stream) + child_assays, resource: assay_stream })
-  end
+	def build_assay_stream_item(assay_stream, child_assays)
+		create_node({ text: assay_stream.title, _type: 'assay_stream', label: 'Assay Stream', _id: assay_stream.id, a_attr: BOLD,
+									children: isa_assay_elements(assay_stream) + child_assays, resource: assay_stream })
+	end
 
-  def build_assay_item(assay)
-    create_node({ text: assay.title, _type: 'assay', label: 'Assay', _id: assay.id, a_attr: BOLD,
-                  children: isa_assay_elements(assay), resource: assay })
-  end
+	def build_assay_item(assay)
+		create_node({ text: assay.title, _type: 'assay', label: 'Assay', _id: assay.id, a_attr: BOLD,
+									children: isa_assay_elements(assay), resource: assay })
+	end
 end
