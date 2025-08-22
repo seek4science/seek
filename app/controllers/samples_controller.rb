53421812622e646d69f80029340996940195dbc6
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2025-08-22 09:29:57 +0200

    set sample default back

--- app/controllers/samples_controller.rb
+++ app/controllers/samples_controller.rb
@@ -342,7 +342,7 @@ class SamplesController < ApplicationController
   end
 
 
-  def update_sample_with_params(parameters = params, sample)
+  def update_sample_with_params(parameters = params, sample = @sample)
     sample.assign_attributes(sample_params(sample.sample_type, parameters))
     update_sharing_policies(sample, parameters)
     update_annotations(parameters[:tag_list], sample)
