faa4b560b5ce6c5789dea7db93cb5da6474afa81
Xiaoming Hu <xiaoming.hu@h-its.org>
2025-10-30 13:37:23 +0100

    create a base_exception.rb

---
+++ lib/seek/doi/base_exception.rb
@@ -0,0 +1,25 @@
+module Seek
+  module Doi
+    class BaseException < RuntimeError
+      def initialize(msg = 'A DOI exception occurred')
+        super(msg)
+      end
+
+      def backtrace
+        cause ? cause.backtrace : super
+      end
+
+      def message
+        cause ? "#{super} (#{cause.class.name}: #{cause.message})" : super
+      end
+    end
+
+    class UnrecognizedTypeException < BaseException; end
+    class FetchException < BaseException; end
+    class ParseException < BaseException; end
+    class MalformedDOIException < BaseException; end
+    class NotFoundException < BaseException; end
+    class RecordNotSupported < BaseException; end
+    class RANotSupported < BaseException; end
+  end
+end
