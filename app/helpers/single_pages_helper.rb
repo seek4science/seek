ac7a370ce81bd310bb5d6eded57dbe97597f0b4e
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2025-12-03 09:42:36 +0100

    Fix dispatcher

--- app/helpers/single_pages_helper.rb
+++ app/helpers/single_pages_helper.rb
@@ -25,21 +25,22 @@ module SinglePagesHelper
   end
 
   def get_values_for_attribute(attribute)
-    case attribute.sample_attribute_type
-    when controlled_vocab?
+    type = attribute.sample_attribute_type
+    if type.controlled_vocab?
       get_values_for_cv(attribute)
-    when seek_sample? || seek_sample_multi?
+    elsif type.seek_sample? || type.seek_sample_multi?
       get_values_for_registered_samples(attribute)
-    when seek_data_file?
+    elsif type.seek_data_file?
       get_values_for_datafiles(attribute)
-    when seek_strain?
+    elsif type.seek_strain?
       get_values_for_strains(attribute)
-    when seek_sop?
+    elsif type.seek_sop?
       get_values_for_sops(attribute)
     else
       []
     end
   end
+
   def get_values_for_cv(sample_attribute)
     sample_attribute.sample_controlled_vocab.labels
   end
