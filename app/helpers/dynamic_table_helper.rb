7eb1be66e842845840b83eec1f26a230d4664c27
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2025-12-03 14:32:34 +0100

    Copilot review

--- app/helpers/dynamic_table_helper.rb
+++ app/helpers/dynamic_table_helper.rb
@@ -93,23 +93,23 @@ module DynamicTableHelper
   end
 
   def transform_non_text_attributes_multi(json_metadata, multi_non_text_attribute_title)
+    return json_metadata if multi_non_text_attribute_title.nil?
     return json_metadata if json_metadata[multi_non_text_attribute_title].nil?
 
-    unless multi_non_text_attribute_title.nil?
-      original_metadata = json_metadata[multi_non_text_attribute_title]
-      json_metadata[multi_non_text_attribute_title] = original_metadata.map do |obj|
-        hide_unauthorized_metadata(obj)
-      end
+    original_metadata = json_metadata[multi_non_text_attribute_title]
+    json_metadata[multi_non_text_attribute_title] = original_metadata.map do |obj|
+      hide_unauthorized_metadata(obj)
     end
+
     json_metadata
   end
 
   def transform_non_text_attributes_single(json_metadata, non_text_attribute_title)
+    return json_metadata if non_text_attribute_title.nil?
     return json_metadata if json_metadata[non_text_attribute_title].nil?
 
-    unless non_text_attribute_title.nil?
-      json_metadata[non_text_attribute_title] = hide_unauthorized_metadata(json_metadata[non_text_attribute_title])
-    end
+    json_metadata[non_text_attribute_title] = hide_unauthorized_metadata(json_metadata[non_text_attribute_title])
+    
     json_metadata
   end
 
