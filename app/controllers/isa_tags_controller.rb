d4e6a6cfd7b246f20a11a22694fff4a180c150fe
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2025-12-09 17:02:16 +0100

    Add controller methods

--- app/controllers/isa_tags_controller.rb
+++ app/controllers/isa_tags_controller.rb
@@ -1,12 +1,39 @@
 class ISATagsController < ApplicationController
   respond_to :json
   api_actions :show, :index
+  before_action :ensure_json_request
+  before_action :isa_json_compliance_enabled?
+  before_action :login_required
 
   def index
-
+    respond_to do |format|
+      format.json {
+        render json: ISATag.all,
+               each_serializer: ISATagSerializer,
+               meta: {
+                 base_url: Seek::Config.site_base_host,
+                 api_version: ActiveModel::Serializer.config.api_version
+               }
+      }
+    end
   end
 
   def show
+    respond_to do |format|
+      format.json { render json: ISATag.find(params[:id]) }
+    end
+  end
 
+
+  private
+
+  def ensure_json_request
+    # Accept header or URL format must request JSON
+    return if request.format.json?
+
+    render json: {
+      error: "Not Acceptable",
+      message: "This endpoint only serves application/json."
+    }, status: :not_acceptable # 406
   end
 end
---
+++ test/functional/isa_tags_controller_test.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+class ISATagsControllerTest < ActionController::TestCase
+  include AuthenticatedTestHelper
+
+  def setup
+    @authenticated_user = FactoryBot.create(:person)
+    Seek::Config.isa_json_compliance_enabled = true
+    [:source_isa_tag, :sample_isa_tag].each { |f| FactoryBot.create(f) }
+  end
+
+  def teardown
+    Seek::Config.isa_json_compliance_enabled = false
+  end
+
+  test 'should return ISA tags if logged in' do
+    login_as @authenticated_user
+
+    get :index, as: :json
+    assert_response :success
+    response_body = JSON.parse(response.body)
+    assert_equal response_body["data"].count, 2
+  end
+
+  test 'should not return ISA tags if not logged in' do
+    get :index, as: :json
+    assert_response :unauthorized
+    assert_equal response.body, "HTTP Basic: Access denied.\n"
+  end
+
+  test 'should not respond to anything else than json requests' do
+    get :index, as: :xml
+    assert_response :not_acceptable
+    response_body = JSON.parse(response.body)
+    assert_equal response_body["error"], "Not Acceptable"
+  end
+
+end
