6c6f95c9af9dc4b22a4e30cb83f9a6eb3faaaa9a
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2025-12-18 11:19:30 +0100

    Check whether the pids are still alive

--- script/check_worker_pids.sh
+++ script/check_worker_pids.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-min_workers=${MIN_WORKERS:-1}
+expected_workers=$(bundle exec rails runner "puts Seek::Workers.active_queues.count")
 
-# Check whether min_workers is a valid number greater than or equal to 1
-if ! [[ "$min_workers" =~ ^[0-9]+$ ]] || [ "$min_workers" -lt 1 ]; then
-  echo "MIN_WORKERS is not a valid number greater than or equal to 1: $min_workers"
-  exit 1
-fi
+shopt -s nullglob || true 2>/dev/null
 
-# Fails if the number of running worker PID files is less than min_workers
-running_workers=$(ls tmp/pids/delayed_job*.pid 2>/dev/null | wc -l)
-if [ "$running_workers" -lt "$min_workers" ]; then
-  echo "Expected at least $min_workers workers, but found $running_workers."
+# Fails if the number of running worker PID files is less than expected_workers
+pid_files=(tmp/pids/delayed_job.*.pid)
+pids=$(cat "${pid_files[@]}")
+running_workers=$(echo "$pids" 2>/dev/null | wc -l)
+if [ "$running_workers" -ne "$expected_workers" ]; then
+  echo "Expected at least $expected_workers workers, but found $running_workers."
   exit 1
+else
+  for pid in $pids; do
+    if ! kill -0 "$pid" 2>/dev/null; then
+      echo "Worker with PID $pid is not running."
+      exit 1
+    fi
+  done
 fi
 
 # Log success
-echo "Found $running_workers running workers (minimum required: $min_workers)."
+echo "Found $running_workers running workers (Expected: $expected_workers)."
 exit 0
