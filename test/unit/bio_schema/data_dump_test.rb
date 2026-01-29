a77a2afb596d775d98d70d651295c54530fad9ad
lasse-cs <lasse.schmieding@manchester.ac.uk>
2026-01-29 15:25:29 +0000

    Update data dump test size

--- test/unit/bio_schema/data_dump_test.rb
+++ test/unit/bio_schema/data_dump_test.rb
@@ -97,7 +97,7 @@ class DataDumpTest < ActiveSupport::TestCase
 
     assert_equal 'workflows-bioschemas-dump.jsonld', dump.file_name
     assert dump.exists?
-    assert_in_delta 3800, dump.size, 500
+    assert_in_delta 5000, dump.size, 500
     assert_in_delta Time.now, dump.date_modified, 60
   end
 
