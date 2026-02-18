2ca9300d97e885f33a0f74f0c5fcc96f9068bc29
Finn Bacall <finn.bacall@manchester.ac.uk>
2026-02-18 14:32:05 +0000

    Fix HTML tags being passed to Solr index

--- lib/seek/markdown.rb
+++ lib/seek/markdown.rb
@@ -19,20 +19,32 @@ module Seek
       end
     end
 
-    MarkdownPipeline = HTMLPipeline.new(
+    markdown_pipeline_config = {
       convert_filter: HTMLPipeline::ConvertFilter::MarkdownFilter.new(context: {
         markdown: {
           render: { unsafe: true, github_pre_lang: true, hardbreaks: false },
           extension: { tagfilter: true, table: true, strikethrough: true, autolink: true }
         }
       }),
-      node_filters: [LinkNofollowFilter.new],
-    )
+      node_filters: [LinkNofollowFilter.new]
+    }
 
+    MarkdownPipeline = HTMLPipeline.new(**markdown_pipeline_config)
+
+    MarkdownPlainTextPipeline = HTMLPipeline.new(**markdown_pipeline_config.merge(sanitization_config: { elements: [] }))
+
+    # Renders a markdown string to HTML
     def self.render(markdown)
       markdown = markdown.encode('UTF-8', invalid: :replace, undef: :replace)
       return '' if markdown.blank?
       MarkdownPipeline.call(markdown)[:output]
+    end
+
+    # Strips markdown tags from a string and returns plain text
+    def self.strip_markdown(markdown)
+      markdown = markdown.encode('UTF-8', invalid: :replace, undef: :replace)
+      return '' if markdown.blank?
+      MarkdownPlainTextPipeline.call(markdown)[:output]
     end
   end
 end
