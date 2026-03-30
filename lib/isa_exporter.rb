ff5145e2a75aabde9a9a9c129242d93e49cca652
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2026-03-30 15:55:58 +0200

    Add safe navigation

--- lib/isa_exporter.rb
+++ lib/isa_exporter.rb
@@ -462,9 +462,9 @@ module ISAExporter
       return '' unless sample.can_view?(@current_user)
 
       if attribute.sample_attribute_type.seek_sample? || attribute.sample_attribute_type.seek_sample_multi? || attribute.sample_attribute_type.seek_strain? || attribute.sample_attribute_type.seek_data_file? || attribute.sample_attribute_type.seek_sop?
-        sample.get_attribute_value(attribute).to_json || ''
+        sample.get_attribute_value(attribute)&.to_json || ''
       elsif attribute.sample_attribute_type.base_type == Seek::Samples::BaseType::CV_LIST
-        sample.get_attribute_value(attribute).join(', ')
+        sample.get_attribute_value(attribute)&.join(', ')
       else
         sample.get_attribute_value(attribute) || ''
       end
