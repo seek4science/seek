73b606c63358d9db9c27109c7efed39a52f41154
Xiaoming Hu <xiaoming.hu@h-its.org>
2026-05-05 16:33:56 +0200

    Move RDF vocabulary files to correct location

--- lib/seek/rdf/vocabularies/seek_health.rb
+++
@@ -1,9 +0,0 @@
-module Seek
-  module Rdf
-    # SEEK extension namespace for HealthDCAT-AP terms that have no standard mapping.
-    # Only add properties here when no existing vocabulary (DCAT, DCTERMS, HDCAT, PROV, SKOS) covers the concept.
-    # URI: https://seek4science.org/vocab/seekh#
-    class SEEKHVocab < RDF::Vocabulary('https://seek4science.org/vocab/seekh#')
-    end
-  end
-end
