aacf905cb7dffa0e002121ee2eaff5fc800678c4
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2026-05-19 11:39:48 +0200

    Add ISA studies API test

---
+++ test/integration/api/isa_studies_api_test.rb
@@ -0,0 +1,139 @@
+require 'test_helper'
+
+class ISAStudiesApiTest < ActionDispatch::IntegrationTest
+  include ApiTestHelper
+
+  def setup
+    user_login
+    @old_isa_json_compliance_config = Seek::Config.isa_json_compliance_enabled
+    @old_single_pages_config = Seek::Config.project_single_page_folders_enabled
+    Seek::Config.isa_json_compliance_enabled = true
+    Seek::Config.project_single_page_folders_enabled = true
+    @project = current_person.projects.first
+    @investigation = FactoryBot.create(:investigation, is_isa_json_compliant: true,
+                                                       projects: [@project],
+                                                       contributor: current_person)
+    @sample_multi_sample_attribute_type = FactoryBot.create(:sample_multi_sample_attribute_type)
+    @string_sample_attribute_type = FactoryBot.create(:string_sample_attribute_type)
+
+    # ISA tags
+    @source_isa_tag = ISATag.find_by(title: 'source') || FactoryBot.create(:source_isa_tag)
+    @sample_isa_tag = ISATag.find_by(title: 'sample') || FactoryBot.create(:sample_isa_tag)
+    @protocol_isa_tag = ISATag.find_by(title: 'protocol') || FactoryBot.create(:protocol_isa_tag)
+    @source_characteristic_isa_tag = ISATag.find_by(title: 'source_characteristic') || FactoryBot.create(:source_characteristic_isa_tag)
+  end
+
+  def teardown
+    Seek::Config.isa_json_compliance_enabled = @old_isa_json_compliance_config
+    Seek::Config.project_single_page_folders_enabled = @old_single_pages_config
+  end
+
+  test 'create ISA study' do
+    source_sample_type_params = {
+      "title": "Source Sample Type via API",
+      "description": "Source sample type created via the API",
+      "sample_attributes_attributes": {
+        "1": {
+          "pos": 1,
+          "title": "Source Name",
+          "sample_attribute_type_id": @string_sample_attribute_type.id,
+          "isa_tag_id": @source_isa_tag.id,
+          "required": true,
+          "is_title": true
+        },
+        "2": {
+          "pos": 2,
+          "title": "Species",
+          "sample_attribute_type_id": @string_sample_attribute_type.id,
+          "isa_tag_id": @source_characteristic_isa_tag.id,
+          "required": false,
+          "is_title": false
+        }
+      }
+    }
+
+    sample_collection_sample_type_params = {
+      "title": "Sample Collection Sample Type via API",
+      "description": "Sample collection sample type created via the API",
+      "sample_attributes_attributes": {
+        "1": {
+          "pos": 1,
+          "title": "Input",
+          "sample_attribute_type_id": @sample_multi_sample_attribute_type.id,
+          "linked_sample_type_id": "self",
+          "required": true,
+          "is_title": false
+        },
+        "2": {
+          "pos": 2,
+          "title": "Sample Name",
+          "sample_attribute_type_id": @string_sample_attribute_type.id,
+          "isa_tag_id": @sample_isa_tag.id,
+          "required": true,
+          "is_title": true
+        },
+        "3": {
+          "pos": 3,
+          "title": "Collection Protocol",
+          "sample_attribute_type_id": @string_sample_attribute_type.id,
+          "isa_tag_id": @protocol_isa_tag.id,
+          "required": true,
+          "is_title": false
+        }
+      }
+    }
+
+    params = {
+      "data": {
+        "type": "isa_studies",
+        "attributes": {
+          "study": {
+            "title": "My new ISA Study via API",
+            "description": "This ISA Study was created via the API",
+            "investigation_id": @investigation.id
+          },
+          "source_sample_type": source_sample_type_params,
+          "sample_collection_sample_type": sample_collection_sample_type_params
+        }
+      }
+    }
+
+    assert_difference('Study.count', 1) do
+      assert_difference('SampleType.count', 2) do
+        assert_difference('SampleAttribute.count', 5) do
+          post isa_studies_path(format: :json), params: params, as: :json,
+               headers: { "Authorization": write_access_auth }
+        end
+      end
+    end
+    assert_response :success
+  end
+
+  test 'update ISA study' do
+    study = FactoryBot.create(:isa_json_compliant_study, contributor: current_person)
+    # Ensure both sample types are editable by the current person
+    study.sample_types.each { |st| st.update_column(:contributor_id, current_person.id) }
+
+    params = {
+      "data": {
+        "id": study.id.to_s,
+        "type": "isa_studies",
+        "attributes": {
+          "study": {
+            "description": "Updated description via API"
+          },
+          "source_sample_type": {
+            "title": "Updated Source Sample Type Title"
+          }
+        }
+      }
+    }
+
+    assert_changes -> { study.reload.description }, to: "Updated description via API" do
+      assert_changes -> { study.sample_types.first.reload.title }, to: "Updated Source Sample Type Title" do
+        patch isa_study_path(study.id, format: :json), params: params, as: :json,
+              headers: { "Authorization": write_access_auth }
+      end
+    end
+  end
+end
