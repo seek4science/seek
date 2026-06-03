d0d45f30240f38e5ce2ece3ed9db5f9c4fdc15c4
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2026-06-03 14:04:42 +0200

    Freeze the array

--- lib/seek/isa_templates/template_group.rb
+++ lib/seek/isa_templates/template_group.rb
@@ -3,7 +3,7 @@ module Seek
   module ISATemplates
     module TemplateGroup
       ISA_MINIMAL_STARTER = "ISA minimal starter"
-      EXCLUDED_FROM_SAMPLE_TYPE_CREATION = [ISA_MINIMAL_STARTER]
+      EXCLUDED_FROM_SAMPLE_TYPE_CREATION = [ISA_MINIMAL_STARTER].freeze
     end
   end
 end
