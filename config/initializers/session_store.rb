4b2ee85c5a5a80c795ab989a464571ac6bcf800d
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2026-06-08 22:08:32 +0200

    Adjust to use :redis_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -4,13 +4,22 @@
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 # FIXME: Use Seek::Config.session_store_timeout somehow
-SEEK::Application.config.session_store(:redis_session_store,
-                                       redis: {
-                                         url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0"),
-                                         expire_after: 30.minutes,
-                                         key_prefix: "session:",
-                                         ssl_params: Rails.env.production? ? { verify_mode: OpenSSL::SSL::VERIFY_PEER } : nil
-                                       },
-                                       key: '_seek_session',
-                                       same_site: :lax,
-                                       secure: Rails.env.production?)
+
+session_url = "#{ENV.fetch('REDIS_URL', 'redis://localhost:6379')}/0/session"
+domain_name = ENV.fetch('DOMAIN_NAME', 'localhost')
+
+session_options = {
+  servers: [session_url],
+  expire_after: 30.minutes,
+  key: '_seek_session',
+  threadsafe: true,
+  same_site: :lax,
+  secure: Rails.env.production?,
+  httponly: true
+}
+
+# Only set the domain constraint if FQDN is configured
+session_options[:domain] = domain_name if domain_name != 'localhost'
+
+# Define the redis session store
+SEEK::Application.config.session_store(:redis_store, **session_options)
