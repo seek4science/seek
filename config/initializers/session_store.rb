9b09069ff8df357d1cfc63fcdcfd2920689bd8f2
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2026-06-11 16:19:34 +0200

    remove outdated comment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-# Use Redis for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# FIXME: Use Seek::Config.session_store_timeout somehow
-
 session_url = "#{ENV.fetch('REDIS_URL', 'redis://localhost:6379/0')}/session"
 
 session_options = {
