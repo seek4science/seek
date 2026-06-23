95725c79561270e71141aac238bb37934dc21590
Abby Gavin <146940380+AbbyGavin@users.noreply.github.com>
2026-06-23 11:45:52 +0100

    Added initial script to validate RO crate

---
+++ script/validate-ro-crate.py
@@ -0,0 +1,16 @@
+from rocrate_validator import services, models
+
+print("starting validation")
+
+settings = services.ValidationSettings(
+    rocrate_uri='/home/abby/workflow-1531-1.crate.zip',
+    profile_identifier='ro-crate-1.1',
+    # Severity options are: REQUIRED, RECOMMENDED, and OPTIONAL
+    requirement_severity=models.Severity.REQUIRED,
+)
+
+result = services.validate(settings)
+
+if result.has_issues():
+    for issue in result.get_issues():
+        print(f"Detected issue of severity {issue.severity.name} with check \"{issue.check.identifier}\": {issue.message}")
