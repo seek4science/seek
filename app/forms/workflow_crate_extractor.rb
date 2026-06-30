b0b94e75963a94e032aefafefbb90f69413f8a16
Abby Gavin <146940380+AbbyGavin@users.noreply.github.com>
2026-06-30 15:13:39 +0100

    Stripping the whitespaces from the ends of the lines

--- app/forms/workflow_crate_extractor.rb
+++ app/forms/workflow_crate_extractor.rb
@@ -89,7 +89,7 @@ class WorkflowCrateExtractor
   def extract_crate
     Open4.open4(Seek::Util.python_exec("script/validate-ro-crate.py #{ro_crate[:data].path}")) do |_pid, _stdin, stdout, _stderr|
       until (line = stdout.gets).nil?
-        errors.add(:base, line)
+        errors.add(:base, line.strip)
       end
     end
 
