4353870f576e86c06349893d3208b87370ed40a0
Kevin De Pelseneer <kevin.depelseneer@vib.be>
2026-07-14 22:57:23 +0200

    Add more tests

--- test/functional/single_pages_controller_test.rb
+++ test/functional/single_pages_controller_test.rb
@@ -146,4 +146,90 @@ class SinglePagesControllerTest < ActionController::TestCase
     assert visible_row.none? { |value| value == '#HIDDEN' }
     assert hidden_row.all? { |value| value == '#HIDDEN' || value.blank? }
   end
+
+  test 'dynamic table data should return an empty array when the requested assay is unauthorized' do
+    investigation = FactoryBot.create(:investigation, is_isa_json_compliant: true, policy: FactoryBot.create(:public_policy))
+    study = FactoryBot.create(:isa_json_compliant_study, investigation: investigation, policy: FactoryBot.create(:public_policy))
+    source_sample_type = study.sample_types.first
+    sample_collection_sample_type = study.sample_types.second
+
+    assay_stream = FactoryBot.create(:assay_stream, study: study, policy: FactoryBot.create(:private_policy))
+    assay = FactoryBot.create(:isa_json_compliant_material_assay, assay_stream: assay_stream, study: study,
+                              linked_sample_type: sample_collection_sample_type,
+                              policy: FactoryBot.create(:private_policy), position: 0)
+    material_assay_sample_type = assay.sample_type
+
+    source = FactoryBot.create(:isa_source, sample_type: source_sample_type, policy: FactoryBot.create(:public_policy))
+    sample = FactoryBot.create(:isa_sample, sample_type: sample_collection_sample_type, linked_samples: [source],
+                      policy: FactoryBot.create(:public_policy))
+    FactoryBot.create(:isa_material_assay_sample, sample_type: material_assay_sample_type, linked_samples: [sample],
+                      policy: FactoryBot.create(:public_policy))
+
+    logout
+
+    get :dynamic_table_data, params: { id: @project.id, study_id: study.id, assay_id: assay.id }
+
+    assert_response :success
+    json = JSON.parse(response.body)
+    refute json.key?('error')
+    # The assay itself is unauthorized, so no data should be returned, even though the parent study is public.
+    assert_equal [], json['data']
+  end
+
+  test 'dynamic table data for a public assay is unaffected by an unauthorized parent study' do
+    investigation = FactoryBot.create(:investigation, is_isa_json_compliant: true, policy: FactoryBot.create(:private_policy))
+    study = FactoryBot.create(:isa_json_compliant_study, investigation: investigation, policy: FactoryBot.create(:private_policy))
+    source_sample_type = study.sample_types.first
+    sample_collection_sample_type = study.sample_types.second
+
+    assay_stream = FactoryBot.create(:assay_stream, study: study, policy: FactoryBot.create(:public_policy))
+    assay = FactoryBot.create(:isa_json_compliant_material_assay, assay_stream: assay_stream, study: study,
+                              linked_sample_type: sample_collection_sample_type,
+                              policy: FactoryBot.create(:public_policy), position: 0)
+    source = FactoryBot.create(:isa_source, sample_type: source_sample_type, policy: FactoryBot.create(:public_policy))
+    sample = FactoryBot.create(:isa_sample, sample_type: sample_collection_sample_type, linked_samples: [source],
+                               policy: FactoryBot.create(:public_policy))
+    FactoryBot.create(:isa_material_assay_sample, sample_type: assay.sample_type, linked_samples: [sample],
+                      policy: FactoryBot.create(:public_policy))
+
+    logout
+
+    get :dynamic_table_data, params: { id: @project.id, study_id: study.id, assay_id: assay.id }
+
+    assert_response :success
+    json = JSON.parse(response.body)
+    refute json.key?('error')
+    # The assay's own policy is public and assay-level aggregation doesn't depend on the parent study,
+    # so its data is still returned even though the study itself is private.
+    assert_equal 1, json['data'].length
+    refute json['data'].flatten.include?('#HIDDEN')
+  end
+
+  test 'should return dynamic table data for a public sample type' do
+    sample_type = FactoryBot.create(:isa_source_sample_type, policy: FactoryBot.create(:public_policy))
+    FactoryBot.create(:isa_source, sample_type: sample_type, policy: FactoryBot.create(:public_policy))
+
+    logout
+
+    get :dynamic_table_data, params: { id: @project.id, sample_type_id: sample_type.id }
+
+    assert_response :success
+    json = JSON.parse(response.body)
+    refute json.key?('error')
+    assert_equal 1, json['data'].length
+  end
+
+  test 'dynamic table data should not error out for an unauthorized sample type' do
+    sample_type = FactoryBot.create(:isa_source_sample_type, policy: FactoryBot.create(:private_policy))
+    FactoryBot.create(:isa_source, sample_type: sample_type, policy: FactoryBot.create(:private_policy))
+
+    logout
+
+    get :dynamic_table_data, params: { id: @project.id, sample_type_id: sample_type.id }
+
+    assert_response :success
+    json = JSON.parse(response.body)
+    refute json.key?('error')
+    assert_equal [], json['data']
+  end
 end
